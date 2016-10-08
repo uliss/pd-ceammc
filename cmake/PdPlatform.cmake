79523de7a77ebb5a2786b47872c49c7213aa441d
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-08 21:55:36 +0300

    macosx bundle added
    use `make app` to for getting Pd.app in dist directory

--- cmake/PdPlatform.cmake
+++ cmake/PdPlatform.cmake
@@ -8,4 +8,12 @@ if(APPLE)
 
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -O3 -funroll-loops -fomit-frame-pointer")
     set(PD_EXTERNAL_LDFLAGS "-bundle -undefined dynamic_lookup")
+
+    add_custom_command(
+        OUTPUT ${PROJECT_BINARY_DIR}/gen_build_mac.sh
+        COMMAND cmake ${CMAKE_COMMAND}
+            -DPROJECT_SOURCE_DIR="${PROJECT_SOURCE_DIR}"
+            -DPROJECT_BINARY_DIR="${PROJECT_BINARY_DIR}"
+            -P ${PROJECT_SOURCE_DIR}/mac/cmake-build-mac.cmake)
+    add_custom_target(app DEPENDS gen_build_mac.sh)
 endif()
