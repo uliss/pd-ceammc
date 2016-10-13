b865a22beed54635183682e8aa032163bf2c2571
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-14 00:11:01 +0300

    midi options check added
    to ensure at least one midi module selected

--- cmake/sound.cmake
+++ cmake/sound.cmake
@@ -79,5 +79,14 @@ if(NOT WITH_ALSA_AUDIO AND
         NOT WITH_OSS_AUDIO AND
         NOT WITH_PORTAUDIO AND
         NOT WITH_DUMMY_AUDIO)
-    message(FATAL_ERROR "At least one sound module required! See `cmake -L` for available config options.")
+    message(FATAL_ERROR "At least one sound module required! See `cmake -L` for available config options.
+        You can build with dummy audio with cmake -DWITH_DUMMY_AUDIO=ON option.")
+endif()
+
+if(NOT WITH_ALSA_MIDI AND
+        NOT WITH_OSS_MIDI AND
+        NOT WITH_PORTMIDI AND
+        NOT WITH_DUMMY_MIDI)
+    message(FATAL_ERROR "At least one midi module required! See `cmake -L` for available config options.
+        You can build with dummy midi with cmake -DWITH_DUMMY_MIDI=ON option.")
 endif()
