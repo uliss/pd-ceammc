23231a03d42e146f7bb02cab12c2ce13d1fa6b8a
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-13 23:55:04 +0300

    broke into modules

---
+++ cmake/config.cmake
@@ -0,0 +1,15 @@
+include(CheckIncludeFiles)
+include(CheckFunctionExists)
+include(FindLibDL)
+
+if(LIBDL_FOUND)
+    set(HAVE_LIBDL 1)
+endif()
+
+check_include_files(unistd.h HAVE_UNISTD_H)
+check_include_files(alloca.h HAVE_ALLOCA_H)
+check_include_files(bstring.h HAVE_BSTRING_H)
+check_function_exists(nanosleep HAVE_NANOSLEEP)
+
+configure_file(${PROJECT_SOURCE_DIR}/config.h.in ${PROJECT_BINARY_DIR}/config.h)
+add_definitions(-include ${PROJECT_BINARY_DIR}/config.h)
---
+++ cmake/sound.cmake
@@ -0,0 +1,75 @@
+include(CMakeDependentOption)
+include(CheckIncludeFiles)
+
+option(WITH_ALSA "Use ALSA sound API" ON)
+cmake_dependent_option(WITH_ALSA_AUDIO "Use OSS audio API" ON "WITH_ALSA" OFF)
+cmake_dependent_option(WITH_ALSA_MIDI "Use OSS midi API" ON "WITH_ALSA" OFF)
+option(WITH_JACK "Use Jack sound API" ON)
+
+option(WITH_OSS "Use OSS sound API" ON)
+cmake_dependent_option(WITH_OSS_AUDIO "Use OSS audio API" ON "WITH_OSS" OFF)
+cmake_dependent_option(WITH_OSS_MIDI "Use OSS midi API" ON "WITH_OSS" OFF)
+
+option(WITH_PORTAUDIO "Use PortAudio sound API" ON)
+option(WITH_PORTMIDI "Use PortMidi sound API" ON)
+
+option(WITH_DUMMY "Build dummy audio and midi modules" OFF)
+cmake_dependent_option(WITH_DUMMY_AUDIO "Build dummy audio module" ON "WITH_DUMMY" OFF)
+cmake_dependent_option(WITH_DUMMY_MIDI "Build dummy midi module" ON "WITH_DUMMY" OFF)
+
+# ALSA
+if(WITH_ALSA_AUDIO OR WITH_ALSA_MIDI)
+    include(FindALSA)
+    if(NOT ALSA_FOUND)
+        set(WITH_ALSA OFF)
+        set(WITH_ALSA_AUDIO OFF)
+        set(WITH_ALSA_MIDI OFF)
+    else()
+        add_definitions(-DUSEAPI_ALSA)
+    endif()
+endif()
+
+# JACK
+if(WITH_JACK)
+    include(FindJack)
+    if(NOT JACK_FOUND)
+        set(WITH_JACK OFF)
+    else()
+         add_definitions(-DUSEAPI_JACK -DJACK_XRUN)
+    endif()
+endif()
+
+# OSS
+if(WITH_OSS_AUDIO OR WITH_OSS_MIDI)
+    check_include_files(sys/soundcard.h HAVE_SYS_SOUNDCARD_H)
+    if(NOT HAVE_SYS_SOUNDCARD_H)
+        set(WITH_OSS OFF CACHE BOOL "" FORCE)
+        set(WITH_OSS_AUDIO OFF CACHE BOOL "" FORCE)
+        set(WITH_OSS_MIDI OFF CACHE BOOL "" FORCE)
+    else()
+        add_definitions(-DUSEAPI_OSS)
+    endif()
+endif()
+
+# PORTAUDIO
+if(WITH_PORTAUDIO)
+    include(FindPortAudio)
+    if(PORTAUDIO_FOUND)
+        add_definitions(-DUSEAPI_PORTAUDIO)
+    else()
+        set(WITH_PORTAUDIO OFF CACHE BOOL "" FORCE)
+    endif()
+endif()
+
+# PORTMIDI
+if(WITH_PORTMIDI)
+    include(FindPortMidi)
+    if(NOT PORTMIDI_FOUND)
+        set(WITH_PORTMIDI OFF CACHE BOOL "" FORCE)
+    endif()
+endif()
+
+# Dummy
+if(WITH_DUMMY_AUDIO OR WITH_DUMMY_MIDI)
+    add_definitions(-DUSEAPI_DUMMY)
+endif()
