00c0dadec8a786e5693281ecb4d3d96c8df82d26
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-14 01:33:27 +0300

    msys: first compile error fix

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -9,6 +9,7 @@ endif()
 check_include_files(unistd.h HAVE_UNISTD_H)
 check_include_files(alloca.h HAVE_ALLOCA_H)
 check_include_files(bstring.h HAVE_BSTRING_H)
+check_include_files(stdint.h HAVE_STDINT_H)
 check_function_exists(nanosleep HAVE_NANOSLEEP)
 
 configure_file(${PROJECT_SOURCE_DIR}/config.h.in ${PROJECT_BINARY_DIR}/config.h)
