8987380827d462f33b36887314f36ce0e97e6c5e
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-16 17:39:55 +0300

    icons update

---
+++ ceammc/gui/icons/make_icns.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+INK='/usr/local/bin/inkscape'
+ICONSET='./pd.iconset'
+
+if [[ -z "$1" ]] 
+then
+	echo "SVG file needed."
+	exit;
+fi
+
+mkdir -p ${ICONSET}
+
+BASE=`pwd`/${ICONSET}/icon
+SVG="`pwd`/$1"
+
+function make_icon() {
+    sz=$1
+    sz2=$(($sz * 2))
+    $INK -z -D -e "${BASE}_${sz}x${sz}.png"    -f $SVG -w ${sz} -h ${sz}
+    $INK -z -D -e "${BASE}_${sz}x${sz}@2x.png" -f $SVG -w ${sz2} -h ${sz2}
+}
+
+make_icon 16
+make_icon 32
+make_icon 128
+make_icon 256
+make_icon 512
+
+iconutil -c icns "${ICONSET}"
+
