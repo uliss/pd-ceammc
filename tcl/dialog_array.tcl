f0440d9e2acc26ba9f1708a1294f9eb3a9794a40
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-19 02:30:01 +0300

    array dialog padding added

--- tcl/dialog_array.tcl
+++ tcl/dialog_array.tcl
@@ -259,7 +259,7 @@ proc ::dialog_array::create_dialog {mytoplevel newone} {
     wm resizable $mytoplevel 0 0
     wm transient $mytoplevel $::focused_window
     $mytoplevel configure -menu $::dialog_menubar
-    $mytoplevel configure -padx 0 -pady 0
+    $mytoplevel configure -padx 10 -pady 5
     ::pd_bindings::dialog_bindings $mytoplevel "array"
 
     # array
