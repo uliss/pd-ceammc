3e187eaadc029e5df5a17a46ab909d6377742129
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-19 17:57:50 +0300

    numbox IEM style update

--- src/g_numbox.c
+++ src/g_numbox.c
@@ -198,7 +198,7 @@ static void my_numbox_draw_new(t_my_numbox *x, t_glist *glist)
              xpos + (x->x_numwidth) * IEMGUI_ZOOM(x), ypos + x->x_gui.x_h,
              xpos, ypos + x->x_gui.x_h,
              stroke_width,
-             STYLE_BORDER_COLOR, x->x_gui.x_bcol, x);
+             STYLE_IEM_BORDER_COLOR, x->x_gui.x_bcol, x);
     sys_vgui(
         ".x%lx.c create line %d %d %d %d %d %d -fill #%06x -tags %lxBASE2\n",
         canvas, xpos, ypos,
@@ -222,14 +222,14 @@ static void my_numbox_draw_new(t_my_numbox *x, t_glist *glist)
              canvas,
              xpos, ypos + x->x_gui.x_h - xlet_height(x),
              xpos+IOWIDTH, ypos + x->x_gui.x_h,
-             STYLE_BORDER_COLOR,
+             STYLE_CONTROL_XLET_COLOR,
              x, 0);
     if(!x->x_gui.x_fsf.x_rcv_able)
         sys_vgui(".x%lx.c create rectangle %d %d %d %d -outline %s -tags [list %lxIN%d inlet]\n",
              canvas,
              xpos, ypos,
              xpos+IOWIDTH, ypos + xlet_height(x),
-             STYLE_BORDER_COLOR,
+             STYLE_CONTROL_XLET_COLOR,
              x, 0);
 }
 
@@ -307,7 +307,7 @@ static void my_numbox_draw_io(t_my_numbox* x,t_glist* glist, int old_snd_rcv_fla
         sys_vgui(".x%lx.c create rectangle %d %d %d %d -fill %s -tags %lxOUT%d\n",
              canvas,
              xpos, ypos + x->x_gui.x_h-1,
-             xpos+IOWIDTH, ypos + x->x_gui.x_h, STYLE_BORDER_COLOR,
+             xpos+IOWIDTH, ypos + x->x_gui.x_h, STYLE_CONTROL_XLET_COLOR,
              x, 0);
     if(!(old_snd_rcv_flags & IEM_GUI_OLD_SND_FLAG) && x->x_gui.x_fsf.x_snd_able)
         sys_vgui(".x%lx.c delete %lxOUT%d\n", canvas, x, 0);
@@ -315,7 +315,7 @@ static void my_numbox_draw_io(t_my_numbox* x,t_glist* glist, int old_snd_rcv_fla
         sys_vgui(".x%lx.c create rectangle %d %d %d %d -fill %s -tags %lxIN%d\n",
              canvas,
              xpos, ypos,
-             xpos+IOWIDTH, ypos+1, STYLE_BORDER_COLOR,
+             xpos+IOWIDTH, ypos+1, STYLE_CONTROL_XLET_COLOR,
              x, 0);
     if(!(old_snd_rcv_flags & IEM_GUI_OLD_RCV_FLAG) && x->x_gui.x_fsf.x_rcv_able)
         sys_vgui(".x%lx.c delete %lxIN%d\n", canvas, x, 0);
@@ -346,7 +346,7 @@ static void my_numbox_draw_select(t_my_numbox *x, t_glist *glist)
     else
     {
         sys_vgui(".x%lx.c itemconfigure %lxBASE1 -outline %s\n",
-            canvas, x, STYLE_BORDER_COLOR);
+            canvas, x, STYLE_IEM_BORDER_COLOR);
         sys_vgui(".x%lx.c itemconfigure %lxBASE2 -fill #%06x\n",
             canvas, x, x->x_gui.x_fcol);
         sys_vgui(".x%lx.c itemconfigure %lxLABEL -fill #%06x\n",
@@ -765,7 +765,7 @@ static void my_numbox_list(t_my_numbox *x, t_symbol *s, int ac, t_atom *av)
 static void *my_numbox_new(t_symbol *s, int argc, t_atom *argv)
 {
     t_my_numbox *x = (t_my_numbox *)pd_new(my_numbox_class);
-    int w=5, h=14;
+    int w=5, h=15;
     int lilo=0, f=0, ldx=0, ldy=-8;
     int fs=10;
     int log_height=256;
