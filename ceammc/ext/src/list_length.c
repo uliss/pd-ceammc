f0b1da44cad2a153c6323225f97c154f7afdc93b
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-22 23:11:29 +0300

    list.length cleanup

--- ceammc/ext/src/list_length.c
+++ ceammc/ext/src/list_length.c
@@ -6,26 +6,24 @@
 t_class* length_class;
 
 typedef struct length {
-    t_object x_ob;
-    t_outlet* x_outlet;
+    t_object x_obj;
 } t_length;
 
-void length_list(t_length* x, t_symbol* s, int argc, t_atom* argv)
+static void length_list(t_length* x, t_symbol* s, int argc, t_atom* argv)
 {
-    outlet_float(x->x_outlet, argc);
+    outlet_float(x->x_obj.te_outlet, argc);
 }
 
-void* length_new()
+static void* length_new()
 {
     t_length* x = (t_length*)pd_new(length_class);
-    x->x_outlet = outlet_new(&x->x_ob, gensym("float"));
+    outlet_new(&x->x_obj, &s_float);
     return (void*)x;
 }
 
 void CEAMMC_LIST_MODULE(length)
 {
-    length_class = class_new(gensym(CEAMMC_LIST_EXT("length")),
-        (t_newmethod)length_new, 0,
-        sizeof(t_length), 0, A_NULL);
+    length_class = class_new(CEAMMC_LIST_GENSYM(length),
+        (t_newmethod)length_new, 0, sizeof(t_length), 0, A_NULL);
     class_addlist(length_class, length_list);
 }
