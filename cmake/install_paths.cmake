1e5499324c207fc5eb397534ff446437076dea14
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-24 01:54:51 +0300

    doc install path

--- cmake/install_paths.cmake
+++ cmake/install_paths.cmake
@@ -3,6 +3,7 @@ set(PD_SEND_INSTALL_PATH lib/puredata/bin)
 set(PD_RECEIVE_INSTALL_PATH lib/puredata/bin)
 set(PD_WATCHDOG_INSTALL_PATH lib/puredata/bin)
 set(PDCORE_LIB_INSTALL_PATH lib)
+set(PD_DOC_INSTALL_PATH lib/puredata)
 
 
 
