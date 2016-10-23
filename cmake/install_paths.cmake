bf0b730fa7b078f39cd2140286810cc47162d228
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-24 02:00:27 +0300

    tcl install fix

--- cmake/install_paths.cmake
+++ cmake/install_paths.cmake
@@ -4,6 +4,6 @@ set(PD_RECEIVE_INSTALL_PATH lib/puredata/bin)
 set(PD_WATCHDOG_INSTALL_PATH lib/puredata/bin)
 set(PDCORE_LIB_INSTALL_PATH lib)
 set(PD_DOC_INSTALL_PATH lib/puredata)
-
+set(PD_TCL_INSTALL_PATH lib/puredata)
 
 
