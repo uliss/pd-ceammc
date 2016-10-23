d1b32c888ebb15823ca247b6261fd8bae5e0031f
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-24 01:03:53 +0300

    install paths edited

---
+++ cmake/install_paths.cmake
@@ -0,0 +1,8 @@
+set(PD_EXE_INSTALL_PATH lib/puredata/bin)
+set(PD_SEND_INSTALL_PATH lib/puredata/bin)
+set(PD_RECEIVE_INSTALL_PATH lib/puredata/bin)
+set(PD_WATCHDOG_INSTALL_PATH lib/puredata/bin)
+set(PDCORE_LIB_INSTALL_PATH lib)
+
+
+
