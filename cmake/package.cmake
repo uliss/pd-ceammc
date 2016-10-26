6c5e2377c6fe92284de57dcfdb644286582bbfd7
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-26 21:13:57 +0300

    XDG linux fixes

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -12,7 +12,7 @@ set(CPACK_PACKAGE_VERSION_MINOR ${PD_MINOR_VERSION})
 set(CPACK_PACKAGE_VERSION_PATCH "${PD_BUGFIX_VERSION}_ceammc")
 
 set(PD_DESKTOP_DIR "${CMAKE_INSTALL_PREFIX}/share/applications")
-set(PD_MIME_DIR "${CMAKE_INSTALL_PREFIX}/share/mime/application")
+set(PD_MIME_DIR "${CMAKE_INSTALL_PREFIX}/share/mime/packages")
 set(PD_ICONS_DIR "${CMAKE_INSTALL_PREFIX}/share/icons")
 set(PD_PIXMAPS_DIR "${CMAKE_INSTALL_PREFIX}/share/pixmaps")
 
@@ -22,6 +22,7 @@ find_program(XDG-DESKTOP-MENU_EXECUTABLE xdg-desktop-menu)
 include(DpkgBuild)
 if(DPKG_FOUND AND NOT WIN32)
     set(DESKTOP_FILE "puredata.desktop")
+    set(MIME_FILE    "pd.xml")
     # substitute version variables
     configure_file("${CMAKE_SOURCE_DIR}/ceammc/gui/linux/${DESKTOP_FILE}" ${CMAKE_CURRENT_BINARY_DIR})
 
@@ -30,15 +31,15 @@ if(DPKG_FOUND AND NOT WIN32)
         DESTINATION ${PD_PIXMAPS_DIR})
     install(FILES "${CMAKE_SOURCE_DIR}/ceammc/gui/icons/puredata-ceammc.png"
         DESTINATION ${PD_ICONS_DIR})
-    install(FILES "${CMAKE_SOURCE_DIR}/ceammc/gui/linux/pd.xml"
-        DESTINATION ${PD_MIME_DIR})
-    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${DESKTOP_FILE}"
-        DESTINATION ${PD_DESKTOP_DIR})
+    # MIME file for .pd documents
+    install(FILES "${CMAKE_SOURCE_DIR}/ceammc/gui/linux/${MIME_FILE}" DESTINATION ${PD_MIME_DIR})
+    # Desktop files
+    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${DESKTOP_FILE}" DESTINATION ${PD_DESKTOP_DIR})
 
     install(CODE "
-        execute_process(COMMAND ${XDG-MIME_EXECUTABLE} install --novendor ${PD_MIME_DIR}/pd.xml)
+        execute_process(COMMAND ${XDG-MIME_EXECUTABLE} install --novendor ${PD_MIME_DIR}/${MIME_FILE})
         execute_process(COMMAND ${XDG-DESKTOP-MENU_EXECUTABLE} install --novendor ${PD_DESKTOP_DIR}/${DESKTOP_FILE})
-        execute_process(COMMAND ${XDG-MIME_EXECUTABLE} default ${PD_DESKTOP_DIR}/${DESKTOP_FILE} application/x-pd-item)
+        execute_process(COMMAND ${XDG-MIME_EXECUTABLE} default ${DESKTOP_FILE} text/x-puredata)
     ")
 
 
