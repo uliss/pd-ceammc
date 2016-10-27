9aee52b322bace8ffb54ee71b995c1ff02619623
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-28 02:29:27 +0300

    is_symbol renaming and refactoring

--- ceammc/ext/src/is_symbol.cpp
+++ ceammc/ext/src/is_symbol.cpp
@@ -1,12 +1,13 @@
-#include <m_pd.h>
 #include "ceammc.h"
+#include <m_pd.h>
 
 t_class* is_symbol_class;
 struct t_is_symbol {
     t_object x_obj;
+    t_outlet* out_value;
 };
 
-static void is_symbol_anything(t_is_symbol *x, t_symbol *s, int argc, t_atom *argv)
+static void is_symbol_anything(t_is_symbol* x, t_symbol* /*s*/, int /*argc*/, t_atom* /*argv*/)
 {
     outlet_float(x->x_obj.te_outlet, 0);
 }
@@ -16,23 +17,24 @@ static void is_symbol_bang(t_is_symbol* x)
     outlet_float(x->x_obj.te_outlet, 0);
 }
 
-static void is_symbol_float(t_is_symbol* x, t_floatarg f)
+static void is_symbol_float(t_is_symbol* x, t_floatarg /*f*/)
 {
     outlet_float(x->x_obj.te_outlet, 0);
 }
 
-static void is_symbol_list(t_is_symbol* x, t_symbol* s, int argc, t_atom* argv)
+static void is_symbol_list(t_is_symbol* x, t_symbol* /*s*/, int /*argc*/, t_atom* /*argv*/)
 {
     outlet_float(x->x_obj.te_outlet, 0);
 }
 
-static void is_symbol_pointer(t_is_symbol* x, t_gpointer *pt)
+static void is_symbol_pointer(t_is_symbol* x, t_gpointer* /*pt*/)
 {
     outlet_float(x->x_obj.te_outlet, 0);
 }
 
-static void is_symbol_symbol(t_is_symbol* x, t_symbol *s)
+static void is_symbol_symbol(t_is_symbol* x, t_symbol* s)
 {
+    outlet_symbol(x->out_value, s);
     outlet_float(x->x_obj.te_outlet, 1);
 }
 
@@ -40,15 +42,20 @@ static void* is_symbol_new()
 {
     t_is_symbol* x = reinterpret_cast<t_is_symbol*>(pd_new(is_symbol_class));
     outlet_new(&x->x_obj, &s_float);
-    
+    x->out_value = outlet_new(&x->x_obj, &s_symbol);
     return static_cast<void*>(x);
 }
 
-extern "C" void setup_is0x2esymbol()
+static void is_symbol_free(t_is_symbol* x)
 {
-    is_symbol_class = class_new(gensym("is.symbol"),
+    outlet_free(x->out_value);
+}
+
+extern "C" void is_symbol_setup()
+{
+    is_symbol_class = class_new(gensym("is_symbol"),
         reinterpret_cast<t_newmethod>(is_symbol_new),
-        reinterpret_cast<t_method>(0),
+        reinterpret_cast<t_method>(is_symbol_free),
         sizeof(t_is_symbol), 0, A_NULL);
     class_addanything(is_symbol_class, is_symbol_anything);
     class_addbang(is_symbol_class, is_symbol_bang);
@@ -57,4 +64,3 @@ extern "C" void setup_is0x2esymbol()
     class_addpointer(is_symbol_class, is_symbol_pointer);
     class_addsymbol(is_symbol_class, is_symbol_symbol);
 }
-
