5a15f669db4b2ccb51460443bbc5d3d32afdb734
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-28 02:20:16 +0300

    is_odd rename and refactoring

--- ceammc/ext/src/is_odd.cpp
+++ ceammc/ext/src/is_odd.cpp
@@ -1,32 +1,41 @@
+#include "ceammc.h"
 #include <m_pd.h>
 #include <stdlib.h>
-#include "ceammc.h"
 
 t_class* is_odd_class;
 struct t_is_odd {
     t_object x_obj;
+    t_outlet* out_value;
 };
 
 static void is_odd_float(t_is_odd* x, t_floatarg f)
 {
-    t_int v = static_cast<t_int>(f);
-    outlet_float(x->x_obj.te_outlet, (labs(v) % 2 == 1) ? 1 : 0);
+    if (static_cast<t_int>(f) % 2 == 1) {
+        outlet_float(x->out_value, f);
+        outlet_float(x->x_obj.te_outlet, 1);
+    } else {
+        outlet_float(x->x_obj.te_outlet, 0);
+    }
 }
 
 static void* is_odd_new()
 {
     t_is_odd* x = reinterpret_cast<t_is_odd*>(pd_new(is_odd_class));
     outlet_new(&x->x_obj, &s_float);
-    
+    x->out_value = outlet_new(&x->x_obj, &s_float);
     return static_cast<void*>(x);
 }
 
-extern "C" void setup_is0x2eodd()
+static void is_odd_free(t_is_odd* x)
 {
-    is_odd_class = class_new(gensym("is.odd"),
-        static_cast<t_newmethod>(is_odd_new),
-        static_cast<t_method>(0),
+    outlet_free(x->out_value);
+}
+
+extern "C" void is_odd_setup()
+{
+    is_odd_class = class_new(gensym("is_odd"),
+        reinterpret_cast<t_newmethod>(is_odd_new),
+        reinterpret_cast<t_method>(is_odd_free),
         sizeof(t_is_odd), 0, A_NULL);
     class_addfloat(is_odd_class, is_odd_float);
 }
-
