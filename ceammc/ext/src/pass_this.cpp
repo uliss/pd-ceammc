e0c988745ce9fe1871a9031200d9dac02c4a78cc
Serge Poltavski <serge.poltavski@gmail.com>
2016-10-28 15:10:14 +0300

    pass => pass.this renaming

--- ceammc/ext/src/pass_this.cpp
+++ ceammc/ext/src/pass_this.cpp
@@ -1,19 +1,19 @@
 #include "ceammc.hpp"
 #include <m_pd.h>
 
-t_class* pass_class;
-struct t_pass {
+t_class* pass_this_class;
+struct t_pass_this {
     t_object x_obj;
     ceammc::pd::atom_list* args;
 };
 
-static void pass_float(t_pass* x, t_floatarg f)
+static void pass_float(t_pass_this* x, t_floatarg f)
 {
     if (ceammc::pd::atom_list_contains(*x->args, f))
         outlet_float(x->x_obj.te_outlet, f);
 }
 
-static void pass_list(t_pass* x, t_symbol* s, int argc, t_atom* argv)
+static void pass_list(t_pass_this* x, t_symbol* s, int argc, t_atom* argv)
 {
     ceammc::pd::atom_list passed_items;
     for (int i = 0; i < argc; i++) {
@@ -25,7 +25,7 @@ static void pass_list(t_pass* x, t_symbol* s, int argc, t_atom* argv)
         ceammc::pd::output(x->x_obj.te_outlet, passed_items);
 }
 
-static void pass_symbol(t_pass* x, t_symbol* s)
+static void pass_symbol(t_pass_this* x, t_symbol* s)
 {
     if (ceammc::pd::atom_list_contains(*x->args, s))
         outlet_symbol(x->x_obj.te_outlet, s);
@@ -33,7 +33,7 @@ static void pass_symbol(t_pass* x, t_symbol* s)
 
 static void* pass_new(t_symbol* s, int argc, t_atom* argv)
 {
-    t_pass* x = reinterpret_cast<t_pass*>(pd_new(pass_class));
+    t_pass_this* x = reinterpret_cast<t_pass_this*>(pd_new(pass_this_class));
     outlet_new(&x->x_obj, &s_float);
 
     x->args = new ceammc::pd::atom_list;
@@ -44,18 +44,18 @@ static void* pass_new(t_symbol* s, int argc, t_atom* argv)
     return static_cast<void*>(x);
 }
 
-static void pass_free(t_pass* x)
+static void pass_free(t_pass_this* x)
 {
     delete x->args;
 }
 
-extern "C" void pass_setup()
+extern "C" void setup_pass0x2ethis()
 {
-    pass_class = class_new(gensym("pass"),
+    pass_this_class = class_new(gensym("pass.this"),
         reinterpret_cast<t_newmethod>(pass_new),
         reinterpret_cast<t_method>(pass_free),
-        sizeof(t_pass), 0, A_GIMME, 0);
-    class_addfloat(pass_class, pass_float);
-    class_addlist(pass_class, pass_list);
-    class_addsymbol(pass_class, pass_symbol);
+        sizeof(t_pass_this), 0, A_GIMME, 0);
+    class_addfloat(pass_this_class, pass_float);
+    class_addlist(pass_this_class, pass_list);
+    class_addsymbol(pass_this_class, pass_symbol);
 }
