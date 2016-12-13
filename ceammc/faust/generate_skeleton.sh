2a7cc18273565da626be21b94f5d382097361393
Serge Poltavski <serge.poltavski@gmail.com>
2016-12-13 15:43:08 +0300

    generation template update

--- ceammc/faust/generate_skeleton.sh
+++ ceammc/faust/generate_skeleton.sh
@@ -59,20 +59,9 @@ cat << EOF > "${CPP_FILE}"
 static void* faust_new(t_symbol* s, int argc, t_atom* argv)
 {
     t_faust* x = reinterpret_cast<t_faust*>(pd_new(faust_class));
-
-    const char* id = NULL;
-    get_nth_symbol_arg(argc, argv, 1, &id);
-    faust_new_internal(x, id);
-
-    t_float param1 = 0;
-    get_nth_float_arg(argc, argv, 1, &param1);
-    pd_float((t_pd*)x, param1);
-
-    t_float param2 = 0;
-    get_nth_float_arg(argc, argv, 2, &param2);
-    x->ui->setElementValue("param2", param2);
-
-    return x;
+    PdArgParser p(x, argc, argv);
+//    p.initFloatArg("arg1", 1);
+    return p.pd_obj();
 }
 
 extern "C" void setup_${PREFIX}0x2e${NAME}_tilde()
