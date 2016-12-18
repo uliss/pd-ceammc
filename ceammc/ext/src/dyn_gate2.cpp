dc3cc7882d68f4cbeb693d4383d9ec76cc9a38f2
Serge Poltavski <serge.poltavski@gmail.com>
2016-12-19 02:56:20 +0300

    faust arch migration

--- ceammc/ext/src/dyn_gate2.cpp
+++ ceammc/ext/src/dyn_gate2.cpp
@@ -13,23 +13,6 @@ static void* faust_new(t_symbol* s, int argc, t_atom* argv)
 
 extern "C" void setup_dyn0x2egate2_tilde()
 {
-    t_symbol* s = gensym("dyn.gate2~");
-    faust_class = class_new(s, reinterpret_cast<t_newmethod>(faust_new),
-        reinterpret_cast<t_method>(faust_free),
-        sizeof(t_faust),
-        CLASS_DEFAULT,
-        A_GIMME, A_NULL);
-    class_addmethod(faust_class, nullfn, &s_signal, A_NULL);
-    class_addmethod(faust_class, reinterpret_cast<t_method>(faust_dsp), gensym("dsp"), A_NULL);
-    CLASS_MAINSIGNALIN(faust_class, t_faust, f);
-    class_addanything(faust_class, faust_any);
-
-    s_button = gensym("button");
-    s_checkbox = gensym("checkbox");
-    s_vslider = gensym("vslider");
-    s_hslider = gensym("hslider");
-    s_nentry = gensym("nentry");
-    s_vbargraph = gensym("vbargraph");
-    s_hbargraph = gensym("hbargraph");
+    internal_setup(gensym("dyn.gate2~"));
 }
 
