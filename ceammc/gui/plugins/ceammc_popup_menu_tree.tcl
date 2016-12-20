b38228871fea6d83fb58deb5e3947f0cb57787fa
Alex Nadzharov <anadjarov@gmail.com>
2016-12-20 12:34:59 +0300

    CEAMMC objects in popup menu

--- ceammc/gui/plugins/ceammc_popup_menu_tree.tcl
+++ ceammc/gui/plugins/ceammc_popup_menu_tree.tcl
@@ -30,3 +30,27 @@
     {reverb
         {rev1~ rev2~ rev3~}}
 }
+{ceammc
+    {audio_osc
+        {osc.impulse~ osc.pulse~ osc.saw~ osc.sinfb~ osc.square~ osc.tri~}
+    }
+    {audio_dynamics
+        {dyn.comp~ dyn.comp2~ dyn.gate~ dyn.gate2~ dyn.limit~ dyn.limit2~ dyn.softclip~}}
+    {audio_envelope
+        {env.adsr~ env.ar~ env.follow~}}
+    {audio_filters
+        {flt.highshelf~ flt.lowshelf~ flt.eq_peak~ flt.dcblock~ flt.dcblock2~}}
+    {audio_fx
+        {fx.pitchshift~}}
+    {list
+        {list.apply_to list.at list.each list.length list.sort}}
+    {math
+        {math.abs math.acos math.acosh math.asin math.asinh math.atan math.atanh math.cbrt math.ceil math.cos math.cosh math.e math.exp math.exp2 math.floor math.inf math.lof math.log10 math.log2 math.nan math.pi math.round math.sin math.sinh math.sqrt math.tan math.tanh math.trunc}
+    }
+    {predicates
+        {is_any is_bang is_even is_float is_list is_odd is_pointer is_symbol}
+    }
+    {data_pass
+        {data.fifo pass.changed}
+    }
+}
