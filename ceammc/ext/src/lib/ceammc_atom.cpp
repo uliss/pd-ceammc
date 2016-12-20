944cc4ffae1ca42451638c4c389313227e68197b
Serge Poltavski <serge.poltavski@gmail.com>
2016-12-20 13:33:00 +0300

    simple t_atom wrapper added

---
+++ ceammc/ext/src/lib/ceammc_atom.cpp
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ * Copyright 2016 Serge Poltavski. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+
+#include "ceammc_atom.h"
+
+namespace ceammc {
+
+Atom::Atom(const t_atom& a)
+    : t_atom(a)
+{
+    if (a_type == A_DEFFLOAT)
+        a_type = A_FLOAT;
+    if (a_type == A_DEFSYMBOL)
+        a_type = A_SYMBOL;
+}
+
+bool Atom::isFloat() const
+{
+    return a_type == A_FLOAT;
+}
+
+bool Atom::isSymbol() const
+{
+    return a_type == A_SYMBOL;
+}
+}
