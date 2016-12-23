a36a93bb194df3debab217296e4392e048fa4fa1
Alex Nadzharov <anadjarov@gmail.com>
2016-12-23 11:05:42 +0300

    namespace fix

--- ceammc/ext/src/ui_link.cpp
+++ ceammc/ext/src/ui_link.cpp
@@ -28,7 +28,7 @@ struct ui_link : ceammc_gui::base_pd_object
     t_symbol *link;
     t_symbol *title;
     
-   // std::string display;
+    // std::string display;
     
 };
 
@@ -41,105 +41,107 @@ template <typename T> std::string to_str(const T& t) {
 
 
 
-
-
-//////
-
-UI_fun(ui_link)::wx_paint(t_object *z, t_object *view)
-{
-    //UI_Prop
+namespace ceammc_gui {
     
-    t_symbol *bgl = gensym("background_layer");
-    float size;
-    t_rect rect;
-    ebox_get_rect_for_view((t_ebox *)z, &rect);
+    //////
     
-    t_elayer *g = ebox_start_layer((t_ebox *)z, bgl, rect.width, rect.height);
-    
-    ui_link *zx = (ui_link*)z;
-    
-    if(g)
+    UI_fun(ui_link)::wx_paint(t_object *z, t_object *view)
     {
-        egraphics_set_color_hex(g, gensym("#E0E0E0"));
+        //UI_Prop
         
-        egraphics_rectangle(g, 0, 0, rect.width, rect.height);
-        egraphics_fill(g);
+        t_symbol *bgl = gensym("background_layer");
+        float size;
+        t_rect rect;
+        ebox_get_rect_for_view((t_ebox *)z, &rect);
         
-        //int argc;
-        //t_atom *argv;
-        //eclass_attr_getter(z, gensym("link"),&argc , &argv);
+        t_elayer *g = ebox_start_layer((t_ebox *)z, bgl, rect.width, rect.height);
         
-//        if (argc)
-//        {
-//            if (argv[0].a_type == A_SYMBOL)
-//            {
-//                zx->display = argv[0].a_w.w_symbol->s_name;
-//            }
-//        }
-        //zx->display.c_str()
+        ui_link *zx = (ui_link*)z;
         
-        etext_layout_set(zx->t_e, zx->title->s_name, zx->t_ef, rect.width/2, rect.height/2, rect.width, rect.height, ETEXT_CENTER, ETEXT_JLEFT, ETEXT_WRAP);
+        if(g)
+        {
+            egraphics_set_color_hex(g, gensym("#E0E0E0"));
+            
+            egraphics_rectangle(g, 0, 0, rect.width, rect.height);
+            egraphics_fill(g);
+            
+            //int argc;
+            //t_atom *argv;
+            //eclass_attr_getter(z, gensym("link"),&argc , &argv);
+            
+            //        if (argc)
+            //        {
+            //            if (argv[0].a_type == A_SYMBOL)
+            //            {
+            //                zx->display = argv[0].a_w.w_symbol->s_name;
+            //            }
+            //        }
+            //zx->display.c_str()
+            
+            etext_layout_set(zx->t_e, zx->title->s_name, zx->t_ef, rect.width/2, rect.height/2, rect.width, rect.height, ETEXT_CENTER, ETEXT_JLEFT, ETEXT_WRAP);
+            
+            etext_layout_draw(zx->t_e, g);
+            
+            
+        }
         
-        etext_layout_draw(zx->t_e, g);
+        ebox_end_layer((t_ebox*)z, bgl);
+        ebox_paint_layer((t_ebox *)z, bgl, 0., 0.);
         
         
     }
     
-    ebox_end_layer((t_ebox*)z, bgl);
-    ebox_paint_layer((t_ebox *)z, bgl, 0., 0.);
+    UI_fun(ui_link)::wx_mousedown_ext(t_object *z, t_object *view, t_pt pt, long modifiers)
+    {
+        ui_link *zx = (ui_link*)z;
+        //
+        //
+        //    t_symbol *dir = canvas_getdir((t_glist*)view);
+        //
+        //    char final_name[FILENAME_MAX];
+        //    sys_expandpathelems(zx->display.c_str(), final_name);
+        
+        //printf("file %s\n", zx->display.c_str() );
+        
+        sys_vgui("open_file [file normalize {%s}] \n",
+                 zx->link->s_name );
+        
+        
+        
+    }
     
+    UI_fun(ui_link)::new_ext(t_object *z, t_symbol *s, int argcl, t_atom *argv)
+    {
+        ui_link *zx = (ui_link*)z;
+        zx->t_e = etext_layout_create();
+        
+        zx->t_ef = efont_create(gensym("Helvetica"), gensym(""), gensym("normal"), 12);
+        
+        
+    }
     
-}
-
-UI_fun(ui_link)::wx_mousedown_ext(t_object *z, t_object *view, t_pt pt, long modifiers)
-{
-    ui_link *zx = (ui_link*)z;
-//    
-//    
-//    t_symbol *dir = canvas_getdir((t_glist*)view);
-//    
-//    char final_name[FILENAME_MAX];
-//    sys_expandpathelems(zx->display.c_str(), final_name);
+    UI_fun(ui_link)::free_ext(t_object *z)
+    {
+        ui_link *zx = (ui_link*)z;
+        efont_destroy(zx->t_ef);
+        
+    }
     
-    //printf("file %s\n", zx->display.c_str() );
+    UI_fun(ui_link)::init_ext(t_eclass *z)
+    {
+        CLASS_ATTR_DEFAULT (z, "size", 0, "120. 15.");
+        
+        CLASS_ATTR_SYMBOL(z, "link", 0, ui_link, link);
+        CLASS_ATTR_DEFAULT(z, "link", 0, "<no link>");
+        CLASS_ATTR_LABEL(z, "link", 0, "Link");
+        CLASS_ATTR_DEFAULT_SAVE_PAINT(z, "link", 0, "<no link>");
+        
+        CLASS_ATTR_SYMBOL(z, "title", 0, ui_link, title);
+        CLASS_ATTR_DEFAULT(z, "title", 0, "<no title>");
+        CLASS_ATTR_LABEL(z, "title", 0, "Title");
+        CLASS_ATTR_DEFAULT_SAVE_PAINT(z, "title", 0, "<no title>");
+    }
     
-    sys_vgui("open_file [file normalize {%s}] \n",
-             zx->link->s_name );
-    
-    
-    
-}
-
-UI_fun(ui_link)::new_ext(t_object *z, t_symbol *s, int argcl, t_atom *argv)
-{
-    ui_link *zx = (ui_link*)z;
-    zx->t_e = etext_layout_create();
-    
-    zx->t_ef = efont_create(gensym("Helvetica"), gensym(""), gensym("normal"), 12);
-    
-    
-}
-
-UI_fun(ui_link)::free_ext(t_object *z)
-{
-    ui_link *zx = (ui_link*)z;
-    efont_destroy(zx->t_ef);
-    
-}
-
-UI_fun(ui_link)::init_ext(t_eclass *z)
-{
-    CLASS_ATTR_DEFAULT (z, "size", 0, "120. 15.");
-    
-    CLASS_ATTR_SYMBOL(z, "link", 0, ui_link, link);
-    CLASS_ATTR_DEFAULT(z, "link", 0, "<no link>");
-    CLASS_ATTR_LABEL(z, "link", 0, "Link");
-    CLASS_ATTR_DEFAULT_SAVE_PAINT(z, "link", 0, "<no link>");
-    
-    CLASS_ATTR_SYMBOL(z, "title", 0, ui_link, title);
-    CLASS_ATTR_DEFAULT(z, "title", 0, "<no title>");
-    CLASS_ATTR_LABEL(z, "title", 0, "Title");
-    CLASS_ATTR_DEFAULT_SAVE_PAINT(z, "title", 0, "<no title>");
 }
 
 extern "C" void setup_ui0x2elink()
--- ceammc/ext/src/ui_sliders.cpp
+++ ceammc/ext/src/ui_sliders.cpp
@@ -19,180 +19,184 @@ struct ui_sliders : ceammc_gui::base_pd_object
     
 };
 
-UI_fun(ui_sliders)::wx_paint(t_object *z, t_object *view)
-{
-    UI_Prop
+namespace ceammc_gui {
     
-    t_symbol *bgl = gensym("background_layer");
-    float size;
-    t_rect rect;
-    ebox_get_rect_for_view((t_ebox *)z, &rect);
+    UI_fun(ui_sliders)::wx_paint(t_object *z, t_object *view)
+    {
+        UI_Prop
+        
+        t_symbol *bgl = gensym("background_layer");
+        float size;
+        t_rect rect;
+        ebox_get_rect_for_view((t_ebox *)z, &rect);
+        
+        t_elayer *g = ebox_start_layer((t_ebox *)z, bgl, rect.width, rect.height);
+        
+        ui_sliders *zx = (ui_sliders*)z;
+        
+        if(g)
+        {
+            
+            if (UI_Pf("count")<1) UI_Pset("count",1);
+            if (UI_Pf("count")>128) UI_Pset("count",128);   //TEMP
+            
+            
+            for (int i =0; i<UI_Pf("count"); i++)
+            {
+                float v = (zx->val_list)[i].a_w.w_float;
+                
+                if (v>1) v=1;
+                if (v<0) v=0;
+                
+                
+                float xx,yy,w,h;
+                
+                if (UI_Pf("_is_vertical")==1.)
+                {
+                    xx = 0;
+                    yy = i*rect.height / UI_Pf("count");
+                    w = v*rect.width;
+                    h = rect.height / UI_Pf("count")-1;
+                }
+                else
+                {
+                    yy = rect.height - v*rect.height;
+                    xx = i*rect.width / UI_Pf("count");
+                    h = rect.height;
+                    w = rect.width / UI_Pf("count")-1;
+                }
+                
+                egraphics_rectangle(g, xx, yy, w, h);
+                egraphics_set_color_hex(g, gensym("#C0C0C0"));
+                egraphics_fill(g);
+                
+            }
+        }
+        
+        ebox_end_layer((t_ebox*)z, bgl);
+        ebox_paint_layer((t_ebox *)z, bgl, 0., 0.);
+        
+        
+    }
     
-    t_elayer *g = ebox_start_layer((t_ebox *)z, bgl, rect.width, rect.height);
+    UI_fun(ui_sliders)::m_set(t_object *z, t_symbol *s, int argc, t_atom *argv)
     
-    ui_sliders *zx = (ui_sliders*)z;
+    {
+        UI_Prop
+        
+        ui_sliders *zx = (ui_sliders*)z;
+        
+        if (zx->val_list) free (zx->val_list);
+        
+        zx->val_list = (t_atom*)malloc(sizeof(t_atom)*argc);
+        zx->val_list_size = argc;
+        
+        UI_Pset("count", argc);
+        
+        for (int i=0;i<argc;i++) { zx->val_list[i] = argv[i]; }
+        
+        ceammc_gui::object<ceammc_gui::base_pd_object>::ws_redraw(z);
+        
+        
+    }
     
-    if(g)
+    UI_fun(ui_sliders)::m_list(t_object *z, t_symbol *s, int argc, t_atom *argv)
     {
         
-        if (UI_Pf("count")<1) UI_Pset("count",1);
-        if (UI_Pf("count")>128) UI_Pset("count",128);   //TEMP
+        ceammc_gui::object<ui_sliders>::m_set(z, s, argc, argv);
+        
+        outlet_list( ((ui_sliders*)z)->out1, &s_list, ((ui_sliders*)z)->val_list_size, ((ui_sliders*)z)->val_list );
         
         
-        for (int i =0; i<UI_Pf("count"); i++)
+    }
+    
+    
+    
+    UI_fun(ui_sliders)::m_bang(t_object *z, t_symbol *s, int argc, t_atom *argv)
+    {
+        UI_Prop
+        
+        ui_sliders *zx = (ui_sliders*)z;
+        
+        outlet_list( ((ui_sliders*)z)->out1, &s_list, ((ui_sliders*)z)->val_list_size, ((ui_sliders*)z)->val_list );
+        
+        
+    }
+    
+    UI_fun(ui_sliders)::wx_oksize(t_object *z, t_rect *newrect)
+    {
+        UI_Prop
+        
+        UI_Pset("_is_vertical", newrect->width<newrect->height);
+    }
+    
+    
+    
+    UI_fun(ui_sliders)::wx_mousedrag_ext(t_object* z, t_object *view, t_pt pt, long modifiers)
+    {
+        UI_Prop
+        
+        t_rect rect;
+        ebox_get_rect_for_view((t_ebox *)z, &rect);
+        
+        int numslider; float val;
+        
+        if (UI_Pf("_is_vertical")==1)
         {
-            float v = (zx->val_list)[i].a_w.w_float;
-
-            if (v>1) v=1;
-            if (v<0) v=0;
-
+            numslider = floor(pt.y/rect.height * ((ui_sliders*)z)->val_list_size);
+            val = pt.x/rect.width;
+        }
+        else
+        {
             
-            float xx,yy,w,h;
+            numslider = floor(pt.x/rect.width * ((ui_sliders*)z)->val_list_size);
             
-            if (UI_Pf("_is_vertical")==1.)
-            {
-                xx = 0;
-                yy = i*rect.height / UI_Pf("count");
-                w = v*rect.width;
-                h = rect.height / UI_Pf("count")-1;
-            }
-            else
-            {
-                yy = rect.height - v*rect.height;
-                xx = i*rect.width / UI_Pf("count");
-                h = rect.height;
-                w = rect.width / UI_Pf("count")-1;
-            }
+            val = 1. - pt.y/rect.height;
+        }
+        
+        ((ui_sliders*)z)->val_list[numslider].a_w.w_float = val;
+        
+        ceammc_gui::object<ceammc_gui::base_pd_object>::ws_redraw(z);
+        
+        outlet_list( ((ui_sliders*)z)->out1, &s_list, ((ui_sliders*)z)->val_list_size, ((ui_sliders*)z)->val_list );
+        
+    }
+    
+    UI_fun(ui_sliders)::wx_mousedown_ext(t_object *z, t_object *view, t_pt pt, long modifiers)
+    {
+        ceammc_gui::object<ui_sliders>::wx_mousedrag_ext(z, view, pt, modifiers);
+    }
+    
+    UI_fun(ui_sliders)::ui_properties_init_ext(ceammc_gui::properties *def_p)
+    {
+        
+        def_p->ui_property_set("count", 8);
+        def_p->ui_property_set("_is_vertical", 0);
+        
+        
+    }
+    
+    UI_fun(ui_sliders)::init_ext(t_eclass *z)
+    {
+        CLASS_ATTR_DEFAULT (z, "size", 0, "150. 100.");
+        
+    }
+    
+    UI_fun(ui_sliders)::new_ext(t_object *x, t_symbol *s, int argcl, t_atom *argv)
+    {
+        ((ui_sliders*)x)->out1 = outlet_new(x, &s_list);
+        
+        ((ui_sliders*)x)->val_list_size = 8;
+        ((ui_sliders*)x)->val_list = (t_atom*)malloc(sizeof(t_atom)*8);
+        for (int i=0;i<8;i++)
+        {
             
-            egraphics_rectangle(g, xx, yy, w, h);
-            egraphics_set_color_hex(g, gensym("#C0C0C0"));
-            egraphics_fill(g);
+            ((ui_sliders*)x)->val_list[i].a_type = A_FLOAT;
+            ((ui_sliders*)x)->val_list[i].a_w.w_float = 0.;
             
         }
     }
     
-    ebox_end_layer((t_ebox*)z, bgl);
-    ebox_paint_layer((t_ebox *)z, bgl, 0., 0.);
-
-    
-}
-
-UI_fun(ui_sliders)::m_set(t_object *z, t_symbol *s, int argc, t_atom *argv)
-
-{
-    UI_Prop
-    
-    ui_sliders *zx = (ui_sliders*)z;
-    
-    if (zx->val_list) free (zx->val_list);
-    
-    zx->val_list = (t_atom*)malloc(sizeof(t_atom)*argc);
-    zx->val_list_size = argc;
-    
-    UI_Pset("count", argc);
-    
-    for (int i=0;i<argc;i++) { zx->val_list[i] = argv[i]; }
-    
-    ceammc_gui::object<ceammc_gui::base_pd_object>::ws_redraw(z);
-    
-
-}
-
-UI_fun(ui_sliders)::m_list(t_object *z, t_symbol *s, int argc, t_atom *argv)
-{
-    
-    ceammc_gui::object<ui_sliders>::m_set(z, s, argc, argv);
-    
-    outlet_list( ((ui_sliders*)z)->out1, &s_list, ((ui_sliders*)z)->val_list_size, ((ui_sliders*)z)->val_list );
-    
-    
-}
-
-
-
-UI_fun(ui_sliders)::m_bang(t_object *z, t_symbol *s, int argc, t_atom *argv)
-{
-    UI_Prop
-    
-    ui_sliders *zx = (ui_sliders*)z;
-    
-    outlet_list( ((ui_sliders*)z)->out1, &s_list, ((ui_sliders*)z)->val_list_size, ((ui_sliders*)z)->val_list );
-    
-    
-}
-
-UI_fun(ui_sliders)::wx_oksize(t_object *z, t_rect *newrect)
-{
-    UI_Prop
-    
-    UI_Pset("_is_vertical", newrect->width<newrect->height);
-}
-
-
-
-UI_fun(ui_sliders)::wx_mousedrag_ext(t_object* z, t_object *view, t_pt pt, long modifiers)
-{
-    UI_Prop
-    
-    t_rect rect;
-    ebox_get_rect_for_view((t_ebox *)z, &rect);
-    
-    int numslider; float val;
-    
-    if (UI_Pf("_is_vertical")==1)
-    {
-        numslider = floor(pt.y/rect.height * ((ui_sliders*)z)->val_list_size);
-        val = pt.x/rect.width;
-    }
-    else
-    {
-
-        numslider = floor(pt.x/rect.width * ((ui_sliders*)z)->val_list_size);
-
-        val = 1. - pt.y/rect.height;
-    }
-    
-    ((ui_sliders*)z)->val_list[numslider].a_w.w_float = val;
-    
-    ceammc_gui::object<ceammc_gui::base_pd_object>::ws_redraw(z);
-    
-    outlet_list( ((ui_sliders*)z)->out1, &s_list, ((ui_sliders*)z)->val_list_size, ((ui_sliders*)z)->val_list );
-    
-}
-
-UI_fun(ui_sliders)::wx_mousedown_ext(t_object *z, t_object *view, t_pt pt, long modifiers)
-{
-    ceammc_gui::object<ui_sliders>::wx_mousedrag_ext(z, view, pt, modifiers);
-}
-
-UI_fun(ui_sliders)::ui_properties_init_ext(ceammc_gui::properties *def_p)
-{
-    
-    def_p->ui_property_set("count", 8);
-    def_p->ui_property_set("_is_vertical", 0);
-    
-    
-}
-
-UI_fun(ui_sliders)::init_ext(t_eclass *z)
-{
-    CLASS_ATTR_DEFAULT (z, "size", 0, "150. 100.");
-    
-}
-
-UI_fun(ui_sliders)::new_ext(t_object *x, t_symbol *s, int argcl, t_atom *argv)
-{
-    ((ui_sliders*)x)->out1 = outlet_new(x, &s_list);
-    
-    ((ui_sliders*)x)->val_list_size = 8;
-    ((ui_sliders*)x)->val_list = (t_atom*)malloc(sizeof(t_atom)*8);
-    for (int i=0;i<8;i++)
-    {
-
-    ((ui_sliders*)x)->val_list[i].a_type = A_FLOAT;
-    ((ui_sliders*)x)->val_list[i].a_w.w_float = 0.;
-
-    }
 }
 
 extern "C" void setup_ui0x2esliders()
