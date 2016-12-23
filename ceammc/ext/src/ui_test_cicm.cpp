ae8b0a50732a0c79c615b0fa5e1a606af8d8cd24
Alex Nadzharov <anadjarov@gmail.com>
2016-12-23 11:23:40 +0300

    removed test ui objects

--- ceammc/ext/src/ui_test_cicm.cpp
+++ ceammc/ext/src/ui_test_cicm.cpp
@@ -1,6 +1,6 @@
 //
 //  tl_test.cpp
-//  
+//
 //
 //  Created by Alex Nadzharov on 15/12/16.
 //
@@ -16,14 +16,15 @@ struct test_cicm : ceammc_gui::base_pd_object
     
 };
 
-
-
-UI_fun(test_cicm)::new_ext(t_object *x, t_symbol *s, int argc, t_atom *argv)
-{
+namespace ceammc_gui {
     
-    printf("additional init\n");
-
-    
+    UI_fun(test_cicm)::new_ext(t_object *x, t_symbol *s, int argc, t_atom *argv)
+    {
+        
+        printf("additional init\n");
+        
+        
+    }
 }
 
 extern "C" void setup_ui0x2etest_cicm()
