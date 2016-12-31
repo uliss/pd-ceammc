c8fad54b37439a243d9e1c4a2f366a04f2246d5d
Serge Poltavski <serge.poltavski@gmail.com>
2016-12-31 04:42:34 +0300

    list.choice added

---
+++ ceammc/ext/src/list_choice.cpp
@@ -0,0 +1,25 @@
+#include "ceammc_factory.h"
+#include "ceammc_object.h"
+#include <cstdlib>
+
+using namespace ceammc;
+
+class ListChoice : public BaseObject {
+public:
+    ListChoice(const PdArgs& a)
+        : BaseObject(a)
+    {
+        createOutlet();
+    }
+
+    void onList(const AtomList& l)
+    {
+        size_t idx = static_cast<size_t>(rand()) % l.size();
+        atomTo(0, l[idx]);
+    }
+};
+
+extern "C" void setup_list0x2echoice()
+{
+    ObjectFactory<ListChoice> obj("list.choice");
+}
