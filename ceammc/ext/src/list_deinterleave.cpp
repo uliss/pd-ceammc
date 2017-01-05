ef30f9eeeb570c540ee3aef2e876d1cecebac5c6
Serge Poltavski <serge.poltavski@gmail.com>
2017-01-05 04:09:05 +0300

    list.deinterleave

--- ceammc/ext/src/list_deinterleave.cpp
+++ ceammc/ext/src/list_deinterleave.cpp
@@ -6,50 +6,62 @@
 //
 //
 
-#include "ceammc_atomlist.h"
-//#include <m_pd.h>
-#include <stdlib.h>
-#include "cicm_wrapper.h"
+#include "ceammc.hpp"
+#include "ceammc_factory.h"
+#include "ceammc_fn_list.h"
+#include "ceammc_log.h"
+#include "ceammc_object.h"
+
+#include <cassert>
 
 using namespace ceammc;
 
-static t_eclass* list_deinterleave_class;
-struct t_list_deinterleave {
-    t_ebox x_gui;
-    
-    t_outlet *out1;
-    t_outlet *out2;
-    //AtomList* stored_list;
+static const size_t MIN_OUTLET = 2;
+static const size_t MAX_OUTLET = 20;
+
+class ListDeinterleave : public BaseObject {
+    const size_t out_count_;
+    AtomList in_list_;
+    std::vector<AtomList> out_lists_;
+
+public:
+    ListDeinterleave(const PdArgs& a)
+        : BaseObject(a)
+        , out_count_(math::clip(atomlistToValue<size_t>(a.args, MIN_OUTLET), MIN_OUTLET, MAX_OUTLET))
+    {
+        for (size_t i = 0; i < out_count_; i++) {
+            out_lists_.push_back(AtomList());
+            createOutlet();
+        }
+    }
+
+    void onBang()
+    {
+        for (size_t i = out_lists_.size(); i > 0; i--)
+            listTo(i - 1, out_lists_[i - 1]);
+    }
+
+    void onList(const AtomList& l)
+    {
+        clearOutputList();
+
+        for (size_t i = 0; i < l.size(); i++) {
+            size_t ndx = i % out_count_;
+            out_lists_[ndx].append(l[i]);
+        }
+
+        onBang();
+    }
+
+private:
+    void clearOutputList()
+    {
+        for (size_t i = 0; i < out_lists_.size(); i++)
+            out_lists_[i].clear();
+    }
 };
 
-static void* list_deinterleave_new()
-{
-    t_list_deinterleave* x = (t_list_deinterleave*)eobj_new(list_deinterleave_class);
-    
-    x->out1 = outlet_new((t_object*)&x->x_gui, &s_list);
-    x->out2 = outlet_new((t_object*)&x->x_gui, &s_list);
-    //    outlet_new(&x->x_obj, &s_float);
-    //    x->stored_list = new AtomList;
-    
-    inlet_new((t_object*)&x->x_gui, (t_pd*)&x, gensym("list"),gensym("list"));
-    
-    return static_cast<void*>(x);
-}
-
-static void list_deinterleave_free(t_list_deinterleave* x)
-{
-    //    delete x->stored_list;
-}
-
-#pragma mark -
 extern "C" void setup_list0x2edeinterleave()
 {
-    list_deinterleave_class = eclass_new("list.deinterleave",
-                                       (method)(&list_deinterleave_new),
-                                       (method)(&list_deinterleave_free),
-                                       sizeof(t_list_deinterleave), CLASS_NOINLET, A_GIMME, 0);
-    //    class_addlist(list_delta_class, list_delta_list);
-    //    class_addanything(list_delta_class, list_delta_clear);
-    
-    eclass_register(CLASS_OBJ, list_deinterleave_class);
+    ObjectFactory<ListDeinterleave> obj("list.deinterleave");
 }
