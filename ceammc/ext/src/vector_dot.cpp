420f371e7a184f949435ccd83c908551d03a03ff
Serge Poltavski <serge.poltavski@gmail.com>
2017-01-08 16:27:22 +0300

    vector.dot product update

--- ceammc/ext/src/vector_dot.cpp
+++ ceammc/ext/src/vector_dot.cpp
@@ -1,78 +1,54 @@
-#include "ceammc.h"
-#include <m_pd.h>
-#include <stdlib.h>
-#include <string.h>
+#include "ceammc_factory.h"
+#include "ceammc_fn_vector.h"
+#include "ceammc_log.h"
+#include "ceammc_object.h"
 
-#define MIN(a, b)  (((a) < (b)) ? (a) : (b))
+using namespace ceammc;
 
-t_class* vector_dot_class;
-struct t_vector_dot {
-    t_object x_obj;
-    t_atom *vec2;
-    int vec2_size;
-    
-    t_inlet *in2;
+class VectorDotProduct : public BaseObject {
+    AtomList a_;
+    AtomList b_;
+    float dot_product_;
+
+public:
+    VectorDotProduct(const PdArgs& a)
+        : BaseObject(a)
+        , dot_product_(0.f)
+    {
+        createInlet();
+        createOutlet();
+
+        parseArguments();
+        b_ = args();
+    }
+
+    void onBang()
+    {
+        floatTo(0, dot_product_);
+    }
+
+    void onList(const AtomList& l)
+    {
+        a_ = l;
+        dot_product_ = static_cast<t_float>(vector::dotProduct(a_, b_));
+        onBang();
+    }
+
+    void onInlet(size_t, const AtomList& l)
+    {
+        b_ = l;
+    }
+
+    void dump() const
+    {
+        BaseObject::dump();
+        OBJ_DBG << "first vector:  " << a_;
+        OBJ_DBG << "second vector: " << b_;
+        OBJ_DBG << "dot product:   " << dot_product_;
+    }
 };
-
-
-static void vector_dot_list2(t_vector_dot* x, t_symbol* s, int argc, t_atom* argv)
-{
-    if (strcmp(s->s_name,"list2")==0)
-    {
-        x->vec2_size = argc;
-        
-        free(x->vec2);
-        
-        x->vec2 = (t_atom*)malloc(sizeof(t_atom)*argc);
-        
-        for (int i=0;i<argc;i++)
-        {
-            x->vec2[i] = argv[i];
-        }
-    }
-}
-
-static void vector_dot_list(t_vector_dot* x, t_symbol* s, int argc, t_atom* argv)
-{
-    if (argc < 1 || x->vec2_size < 1)
-        return;
-    
-    float dot1 = 0;
-    
-    int min_size = MIN(argc, x->vec2_size);
-    
-    for (int i=0;i<min_size;i++)
-    {
-        dot1 += argv[i].a_w.w_float * x->vec2[i].a_w.w_float;
-        
-    }
-    
-    outlet_float(x->x_obj.te_outlet, dot1);
-    
-}
-
-static void* vector_dot_new()
-{
-    t_vector_dot* x = reinterpret_cast<t_vector_dot*>(pd_new(vector_dot_class));
-    outlet_new(&x->x_obj, &s_float);
-    
-    inlet_new(&x->x_obj, &x->x_obj.ob_pd, gensym("list"), gensym("list2"));
-    
-    x->vec2 = (t_atom*)malloc(0);    //dummy
-    x->vec2_size=0;
-    
-    return static_cast<void*>(x);
-}
 
 extern "C" void setup_vector0x2edot()
 {
-    vector_dot_class = class_new(gensym("vector.dot"),
-                                      reinterpret_cast<t_newmethod>(vector_dot_new),
-                                      reinterpret_cast<t_method>(0),
-                                      sizeof(t_vector_dot), 0, A_NULL);
-    class_addlist(vector_dot_class, vector_dot_list);
-    
-    class_addanything(vector_dot_class, vector_dot_list2);
-    
-    
+    ObjectFactory<VectorDotProduct> obj("vector.dot");
 }
