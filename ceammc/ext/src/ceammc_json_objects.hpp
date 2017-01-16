7821af44512348d1bf83d641e733f41da93f1b8a
Alex Nadzharov <anadjarov@gmail.com>
2017-01-16 19:02:55 +0300

    new json.object

--- ceammc/ext/src/ceammc_json_objects.hpp
+++ ceammc/ext/src/ceammc_json_objects.hpp
@@ -22,10 +22,12 @@
 
 #include <stdlib.h>
 #include <vector>
-#include "json.hpp"
+#include <map>
+#include <string>
+//#include "json.hpp"
 
 //todo: check Windows/MinGW C++11 compatibility. Change JSON library if needed.
-using json = nlohmann::json;
+//using json = nlohmann::json;
 
 //moving
 #include "rapidjson/prettywriter.h"
@@ -34,143 +36,214 @@ using json = nlohmann::json;
 
 #include "ceammc_globaldata.h"
 
+#include "ceammc_atomlist.h"
+
 using namespace ceammc;
 using namespace rapidjson;
 
-typedef struct {
-    t_symbol *symbol;
-    json *object;
-} json_object_ptr;   //todo refcounter?
+//enum json_object_type
+//{
+//    oAtomList,
+//    oObject
+//
+//} t_jsonObjectType;
+//
+//typedef class {
+//    t_symbol *symbol;
+//    //json *object;
+//    void* object;
+//    t_jsonObjectType type;
+//
+//
+//} json_object_ptr;
+//
+//std::vector<json_object_ptr> json_objects;
 
-std::vector<json_object_ptr> json_objects;
 
+//static inline Document* cm_json_from_atom(t_atom atom)
+//{
+//    Document *ret = new Document;
+//    Document::AllocatorType& allocator = ret->GetAllocator();
+//
+//    ret->AddMember("type", (int)atom.a_type, allocator);
+//    ret->AddMember("symbol", *atom.a_w.w_symbol->s_name, allocator);
+//    ret->AddMember("float", atom.a_w.w_float, allocator);
+//
+//    return ret;
+//}
 
-static inline json cm_json_from_atom(t_atom atom)
+static inline Document* cmJSONFromAtom(Atom atom, Document::AllocatorType& allocator)
 {
-    json ret = json::object();
+    Document *ret = new Document;
     
-    int at1 = (int)atom.a_type;
-    ret["type"] = at1;
-    ret["symbol"] = atom.a_w.w_symbol->s_name;
-    ret["float"] = atom.a_w.w_float;
-    
-    return ret;
-}
-
-static inline t_atom cm_atom_from_json(json in1)
-{
-    t_atom ret;
-    
-    ret.a_type = A_SYMBOL;
-    ret.a_w.w_symbol = gensym("<>");
-    
-    if (in1.is_object())
+    if (atom.isSymbol())
     {
-        int at1 = in1["type"];
-        ret.a_type = (t_atomtype)at1;
-        std::string sym = in1["symbol"];
-        ret.a_w.w_symbol = gensym(sym.c_str());
-        ret.a_w.w_float = in1["float"];         //check PD_FLOATSIZE
+        ret->SetString(atom.asString().c_str(), (int)atom.asString().size(), allocator);
     }
-    
-    return ret;
-}
-
-static inline t_atom cm_atom_from_json_string(json in1)
-{
-    t_atom ret;
-    
-    ret.a_type = A_SYMBOL;
-    ret.a_w.w_symbol = gensym("<>");
-    
-    if (in1.is_string())
+    else if (atom.isInteger())
     {
-        std::string sym = in1;
-        ret.a_w.w_symbol = gensym(sym.c_str());
+        ret->SetInt(atom.asInt());
     }
-    
-    return ret;
-}
-
-#pragma mark -
-
-static inline Document ncm_json_from_atom(t_atom atom)
-{
-    Document ret;
-    
-    Document::AllocatorType& allocator = ret.GetAllocator();
-    
-    ret.AddMember("type", (int)atom.a_type, allocator);
-    ret.AddMember("symbol", *atom.a_w.w_symbol->s_name, allocator);
-    ret.AddMember("float", atom.a_w.w_float, allocator);
-    
-    return ret;
-}
-
-static inline t_atom ncm_atom_from_json(Document in1)
-{
-    t_atom ret;
-    
-    ret.a_type = A_SYMBOL;
-    ret.a_w.w_symbol = gensym("<>");
-    
-    if (in1["type"].IsNumber() && in1["symbol"].IsString() && in1["float"].IsNumber())
+    else if (atom.isFloat())
     {
-        int at1 = in1["type"].GetInt();
-        ret.a_type = (t_atomtype)at1;
-        std::string sym = in1["symbol"].GetString();
-        ret.a_w.w_symbol = gensym(sym.c_str());
-        ret.a_w.w_float = in1["float"].GetFloat();      //check PD_FLOATSIZE
+        ret->SetFloat(atom.asFloat());
     }
     
     return ret;
     
 }
 
-
-static inline t_atom ncm_atom_from_json_string(Document in1)
+static inline void cmJSONFromAtomList(Document *doc, AtomList list)
 {
-    t_atom ret;
+    Document::AllocatorType& allocator = doc->GetAllocator();
     
-    ret.a_type = A_SYMBOL;
-    ret.a_w.w_symbol = gensym("<>");
+    Value arr;
+    arr.SetArray();
     
-    if (in1.IsString())
+    for (int i=1; i<list.size();i++)    //iterator!
     {
-        std::string sym = in1.GetString();
-        ret.a_w.w_symbol = gensym(sym.c_str());
+        Atom a = list.at(i);
+        arr.PushBack(*cmJSONFromAtom(a, allocator), allocator);
+        
     }
     
-    return ret;
-}
-
-static inline std::string cm_jsonkey_from_atom(t_atom* atom)
-{
-    std::string key;
-    if (atom->a_type == A_FLOAT)
-    {
-        key = std::to_string((int)atom->a_w.w_float);
-    }
-    if (atom->a_type == A_SYMBOL)
-    {
-        key = atom->a_w.w_symbol->s_name;
-    }
-    return key;
-}
-
-
-#pragma mark -
-
-static inline void cm_setjson(std::string jsonkey)
-{
-
-}
-static inline std::string cm_getjson(std::string jsonkey)
-{
-    std::string ret = "";
+    Value *vKey = new Value;
     
-    return ret;
+    vKey->SetString(list.at(0).asString().c_str() , (int)(list.at(0).asString().size()), allocator );
+    
+    if (doc->HasMember(*vKey))
+    {
+        doc->RemoveMember(*vKey);
+    }
+    
+    doc->AddMember( *vKey , arr, allocator);
+    
 }
+
+static inline AtomList cmAtomListFromJSON(Document *doc, std::string key)
+{
+    AtomList list;
+    
+    Document::AllocatorType& allocator = doc->GetAllocator();
+    
+    Value *vKey = new Value;
+    
+    vKey->SetString(key.c_str() , (int)(key.size()), allocator );
+    
+    
+    Value::ConstMemberIterator itr = doc->FindMember(*vKey);
+    if (itr != doc->MemberEnd())
+    {
+        const Value& arr  = itr->value;
+        
+        if (arr.IsArray())
+        {
+            for (int i=0; i<arr.Size(); i++)
+            {
+                Atom a;
+                
+                if (arr[i].IsString())
+                {
+                    a = Atom( gensym(arr[i].GetString()) );
+                    
+                    list.append(a);
+                    
+                }
+                
+                if (arr[i].IsFloat())
+                {
+                    a= Atom( (arr[i].GetFloat()) );
+                    
+                    list.append(a);
+                }
+                
+                if (arr[i].IsInt())
+                {
+                    a = Atom( (arr[i].GetInt()) );
+                    
+                    list.append(a);
+                }
+            }
+        }
+        else
+        {
+            
+            error("bad array");
+            error("type: %d",arr.GetType());
+        }
+    }
+    else
+    {
+        error("key not found");
+    }
+    
+    return list;
+}
+
+//static inline t_atom cm_atom_from_json(Document in1)
+//{
+//    t_atom ret;
+//
+//    ret.a_type = A_SYMBOL;
+//    ret.a_w.w_symbol = gensym("<>");
+//
+//    if (in1["type"].IsNumber() && in1["symbol"].IsString() && in1["float"].IsNumber())
+//    {
+//        int at1 = in1["type"].GetInt();
+//        ret.a_type = (t_atomtype)at1;
+//        std::string sym = in1["symbol"].GetString();
+//        ret.a_w.w_symbol = gensym(sym.c_str());
+//        ret.a_w.w_float = in1["float"].GetFloat();      //check PD_FLOATSIZE
+//    }
+//
+//    return ret;
+//
+//}
+//
+//
+//static inline t_atom cm_atom_from_json_string(Document in1)
+//{
+//    t_atom ret;
+//
+//    ret.a_type = A_SYMBOL;
+//    ret.a_w.w_symbol = gensym("<>");
+//
+//    if (in1.IsString())
+//    {
+//        std::string sym = in1.GetString();
+//        ret.a_w.w_symbol = gensym(sym.c_str());
+//    }
+//
+//    return ret;
+//}
+//
+//static inline std::string cm_jsonkey_from_atom(t_atom* atom)
+//{
+//    std::string key;
+//    if (atom->a_type == A_FLOAT)
+//    {
+//        key = std::to_string((int)atom->a_w.w_float);
+//    }
+//    if (atom->a_type == A_SYMBOL)
+//    {
+//        key = atom->a_w.w_symbol->s_name;
+//    }
+//    return key;
+//}
+//
+//
+//#pragma mark -
+//
+//static inline void cm_setjson(std::string jsonkey)
+//{
+//
+//}
+//static inline std::string cm_getjson(std::string jsonkey)
+//{
+//    std::string ret = "";
+//
+//    return ret;
+//}
 
 
 #endif /* ceammc_json_objects_h */
