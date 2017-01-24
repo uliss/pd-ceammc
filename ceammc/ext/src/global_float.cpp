45a707a28c84ff322bcc52db3fd499c96b83cc9c
Serge Poltavski <serge.poltavski@gmail.com>
2017-01-24 13:25:54 +0300

    global.* rewritten with C++

--- ceammc/ext/src/global_float.cpp
+++ ceammc/ext/src/global_float.cpp
@@ -1,68 +1,33 @@
 #include "ceammc_factory.h"
-#include "ceammc_format.h"
-#include "ceammc_globaldata.h"
-#include "ceammc_log.h"
-#include "ceammc_object.h"
+#include "global_base.h"
 
 using namespace ceammc;
 
-class GlobalFloat : public BaseObject {
-    GlobalData<t_float> data_;
-
+class GlobalFloat : public GlobalBase<float> {
 public:
     GlobalFloat(const PdArgs& a)
-        : BaseObject(a)
-        , data_(a.args.empty() ? "default" : to_string(a.args[0]), "global.float")
+        : GlobalBase<float>(a, "global.float")
     {
-        if (args().empty())
-            OBJ_ERR << "global object ID required! Using default: " << data_.name();
-
-        createOutlet();
-        createCbProperty("@id", &GlobalFloat::m_id);
-        createCbProperty("@refs", &GlobalFloat::m_refs);
-        createCbProperty("@keys", &GlobalFloat::m_keys);
     }
 
     void onBang()
     {
-        floatTo(0, data_.ref());
+        floatTo(0, ref());
     }
 
     void onFloat(float v)
     {
-        data_.ref() = v;
+        ref() = v;
         onBang();
     }
 
     void m_set(t_symbol* /*s*/, const AtomList& lst)
     {
         if (lst.size() > 0 && lst.at(0).isFloat()) {
-            data_.ref() = lst.at(0).asFloat();
+            ref() = lst.at(0).asFloat();
         } else {
             OBJ_ERR << "invalid arguments given: " << lst;
         }
-    }
-
-    AtomList m_id() const
-    {
-        return listFrom(data_.name());
-    }
-
-    AtomList m_keys() const
-    {
-        std::vector<std::string> keys;
-        data_.keys(keys);
-        return listFrom(keys);
-    }
-
-    AtomList m_refs() const
-    {
-        return listFrom(data_.refCount());
-    }
-
-    void dump() const
-    {
-        BaseObject::dump();
     }
 };
 
--- ceammc/ext/src/global_int.cpp
+++ ceammc/ext/src/global_int.cpp
@@ -1,60 +1,38 @@
-#include "ceammc_globaldata.h"
-#include <m_pd.h>
+#include "ceammc_factory.h"
+#include "global_base.h"
 
-#define OBJ_NAME "global.int"
-#define MSG_PREFIX "[" OBJ_NAME "] "
+using namespace ceammc;
 
-typedef ceammc::GlobalData<int> GlobalInt;
+class GlobalInt : public GlobalBase<int> {
+public:
+    GlobalInt(const PdArgs& a)
+        : GlobalBase<int>(a, "global.int")
+    {
+    }
 
-static t_class* global_int_class;
-struct t_global_int {
-    t_object x_obj;
-    GlobalInt* x_global;
+    void onBang()
+    {
+        floatTo(0, ref());
+    }
+
+    void onFloat(float v)
+    {
+        ref() = static_cast<int>(v);
+        onBang();
+    }
+
+    void m_set(t_symbol* /*s*/, const AtomList& lst)
+    {
+        if (lst.size() > 0 && lst.at(0).isFloat()) {
+            ref() = static_cast<int>(lst.at(0).asFloat());
+        } else {
+            OBJ_ERR << "invalid arguments given: " << lst;
+        }
+    }
 };
-
-static void global_int_set(t_global_int* x, t_floatarg f)
-{
-    x->x_global->ref() = static_cast<int>(f);
-}
-
-static void global_int_output(t_global_int* x)
-{
-    outlet_float(x->x_obj.te_outlet, x->x_global->ref());
-}
-
-static void global_int_bang(t_global_int* x)
-{
-    global_int_output(x);
-}
-
-static void global_int_float(t_global_int* x, t_floatarg f)
-{
-    global_int_set(x, f);
-    global_int_output(x);
-}
-
-static void* global_int_new(t_symbol* id)
-{
-    t_global_int* x = reinterpret_cast<t_global_int*>(pd_new(global_int_class));
-    outlet_new(&x->x_obj, &s_float);
-    x->x_global = new GlobalInt(id->s_name, OBJ_NAME);
-    return static_cast<void*>(x);
-}
-
-static void global_int_free(t_global_int* x)
-{
-    delete x->x_global;
-}
 
 extern "C" void setup_global0x2eint()
 {
-    global_int_class = class_new(gensym(OBJ_NAME),
-        reinterpret_cast<t_newmethod>(global_int_new),
-        reinterpret_cast<t_method>(global_int_free),
-        sizeof(t_global_int), 0, A_DEFSYMBOL, A_NULL);
-    class_addbang(global_int_class, global_int_bang);
-    class_addfloat(global_int_class, global_int_float);
-    class_addmethod(global_int_class,
-        reinterpret_cast<t_method>(global_int_set),
-        gensym("set"), A_DEFFLOAT, A_NULL);
+    ObjectFactory<GlobalInt> obj("global.int");
+    obj.addMethod("set", &GlobalInt::m_set);
 }
