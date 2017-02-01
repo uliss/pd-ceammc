b5946701ec0770425b3c05e53e7ea40dc7f1fdaf
Serge Poltavski <serge.poltavski@gmail.com>
2017-02-02 02:45:21 +0300

    UI keyboard cleanup

--- ceammc/ext/src/ui_keyboard.cpp
+++ ceammc/ext/src/ui_keyboard.cpp
@@ -14,11 +14,6 @@
 struct ui_keyboard : public ceammc_gui::BaseGuiObject {
     t_ebox x_gui;
 
-    float mouse_x;
-    float mouse_y;
-    int mouse_dn;
-    bool _selected;
-
     t_outlet* out1;
     t_atom out_list[2];
 
@@ -29,8 +24,17 @@ struct ui_keyboard : public ceammc_gui::BaseGuiObject {
     int _vel;
     int _pitch_prev;
 
-    //t_rgba b_color_background;
     t_rgba b_color_border;
+
+public:
+    t_float realPitch() const { return _pitch + shift; }
+    void output()
+    {
+        atom_setfloat(&out_list[0], realPitch());
+        atom_setfloat(&out_list[1], _vel);
+
+        outlet_list(out1, &s_list, 2, out_list);
+    }
 };
 
 namespace ceammc_gui {
@@ -40,23 +44,23 @@ struct kRect {
     bool is_black;
 };
 
-kRect get_black_key_r(int offset, int kWidth, int kHeight)
+static kRect get_black_key_r(int offset, int kWidth, int kHeight)
 {
     kRect ret;
 
     int n_number = offset % 12;
     int n_octave = offset / 12;
 
-    ret.x = (offset + (n_number > 4) + 2 * n_octave) * kWidth + .5 * kWidth;
+    ret.x = (offset + (n_number > 4) + 2 * n_octave) * kWidth + 0.5f * kWidth;
     ret.y = 0;
     ret.w = kWidth;
-    ret.h = kHeight * .6;
+    ret.h = kHeight * 0.6f;
     ret.is_black = true;
 
     return ret;
 }
 
-kRect get_white_key_r(int offset, int kWidth, int kHeight)
+static kRect get_white_key_r(int offset, int kWidth, int kHeight)
 {
     kRect ret;
 
@@ -72,7 +76,7 @@ kRect get_white_key_r(int offset, int kWidth, int kHeight)
     return ret;
 }
 
-kRect get_key_r(int number, int kWidth, int kHeight)
+static kRect get_key_r(int number, int kWidth, int kHeight)
 {
     int n_number = number % 12;
     bool is_black = ((n_number - (n_number < 5)) % 2);
@@ -82,32 +86,28 @@ kRect get_key_r(int number, int kWidth, int kHeight)
     return ret;
 }
 
-bool mouse_in_rect(int x, int y, kRect kr)
+static bool mouse_in_rect(int x, int y, const kRect& kr)
 {
     return (x > kr.x) && x < (kr.x + kr.w) && y > (kr.y) && y < (kr.y + kr.h);
 }
 
-UI_fun(ui_keyboard)::wx_paint(t_object* z, t_object* view)
+UI_fun(ui_keyboard)::wx_paint(t_object* z, t_object* /*view*/)
 {
-    //UI_Prop
-
-    t_symbol* bgl = gensym("background_layer");
-    //float size;
+    t_symbol* bgl = BG_LAYER;
     t_rect rect;
-    ebox_get_rect_for_view((t_ebox*)z, &rect);
+    ebox_get_rect_for_view(asBox(z), &rect);
 
-    t_elayer* g = ebox_start_layer((t_ebox*)z, bgl, rect.width, rect.height);
-    if (g)
+    t_elayer* g = ebox_start_layer(asBox(z), bgl, rect.width, rect.height);
 
-    {
-        ui_keyboard* zx = (ui_keyboard*)z;
+    if (g) {
+        ui_keyboard* zx = asStruct(z);
 
         if (zx->keys < 3)
             zx->keys = 3; // rapper's piano meme
         if (zx->keys > 127)
             zx->keys = 127;
 
-        float kWidth = floorf(rect.width / zx->keys) * .9; // weird
+        float kWidth = floorf(rect.width / zx->keys) * 0.9f; // weird
 
         t_symbol* color_wh_f = gensym("#F0F0F0");
         t_symbol* color_wh_b = gensym("#C0C0C0");
@@ -177,14 +177,14 @@ UI_fun(ui_keyboard)::wx_paint(t_object* z, t_object* view)
 
 #pragma mark -
 
-UI_fun(ui_keyboard)::wx_mousemove_ext(t_object* z, t_object* view, t_pt pt, long modifiers)
+UI_fun(ui_keyboard)::wx_mousemove_ext(t_object* z, t_object* /*view*/, t_pt /*pt*/, long /*modifiers*/)
 {
-    ui_keyboard* zx = (ui_keyboard*)z;
+    ui_keyboard* zx = asStruct(z);
 
     t_rect rect;
-    ebox_get_rect_for_view((t_ebox*)z, &rect);
+    ebox_get_rect_for_view(asBox(z), &rect);
 
-    float kWidth = floorf(rect.width / zx->keys) * .9;
+    float kWidth = floorf(rect.width / zx->keys) * 0.9f;
 
     for (int i = 0; i < zx->keys; i++) {
         kRect k = get_key_r(i, kWidth, rect.height);
@@ -207,66 +207,46 @@ UI_fun(ui_keyboard)::wx_mousemove_ext(t_object* z, t_object* view, t_pt pt, long
     }
 }
 
-UI_fun(ui_keyboard)::wx_mousedown_ext(t_object* z, t_object* view, t_pt pt, long modifiers)
+UI_fun(ui_keyboard)::wx_mousedown_ext(t_object* z, t_object* /*view*/, t_pt pt, long /*modifiers*/)
 {
-    ui_keyboard* zx = (ui_keyboard*)z;
+    ui_keyboard* zx = asStruct(z);
 
     t_rect rect;
-    ebox_get_rect_for_view((t_ebox*)z, &rect);
+    ebox_get_rect_for_view(reinterpret_cast<t_ebox*>(z), &rect);
 
-    zx->_vel = int(pt.y / rect.height * 127.);
-
-    atom_setfloat(&((ui_keyboard*)z)->out_list[0], (float)zx->_pitch + (float)zx->shift);
-    atom_setfloat(&((ui_keyboard*)z)->out_list[1], (float)zx->_vel);
-
-    outlet_list(((ui_keyboard*)z)->out1, &s_list, 2, ((ui_keyboard*)z)->out_list);
-
-    ceammc_gui::GuiFactory<ceammc_gui::BaseGuiObject>::ws_redraw(z);
+    zx->_vel = int(pt.y / rect.height * 127.f);
+    zx->output();
+    ws_redraw(z);
 }
 
-UI_fun(ui_keyboard)::wx_mouseup_ext(t_object* z, t_object* view, t_pt pt, long modifiers)
+UI_fun(ui_keyboard)::wx_mouseup_ext(t_object* z, t_object* /*view*/, t_pt /*pt*/, long /*modifiers*/)
 {
-    ui_keyboard* zx = (ui_keyboard*)z;
-
-    t_rect rect;
-    ebox_get_rect_for_view((t_ebox*)z, &rect);
+    ui_keyboard* zx = asStruct(z);
 
     zx->_vel = 0;
-
-    atom_setfloat(&((ui_keyboard*)z)->out_list[0], (float)zx->_pitch + (float)zx->shift);
-    atom_setfloat(&((ui_keyboard*)z)->out_list[1], (float)zx->_vel);
-
-    outlet_list(((ui_keyboard*)z)->out1, &s_list, 2, ((ui_keyboard*)z)->out_list);
-
-    ceammc_gui::GuiFactory<ceammc_gui::BaseGuiObject>::ws_redraw(z);
+    zx->output();
+    ws_redraw(z);
 }
 
 UI_fun(ui_keyboard)::wx_mousedrag_ext(t_object* z, t_object* view, t_pt pt, long modifiers)
 {
+    ui_keyboard* zx = asStruct(z);
 
-    ui_keyboard* zx = (ui_keyboard*)z;
-
-    ceammc_gui::GuiFactory<ui_keyboard>::wx_mousemove_ext(z, view, pt, modifiers);
+    wx_mousemove_ext(z, view, pt, modifiers);
 
     if (zx->_pitch_prev != zx->_pitch) {
-
         zx->_pitch_prev = zx->_pitch;
-
-        atom_setfloat(&((ui_keyboard*)z)->out_list[0], (float)zx->_pitch + (float)zx->shift);
-        atom_setfloat(&((ui_keyboard*)z)->out_list[1], (float)zx->_vel);
-
-        outlet_list(((ui_keyboard*)z)->out1, &s_list, 2, ((ui_keyboard*)z)->out_list);
+        zx->output();
     }
 
-    ceammc_gui::GuiFactory<ceammc_gui::BaseGuiObject>::ws_redraw(z);
+    ws_redraw(z);
 }
 
-UI_fun(ui_keyboard)::wx_mouseleave_ext(t_object* z, t_object* view, t_pt pt, long modifiers)
+UI_fun(ui_keyboard)::wx_mouseleave_ext(t_object* z, t_object* /*view*/, t_pt /*pt*/, long /*modifiers*/)
 {
-    ui_keyboard* zx = (ui_keyboard*)z;
+    ui_keyboard* zx = asStruct(z);
     zx->_pitch = -1;
-
-    ceammc_gui::GuiFactory<ceammc_gui::BaseGuiObject>::ws_redraw(z);
+    ws_redraw(z);
 }
 
 // yet disabled
@@ -282,35 +262,35 @@ UI_fun(ui_keyboard)::wx_mouseleave_ext(t_object* z, t_object* view, t_pt pt, lon
 //        newrect->width = kWidth * zx->keys/.9;
 //    }
 
-static void ui_k_getdrawparams(ui_keyboard* x, t_object* patcherview, t_edrawparams* params)
+static void ui_k_getdrawparams(ui_keyboard* x, t_object* /*patcherview*/, t_edrawparams* params)
 {
     params->d_borderthickness = 1;
     params->d_cornersize = 2;
     params->d_bordercolor = x->b_color_border;
-    //params->d_boxfillcolor      = x->b_color_background;
 }
 
 #pragma mark -
 
 UI_fun(ui_keyboard)::init_ext(t_eclass* z)
 {
-    CLASS_ATTR_DEFAULT(z, "size", 0, "433. 60.");
+    // clang-format off
+    CLASS_ATTR_DEFAULT              (z, "size", 0, "433. 60.");
 
-    CLASS_ATTR_INT(z, "shift", 0, ui_keyboard, shift);
-    CLASS_ATTR_DEFAULT(z, "shift", 0, "36");
-    CLASS_ATTR_LABEL(z, "shift", 0, "shift");
-    CLASS_ATTR_STYLE(z, "shift", 0, "number");
-    CLASS_ATTR_FILTER_CLIP(z, "shift", 0, 127);
-    CLASS_ATTR_STEP(z, "shift", 1);
-    CLASS_ATTR_DEFAULT_SAVE_PAINT(z, "shift", 0, "36");
+    CLASS_ATTR_INT                  (z, "shift", 0, ui_keyboard, shift);
+    CLASS_ATTR_DEFAULT              (z, "shift", 0, "36");
+    CLASS_ATTR_LABEL                (z, "shift", 0, "shift");
+    CLASS_ATTR_STYLE                (z, "shift", 0, "number");
+    CLASS_ATTR_FILTER_CLIP          (z, "shift", 0, 127);
+    CLASS_ATTR_STEP                 (z, "shift", 1);
+    CLASS_ATTR_DEFAULT_SAVE_PAINT   (z, "shift", 0, "36");
 
-    CLASS_ATTR_INT(z, "keys", 0, ui_keyboard, keys);
-    CLASS_ATTR_DEFAULT(z, "keys", 0, "61");
-    CLASS_ATTR_LABEL(z, "keys", 0, "keys");
-    CLASS_ATTR_STYLE(z, "keys", 0, "number");
-    CLASS_ATTR_FILTER_CLIP(z, "keys", 5, 88);
-    CLASS_ATTR_STEP(z, "keys", 1);
-    CLASS_ATTR_DEFAULT_SAVE_PAINT(z, "keys", 0, "61");
+    CLASS_ATTR_INT                  (z, "keys", 0, ui_keyboard, keys);
+    CLASS_ATTR_DEFAULT              (z, "keys", 0, "61");
+    CLASS_ATTR_LABEL                (z, "keys", 0, "keys");
+    CLASS_ATTR_STYLE                (z, "keys", 0, "number");
+    CLASS_ATTR_FILTER_CLIP          (z, "keys", 5, 88);
+    CLASS_ATTR_STEP                 (z, "keys", 1);
+    CLASS_ATTR_DEFAULT_SAVE_PAINT   (z, "keys", 0, "61");
 
     //        CLASS_ATTR_RGBA                 (z, "bgcolor", 0, ui_keyboard, b_color_background);
     //        CLASS_ATTR_LABEL                (z, "bgcolor", 0, "Background Color");
@@ -318,18 +298,20 @@ UI_fun(ui_keyboard)::init_ext(t_eclass* z)
     //        CLASS_ATTR_DEFAULT_SAVE_PAINT   (z, "bgcolor", 0, "0.93 0.93 0.93 1.");
     //        CLASS_ATTR_STYLE                (z, "bgcolor", 0, "color");
 
-    CLASS_ATTR_RGBA(z, "bdcolor", 0, ui_keyboard, b_color_border);
-    CLASS_ATTR_LABEL(z, "bdcolor", 0, "Border Color");
-    CLASS_ATTR_ORDER(z, "bdcolor", 0, "2");
-    CLASS_ATTR_DEFAULT_SAVE_PAINT(z, "bdcolor", 0, "0. 0. 0. 1.");
-    CLASS_ATTR_STYLE(z, "bdcolor", 0, "color");
+    CLASS_ATTR_RGBA                (z, "bdcolor", 0, ui_keyboard, b_color_border);
+    CLASS_ATTR_LABEL               (z, "bdcolor", 0, "Border Color");
+    CLASS_ATTR_ORDER               (z, "bdcolor", 0, "2");
+    CLASS_ATTR_DEFAULT_SAVE_PAINT  (z, "bdcolor", 0, "0. 0. 0. 1.");
+    CLASS_ATTR_STYLE               (z, "bdcolor", 0, "color");
+    // clang-format on
 
-    eclass_addmethod(z, (method)ui_k_getdrawparams, "getdrawparams", A_NULL, 0);
+    eclass_addmethod(z, reinterpret_cast<t_typ_method>(ui_k_getdrawparams), "getdrawparams", A_NULL, 0);
 }
 
-UI_fun(ui_keyboard)::new_ext(t_object* x, t_symbol* s, int argcl, t_atom* argv)
+UI_fun(ui_keyboard)::new_ext(t_object* x, t_symbol* /*s*/, int /*argcl*/, t_atom* /*argv*/)
 {
-    ((ui_keyboard*)x)->out1 = outlet_new(x, &s_bang);
+    ui_keyboard* zx = asStruct(x);
+    zx->out1 = outlet_new(x, &s_anything);
 }
 }
 
