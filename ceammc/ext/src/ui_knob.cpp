b2edd61e5ee92d8cf6e64c35d76f16a051901614
Serge Poltavski <serge.poltavski@gmail.com>
2017-02-03 04:02:46 +0300

    ui.knob cleanup

--- ceammc/ext/src/ui_knob.cpp
+++ ceammc/ext/src/ui_knob.cpp
@@ -25,6 +25,12 @@ struct ui_knob : public ceammc_gui::BaseGuiObject {
 
     t_rgba b_color_background;
     t_rgba b_color_border;
+
+public:
+    t_float realValue() const
+    {
+        return _value * range + shift;
+    }
 };
 
 namespace ceammc_gui {
@@ -35,11 +41,11 @@ void e_polyarc(t_elayer* g, float xc, float yc, float radius, float angle1, floa
     for (int i = 0; i < lod; i++) {
         float x1, x2, y1, y2;
 
-        x1 = xc + cos(angle1 + d_angle * float(i / lod)) * radius;
-        x2 = xc + cos(angle1 + d_angle * float((i + 1) / lod)) * radius;
+        x1 = xc + cosf(angle1 + d_angle * float(i / lod)) * radius;
+        x2 = xc + cosf(angle1 + d_angle * float((i + 1) / lod)) * radius;
 
-        y1 = yc - sin(angle1 + d_angle * float(i / lod)) * radius;
-        y2 = yc - sin(angle1 + d_angle * float((i + 1) / lod)) * radius;
+        y1 = yc - sinf(angle1 + d_angle * float(i / lod)) * radius;
+        y2 = yc - sinf(angle1 + d_angle * float((i + 1) / lod)) * radius;
 
         egraphics_line(g, x1, y1, x2, y2);
     }
@@ -50,13 +56,13 @@ void e_marc(t_elayer* g, float xc, float yc, float radius, float angle1, float a
     float x1, x2, y1, y2;
     float d_angle = angle2 - angle1;
 
-    x1 = xc + cos(angle1) * radius; // + d_angle* float(i/lod)) *radius;
-    x2 = xc + cos(angle1 + d_angle) * radius;
+    x1 = xc + cosf(angle1) * radius; // + d_angle* float(i/lod)) *radius;
+    x2 = xc + cosf(angle1 + d_angle) * radius;
 
-    y1 = yc - sin(angle1) * radius;
-    y2 = yc - sin(angle1 + d_angle) * radius;
+    y1 = yc - sinf(angle1) * radius;
+    y2 = yc - sinf(angle1 + d_angle) * radius;
 
-    float angle_l = 1. - 0.99 * ((angle1 + d_angle) / d_angle) * anglem;
+    float angle_l = 1.f - 0.99f * ((angle1 + d_angle) / d_angle) * anglem;
 
     egraphics_move_to(g, x1, y1);
     egraphics_arc_to(g, xc, yc, angle_l);
@@ -64,26 +70,24 @@ void e_marc(t_elayer* g, float xc, float yc, float radius, float angle1, float a
 
 UI_fun(ui_knob)::wx_paint(t_object* z, t_object* view)
 {
-
-    t_symbol* bgl = gensym("background_layer");
     t_rect rect;
-    ebox_get_rect_for_view((t_ebox*)z, &rect);
+    ebox_get_rect_for_view(asBox(z), &rect);
 
-    t_elayer* g = ebox_start_layer((t_ebox*)z, bgl, rect.width, rect.height);
+    t_elayer* g = ebox_start_layer(asBox(z), BG_LAYER, rect.width, rect.height);
 
-    ui_knob* zx = (ui_knob*)z;
+    ui_knob* zx = asStruct(z);
 
     if (g) {
 
-        float rmin = .5 * rect.width * .7 - 2;
-        float rmid = .5 * rect.width - 2;
-        float rmax = .5 * rect.width - 2;
-        float amin = 5 / 8. * M_PI * 2.;
-        float amax = -1 / 8. * M_PI * 2.;
-        float aval = M_PI * 2. * 6 / 8. * zx->_value; //UI_Pf("_value");
+        float rmin = 0.5f * rect.width * 0.7f - 2;
+        float rmid = 0.5f * rect.width - 2;
+        float rmax = 0.5f * rect.width - 2;
+        float amin = 5 / 8.f * M_PI * 2.f;
+        float amax = -1 / 8.f * M_PI * 2.f;
+        float aval = M_PI * 2.f * 6 / 8.f * zx->_value; //UI_Pf("_value");
 
-        float cx = rect.width * .5;
-        float cy = rect.height * .5;
+        float cx = rect.width * 0.5f;
+        float cy = rect.height * 0.5f;
 
         egraphics_set_line_width(g, 1);
         egraphics_set_color_hex(g, gensym("#C0C0C0"));
@@ -92,8 +96,8 @@ UI_fun(ui_knob)::wx_paint(t_object* z, t_object* view)
         e_polyarc(g, cx, cy, rmax, amin, amax, 30);
         //egraphics_circle(g, cx, cy, rmax);
 
-        egraphics_line(g, cx + cos(amin) * 0, cy - sin(amin) * 0, cx + cos(amin) * rmax, cy - sin(amin) * rmax);
-        egraphics_line(g, cx + cos(amax) * 0, cy - sin(amax) * 0, cx + cos(amax) * rmax, cy - sin(amax) * rmax);
+        egraphics_line(g, cx + cosf(amin) * 0, cy - sinf(amin) * 0, cx + cosf(amin) * rmax, cy - sinf(amin) * rmax);
+        egraphics_line(g, cx + cosf(amax) * 0, cy - sinf(amax) * 0, cx + cosf(amax) * rmax, cy - sinf(amax) * rmax);
 
         egraphics_stroke(g);
 
@@ -102,8 +106,8 @@ UI_fun(ui_knob)::wx_paint(t_object* z, t_object* view)
         //e_marc(g, cx, cy, rmid, amax + aval, amin, EPD_2PI*6/8., 20);
         e_polyarc(g, cx, cy, rmid, amax + aval, amin, 30 - floor(29 * zx->_value)); //UI_Pf("_value")
         //egraphics_arc(g, cx, cy, rmid, amax+aval, amin);
-        egraphics_line(g, cx, cy, cx + cos(amax + aval) * rmid, cy - sin(amax + aval) * rmid);
-        egraphics_line(g, cx, cy, cx + cos(amin) * rmid, cy - sin(amin) * rmid);
+        egraphics_line(g, cx, cy, cx + cosf(amax + aval) * rmid, cy - sinf(amax + aval) * rmid);
+        egraphics_line(g, cx, cy, cx + cosf(amin) * rmid, cy - sinf(amin) * rmid);
         egraphics_stroke(g);
 
         char c_min[10];
@@ -118,23 +122,24 @@ UI_fun(ui_knob)::wx_paint(t_object* z, t_object* view)
         etext_layout_set(zx->txt_max, c_max, zx->txt_font, rect.width - 3, rect.height - 12, rect.width, rect.height / 2, ETEXT_UP_RIGHT, ETEXT_JRIGHT, ETEXT_WRAP);
         etext_layout_draw(zx->txt_max, g);
 
-        ebox_end_layer((t_ebox*)z, bgl);
+        ebox_end_layer(asBox(z), BG_LAYER);
     }
 
-    ebox_paint_layer((t_ebox*)z, bgl, 0., 0.);
+    ebox_paint_layer(asBox(z), BG_LAYER, 0., 0.);
 }
 
-UI_fun(ui_knob)::wx_oksize(t_object* z, t_rect* newrect)
+UI_fun(ui_knob)::wx_oksize(t_object*, t_rect* newrect)
 {
-    newrect->width = floorf(newrect->width / 5.) * 5;
+    newrect->width = floorf(newrect->width / 5.f) * 5;
     newrect->height = newrect->width;
 }
 
-UI_fun(ui_knob)::wx_mousedrag_ext(t_object* z, t_object* view, t_pt pt, long modifiers)
+UI_fun(ui_knob)::wx_mousedrag_ext(t_object* z, t_object*, t_pt pt, long)
 {
+    ui_knob* zx = asStruct(z);
 
     t_rect rect;
-    ebox_get_rect_for_view((t_ebox*)z, &rect);
+    ebox_get_rect_for_view(asBox(z), &rect);
 
     float val;
     val = 1 - pt.y / rect.height;
@@ -144,37 +149,34 @@ UI_fun(ui_knob)::wx_mousedrag_ext(t_object* z, t_object* view, t_pt pt, long mod
     if (val < 0)
         val = 0;
 
-    ui_knob* zx = (ui_knob*)z;
     zx->_value = val;
 
-    ceammc_gui::GuiFactory<ceammc_gui::BaseGuiObject>::ws_redraw(z);
+    ws_redraw(z);
 
-    outlet_float(((ui_knob*)z)->out1, (1 - val) * zx->range + zx->shift);
+    outlet_float(zx->out1, (1 - val) * zx->range + zx->shift);
 }
 
 UI_fun(ui_knob)::wx_mousedown_ext(t_object* z, t_object* view, t_pt pt, long modifiers)
 {
-    ceammc_gui::GuiFactory<ui_knob>::wx_mousedrag_ext(z, view, pt, modifiers);
+    wx_mousedrag_ext(z, view, pt, modifiers);
 }
 
 UI_fun(ui_knob)::m_float(t_object* z, t_float f)
 {
-    ui_knob* zx = (ui_knob*)z;
-    zx->_value = 1. - f / 127.;
+    ui_knob* zx = asStruct(z);
+    zx->_value = 1.f - (f / 127.f);
 
-    ceammc_gui::GuiFactory<ceammc_gui::BaseGuiObject>::ws_redraw(z);
-
-    outlet_float(((ui_knob*)z)->out1, zx->_value * zx->range + zx->shift);
+    ws_redraw(z);
+    outlet_float(zx->out1, zx->realValue());
 }
 
-UI_fun(ui_knob)::m_bang(t_object* z, t_symbol* s, int argc, t_atom* argv)
+UI_fun(ui_knob)::m_bang(t_object* z, t_symbol*, int, t_atom*)
 {
-    ui_knob* zx = (ui_knob*)z;
-
-    outlet_float(((ui_knob*)z)->out1, zx->_value * zx->range + zx->shift);
+    ui_knob* zx = asStruct(z);
+    outlet_float(zx->out1, zx->realValue());
 }
 
-static void ui_kn_getdrawparams(ui_knob* x, t_object* patcherview, t_edrawparams* params)
+static void ui_kn_getdrawparams(ui_knob* x, t_object*, t_edrawparams* params)
 {
     params->d_borderthickness = 1;
     params->d_cornersize = 2;
@@ -210,15 +212,14 @@ UI_fun(ui_knob)::init_ext(t_eclass* z)
     CLASS_ATTR_STYLE                (z, "bdcolor", 0, "color");
     // clang-format on
 
-    eclass_addmethod(z, (method)ui_kn_getdrawparams, "getdrawparams", A_NULL, 0);
+    eclass_addmethod(z, reinterpret_cast<t_typ_method>(ui_kn_getdrawparams), "getdrawparams", A_NULL, 0);
 }
 
-UI_fun(ui_knob)::new_ext(t_object* x, t_symbol* s, int argcl, t_atom* argv)
+UI_fun(ui_knob)::new_ext(t_object* x, t_symbol*, int, t_atom*)
 {
-    ((ui_knob*)x)->out1 = outlet_new(x, &s_float);
-
-    ui_knob* zx = (ui_knob*)x;
-    zx->_value = 1.;
+    ui_knob* zx = asStruct(x);
+    zx->out1 = outlet_new(x, &s_float);
+    zx->_value = 1.f;
 
     zx->txt_max = etext_layout_create();
     zx->txt_min = etext_layout_create();
