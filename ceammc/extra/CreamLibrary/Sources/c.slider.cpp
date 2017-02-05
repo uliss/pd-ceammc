fc3814aebb06d406414b7cc5cda96e20e1e79e5d
Serge Poltavski <serge.poltavski@gmail.com>
2017-02-06 01:34:38 +0300

    ui.slider memory leak

--- ceammc/extra/CreamLibrary/Sources/c.slider.cpp
+++ ceammc/extra/CreamLibrary/Sources/c.slider.cpp
@@ -250,6 +250,8 @@ static void *slider_new(t_symbol *s, int argc, t_atom *argv)
         ebox_attrprocess_viabinbuf(x, d);
         x->f_value = x->f_min;
         ebox_ready((t_ebox *)x);
+
+        binbuf_free(d);
         return x;
     }
     
