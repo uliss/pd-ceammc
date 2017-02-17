1a6bfbd0c86b347ff25099cfa0877e3eea937d37
Serge Poltavski <serge.poltavski@gmail.com>
2017-02-17 12:11:40 +0300

    ui.slider2d number box field used for properties

--- ceammc/ext/src/ui_slider2d.cpp
+++ ceammc/ext/src/ui_slider2d.cpp
@@ -240,21 +240,25 @@ UI_fun(ui_slider2d)::init_ext(t_eclass* z)
     CLASS_ATTR_DEFAULT              (z, "shift_x", 0, "-1");
     CLASS_ATTR_LABEL                (z, "shift_x", 0, "Leftmost value");
     CLASS_ATTR_DEFAULT_SAVE_PAINT   (z, "shift_x", 0, "-1");
+    CLASS_ATTR_STYLE                (z, "shift_x", 0, "number");
 
     CLASS_ATTR_FLOAT                (z, "range_x", 0, ui_slider2d, range_x);
     CLASS_ATTR_DEFAULT              (z, "range_x", 0, "2");
     CLASS_ATTR_LABEL                (z, "range_x", 0, "Horizontal range");
     CLASS_ATTR_DEFAULT_SAVE_PAINT   (z, "range_x", 0, "2");
+    CLASS_ATTR_STYLE                (z, "range_x", 0, "number");
 
     CLASS_ATTR_FLOAT                (z, "shift_y", 0, ui_slider2d, shift_y);
     CLASS_ATTR_DEFAULT              (z, "shift_y", 0, "-1");
     CLASS_ATTR_LABEL                (z, "shift_y", 0, "Topmost value");
     CLASS_ATTR_DEFAULT_SAVE_PAINT   (z, "shift_y", 0, "-1");
+    CLASS_ATTR_STYLE                (z, "shift_y", 0, "number");
 
     CLASS_ATTR_FLOAT                (z, "range_y", 0, ui_slider2d, range_y);
     CLASS_ATTR_DEFAULT              (z, "range_y", 0, "2");
     CLASS_ATTR_LABEL                (z, "range_y", 0, "Vertical range");
     CLASS_ATTR_DEFAULT_SAVE_PAINT   (z, "range_y", 0, "2");
+    CLASS_ATTR_STYLE                (z, "range_y", 0, "number");
     // clang-format on
 
     eclass_addmethod(z, reinterpret_cast<t_typ_method>(ui_s2_getdrawparams), "getdrawparams", A_NULL, 0);
