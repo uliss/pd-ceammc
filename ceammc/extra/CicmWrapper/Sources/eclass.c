31815a2485fd7bea0cafbc3b5b29bf12e4f1ed50
Serge Poltavski <serge.poltavski@gmail.com>
2017-02-20 01:26:51 +0300

    i18n support added for dialog label

--- ceammc/extra/CicmWrapper/Sources/eclass.c
+++ ceammc/extra/CicmWrapper/Sources/eclass.c
@@ -1073,7 +1073,7 @@ static void eclass_properties_dialog(t_eclass* c)
 
             // ATTRIBUTES NAMES //
 
-            sys_vgui("ttk::label %s -justify left -text \"%s :\"\n", LABEL_ID, c->c_attr[i]->label->s_name);
+            sys_vgui("ttk::label %s -justify left -text [string cat [_ \"%s\" ] \":\"]\n", LABEL_ID, c->c_attr[i]->label->s_name);
             sys_vgui("pack %s -side left\n", LABEL_ID);
 
             // SELECTOR //
