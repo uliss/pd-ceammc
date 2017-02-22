11f87fdd6c2112b63916127914f28aa5fd313464
serj <serj@debian64>
2017-02-22 07:03:44 -0600

    symlink: fix

--- cmake/install_symlink.cmake
+++ cmake/install_symlink.cmake
@@ -28,14 +28,11 @@ macro(install_symlink _filepath _sympath)
         # for CMake 2.6.x and 2.8.x
         install(CODE "
             if (\"\$ENV{DESTDIR}\" STREQUAL \"\")
-                execute_process(COMMAND echo create symlink: ${_filepath} to ${_installdir}/${_symname}';
-                                \"${CMAKE_COMMAND}\" -E create_symlink
+                execute_process(COMMAND \"${CMAKE_COMMAND}\" -E create_symlink
                                 ${_filepath}
                                 ${_installdir}/${_symname})
             else ()
-                execute_process(COMMAND
-                                echo create symlink: ${_filepath} to ${_installdir}/${_symname}';
-                                \"${CMAKE_COMMAND}\" -E create_symlink
+                execute_process(COMMAND \"${CMAKE_COMMAND}\" -E create_symlink
                                 ${_filepath}
                                 \$ENV{DESTDIR}/${_installdir}/${_symname})
             endif ()
