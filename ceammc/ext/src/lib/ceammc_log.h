a7fe79366e30d5b918ddf641321ab9e789cd9a46
Alex Nadzharov <anadjarov@gmail.com>
2017-02-22 21:48:15 +0300

    fix4

--- ceammc/ext/src/lib/ceammc_log.h
+++ ceammc/ext/src/lib/ceammc_log.h
@@ -68,7 +68,7 @@ static std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
 }
 
 template <>
-static std::ostream& operator<<(std::ostream& os, const std::vector<_symbol*>& v)
+std::ostream& operator<<(std::ostream& os, const std::vector<_symbol*>& v)
 {
     os << '[';
     std::vector<_symbol*>::const_iterator it;
