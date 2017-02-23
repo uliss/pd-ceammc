925c1ddc4c0b77aac75d5ab3ab8d97b1170e2d87
Alex Nadzharov <anadjarov@gmail.com>
2017-02-23 09:55:21 +0300

    12/24 dB filters added

---
+++ ceammc/ext/src/flt_highpass12.cpp
@@ -0,0 +1,15 @@
+#include "flt_highpass12.h"
+
+static void* faust_new(t_symbol* s, int argc, t_atom* argv)
+{
+    t_faust* x = reinterpret_cast<t_faust*>(pd_new(faust_class));
+    PdArgParser p(x, argc, argv);
+//    p.initFloatArg("arg1", 1);
+    return p.pd_obj();
+}
+
+extern "C" void setup_flt0x2ehighpass12_tilde()
+{
+    internal_setup(gensym("flt.highpass12~"));
+}
+
