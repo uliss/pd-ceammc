2e9cb9ef6f6552492d0ef8df8d0406442fb74156
Serge Poltavski <serge.poltavski@gmail.com>
2017-03-10 16:01:33 +0300

    faust: flt.c_notch added

--- ceammc/ext/src/flt_c_notch.cpp
+++ ceammc/ext/src/flt_c_notch.cpp
@@ -4,7 +4,8 @@ static void* faust_new(t_symbol* s, int argc, t_atom* argv)
 {
     t_faust* x = reinterpret_cast<t_faust*>(pd_new(faust_class));
     PdArgParser p(x, argc, argv);
-//    p.initFloatArg("arg1", 1);
+    p.initFloatArg("freq", 1);
+    p.initFloatArg("q", 2);
     return p.pd_obj();
 }
 
@@ -12,4 +13,3 @@ extern "C" void setup_flt0x2ec_notch_tilde()
 {
     internal_setup(gensym("flt.c_notch~"));
 }
-
