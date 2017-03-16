06d9910188754bf7fada26f38685d6a038341d9e
Serge Poltavski <serge.poltavski@gmail.com>
2017-03-16 13:32:17 +0300

    mingw: link fix

--- ceammc/ext/src/tests/tests.cmake
+++ ceammc/ext/src/tests/tests.cmake
@@ -29,7 +29,7 @@ endmacro()
 macro(ceammc_add_extension_test name extpath)
     set(_target "test_${name}")
     add_executable(${_target} "${_target}.cpp" ${extpath})
-    target_link_libraries(${_target} ceammc_static puredata-core ceammc_timeline ceammc_sound)
+    target_link_libraries(${_target} puredata-core ceammc_static puredata-core ceammc_timeline ceammc_sound)
     add_test("Extension::${name}" ${_target})
 
     if(${WITH_COVERAGE})
