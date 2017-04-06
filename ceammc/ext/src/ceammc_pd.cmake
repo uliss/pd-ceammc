af20be668ff40153ce8329af75cc0c3a11db8c86
Serge Poltavski <serge.poltavski@gmail.com>
2017-04-06 21:37:46 +0300

    lib: flt ans noise migration

--- ceammc/ext/src/ceammc_pd.cmake
+++ ceammc/ext/src/ceammc_pd.cmake
@@ -59,17 +59,21 @@ macro(ceammc_glib_extension__ module name)
     ceammc_glib_extension_sep(${module} ${name} "_")
 endmacro()
 
+# adds _underscored_ target linked with *glib* library: MODULE_NAME
+macro(ceammc_faust_gen module name)
+    add_custom_target("faust_${module}_${name}"
+        COMMAND ${FAUST_BIN} -i
+            -a ${CMAKE_SOURCE_DIR}/ceammc/faust/simple_pd_control_ext.cpp
+            -cn ${name}
+            "${CMAKE_SOURCE_DIR}/ceammc/faust/${module}_${name}.dsp"
+            -o ${CMAKE_CURRENT_SOURCE_DIR}/${module}_${name}.h)
+endmacro()
+
 # adds target "faust_MODULE_NAME" for updating faust DSP extension.
 # adds extension target "MODULE_NAME~"
 # file named "MODULE_NAME.dsp" should exists in ceammc/faust directory
 macro(ceammc_faust_extension module name ext)
-    add_custom_target("faust_${module}_${name}"
-        COMMAND ${FAUST_BIN} -i
-            -a ${CMAKE_SOURCE_DIR}/ceammc/faust/${ext}.cpp
-            -cn ${name}
-            "${CMAKE_SOURCE_DIR}/ceammc/faust/${module}_${name}.dsp"
-            -o ${CMAKE_CURRENT_SOURCE_DIR}/${module}_${name}.h)
-
+    ceammc_faust_gen(${module} ${name})
     pd_add_extension(NAME "${module}.${name}~"
         FILES "${module}_${name}.cpp" INTERNAL TRUE LINK ceammc_core)
     set_target_properties("${module}.${name}~" PROPERTIES COMPILE_FLAGS "-DFAUST_MACRO")
