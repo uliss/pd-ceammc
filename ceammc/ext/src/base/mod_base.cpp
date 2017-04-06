818a438e7228196f1d0a2a09f7289830ee6f9757
Serge Poltavski <serge.poltavski@gmail.com>
2017-04-06 21:53:43 +0300

    base migration

--- ceammc/ext/src/base/mod_base.cpp
+++ ceammc/ext/src/base/mod_base.cpp
@@ -1,13 +1,35 @@
 #include "mod_base.h"
 
+extern "C" void is_any_setup();
+extern "C" void is_bang_setup();
+extern "C" void is_even_setup();
+extern "C" void is_file_setup();
+extern "C" void is_float_setup();
+extern "C" void is_list_setup();
+extern "C" void is_odd_setup();
+extern "C" void is_pointer_setup();
+extern "C" void is_symbol_setup();
 extern "C" void msg_setup();
 extern "C" void replace_setup();
 extern "C" void setup_prop0x2eget();
 extern "C" void setup_prop0x2eset();
+extern "C" void setup_reject0x2eif();
+extern "C" void setup_reject0x2ethis();
 
 void ceammc_base_setup() {
+     is_any_setup();
+     is_bang_setup();
+     is_even_setup();
+     is_file_setup();
+     is_float_setup();
+     is_list_setup();
+     is_odd_setup();
+     is_pointer_setup();
+     is_symbol_setup();
      msg_setup();
      replace_setup();
      setup_prop0x2eget();
      setup_prop0x2eset();
+     setup_reject0x2eif();
+     setup_reject0x2ethis();
 }
