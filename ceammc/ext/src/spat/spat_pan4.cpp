d04d9541efc3a6e72b3128acfc1efd73c99ce48d
Serge Poltavski <serge.poltavski@gmail.com>
2017-04-08 03:08:36 +0300

    faust: spat update

--- ceammc/ext/src/spat/spat_pan4.cpp
+++ ceammc/ext/src/spat/spat_pan4.cpp
@@ -1,14 +1,11 @@
 #include "spat_pan4.h"
 
-static void* faust_new(t_symbol* s, int argc, t_atom* argv)
+EXTERNAL_NEW
 {
-    t_faust* x = reinterpret_cast<t_faust*>(pd_new(faust_class));
+    FAUST_EXT* x = reinterpret_cast<FAUST_EXT*>(pd_new(FAUST_EXT_CLASS));
     PdArgParser p(x, argc, argv);
     p.initFloatArg("angle", 1);
     return p.pd_obj();
 }
 
-extern "C" void setup_spat0x2epan4_tilde()
-{
-    internal_setup(gensym("spat.pan4~"));
-}
+EXTERNAL_SETUP(spat);
--- ceammc/ext/src/spat/spat_pan4.h
+++ ceammc/ext/src/spat/spat_pan4.h
@@ -479,7 +479,6 @@ struct pan4 : public dsp {
 #include "ceammc_faust.h"
 using namespace ceammc::faust;
 
-
 /******************************************************************************
 *******************************************************************************
 
@@ -600,10 +599,12 @@ class pan4 : public dsp {
 #define xfaust_setup(name) name##_tilde_setup(void)
 // time for "active" toggle xfades in secs
 #define XFADE_TIME 0.1f
-static t_class* faust_class;
+static t_class* pan4_faust_class;
+#define FAUST_EXT t_faust_pan4
+#define FAUST_EXT_CLASS pan4_faust_class
 // clang-format on
 
-struct t_faust {
+struct t_faust_pan4 {
     t_object x_obj;
 #ifdef __MINGW32__
     /* This seems to be necessary as some as yet undetermined Pd routine seems
@@ -638,7 +639,7 @@ static inline void copy_samples(int k, int n, t_sample** out, t_sample** in)
 
 static t_int* faust_perform(t_int* w)
 {
-    t_faust* x = reinterpret_cast<t_faust*>(w[1]);
+    t_faust_pan4* x = reinterpret_cast<t_faust_pan4*>(w[1]);
     int n = static_cast<int>(w[2]);
     if (!x->dsp || !x->buf)
         return (w + 3);
@@ -687,7 +688,7 @@ static t_int* faust_perform(t_int* w)
     return (w + 3);
 }
 
-static void faust_dsp(t_faust* x, t_signal** sp)
+static void pan4_faust_dsp(t_faust_pan4* x, t_signal** sp)
 {
     const int n = sp[0]->s_n;
     const int sr = static_cast<int>(sp[0]->s_sr);
@@ -726,7 +727,25 @@ static void faust_dsp(t_faust* x, t_signal** sp)
     }
 }
 
-static void faust_any(t_faust* x, t_symbol* s, int argc, t_atom* argv)
+static void pan4_dump_to_console(t_faust_pan4* x)
+{
+    t_object* xobj = &x->x_obj;
+    t_class* xc = xobj->te_pd;
+    const char* name = class_getname(xc);
+
+    // print xlets
+    post("[%s] inlets: %i", name, x->dsp->getNumInputs());
+    int info_outlet = (x->out == 0) ? 0 : 1;
+    post("[%s] outlets: %i", name, x->dsp->getNumOutputs() + info_outlet);
+
+    // print properties
+    for (size_t i = 0; i < x->ui->uiCount(); i++) {
+        UIElement* el = x->ui->uiAt(i);
+        post("[%s] property: %s = %g", name, el->setPropertySym()->s_name, static_cast<double>(el->value()));
+    }
+}
+
+static void pan4_faust_any(t_faust_pan4* x, t_symbol* s, int argc, t_atom* argv)
 {
     if (!x->dsp)
         return;
@@ -738,6 +757,8 @@ static void faust_any(t_faust* x, t_symbol* s, int argc, t_atom* argv)
         ui->outputAllProperties(x->out);
     } else if (isGetProperty(s)) {
         ui->outputProperty(s, x->out);
+    } else if (isSetProperty(s)) {
+        ui->setProperty(s, argc, argv);
     } else {
         const char* label = s->s_name;
         int count = 0;
@@ -772,33 +793,33 @@ static void faust_any(t_faust* x, t_symbol* s, int argc, t_atom* argv)
     }
 }
 
-static void faust_free_dsp(t_faust* x)
+static void faust_free_dsp(t_faust_pan4* x)
 {
     delete x->dsp;
     x->dsp = NULL;
 }
 
-static void faust_free_ui(t_faust* x)
+static void faust_free_ui(t_faust_pan4* x)
 {
     delete x->ui;
     x->ui = NULL;
 }
 
-static void faust_free_inputs(t_faust* x)
+static void faust_free_inputs(t_faust_pan4* x)
 {
     if (x->inputs)
         free(x->inputs);
     x->inputs = NULL;
 }
 
-static void faust_free_outputs(t_faust* x)
+static void faust_free_outputs(t_faust_pan4* x)
 {
     if (x->outputs)
         free(x->outputs);
     x->outputs = NULL;
 }
 
-static void faust_free_buf(t_faust* x)
+static void faust_free_buf(t_faust_pan4* x)
 {
     if (x->buf) {
         for (int i = 0; i < x->n_out; i++) {
@@ -810,7 +831,7 @@ static void faust_free_buf(t_faust* x)
     }
 }
 
-static void faust_free(t_faust* x)
+static void pan4_faust_free(t_faust_pan4* x)
 {
     faust_free_dsp(x);
     faust_free_ui(x);
@@ -819,7 +840,7 @@ static void faust_free(t_faust* x)
     faust_free_buf(x);
 }
 
-static bool faust_init_inputs(t_faust* x)
+static bool faust_init_inputs(t_faust_pan4* x)
 {
     x->inputs = NULL;
     x->n_in = x->dsp->getNumInputs();
@@ -841,7 +862,7 @@ static bool faust_init_inputs(t_faust* x)
     return true;
 }
 
-static bool faust_init_outputs(t_faust* x, bool info_outlet)
+static bool faust_init_outputs(t_faust_pan4* x, bool info_outlet)
 {
     x->outputs = NULL;
     x->buf = NULL;
@@ -880,7 +901,7 @@ static bool faust_init_outputs(t_faust* x, bool info_outlet)
     return true;
 }
 
-static bool faust_new_internal(t_faust* x, const std::string& objId = "", bool info_outlet = true)
+static bool faust_new_internal(t_faust_pan4* x, const std::string& objId = "", bool info_outlet = true)
 {
     int sr = 44100;
     x->active = 1;
@@ -892,12 +913,12 @@ static bool faust_new_internal(t_faust* x, const std::string& objId = "", bool i
     x->ui = new PdUI<UI>(sym(pan4), objId);
 
     if (!faust_init_inputs(x)) {
-        faust_free(x);
+        pan4_faust_free(x);
         return false;
     }
 
     if (!faust_init_outputs(x, info_outlet)) {
-        faust_free(x);
+        pan4_faust_free(x);
         return false;
     }
 
@@ -997,7 +1018,7 @@ static bool get_nth_symbol_arg(int argc, t_atom* argv, int nth, const char** des
 }
 
 class PdArgParser {
-    t_faust* x_;
+    t_faust_pan4* x_;
     int argc_;
     t_atom* argv_;
     bool control_outlet_;
@@ -1009,7 +1030,7 @@ public:
      * @param argc arguments count
      * @param argv pointer to argument vector
      */
-    PdArgParser(t_faust* x, int argc, t_atom* argv, bool info_outlet = true)
+    PdArgParser(t_faust_pan4* x, int argc, t_atom* argv, bool info_outlet = true)
         : x_(x)
         , argc_(argc)
         , argv_(argv)
@@ -1060,23 +1081,44 @@ public:
             pd_float(reinterpret_cast<t_pd*>(this->x_), arg);
     }
 
-    t_faust* pd_obj()
+    t_faust_pan4* pd_obj()
     {
         return this->x_;
     }
 };
 
-static void* faust_new(t_symbol* s, int argc, t_atom* argv);
+static void* pan4_faust_new(t_symbol* s, int argc, t_atom* argv);
 
 static void internal_setup(t_symbol* s)
 {
-    faust_class = class_new(s, reinterpret_cast<t_newmethod>(faust_new),
-        reinterpret_cast<t_method>(faust_free),
-        sizeof(t_faust),
+    pan4_faust_class = class_new(s, reinterpret_cast<t_newmethod>(pan4_faust_new),
+        reinterpret_cast<t_method>(pan4_faust_free),
+        sizeof(t_faust_pan4),
         CLASS_DEFAULT,
         A_GIMME, A_NULL);
-    class_addmethod(faust_class, nullfn, &s_signal, A_NULL);
-    class_addmethod(faust_class, reinterpret_cast<t_method>(faust_dsp), gensym("dsp"), A_NULL);
-    CLASS_MAINSIGNALIN(faust_class, t_faust, f);
-    class_addanything(faust_class, faust_any);
+    class_addmethod(pan4_faust_class, nullfn, &s_signal, A_NULL);
+    class_addmethod(pan4_faust_class, reinterpret_cast<t_method>(pan4_faust_dsp), gensym("dsp"), A_NULL);
+    class_addmethod(pan4_faust_class, reinterpret_cast<t_method>(pan4_dump_to_console), gensym("dump"), A_NULL);
+    CLASS_MAINSIGNALIN(pan4_faust_class, t_faust_pan4, f);
+    class_addanything(pan4_faust_class, pan4_faust_any);
 }
+
+#define EXTERNAL_NEW void* pan4_faust_new(t_symbol*, int argc, t_atom* argv)
+
+#define EXTERNAL_SIMPLE_NEW()                                                           \
+    static void* pan4_faust_new(t_symbol*, int argc, t_atom* argv)                     \
+    {                                                                                   \
+        t_faust_pan4* x = reinterpret_cast<t_faust_pan4*>(pd_new(pan4_faust_class)); \
+        PdArgParser p(x, argc, argv, false);                                            \
+        return p.pd_obj();                                                              \
+    }
+
+#define EXTERNAL_SETUP(MOD)                        \
+    extern "C" void setup_##MOD##0x2epan4_tilde() \
+    {                                              \
+        internal_setup(gensym(#MOD ".pan4~"));    \
+    }
+
+#define SIMPLE_EXTERNAL(MOD) \
+    EXTERNAL_SIMPLE_NEW();   \
+    EXTERNAL_SETUP(MOD);
