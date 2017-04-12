76f37e539627a2e54e0d8968d241d6c70fc9c40b
Serge Poltavski <serge.poltavski@gmail.com>
2017-04-12 15:57:15 +0300

    osc.sinfb~ update

--- ceammc/ext/src/osc/osc_sinfb.cpp
+++ ceammc/ext/src/osc/osc_sinfb.cpp
@@ -5,10 +5,7 @@ EXTERNAL_NEW
     FAUST_EXT* x = reinterpret_cast<FAUST_EXT*>(pd_new(FAUST_EXT_CLASS));
 
     PdArgParser p(x, argc, argv);
-    t_float freq = 0;
-    get_nth_float_arg(argc, argv, 1, &freq);
-    pd_float((t_pd*)x, freq);
-
+    p.signalFloatArg("freq", 1);
     p.initFloatArg("feedback", 2);
     return p.pd_obj();
 }
