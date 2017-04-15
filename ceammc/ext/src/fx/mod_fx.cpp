0b197dbbd4b6d833203e3f305eb2f5b844c5839c
Serge Poltavski <serge.poltavski@gmail.com>
2017-04-15 03:09:38 +0300

    fx.distortion added

--- ceammc/ext/src/fx/mod_fx.cpp
+++ ceammc/ext/src/fx/mod_fx.cpp
@@ -1,11 +1,14 @@
 #include "mod_fx.h"
 
+extern "C" void setup_fx0x2edistortion_tilde();
 extern "C" void setup_fx0x2efreeverb2_tilde();
 extern "C" void setup_fx0x2efreeverb_tilde();
 extern "C" void setup_fx0x2efreqshift_tilde();
 extern "C" void setup_fx0x2epitchshift_tilde();
 
+
 void ceammc_fx_setup() {
+     setup_fx0x2edistortion_tilde();
      setup_fx0x2efreeverb2_tilde();
      setup_fx0x2efreeverb_tilde();
      setup_fx0x2efreqshift_tilde();
