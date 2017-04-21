72dc4bc44661fbf8aa637bc0ee5f4cb36f66bc31
Serge Poltavski <serge.poltavski@gmail.com>
2017-04-21 12:11:14 +0300

    conv.sec2samp and back  added

--- ceammc/ext/src/conv/mod_conv.cpp
+++ ceammc/ext/src/conv/mod_conv.cpp
@@ -13,17 +13,24 @@ extern "C" void setup_conv0x2evline2bpf();
 
 extern "C" void setup_conv0x2ecc2amp();
 
-void ceammc_conv_setup() {
-     setup_conv0x2esec2str();
-     setup_conv0x2estr2sec();
-     setup_conv0x2ebpf2array();
-     setup_conv0x2ebpf2env();
-     setup_conv0x2ebpf2list();
-     setup_conv0x2ebpf2vline();
-     setup_conv0x2eenv2bpf();
-     setup_conv0x2elin2lin();
-     setup_conv0x2elist2bpf();
-     setup_conv0x2evline2bpf();
+extern "C" void setup_conv0x2esamp2sec();
+extern "C" void setup_conv0x2esec2samp();
 
-     setup_conv0x2ecc2amp();
+void ceammc_conv_setup()
+{
+    setup_conv0x2esec2str();
+    setup_conv0x2estr2sec();
+    setup_conv0x2ebpf2array();
+    setup_conv0x2ebpf2env();
+    setup_conv0x2ebpf2list();
+    setup_conv0x2ebpf2vline();
+    setup_conv0x2eenv2bpf();
+    setup_conv0x2elin2lin();
+    setup_conv0x2elist2bpf();
+    setup_conv0x2evline2bpf();
+
+    setup_conv0x2ecc2amp();
+
+    setup_conv0x2esamp2sec();
+    setup_conv0x2esec2samp();
 }
