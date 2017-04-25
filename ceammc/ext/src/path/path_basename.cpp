36f7d1fb9f130faff59a29b7b41de3549d1bddad
Serge Poltavski <serge.poltavski@gmail.com>
2017-04-25 14:07:39 +0300

    path.basename no glib

--- ceammc/ext/src/path/path_basename.cpp
+++ ceammc/ext/src/path/path_basename.cpp
@@ -1,37 +1,24 @@
-#include "ceammc.h"
-#include <m_pd.h>
-#include <glib.h>
+#include "ceammc_factory.h"
+#include "ceammc_object.h"
+#include "ceammc_platform.h"
 
-static t_class* path_basename_class;
+using namespace ceammc;
 
-struct t_path_basename {
-    t_object x_ob;
-    t_outlet* x_out;
+class PathBasename : public BaseObject {
+public:
+    PathBasename(const PdArgs& a)
+        : BaseObject(a)
+    {
+        createOutlet();
+    }
+
+    void onSymbol(t_symbol* s)
+    {
+        symbolTo(0, gensym(platform::basename(s->s_name).c_str()));
+    }
 };
-
-static void path_basename_free(t_path_basename* x)
-{
-    outlet_free(x->x_out);
-}
-
-static void* path_basename_new()
-{
-    t_path_basename* x = reinterpret_cast<t_path_basename*>(pd_new(path_basename_class));
-    x->x_out = outlet_new(&x->x_ob, &s_symbol);
-    return static_cast<void*>(x);
-}
-
-static void path_basename_symbol(t_path_basename* x, t_symbol* s)
-{
-    gchar* bn = g_path_get_basename(s->s_name);
-    outlet_symbol(x->x_out, gensym(bn));
-    g_free(bn);
-}
 
 extern "C" void setup_path0x2ebasename()
 {
-    path_basename_class = class_new(CEAMMC_PATH_GENSYM(basename),
-        (t_newmethod)path_basename_new, (t_method)path_basename_free,
-        sizeof(t_path_basename), 0, A_NULL);
-    class_addsymbol(path_basename_class, path_basename_symbol);
+    ObjectFactory<PathBasename> obj("path.basename");
 }
