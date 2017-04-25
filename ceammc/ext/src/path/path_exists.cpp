3f0a87b83e84cd7d7aa54ebc818b6910dd4e92c2
Serge Poltavski <serge.poltavski@gmail.com>
2017-04-25 16:24:31 +0300

    path.exists added

--- ceammc/ext/src/path/path_exists.cpp
+++ ceammc/ext/src/path/path_exists.cpp
@@ -1,29 +1,37 @@
-#include "ceammc.h"
-#include <glib.h>
-#include <m_pd.h>
+#include "ceammc_factory.h"
+#include "ceammc_object.h"
+#include "ceammc_platform.h"
 
-t_class* path_exists_class;
+using namespace ceammc;
 
-struct t_path_exists {
-    t_object x_obj;
+static Atom exists(const Atom& a)
+{
+    if (!a.isSymbol())
+        return a;
+
+    return Atom(platform::path_exists(a.asSymbol()->s_name) ? 1.f : 0.f);
+}
+
+class PathExists : public BaseObject {
+public:
+    PathExists(const PdArgs& a)
+        : BaseObject(a)
+    {
+        createOutlet();
+    }
+
+    void onSymbol(t_symbol* s)
+    {
+        floatTo(0, platform::path_exists(s->s_name) ? 1 : 0);
+    }
+
+    void onList(const AtomList& l)
+    {
+        listTo(0, l.map(exists));
+    }
 };
 
-static void* path_exists_new()
+extern "C" void setup_path0x2eexists()
 {
-    t_path_exists* x = reinterpret_cast<t_path_exists*>(pd_new(path_exists_class));
-    outlet_new(&x->x_obj, &s_float);
-    return static_cast<void*>(x);
-}
-
-static void path_exists_symbol(t_path_exists* x, t_symbol* s)
-{
-    outlet_float(x->x_obj.ob_outlet, g_file_test(s->s_name, G_FILE_TEST_EXISTS) ? 1 : 0);
-}
-
-extern "C" void CEAMMC_PATH_MODULE(exists)
-{
-    path_exists_class = class_new(CEAMMC_PATH_GENSYM(exists),
-        (t_newmethod)path_exists_new, 0,
-        sizeof(t_path_exists), 0, A_NULL);
-    class_addsymbol(path_exists_class, path_exists_symbol);
+    ObjectFactory<PathExists> obj("path.exists");
 }
