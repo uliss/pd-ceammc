aea04a3993dba9c574a3344fe8a12d5ef65a654f
Serge Poltavski <serge.poltavski@gmail.com>
2017-05-03 03:45:46 +0300

    cmake:  fftw cleanup

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -1,7 +1,7 @@
 # - Find the FFTW library
 #
 # Usage:
-#   find_package(FFTW [REQUIRED] [QUIET] )
+#   find_package(FFTW [REQUIRED] [QUIET])
 #     
 # It sets the following variables:
 #   FFTW_FOUND               ... true if fftw is found on the system
@@ -9,87 +9,70 @@
 #   FFTW_INCLUDES            ... fftw include directory
 #
 # The following variables will be checked by the function
-#   FFTW_USE_STATIC_LIBS    ... if true, only static libraries are found
 #   FFTW_ROOT               ... if set, the libraries are exclusively searched
 #                               under this path
-#   FFTW_LIBRARY            ... fftw library to use
-#   FFTW_INCLUDE_DIR        ... fftw include directory
-#
 
 #If environment variable FFTWDIR is specified, it has same effect as FFTW_ROOT
-if( NOT FFTW_ROOT AND ENV{FFTWDIR} )
-  set( FFTW_ROOT $ENV{FFTWDIR} )
+if(NOT FFTW_ROOT AND ENV{FFTWDIR})
+    set(FFTW_ROOT $ENV{FFTWDIR})
 endif()
 
 # Check if we can use PkgConfig
 find_package(PkgConfig)
-
-#Determine from PKG
-if( PKG_CONFIG_FOUND AND NOT FFTW_ROOT )
-  pkg_check_modules( PKG_FFTW QUIET "fftw3" )
+if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PKGCONFIG_FFTW "fftw3")
 endif()
 
-#Check whether to search static or dynamic libs
-set( CMAKE_FIND_LIBRARY_SUFFIXES_SAV ${CMAKE_FIND_LIBRARY_SUFFIXES} )
+if(FFTW_ROOT)
 
-if( ${FFTW_USE_STATIC_LIBS} )
-  set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX} )
-else()
-  set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_SHARED_LIBRARY_SUFFIX} )
-endif()
+    find_path(FFTW_INCLUDE_DIR
+        NAMES "fftw3.h"
+        PATHS ${FFTW_ROOT}
+        PATH_SUFFIXES "include"
+        NO_DEFAULT_PATH
+    )
 
-if( FFTW_ROOT )
-  find_library(
-    FFTWF_LIB
-    NAMES "fftw3f"
-    PATHS ${FFTW_ROOT}
-    PATH_SUFFIXES "lib" "lib64"
-    NO_DEFAULT_PATH
-  )
+    find_library(FFTW_FLOAT_LIBRARY
+        NAMES "fftw3f"
+        PATHS ${FFTW_ROOT}
+        PATH_SUFFIXES "lib" "lib64"
+        NO_DEFAULT_PATH
+    )
 
-  #find includes
-  find_path(
-    FFTW_INCLUDES
-    NAMES "fftw3.h"
-    PATHS ${FFTW_ROOT}
-    PATH_SUFFIXES "include"
-    NO_DEFAULT_PATH
-  )
+elseif(PKGCONFIG_FFTW_FOUND)
+
+    set(FFTW_INCLUDE_DIR ${PKGCONFIG_FFTW_INCLUDE_DIRS})
+    set(FFTW_FLOAT_LIBRARY ${PKGCONFIG_FFTW_LIBRARIES})
 
 else()
 
-  find_library(
-    FFTWF_LIB
-    NAMES "fftw3f"
-    PATHS ${PKG_FFTW_LIBRARY_DIRS} 
-        ${LIB_INSTALL_DIR}
-        /usr/local/lib
-        C:/MinGW/msys/1.0/lib
-        C:/MinGW/msys/1.0/local/lib
-  )
+    find_path(FFTW_INCLUDE_DIR
+        NAMES "fftw3.h"
+        PATHS
+            ${INCLUDE_INSTALL_DIR}
+            C:/MinGW/msys/1.0/include
+            C:/MinGW/msys/1.0/local/include
+    )
 
-  find_path(
-    FFTW_INCLUDES
-    NAMES "fftw3.h"
-    PATHS ${PKG_FFTW_INCLUDE_DIRS} 
-        ${INCLUDE_INSTALL_DIR}
-        C:/MinGW/msys/1.0/include
-        C:/MinGW/msys/1.0/local/include
-  )
+    find_library(FFTW_FLOAT_LIBRARY
+        NAMES "fftw3f"
+        PATHS
+            ${LIB_INSTALL_DIR}
+            /usr/local/lib
+            C:/MinGW/msys/1.0/lib
+            C:/MinGW/msys/1.0/local/lib
+    )
 
-endif( FFTW_ROOT )
-
-set(FFTW_LIBRARIES ${FFTW_LIB} ${FFTWF_LIB})
-
-if(FFTWL_LIB)
-  set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTWL_LIB})
 endif()
-
-set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES_SAV} )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FFTW DEFAULT_MSG
-                                  FFTW_INCLUDES FFTW_LIBRARIES)
+find_package_handle_standard_args(FFTW DEFAULT_MSG FFTW_INCLUDE_DIR FFTW_FLOAT_LIBRARY)
 
-mark_as_advanced(FFTW_INCLUDES FFTW_LIBRARIES FFTW_LIB FFTWF_LIB FFTWL_LIB)
+if(FFTW_FOUND)
+    set(FFTW_INCLUDES ${FFTW_INCLUDE_DIR})
+    set(FFTW_LIBRARIES ${FFTW_FLOAT_LIBRARY})
+endif()
+
+
+mark_as_advanced(FFTW_INCLUDES FFTW_LIBRARIES FFTWF_LIB)
 
