13fd9a344260d30372e4bf60e5323ed69a6a6426
Serge Poltavski <serge.poltavski@gmail.com>
2017-05-03 04:16:26 +0300

    cmake: pkg-config libsndfile update

--- cmake/FindLibSndFile.cmake
+++ cmake/FindLibSndFile.cmake
@@ -5,53 +5,61 @@
 #  LIBSNDFILE_INCLUDE_DIRS - the libsndfile include directories
 #  LIBSNDFILE_LIBRARIES - link these to use libsndfile
 
-#If environment variable FFTWDIR is specified, it has same effect as FFTW_ROOT
+#If environment variable LIBSNDFILE_DIR is specified, it has same effect as LIBSNDFILE_ROOT
 if(NOT LIBSNDFILE_ROOT AND ENV{LIBSNDFILE_DIR})
-  set(LIBSNDFILE_ROOT $ENV{LIBSNDFILE_DIR})
+    set(LIBSNDFILE_ROOT $ENV{LIBSNDFILE_DIR})
 endif()
 
 # Use pkg-config to get hints about paths
 find_package(PkgConfig QUIET)
 if(PKG_CONFIG_FOUND)
-    pkg_check_modules(LIBSNDFILE_PKGCONF sndfile)
+    pkg_check_modules(PKGCONFIG_LIBSNDFILE sndfile)
 endif()
 
-# Include dir
-find_path(LIBSNDFILE_INCLUDE_DIR
-    NAMES sndfile.h
-    PATHS ${LIBSNDFILE_PKGCONF_INCLUDE_DIRS}
-)
-
-message(STATUS "pkgconfig: ${LIBSNDFILE_PKGCONF_LIBRARY_DIRS}")
-
-# Library
+# libsndfile source root specified
 if(LIBSNDFILE_ROOT)
+
+    find_path(LIBSNDFILE_INCLUDE_DIR
+        NAMES sndfile.h
+        PATHS ${LIBSNDFILE_ROOT}
+        PATH_SUFFIXES "include"
+        NO_DEFAULT_PATH)
+
     find_library(LIBSNDFILE_LIBRARY
         NAMES sndfile libsndfile-1
-        PATHS
-        ${LIBSNDFILE_PKGCONF_LIBRARY_DIRS}
+        PATHS ${LIBSNDFILE_ROOT}
         PATH_SUFFIXES "lib" "lib64"
-        NO_DEFAULT_PATH
-    )
+        NO_DEFAULT_PATH)
+
+elseif(PKGCONFIG_LIBSNDFILE_FOUND)
+
+    set(LIBSNDFILE_INCLUDE_DIR ${PKGCONFIG_LIBSNDFILE_INCLUDE_DIRS})
+    set(LIBSNDFILE_LIBRARY ${PKGCONFIG_LIBSNDFILE_LIBRARIES})
+
 else()
-    find_library(LIBSNDFILE_LIBRARY
-        NAMES sndfile libsndfile-1
+
+    find_path(LIBSNDFILE_INCLUDE_DIR
+        NAMES sndfile.h
         PATHS
-        ${LIBSNDFILE_PKGCONF_LIBRARY_DIRS}
-        /usr/lib
-        /usr/local/lib
-        /opt/local/lib
-        /usr/lib/x86_64-linux-gnu
-        /usr/lib/i386-linux-gnu
-    )
+          ${INCLUDE_INSTALL_DIR}
+          C:/MinGW/msys/1.0/usr/lib
+          C:/MinGW/msys/1.0/usr/local/lib)
+
+    find_library(LIBSNDFILE_LIBRARY
+          NAMES sndfile libsndfile-1
+          PATHS
+            ${LIB_INSTALL_DIR}
+            C:/MinGW/msys/1.0/usr/lib
+            C:/MinGW/msys/1.0/usr/local/lib)
+
 endif()
 
 find_package(PackageHandleStandardArgs)
-find_package_handle_standard_args(LibSndFile  DEFAULT_MSG  LIBSNDFILE_LIBRARY LIBSNDFILE_INCLUDE_DIR)
+find_package_handle_standard_args(LibSndFile DEFAULT_MSG LIBSNDFILE_INCLUDE_DIR LIBSNDFILE_LIBRARY)
 
 if(LIBSNDFILE_FOUND)
-  set(LIBSNDFILE_LIBRARIES ${LIBSNDFILE_LIBRARY})
-  set(LIBSNDFILE_INCLUDE_DIRS ${LIBSNDFILE_INCLUDE_DIR})
-endif(LIBSNDFILE_FOUND)
+    set(LIBSNDFILE_LIBRARIES ${LIBSNDFILE_LIBRARY})
+    set(LIBSNDFILE_INCLUDE_DIRS ${LIBSNDFILE_INCLUDE_DIR})
+endif()
 
 mark_as_advanced(LIBSNDFILE_LIBRARY LIBSNDFILE_LIBRARIES LIBSNDFILE_INCLUDE_DIR LIBSNDFILE_INCLUDE_DIRS)
