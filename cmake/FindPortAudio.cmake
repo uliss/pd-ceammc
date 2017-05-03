0e2c64b737b5563c1ee413f38ea537ff20e501df
Serge Poltavski <serge.poltavski@gmail.com>
2017-05-03 03:20:06 +0300

    cmake: portaudio cleanup

--- cmake/FindPortAudio.cmake
+++ cmake/FindPortAudio.cmake
@@ -1,60 +1,68 @@
 #  PORTAUDIO_FOUND - system has libportaudio
 #  PORTAUDIO_INCLUDE_DIRS - the libportaudio include directory
 #  PORTAUDIO_LIBRARIES - Link these to use libportaudio
+#  PORTAUDIO_ROOT - manually set where portaudio exists
 
-if (PORTAUDIO_LIBRARIES AND PORTAUDIO_INCLUDE_DIRS)
-  # in cache already
-  set(PORTAUDIO_FOUND TRUE)
-else (PORTAUDIO_LIBRARIES AND PORTAUDIO_INCLUDE_DIRS)
+# Check if we can use PkgConfig
+find_package(PkgConfig)
 
-  find_path(PORTAUDIO_INCLUDE_DIR
-    NAMES
-      portaudio.h
-    PATHS
-      /usr/include
-      /usr/local/include
-      /opt/local/include
-      /sw/include
-      /usr/local/mingw/i686-w64-mingw32
-      C:/MinGW/msys/1.0/usr/lib
-      C:/MinGW/msys/1.0/usr/local/lib
-  )
-  
-  find_library(PORTAUDIO_LIBRARY
-    NAMES
-      portaudio
-    PATHS
-      /usr/lib
-      /usr/local/lib
-      /opt/local/lib
-      /sw/lib
-      /usr/local/mingw/i686-w64-mingw32
-      C:/MinGW/msys/1.0/usr/lib
-      C:/MinGW/msys/1.0/usr/local/lib
-  )
+# portaudio source root specified
+if(PORTAUDIO_ROOT)
 
-  set(PORTAUDIO_INCLUDE_DIRS
-    ${PORTAUDIO_INCLUDE_DIR}
-  )
-  set(PORTAUDIO_LIBRARIES
-    ${PORTAUDIO_LIBRARY}
-  )
+    find_path(PORTAUDIO_INCLUDE_DIR
+        NAMES portaudio.h
+        PATHS ${PORTAUDIO_ROOT}
+        PATH_SUFFIXES "include"
+        NO_DEFAULT_PATH)
 
-  if (PORTAUDIO_INCLUDE_DIRS AND PORTAUDIO_LIBRARIES)
-    set(PORTAUDIO_FOUND TRUE)
-  endif (PORTAUDIO_INCLUDE_DIRS AND PORTAUDIO_LIBRARIES)
+    find_library(PORTAUDIO_LIBRARY
+        NAMES portaudio libportaudio
+        PATHS ${PORTAUDIO_ROOT}
+        PATH_SUFFIXES "lib" "lib64"
+        NO_DEFAULT_PATH)
 
-  if (PORTAUDIO_FOUND)
-    if (NOT Portaudio_FIND_QUIETLY)
-      message(STATUS "Found libportaudio: ${PORTAUDIO_LIBRARIES}")
-    endif (NOT Portaudio_FIND_QUIETLY)
-  else (PORTAUDIO_FOUND)
-    if (Portaudio_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find libportaudio")
-    endif (Portaudio_FIND_REQUIRED)
-  endif (PORTAUDIO_FOUND)
+elseif(PKG_CONFIG_FOUND)
 
-  # show the PORTAUDIO_INCLUDE_DIRS and PORTAUDIO_LIBRARIES variables only in the advanced view
-  mark_as_advanced(PORTAUDIO_INCLUDE_DIRS PORTAUDIO_LIBRARIES)
+    pkg_check_modules(PKGCONFIG_PORTAUDIO portaudio-2.0)
 
-endif (PORTAUDIO_LIBRARIES AND PORTAUDIO_INCLUDE_DIRS)
+    if(PKGCONFIG_PORTAUDIO_FOUND)
+        set(PORTAUDIO_INCLUDE_DIR ${PKGCONFIG_PORTAUDIO_INCLUDE_DIRS})
+        set(PORTAUDIO_LIBRARY ${PKGCONFIG_PORTAUDIO_LIBRARIES})
+    endif()
+
+else()
+
+    find_path(PORTAUDIO_INCLUDE_DIR
+        NAMES portaudio.h
+        PATHS
+            /usr/include
+            /usr/local/include
+            /opt/local/include
+            /sw/include
+            /usr/local/mingw/i686-w64-mingw32
+            C:/MinGW/msys/1.0/usr/lib
+            C:/MinGW/msys/1.0/usr/local/lib
+      )
+
+      find_library(PORTAUDIO_LIBRARY
+        NAMES portaudio
+        PATHS
+            /usr/lib
+            /usr/local/lib
+            /opt/local/lib
+            /sw/lib
+            /usr/local/mingw/i686-w64-mingw32
+            C:/MinGW/msys/1.0/usr/lib
+            C:/MinGW/msys/1.0/usr/local/lib
+      )
+endif()
+
+find_package(PackageHandleStandardArgs)
+find_package_handle_standard_args(PortAudio DEFAULT_MSG PORTAUDIO_INCLUDE_DIR PORTAUDIO_LIBRARY)
+
+if(PORTAUDIO_FOUND)
+    set(PORTAUDIO_INCLUDE_DIRS ${PORTAUDIO_INCLUDE_DIR})
+    set(PORTAUDIO_LIBRARIES ${PORTAUDIO_LIBRARY})
+endif()
+
+mark_as_advanced(PORTAUDIO_INCLUDE_DIRS PORTAUDIO_LIBRARIES)
