9a1cfecb3e8e13436cbba72e520bfc222ecbe994
Serge Poltavski <serge.poltavski@gmail.com>
2017-06-21 02:48:05 +0300

    noise.baker compat fix

--- ceammc/ext/src/noise/noise_baker.c
+++ ceammc/ext/src/noise/noise_baker.c
@@ -7,20 +7,19 @@ a port of Richard's chaos-baker from chaos collection
 ————————————————————————————————————————————————————————————————
 */
 
-// CEAMMC pd library version
-#include "cicm_wrapper.h"
+#include "m_pd.h"
 
 #include <math.h>
 #include <stdbool.h>
 
 typedef struct _baker {
-    t_eobj c_ob;
+    t_object x_obj;
     t_outlet* c_out; // outlet
     double init, eval;
     bool fold_cut, om;
 } baker;
 
-static t_eclass* noise_baker_class;
+static t_class* noise_baker_class;
 
 static void* baker_new(t_symbol* msg, int argc, t_atom* argv);
 static void baker_bang(baker* x);
@@ -33,10 +32,10 @@ static void baker_fold_cut(baker* x, t_float max);
 
 void* baker_new(t_symbol* msg, int argc, t_atom* argv) //input the args
 {
-    baker* x = (baker*)eobj_new(noise_baker_class);
+    baker* x = (baker*)pd_new(noise_baker_class);
 
     if (x) {
-        x->c_out = floatout(x);
+        x->c_out = outlet_new(&x->x_obj, &s_float);
 
         //init
         x->init = 0.85;
@@ -139,17 +138,17 @@ void baker_om(baker* x, t_float max)
 
 void setup_noise0x2ebaker()
 {
-    noise_baker_class = eclass_new(("noise.baker"),
-        (t_typ_method)(baker_new),
-        (t_typ_method)(baker_free),
+    noise_baker_class = class_new(gensym("noise.baker"),
+        (t_newmethod)(baker_new),
+        (t_method)(baker_free),
         sizeof(baker), 0, A_GIMME, 0);
 
-    eclass_addmethod(noise_baker_class, (method)baker_bang, "bang", A_NULL, 0);
-    eclass_addmethod(noise_baker_class, (method)baker_set, "set", A_GIMME, 0);
-    eclass_addmethod(noise_baker_class, (method)baker_reset, "reset", A_NULL, 0);
-    eclass_addmethod(noise_baker_class, (method)baker_eval, "seed", A_FLOAT, 0);
-    eclass_addmethod(noise_baker_class, (method)baker_fold, "fold", A_FLOAT, 0);
-    eclass_addmethod(noise_baker_class, (method)baker_cut, "cut", A_FLOAT, 0);
-    eclass_addmethod(noise_baker_class, (method)baker_fold_cut, "fold_cut", A_FLOAT, 0);
-    eclass_addmethod(noise_baker_class, (method)baker_om, "om", A_FLOAT, 0);
+    class_addmethod(noise_baker_class, (t_method)baker_bang, gensym("bang"), A_NULL, 0);
+    class_addmethod(noise_baker_class, (t_method)baker_set, gensym("set"), A_GIMME, 0);
+    class_addmethod(noise_baker_class, (t_method)baker_reset, gensym("reset"), A_NULL, 0);
+    class_addmethod(noise_baker_class, (t_method)baker_eval, gensym("seed"), A_FLOAT, 0);
+    class_addmethod(noise_baker_class, (t_method)baker_fold, gensym("fold"), A_FLOAT, 0);
+    class_addmethod(noise_baker_class, (t_method)baker_cut, gensym("cut"), A_FLOAT, 0);
+    class_addmethod(noise_baker_class, (t_method)baker_fold_cut, gensym("fold_cut"), A_FLOAT, 0);
+    class_addmethod(noise_baker_class, (t_method)baker_om, gensym("om"), A_FLOAT, 0);
 }
