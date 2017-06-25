73cd1298de12ad09bf16cd24fb98d0f995c4a8f6
Serge Poltavski <serge.poltavski@gmail.com>
2017-06-25 13:08:17 +0300

    list.count fix: method pattern -> @pattern property

--- ceammc/ext/src/list/list_count.cpp
+++ ceammc/ext/src/list/list_count.cpp
@@ -1,48 +1,37 @@
+#include "list_count.h"
 #include "ceammc_factory.h"
 #include "ceammc_log.h"
-#include "ceammc_object.h"
 
-using namespace ceammc;
+ListCount::ListCount(const PdArgs& a)
+    : BaseObject(a)
+    , pattern_(0)
+{
+    createInlet();
+    createOutlet();
 
-class ListCount : public BaseObject {
-    Atom pattern_;
+    pattern_ = new AtomProperty("@pattern", positionalArgument(0));
+    createProperty(pattern_);
+}
 
-public:
-    ListCount(const PdArgs& a)
-        : BaseObject(a)
-    {
-        createInlet();
-        createOutlet();
+void ListCount::onList(const AtomList& l)
+{
+    floatTo(0, l.count(pattern_->value()));
+}
 
-        createCbProperty("@pattern", &ListCount::pattern);
-        pattern_ = positionalArgument(0);
+void ListCount::onInlet(size_t n, const AtomList& l)
+{
+    if (n != 1)
+        return;
+
+    if (l.size() < 1)
+        return;
+    else if (l.size() == 1)
+        pattern_->setValue(l[0]);
+    else {
+        OBJ_DBG << "search pattern can be only single element, not list. Using first element from " << l;
+        pattern_->setValue(l[0]);
     }
-
-    void onList(const AtomList& l)
-    {
-        floatTo(0, l.count(pattern_));
-    }
-
-    void onInlet(size_t n, const AtomList& l)
-    {
-        if (n != 1)
-            return;
-
-        if (l.size() < 1)
-            return;
-        else if (l.size() == 1)
-            pattern_ = l[0];
-        else {
-            OBJ_DBG << "search pattern can be only single element, not list. Using first element from " << l;
-            pattern_ = l[0];
-        }
-    }
-
-    AtomList pattern() const
-    {
-        return listFrom(pattern_);
-    }
-};
+}
 
 extern "C" void setup_list0x2ecount()
 {
