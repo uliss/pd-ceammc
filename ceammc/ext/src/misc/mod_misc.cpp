8ee6913f0c0d3de1d159b7f77f58c350ea7cacfd
Serge Poltavski <serge.poltavski@gmail.com>
2017-07-13 16:32:31 +0300

    mikmod compile fix

--- ceammc/ext/src/misc/mod_misc.cpp
+++ ceammc/ext/src/misc/mod_misc.cpp
@@ -1,10 +1,16 @@
 #include "mod_misc.h"
+
+#ifdef MIKMOD_FOUND
 #include "misc_mikmod.h"
+#endif
 
 extern "C" void setup_live0x2ecapture_tilde();
 
 void ceammc_misc_setup()
 {
     setup_live0x2ecapture_tilde();
+
+#ifdef MIKMOD_FOUND
     setup_misc0x2emikmod_tilde();
+#endif
 }
