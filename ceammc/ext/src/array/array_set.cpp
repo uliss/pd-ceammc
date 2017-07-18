4b3a6f2fb3cb86288dbdda11fd6070a410fe4952
Serge Poltavski <serge.poltavski@gmail.com>
2017-07-19 00:52:40 +0300

    array.set object added

---
+++ ceammc/ext/src/array/array_set.cpp
@@ -0,0 +1,57 @@
+#include "array_set.h"
+#include "ceammc_factory.h"
+#include "ceammc_fn_list.h"
+
+ArraySet::ArraySet(const PdArgs& a)
+    : ArrayMod(a)
+    , normalize_(0)
+{
+    normalize_ = new FlagProperty("@normalize");
+    createProperty(normalize_);
+}
+
+void ArraySet::onSymbol(t_symbol* s)
+{
+    setArray(s);
+}
+
+void ArraySet::onList(const AtomList& values)
+{
+    if (values.empty()) {
+        OBJ_ERR << "empty list is given";
+        return;
+    }
+
+    if (normalize_->value()) {
+        AtomList norm;
+        if (!list::normalizeByRange(values, norm)) {
+            OBJ_ERR << "normalization error";
+            return;
+        }
+
+        set(norm);
+    } else
+        set(values);
+}
+
+void ArraySet::set(const AtomList& v)
+{
+    if (!check())
+        return;
+
+    if (!array_.resize(v.size())) {
+        OBJ_ERR << "can't resize array: " << array_.name();
+        return;
+    }
+
+    for (size_t i = 0; i < v.size(); i++)
+        array_[i] = v[i].asFloat();
+
+    array_.redraw();
+}
+
+extern "C" void setup_array0x2eset()
+{
+    ObjectFactory<ArraySet> obj("array.set");
+    obj.addAlias("list->array");
+}
---
+++ ceammc/ext/src/array/array_set.h
@@ -0,0 +1,18 @@
+#ifndef ARRAY_SET_H
+#define ARRAY_SET_H
+
+#include "array_base.h"
+
+class ArraySet : public ArrayMod {
+    FlagProperty* normalize_;
+
+public:
+    ArraySet(const PdArgs& a);
+    void onSymbol(t_symbol* s);
+    void onList(const AtomList& values);
+    void set(const AtomList& v);
+};
+
+extern "C" void setup_array0x2eset();
+
+#endif // ARRAY_SET_H
