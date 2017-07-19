45186cd30769ea9da9ce42d0f2eccfe86209132d
Serge Poltavski <serge.poltavski@gmail.com>
2017-07-19 17:07:32 +0300

    soundtouch first source import

--- ceammc/ext/src/random/mod_random.cpp
+++ ceammc/ext/src/random/mod_random.cpp
@@ -6,8 +6,6 @@
 #include "random_pwconst.h"
 #include "random_pwlin.h"
 
-#include <boost/version.hpp>
-
 extern "C" void setup_random0x2eint();
 
 void ceammc_random_setup()
@@ -16,10 +14,8 @@ void ceammc_random_setup()
     setup_random0x2egauss();
     setup_random0x2eint();
 
-#if BOOST_VERSION > 105000
     setup_random0x2ediscrete();
     setup_random0x2elinear();
     setup_random0x2epw_const();
     setup_random0x2epw_lin();
-#endif
 }
