f186e3d438ab1d018916186094d8c754c8d3e074
Serge Poltavski <serge.poltavski@gmail.com>
2017-07-20 00:23:00 +0300

    array_bpm tests added

--- ceammc/ext/src/array/array_bpm.cpp
+++ ceammc/ext/src/array/array_bpm.cpp
@@ -16,15 +16,18 @@ void ArrayBPM::onBang()
     if (!checkArray())
         return;
 
-    soundtouch::BPMDetect bpm(1, sys_getsr());
+    int SR = sys_getsr();
+    soundtouch::BPMDetect bpm(1, SR ? SR : 44100);
 
     float buffer[BUF_SIZE];
     const size_t N = array_.size();
+    Array::iterator it = array_.begin();
     for (size_t i = 0, hop = BUF_SIZE; i < N; i += BUF_SIZE) {
         if ((i + BUF_SIZE) > N)
             hop = N - i;
 
-        std::copy(array_.begin() + i, array_.begin() + i + hop, buffer);
+        std::copy(it, it + hop, buffer);
+        it += hop;
         bpm.inputSamples(buffer, BUF_SIZE);
     }
 
