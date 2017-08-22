4931b9b02a1c0ecf9cc9475d23f1a255d6c4eae4
Serge Poltavski <serge.poltavski@gmail.com>
2017-08-23 02:16:55 +0300

    warning fixes

--- ceammc/extra/lyonpotpourri/MSPd.h
+++ ceammc/extra/lyonpotpourri/MSPd.h
@@ -1,27 +1,26 @@
+#ifndef MSPD_H
+#define MSPD_H
+
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <time.h>
-#include <strings.h>
 #include <string.h>
-
+#include <strings.h>
+#include <time.h>
 
 #define LYONPOTPOURRI_MSG "-<LyonPotpourri 3.0>-"
 
 #define NO_FREE_FUNCTION 0
 
-
 /* because Max and Pd have different ideas of what A_FLOAT is, use t_floatarg
 to force consistency. Otherwise functions that look good will fail on some
 hardware. Also note that Pd messages cannot accept arguments of type A_LONG. */
-
 
 #include "m_pd.h"
 // #define t_floatarg float
 #define t_double double
 
 #define atom_getsymarg atom_getsymbolarg
-
 
 #ifndef PIOVERTWO
 #define PIOVERTWO 1.5707963268
@@ -34,9 +33,11 @@ hardware. Also note that Pd messages cannot accept arguments of type A_LONG. */
 #endif
 
 /*** MSP helper functions **/
-void atom_arg_getfloat(float *c, long idx, long ac, t_atom *av);
-void atom_arg_getsym(t_symbol **c, long idx, long ac, t_atom *av);
+void atom_arg_getfloat(float* c, long idx, long ac, t_atom* av);
+void atom_arg_getsym(t_symbol** c, long idx, long ac, t_atom* av);
 
 // #define potpourri_announce(objname)  post("( %s )\t%s",objname,LYONPOTPOURRI_MSG)
 
-#define potpourri_announce(objname)  post("%s (  %s  )",LYONPOTPOURRI_MSG,objname)
+#define potpourri_announce(objname) post("%s (  %s  )", LYONPOTPOURRI_MSG, objname)
+
+#endif
