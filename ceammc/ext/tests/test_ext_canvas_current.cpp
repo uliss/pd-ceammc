34a19fe7b61912efa86b1b20bda7537ba74e67e4
Serge Poltavski <serge.poltavski@gmail.com>
2017-09-12 23:48:21 +0300

    canvas.current tests added

--- ceammc/ext/tests/test_ext_canvas_current.cpp
+++ ceammc/ext/tests/test_ext_canvas_current.cpp
@@ -14,6 +14,7 @@
 #include "../base/canvas_current.h"
 #include "base_extension_test.h"
 #include "catch.hpp"
+#include "ceammc_pd.h"
 
 #include <stdio.h>
 
@@ -28,5 +29,42 @@ TEST_CASE("canvas.current", "[externals]")
         REQUIRE(t.numOutlets() == 1);
 
         REQUIRE_PROPERTY(t, @name, "");
+        REQUIRE_PROPERTY(t, @dir, "");
+        REQUIRE_PROPERTY(t, @root, Atom(0.f));
+        REQUIRE_PROPERTY(t, @abstraction, Atom(0.f));
+        REQUIRE_PROPERTY(t, @font, Atom(0.f));
+        REQUIRE_PROPERTY(t, @width, Atom(0.f));
+        REQUIRE_PROPERTY(t, @height, Atom(0.f));
+        REQUIRE_PROPERTY(t, @x, Atom(0.f));
+        REQUIRE_PROPERTY(t, @y, Atom(0.f));
+
+        REQUIRE_PROPERTY_LIST(t, @args, AtomList());
+        REQUIRE_PROPERTY_LIST(t, @paths, AtomList());
+        REQUIRE_PROPERTY_LIST(t, @size, AtomList(0.f, 0.f));
+
+        CanvasPtr cnv = PureData::instance().createTopCanvas("patch");
+
+        {
+            CanvasCurrentTest t("canvas.current");
+            REQUIRE(t.numInlets() == 1);
+            REQUIRE(t.numOutlets() == 1);
+
+            REQUIRE_PROPERTY(t, @name, "patch");
+            REQUIRE_PROPERTY(t, @dir, "");
+            REQUIRE_PROPERTY(t, @root, 1);
+            REQUIRE_PROPERTY(t, @abstraction, Atom(0.f));
+            REQUIRE_PROPERTY(t, @font, 10);
+            REQUIRE_PROPERTY(t, @width, 600);
+            REQUIRE_PROPERTY(t, @height, 400);
+
+#ifdef __APPLE__
+            REQUIRE_PROPERTY(t, @x, Atom(0.f));
+            REQUIRE_PROPERTY(t, @y, 22);
+#endif
+
+            REQUIRE_PROPERTY_LIST(t, @args, AtomList());
+            REQUIRE_PROPERTY_LIST(t, @size, L2(600, 400));
+            REQUIRE_PROPERTY_LIST(t, @paths, AtomList());
+        }
     }
 }
