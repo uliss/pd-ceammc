941d67c11f3a3041bf0ed470580c3d3f0898e1cf
serj <serj@debian64>
2017-10-13 15:12:20 -0500

    linux: make ceammc_lib support

--- ceammc/distrib/linux/make_lib_linux.sh
+++ ceammc/distrib/linux/make_lib_linux.sh
@@ -5,12 +5,23 @@ then
     echo "Usage: $0 SRCDIR BINDIR OUTDIR VERSION"
 fi
 
+if [ -f /etc/os-release ]
+then
+    source /etc/os-release
+fi
+
+SYSVER="linux"
+
+if [ ! -z $ID ] && [ ! -z $VERSION_ID ]
+then
+    SYSVER="${ID}${VERSION_ID}"
+fi
+
 SRCDIR="$1"
 BINDIR="$2"
 VERSION="$4"
 OUTDIR="$3/ceammc"
-SYSVER=$(sw_vers | grep ProductVersion | cut -f2 | cut -f1,2 -d.)
-OUTFILE="ceammc-${VERSION}-linux-${SYSVER}-pd-@PD_MAJOR_VERSION@.@PD_MINOR_VERSION@.tar.gz"
+OUTFILE="ceammc-${VERSION}-${SYSVER}-pd-@PD_MAJOR_VERSION@.@PD_MINOR_VERSION@.tar.gz"
 INSTALL_DIR="@CEAMMC_INSTALL_PREFIX@"
 
 function skip_ext {
@@ -26,19 +37,31 @@ function skip_ext {
 
 echo "Making CEAMMC library from build directory: $BINDIR"
 mkdir -p "${OUTDIR}"
-rm "${OUTDIR}/*"
+rm -f "${OUTDIR}/*"
 
 echo "Copying libraries to ${OUTDIR} ..."
-find "${BINDIR}" -name *.dylib -print0 | while read -r -d '' file
+find "${BINDIR}" -name *.so -print0 | while read -r -d '' file
 do
+    if [[ -z $(echo $file | grep CMakeRelink) ]]
+    then
+	echo "skipping... $file"
+	continue
+    fi
+
     cp "$file" "${OUTDIR}"
     echo "+ Lib:  $(basename $file)"
 done
 
 
 echo "Copying extension files to ${OUTDIR} ..."
-find "${BINDIR}" -name *.d_fat -print0 | while read -r -d '' file
+find "${BINDIR}" -name *.pd_linux -print0 | while read -r -d '' file
 do
+    if [[ -z $(echo $file | grep CMakeRelink) ]]
+    then
+	echo "skipping... $file"
+	continue
+    fi
+
     ext_name=$(basename $file)
     skip_ext $file
     if [ $? -eq 1 ]
@@ -49,12 +72,17 @@ do
 
     cp "$file" "${OUTDIR}/${ext_name}"
     echo "+ Copy: '$ext_name'"
-    ${DYLIBBUNDLER} -x ${OUTDIR}/$ext_name -b -d ${OUTDIR} -p @loader_path/ -of
 done
 
 echo "Copying [system.serial] extension files to ${OUTDIR} ..."
-find "${BINDIR}/../extra/comport" -name *.d_fat -print0 | while read -r -d '' file
+find "${BINDIR}/../extra/comport" -name *.pd_linux -print0 | while read -r -d '' file
 do
+    if [[ -z $(echo $file | grep CMakeRelink) ]]
+    then
+	echo "skipping... $file"
+	continue
+    fi
+
     ext_name=$(basename $file)
     skip_ext $file
     if [ $? -eq 1 ]
@@ -65,12 +93,10 @@ do
 
     cp "$file" "${OUTDIR}/${ext_name}"
     echo "+ Copy: '$ext_name'"
-    ${DYLIBBUNDLER} -x ${OUTDIR}/$ext_name -b -d ${OUTDIR} -p @loader_path/ -of
 done
 
-ceammc_lib=$(find "${BINDIR}" -name ceammc\\.d_fat)
+ceammc_lib=$(find "${BINDIR}" -name ceammc\\.pd_linux | grep CMakeRelink)
 cp $ceammc_lib "${OUTDIR}"
-${DYLIBBUNDLER} -x ${OUTDIR}/ceammc.d_fat -b -d ${OUTDIR} -p @loader_path/ -of
 
 echo "Copying help files to ${OUTDIR} ..."
 find "${SRCDIR}/ext/doc" -name *-help\\.pd | while read file
@@ -88,15 +114,15 @@ echo "    stargazing.mod"
 cp "${SRCDIR}/ext/doc/stargazing.mod" "${OUTDIR}"
 echo "    prs.txt"
 cp "${SRCDIR}/ext/doc/prs.txt" "${OUTDIR}"
-echo "    soundtouch~.d_fat"
-cp "${BINDIR}/../extra/SoundTouch/pd/soundtouch~.d_fat" "${OUTDIR}"
+echo "    soundtouch~.pd_linux"
+cp "${BINDIR}/../extra/SoundTouch/pd/soundtouch~.pd_linux" "${OUTDIR}"
 echo "    soundtouch~-help.pd"
 cp "${BINDIR}/../extra/SoundTouch/pd/soundtouch~-help.pd" "${OUTDIR}"
 echo "    soundtouch-help.pd"
 cp "${BINDIR}/../extra/SoundTouch/pd/soundtouch-help.pd" "${OUTDIR}"
 
 echo "+ Fix soundtouch link in index-help.pd..."
-sed -i "" 's/ceammc\/soundtouch-help\.pd/soundtouch-help.pd/' "${OUTDIR}/index-help.pd"
+sed -i 's/ceammc\/soundtouch-help\.pd/soundtouch-help.pd/' "${OUTDIR}/index-help.pd"
 
 cd "$3"
 tar cfvz "${OUTFILE}" $(basename $OUTDIR)
