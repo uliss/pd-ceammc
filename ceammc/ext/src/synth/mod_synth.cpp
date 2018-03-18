49ba2a927e3f23b66d7c3d718c7d729fe67d197b
Serge Poltavski <serge.poltavski@gmail.com>
2018-03-19 00:48:28 +0300

    8bit synth removal

--- ceammc/ext/src/synth/mod_synth.cpp
+++ ceammc/ext/src/synth/mod_synth.cpp
@@ -1,10 +1,8 @@
 #include "mod_synth.h"
-#include "synth_8bit.h"
 
 extern "C" void setup_synth0x2erisset_tone_tilde();
 
 void ceammc_synth_setup()
 {
     setup_synth0x2erisset_tone_tilde();
-    setup_fx_8bit();
 }
