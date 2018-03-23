8387d5e4d3600450ba8d835a5b6d64ba8ebebce6
Serge Poltavski <serge.poltavski@gmail.com>
2018-03-24 00:48:59 +0300

    more travis package

---
+++ ceammc/scripts/travis_configure.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+uname -a
+printenv
+
+if [[ ${CEAMMC_COVERAGE} == "ON" ]]
+then
+	gcov --version
+fi
+
+mkdir ${CEAMMC_BUILD_DIR} 
+cd ${CEAMMC_BUILD_DIR}
+
+cmake -DCMAKE_BUILD_TYPE=${CEAMMC_BUILD_TYPE} \
+	-DWITH_ALSA_MIDI=ON \
+	-DWITH_PORTMIDI=ON \
+	-DWITH_PORTAUDIO=OFF \
+	-DWITH_COVERAGE=${CEAMMC_COVERAGE} \
+	..
+
