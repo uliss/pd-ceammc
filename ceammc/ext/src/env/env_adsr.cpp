e6aea9e90ed2dbfda8f9a4d0fd68851a0370e4fb
Serge Poltavski <serge.poltavski@gmail.com>
2018-03-30 13:28:46 +0300

    env.fix*

--- ceammc/ext/src/env/env_adsr.cpp
+++ ceammc/ext/src/env/env_adsr.cpp
@@ -1,8 +1,9 @@
 #include "env_adsr.h"
 
-static void* adsr_faust_new(t_symbol* s, int argc, t_atom* argv)
+EXTERNAL_NEW
 {
-    t_faust_adsr* x = reinterpret_cast<t_faust_adsr*>(pd_new(adsr_faust_class));
+    FAUST_EXT* x = reinterpret_cast<FAUST_EXT*>(pd_new(FAUST_EXT_CLASS));
+
     PdArgParser p(x, argc, argv);
     p.initFloatArg("attack", 1);
     p.initFloatArg("decay", 2);
@@ -11,7 +12,5 @@ static void* adsr_faust_new(t_symbol* s, int argc, t_atom* argv)
     return p.pd_obj();
 }
 
-extern "C" void setup_env0x2eadsr_tilde()
-{
-    internal_setup(gensym("env.adsr~"));
-}
+EXTERNAL_SETUP(env)
+
