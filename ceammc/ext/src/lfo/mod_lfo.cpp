6a8233ff2d391ba48ed9d4e68b0036bb5bfa25f0
Serge Poltavski <serge.poltavski@gmail.com>
2018-04-05 12:25:04 +0300

    lfo.pulse~ added

--- ceammc/ext/src/lfo/mod_lfo.cpp
+++ ceammc/ext/src/lfo/mod_lfo.cpp
@@ -3,9 +3,11 @@
 extern "C" void setup_lfo0x2eimpulse_tilde();
 extern "C" void setup_lfo0x2esquare_tilde();
 extern "C" void setup_lfo0x2etri_tilde();
+extern "C" void setup_lfo0x2epulse_tilde();
 
 void ceammc_lfo_setup() {
      setup_lfo0x2eimpulse_tilde();
      setup_lfo0x2esquare_tilde();
      setup_lfo0x2etri_tilde();
+     setup_lfo0x2epulse_tilde();
 }
