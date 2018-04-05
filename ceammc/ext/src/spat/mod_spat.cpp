9e6ec85108e485c7f74eab8fa8903714d5e58ce3
Serge Poltavski <serge.poltavski@gmail.com>
2018-04-05 15:07:25 +0300

    spat.pan4 update

--- ceammc/ext/src/spat/mod_spat.cpp
+++ ceammc/ext/src/spat/mod_spat.cpp
@@ -10,7 +10,7 @@ extern "C" void setup_spat0x2epan4_tilde();
 void ceammc_spat_setup()
 {
     //     setup_spat0x2epan2_tilde();
-    //     setup_spat0x2epan4_tilde();
+    setup_spat0x2epan4_tilde();
     setup_pan_linear();
     setup_pan_cosine();
     setup_pan_sqrt();
