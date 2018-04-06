ad2ccaa7fd537c0af100d6b77750e66ad3e4a3ad
Serge Poltavski <serge.poltavski@gmail.com>
2018-04-06 20:17:46 +0300

    synth.birds~ added

--- ceammc/ext/src/synth/mod_synth.cpp
+++ ceammc/ext/src/synth/mod_synth.cpp
@@ -3,9 +3,11 @@
 extern "C" void setup_synth0x2erisset_tone_tilde();
 extern "C" void setup_synth0x2erisset_arp_tilde();
 extern "C" void setup_synth0x2etambura_tilde();
+extern "C" void setup_synth0x2ebirds_tilde();
 
 void ceammc_synth_setup()
 {
     setup_synth0x2erisset_tone_tilde();
     setup_synth0x2erisset_arp_tilde();
+    setup_synth0x2ebirds_tilde();
 }
---
+++ ceammc/ext/src/synth/synth_birds.cpp
@@ -0,0 +1,4 @@
+#include "synth_birds.h"
+
+EXTERNAL_SIMPLE_NEW();
+EXTERNAL_SETUP_NO_IN(synth);
