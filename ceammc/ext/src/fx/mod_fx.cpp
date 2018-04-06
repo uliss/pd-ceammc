e3dfdbb1e6fb1c66cc2480a1744a96f263fdcba6
Serge Poltavski <serge.poltavski@gmail.com>
2018-04-07 01:24:03 +0300

    fx.echo~ added

--- ceammc/ext/src/fx/mod_fx.cpp
+++ ceammc/ext/src/fx/mod_fx.cpp
@@ -2,29 +2,31 @@
 #include "fx_looper.h"
 
 extern "C" void setup_fx0x2edistortion_tilde();
+extern "C" void setup_fx0x2edrone_box_tilde();
+extern "C" void setup_fx0x2eecho_tilde();
 extern "C" void setup_fx0x2eflanger_tilde();
 extern "C" void setup_fx0x2efreeverb2_tilde();
 extern "C" void setup_fx0x2efreeverb_tilde();
 extern "C" void setup_fx0x2efreqshift_tilde();
-extern "C" void setup_fx0x2epitchshift_tilde();
-extern "C" void setup_fx0x2ezita_rev1_tilde();
-extern "C" void setup_fx0x2egreyhole_tilde();
 extern "C" void setup_fx0x2egranulator_tilde();
-extern "C" void setup_fx0x2edrone_box_tilde();
+extern "C" void setup_fx0x2egreyhole_tilde();
+extern "C" void setup_fx0x2epitchshift_tilde();
 extern "C" void setup_fx0x2ewahwah_tilde();
+extern "C" void setup_fx0x2ezita_rev1_tilde();
 
 void ceammc_fx_setup()
 {
     setup_fx0x2edistortion_tilde();
+    setup_fx0x2edrone_box_tilde();
+    setup_fx0x2eecho_tilde();
     setup_fx0x2eflanger_tilde();
     setup_fx0x2efreeverb2_tilde();
     setup_fx0x2efreeverb_tilde();
     setup_fx0x2efreqshift_tilde();
-    setup_fx0x2epitchshift_tilde();
-    setup_fx0x2ezita_rev1_tilde();
-    setup_fx0x2egreyhole_tilde();
     setup_fx0x2egranulator_tilde();
-    setup_fx0x2edrone_box_tilde();
+    setup_fx0x2egreyhole_tilde();
+    setup_fx0x2epitchshift_tilde();
     setup_fx0x2ewahwah_tilde();
+    setup_fx0x2ezita_rev1_tilde();
     setup_fx_looper();
 }
