ae1d47c0e10aa3fde0160f46e6b03a772837f54f
Serge Poltavski <serge.poltavski@gmail.com>
2018-04-06 19:26:31 +0300

    risset arpeggio added

--- ceammc/ext/src/synth/mod_synth.cpp
+++ ceammc/ext/src/synth/mod_synth.cpp
@@ -1,9 +1,11 @@
 #include "mod_synth.h"
 
 extern "C" void setup_synth0x2erisset_tone_tilde();
+extern "C" void setup_synth0x2erisset_arp_tilde();
 extern "C" void setup_synth0x2etambura_tilde();
 
 void ceammc_synth_setup()
 {
     setup_synth0x2erisset_tone_tilde();
+    setup_synth0x2erisset_arp_tilde();
 }
