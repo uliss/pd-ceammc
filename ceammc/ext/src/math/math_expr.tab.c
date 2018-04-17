c4e874fa6abe09608b4de1c382100018c808f4f8
Serge Poltavski <serge.poltavski@gmail.com>
2018-04-17 17:37:06 +0300

    math.expr cleanup

--- ceammc/ext/src/math/math_expr.tab.c
+++ ceammc/ext/src/math/math_expr.tab.c
@@ -1671,8 +1671,6 @@ void math_expr_error(Ast* tree, const char* s)
 
 int math_expr_parse_ast(Ast* tree, const char* s)
 {
-    post("parse: %s", s);
-
     YY_BUFFER_STATE b;
     b = math_expr__scan_string(s);
 
