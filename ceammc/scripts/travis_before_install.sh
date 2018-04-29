b306d1aa8e5e724c3208cb80c83cc59075e9c84f
Serge Poltavski <serge.poltavski@gmail.com>
2018-04-29 14:49:48 +0300

    travis update

--- ceammc/scripts/travis_before_install.sh
+++ ceammc/scripts/travis_before_install.sh
@@ -4,7 +4,7 @@ sudo apt-get install -qq libboost-dev libsndfile1 libsndfile-dev\
 	libportaudio-dev \
 	libfftw3-dev libasound2-dev libjack-dev \
     libmodplug-dev libavahi-compat-libdnssd-dev \
-    libfluidsynth-dev
+    libglib2.0-dev
 
 if [[ ${CEAMMC_COVERAGE} == "ON" ]]
 then
