857cdc3e830b59c4fd39738a43d7d607db7afeb0
Serge Poltavski <serge.poltavski@gmail.com>
2018-04-29 03:06:50 +0300

    travis fluidsynth added

--- ceammc/scripts/travis_before_install.sh
+++ ceammc/scripts/travis_before_install.sh
@@ -3,7 +3,8 @@
 sudo apt-get install -qq libboost-dev libsndfile1 libsndfile-dev\
 	libportaudio-dev \
 	libfftw3-dev libasound2-dev libjack-dev \
-	libmodplug-dev libavahi-compat-libdnssd-dev
+    libmodplug-dev libavahi-compat-libdnssd-dev \
+    libfluidsynth-dev
 
 if [[ ${CEAMMC_COVERAGE} == "ON" ]]
 then
