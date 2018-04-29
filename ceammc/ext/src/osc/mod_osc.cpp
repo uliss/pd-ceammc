bae61afc6dda8e36caa5afc5c2c6b766a8c45c77
Serge Poltavsky <serge.poltavski@gmail.com>
2018-04-29 16:53:01 +0300

    linux: fix osc.*

--- ceammc/ext/src/osc/mod_osc.cpp
+++ ceammc/ext/src/osc/mod_osc.cpp
@@ -8,10 +8,10 @@ extern "C" void setup_osc0x2esquare_tilde();
 extern "C" void setup_osc0x2etri_tilde();
 
 void ceammc_osc_setup() {
+     setup_osc0x2etri_tilde();
      setup_osc0x2eimpulse_tilde();
      setup_osc0x2epulse_tilde();
      setup_osc0x2esaw_tilde();
      setup_osc0x2esinfb_tilde();
      setup_osc0x2esquare_tilde();
-     setup_osc0x2etri_tilde();
 }
