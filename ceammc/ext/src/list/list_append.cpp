1457fa399bd2b0ba1ac07ca26405f1ef5963770d
Serge Poltavski <serge.poltavski@gmail.com>
2018-05-10 16:18:49 +0300

    list.append added

---
+++ ceammc/ext/src/list/list_append.cpp
@@ -0,0 +1,68 @@
+/*****************************************************************************
+ * Copyright 2018 Serge Poltavsky. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#include "list_append.h"
+#include "ceammc_factory.h"
+
+ListAppend::ListAppend(const PdArgs& args)
+    : BaseObject(args)
+    , lst_(args.args)
+{
+    createInlet();
+    createOutlet();
+}
+
+void ListAppend::parseProperties()
+{
+    // empty
+}
+
+void ListAppend::onBang()
+{
+    // bang processed as empty list
+    listTo(0, lst_.toList());
+}
+
+void ListAppend::onFloat(t_float f)
+{
+    onList(AtomList(f));
+}
+
+void ListAppend::onSymbol(t_symbol* s)
+{
+    onList(AtomList(s));
+}
+
+void ListAppend::onList(const AtomList& lst)
+{
+    AtomList res(lst);
+    res.reserve(lst.size() + lst_.size());
+    res.append(lst_.toList());
+    listTo(0, res);
+}
+
+void ListAppend::onData(const DataPtr& d)
+{
+    onList(AtomList(d.asAtom()));
+}
+
+void ListAppend::onInlet(size_t n, const AtomList& lst)
+{
+    lst_.set(lst);
+}
+
+void setup_list_append()
+{
+    ObjectFactory<ListAppend> obj("list.append");
+    obj.processData();
+}
---
+++ ceammc/ext/tests/test_ext_list_append.cpp
@@ -0,0 +1,95 @@
+/*****************************************************************************
+ * Copyright 2017 Serge Poltavsky. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#include "../list/list_append.h"
+#include "base_extension_test.h"
+#include "catch.hpp"
+
+#include <stdio.h>
+
+typedef TestExtension<ListAppend> ListAppendTest;
+
+TEST_CASE("list.append", "[externals]")
+{
+    SECTION("create")
+    {
+        ListAppendTest t("list.append");
+        REQUIRE(t.numInlets() == 2);
+        REQUIRE(t.numOutlets() == 1);
+    }
+
+    SECTION("empty")
+    {
+        ListAppendTest t("list.append");
+
+        WHEN_SEND_BANG_TO(0, t);
+        REQUIRE_LIST_AT_OUTLET(0, t, AtomList());
+
+        WHEN_SEND_FLOAT_TO(0, t, 10);
+        REQUIRE_LIST_AT_OUTLET(0, t, L1(10));
+
+        WHEN_SEND_SYMBOL_TO(0, t, "ABC");
+        REQUIRE_LIST_AT_OUTLET(0, t, L1("ABC"));
+
+        DataPtr d0(new IntData(12));
+        WHEN_SEND_DATA_TO(0, t, d0);
+        REQUIRE_LIST_AT_OUTLET(0, t, L1(d0));
+
+        WHEN_SEND_LIST_TO(0, t, AtomList());
+        REQUIRE_LIST_AT_OUTLET(0, t, AtomList());
+
+        WHEN_SEND_LIST_TO(0, t, L2(1, 2));
+        REQUIRE_LIST_AT_OUTLET(0, t, L2(1, 2));
+    }
+
+    SECTION("args")
+    {
+        ListAppendTest t("list.append", L2("@prop", 1));
+
+        WHEN_SEND_BANG_TO(0, t);
+        REQUIRE_LIST_AT_OUTLET(0, t, L2("@prop", 1));
+
+        WHEN_SEND_FLOAT_TO(0, t, 10);
+        REQUIRE_LIST_AT_OUTLET(0, t, L3(10, "@prop", 1));
+
+        WHEN_SEND_SYMBOL_TO(0, t, "ABC");
+        REQUIRE_LIST_AT_OUTLET(0, t, L3("ABC", "@prop", 1));
+
+        DataPtr d0(new IntData(12));
+        WHEN_SEND_DATA_TO(0, t, d0);
+        REQUIRE_LIST_AT_OUTLET(0, t, L3(d0, "@prop", 1));
+
+        WHEN_SEND_LIST_TO(0, t, AtomList());
+        REQUIRE_LIST_AT_OUTLET(0, t, L2("@prop", 1));
+
+        WHEN_SEND_LIST_TO(0, t, L2(1, 2));
+        REQUIRE_LIST_AT_OUTLET(0, t, L4(1, 2, "@prop", 1));
+
+        WHEN_SEND_LIST_TO(1, t, L1(100));
+        WHEN_SEND_LIST_TO(0, t, L2(1, 2));
+        REQUIRE_LIST_AT_OUTLET(0, t, L3(1, 2, 100));
+    }
+
+    SECTION("data")
+    {
+        DataPtr d1(new IntData(10));
+        DataPtr d2(new IntData(20));
+        DataPtr d3(new IntData(30));
+
+        ListAppendTest t("list.append");
+
+        WHEN_SEND_LIST_TO(1, t, L2(d1, d2));
+        WHEN_SEND_DATA_TO(0, t, d3);
+        REQUIRE_LIST_AT_OUTLET(0, t, L3(d3, d1, d2));
+    }
+}
