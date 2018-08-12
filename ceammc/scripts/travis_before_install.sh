7538bf137556631defea0bce6e3084c605e8d4a0
Serge Poltavski <serge.poltavski@gmail.com>
2018-08-13 01:00:44 +0300

    travis gcc

--- ceammc/scripts/travis_before_install.sh
+++ ceammc/scripts/travis_before_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+
 sudo apt-get install -qq libboost-dev libsndfile1 libsndfile-dev\
 	libportaudio-dev \
 	libfftw3-dev libasound2-dev libjack-dev \
