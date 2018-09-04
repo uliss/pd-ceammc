26d2ceb0827257597b08dfe38e1d2199bdaf517f
Serge Poltavski <serge.poltavski@gmail.com>
2018-09-05 00:41:43 +0300

    cmake: ceammc_faust_gen_obj macro added
    for OOP FaustExternals

--- ceammc/ext/src/ceammc_pd.cmake
+++ ceammc/ext/src/ceammc_pd.cmake
@@ -39,11 +39,31 @@ macro(ceammc_faust_gen module name)
         COMMAND gsed -i 's/mydsp/${name}/g' ${CMAKE_CURRENT_SOURCE_DIR}/${module}_${name}.h)
 endmacro()
 
+# adds _underscored_ target MODULE_NAME
+macro(ceammc_faust_gen_obj module name)
+    # since faust 2.8.* mydsp global replacement removed
+    # so we are using GNU sed
+    add_custom_target("faust_${module}_${name}"
+        COMMAND ${FAUST_BIN} -i
+            -a ${CMAKE_SOURCE_DIR}/ceammc/faust/ceammc_dsp_ext.cpp
+            --class-name ${name}
+            "${CMAKE_SOURCE_DIR}/ceammc/faust/${module}_${name}.dsp"
+            -o ${CMAKE_CURRENT_SOURCE_DIR}/${module}_${name}.h
+        COMMAND gsed -i 's/mydsp/${name}/g' ${CMAKE_CURRENT_SOURCE_DIR}/${module}_${name}.h)
+endmacro()
+
 # adds target "faust_MODULE_NAME" for updating faust DSP extension.
 # adds extension target "MODULE_NAME~"
 # file named "MODULE_NAME.dsp" should exists in ceammc/faust directory
 macro(ceammc_faust_extension module name ext)
     ceammc_faust_gen(${module} ${name})
+    pd_add_external(NAME "${module}.${name}~"
+        FILES "${module}_${name}.cpp" INTERNAL TRUE LINK ceammc_core)
+    set_target_properties("${module}.${name}~" PROPERTIES COMPILE_FLAGS "-DFAUST_MACRO")
+endmacro()
+
+macro(ceammc_faust_dsp module name ext)
+    ceammc_faust_gen_obj(${module} ${name})
     pd_add_external(NAME "${module}.${name}~"
         FILES "${module}_${name}.cpp" INTERNAL TRUE LINK ceammc_core)
     set_target_properties("${module}.${name}~" PROPERTIES COMPILE_FLAGS "-DFAUST_MACRO")
