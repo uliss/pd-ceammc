57ad4549a148b679e15557a86a8c764f93156052
Serge Poltavski <serge.poltavski@gmail.com>
2018-09-06 02:28:34 +0300

    cmake: faust generation options added

--- ceammc/ext/src/ceammc_pd.cmake
+++ ceammc/ext/src/ceammc_pd.cmake
@@ -30,10 +30,26 @@ endmacro()
 macro(ceammc_faust_gen module name)
     # since faust 2.8.* mydsp global replacement removed
     # so we are using GNU sed
+    set(options JSON VEC FTZ)
+    cmake_parse_arguments(FAUST_OPT "${options}" "" "" ${ARGN})
+
+    set(_args "")
+    if(FAUST_OPT_VEC)
+        list(APPEND _args "-vec" "-vs" "64")
+    endif()
+
+    if(FAUST_OPT_FTZ)
+        list(APPEND _args "-ftz" "2")
+    endif()
+
+    if(FAUST_OPT_JSON)
+        list(APPEND _args "-json")
+    endif()
+
     add_custom_target("faust_${module}_${name}"
         COMMAND ${FAUST_BIN} -i
             -a ${CMAKE_SOURCE_DIR}/ceammc/faust/simple_pd_control_ext.cpp
-            --class-name ${name}
+            --class-name ${name} ${_args}
             "${CMAKE_SOURCE_DIR}/ceammc/faust/${module}_${name}.dsp"
             -o ${CMAKE_CURRENT_SOURCE_DIR}/${module}_${name}.h
         COMMAND gsed -i 's/mydsp/${name}/g' ${CMAKE_CURRENT_SOURCE_DIR}/${module}_${name}.h)
@@ -43,10 +59,26 @@ endmacro()
 macro(ceammc_faust_gen_obj module name)
     # since faust 2.8.* mydsp global replacement removed
     # so we are using GNU sed
+    set(options JSON VEC FTZ)
+    cmake_parse_arguments(FAUST_OPT "${options}" "" "" ${ARGN})
+
+    set(_args "")
+    if(FAUST_OPT_VEC)
+        list(APPEND _args "-vec" "-vs" "64")
+    endif()
+
+    if(FAUST_OPT_FTZ)
+        list(APPEND _args "-ftz" "2")
+    endif()
+
+    if(FAUST_OPT_JSON)
+        list(APPEND _args "-json")
+    endif()
+
     add_custom_target("faust_${module}_${name}"
         COMMAND ${FAUST_BIN} -i
             -a ${CMAKE_SOURCE_DIR}/ceammc/faust/ceammc_dsp_ext.cpp
-            --class-name ${name}
+            --class-name ${name} ${_args}
             "${CMAKE_SOURCE_DIR}/ceammc/faust/${module}_${name}.dsp"
             -o ${CMAKE_CURRENT_SOURCE_DIR}/${module}_${name}.h
         COMMAND gsed -i 's/mydsp/${name}/g' ${CMAKE_CURRENT_SOURCE_DIR}/${module}_${name}.h)
@@ -62,9 +94,9 @@ macro(ceammc_faust_extension module name ext)
     set_target_properties("${module}.${name}~" PROPERTIES COMPILE_FLAGS "-DFAUST_MACRO")
 endmacro()
 
-macro(ceammc_faust_dsp module name ext)
+function(ceammc_faust_dsp module name ext)
     ceammc_faust_gen_obj(${module} ${name})
     pd_add_external(NAME "${module}.${name}~"
         FILES "${module}_${name}.cpp" INTERNAL TRUE LINK ceammc_core)
     set_target_properties("${module}.${name}~" PROPERTIES COMPILE_FLAGS "-DFAUST_MACRO")
-endmacro()
+endfunction()
