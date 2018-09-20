23187d1ae0e21e0c0db7be163793702320eff3c5
Serge Poltavski <serge.poltavski@gmail.com>
2018-09-20 14:50:15 +0300

    list al documented externals script added

---
+++ ceammc/distrib/list_all_doc_externals.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IFS=' '
+all=$(xmllint --xpath '//entry[@name]/@name'  @PROJECT_SOURCE_DIR@/ceammc/ext/doc/ceammc_lib.xml)
+
+for x in $all
+do
+    echo $x | cut -d= -f2 | tr -d '"'
+done
+
