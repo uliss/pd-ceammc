732dae44aaefce42427c421818a4c0799cd09f4c
Serge Poltavski <serge.poltavski@gmail.com>
2018-09-22 00:08:14 +0300

    osc.pulse~ faust update

--- ceammc/ext/src/osc/mod_osc.cpp
+++ ceammc/ext/src/osc/mod_osc.cpp
@@ -1,9 +1,9 @@
 #include "mod_osc.h"
 
 extern "C" void setup_osc0x2eimpulse_tilde();
-extern "C" void setup_osc0x2epulse_tilde();
 
 extern void setup_osc_blit();
+extern void setup_osc_pulse_tilde();
 extern void setup_osc_saw_tilde();
 extern void setup_osc_saw4_tilde();
 extern void setup_osc_sinfb_tilde();
@@ -13,8 +13,8 @@ extern void setup_osc_tri_tilde();
 void ceammc_osc_setup()
 {
     setup_osc0x2eimpulse_tilde();
-    setup_osc0x2epulse_tilde();
 
+    setup_osc_pulse_tilde();
     setup_osc_saw_tilde();
     setup_osc_saw4_tilde();
     setup_osc_sinfb_tilde();
