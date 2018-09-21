bc155da612a6c694fdba99b19ef51899d3195e6d
Serge Poltavski <serge.poltavski@gmail.com>
2018-09-21 19:29:00 +0300

    appveyor: package fixes

--- ceammc/distrib/appveyor/install.bat
+++ ceammc/distrib/appveyor/install.bat
@@ -21,9 +21,9 @@ bash -lc "pacman -Syu --noconfirm"
 REM build tools
 bash -lc "pacman -S --needed --noconfirm mingw-w64-${MSYS2_ARCH}-toolchain make patch"
 REM dependencies
-bash -lc "pacman -S --needed --noconfirm mingw-w64-${MSYS2_ARCH}-{cmake,glib2,libmodplug,tcllib,tklib,portaudio,fftw,libsndfile,boost}"
+bash -lc "pacman -S --needed --noconfirm mingw-w64-${MSYS2_ARCH}-{cmake,glib2,libmodplug,tcllib,tcl,tk,tklib,portaudio,fftw,libsndfile,boost}"
 
 cd %APPVEYOR_BUILD_FOLDER%
-bash -lc "git submodule foreach --recursive 'git rev-parse HEAD | xargs -I {} git fetch origin {} && git reset --hard FETCH_HEAD'"
+bash -lc "cd ${APPVEYOR_BUILD_FOLDER} && git submodule foreach --recursive 'git rev-parse HEAD | xargs -I {} git fetch origin {} && git reset --hard FETCH_HEAD'"
 bash -lc "cd ${APPVEYOR_BUILD_FOLDER}/build && cmake -G 'MSYS Makefiles' -DCMAKE_BUILD_TYPE=Release -DWITH_FFTW=ON .."
 
