360e77aadce5f532caa66389768f6ec00085f9b5
Serge Poltavski <serge.poltavski@gmail.com>
2018-09-21 19:05:48 +0300

    appveyor: glib2 fix

--- ceammc/distrib/appveyor/install.bat
+++ ceammc/distrib/appveyor/install.bat
@@ -13,7 +13,6 @@ mkdir %APPVEYOR_BUILD_FOLDER%\build
 
 @echo on
 set "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
-echo Path: %PATH%
 bash -lc "pacman -S --needed --noconfirm pacman-mirrors"
 bash -lc "pacman -S --needed --noconfirm git"
 REM Update
@@ -22,7 +21,7 @@ bash -lc "pacman -Syu --noconfirm"
 REM build tools
 bash -lc "pacman -S --needed --noconfirm mingw-w64-${MSYS2_ARCH}-toolchain make patch mingw-w64-${MSYS2_ARCH}-libtool"
 REM dependencies
-bash -lc "pacman -S --needed --noconfirm mingw-w64-${MSYS2_ARCH}-{cmake,glib,libmodplug,tcllib,tklib,portaudio,fftw}"
+bash -lc "pacman -S --needed --noconfirm mingw-w64-${MSYS2_ARCH}-{cmake,glib2,libmodplug,tcllib,tklib,portaudio,fftw}"
 
 bash -lc "cd ${APPVEYOR_BUILD_FOLDER}/build && cmake -G 'MSYS Makefiles' -DCMAKE_BUILD_TYPE=Release -DWITH_FFTW=ON .."
 
