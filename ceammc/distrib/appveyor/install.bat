c34db9c1fe2d7574ae2ba1b290db207574f46e35
Serge Poltavski <serge.poltavski@gmail.com>
2018-09-21 17:43:36 +0300

    appveyor

--- ceammc/distrib/appveyor/install.bat
+++ ceammc/distrib/appveyor/install.bat
@@ -1,5 +1,18 @@
 @echo off
 
 cd %APPVEYOR_BUILD_FOLDER%
+
+echo Compiler: %COMPILER%
+echo Architecture: %MSYS2_ARCH%
+echo Platform: %PLATFORM%
+echo MSYS2 directory: %MSYS2_DIR%
+echo MSYS2 system: %MSYSTEM%
+echo Bits: %BIT%
+
+mkdir %APPVEYOR_BUILD_FOLDER%\build
+set TMPDIR=%APPVEYOR_BUILD_FOLDER%\build
+
 dir
+
+\msys64\mingw32.exe
 
