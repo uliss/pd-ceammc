b4b4a28e1e3e00fa9dd051750eb980c7a3ee83c3
Serge Poltavski <serge.poltavski@gmail.com>
2018-09-22 20:21:36 +0300

    init asr

--- ceammc/ext/src/env/env_ar.cpp
+++ ceammc/ext/src/env/env_ar.cpp
@@ -3,17 +3,42 @@
 #include "ceammc_factory.h"
 #include "datatype_env.h"
 
-#include "env_faust_base.h"
+static t_symbol* SYM_PROP_ATTACK = gensym("@attack");
+static t_symbol* SYM_PROP_RELEASE = gensym("@release");
+static t_symbol* SYM_PROP_GATE = gensym("@gate");
 
 using namespace ceammc;
 
-class EnvAr : public EnvFaustBase<faust_env_ar_tilde> {
+class EnvAr : public faust_env_ar_tilde {
+    ClockMemberFunction<EnvAr> auto_release_;
+    ClockMemberFunction<EnvAr> done_;
+
+    UIProperty* prop_attack_;
+    UIProperty* prop_release_;
+    UIProperty* prop_gate_;
+
 public:
     EnvAr(const PdArgs& args)
-        : EnvFaustBase<faust_env_ar_tilde>(args)
+        : faust_env_ar_tilde(args)
+        , auto_release_(this, &EnvAr::release)
+        , done_(this, &EnvAr::done)
+        , prop_attack_((UIProperty*)property(SYM_PROP_ATTACK))
+        , prop_release_((UIProperty*)property(SYM_PROP_RELEASE))
+        , prop_gate_((UIProperty*)property(SYM_PROP_GATE))
     {
         bindPositionalArgsToProps({ SYM_PROP_ATTACK, SYM_PROP_RELEASE });
         createProperty(new CombinedProperty("@ar", { property(SYM_PROP_ATTACK), property(SYM_PROP_RELEASE) }));
+        createCbProperty("@length", &EnvAr::propLength);
+
+        createOutlet();
+    }
+
+    void onBang() override
+    {
+        done_.unset();
+        prop_gate_->setValue(1);
+        auto_release_.delay(10);
+        done_.delay(length());
     }
 
     void onList(const AtomList& l) override
@@ -41,7 +66,33 @@ public:
             OBJ_ERR << "can't set envelope";
     }
 
+    void m_reset(t_symbol*, const AtomList&)
+    {
+        dsp_->instanceClear();
+        auto_release_.unset();
+    }
+
+    void m_click(t_symbol*, const AtomList& l)
+    {
+        onBang();
+    }
+
+    AtomList propLength() const
+    {
+        return Atom(length());
+    }
+
 private:
+    void release()
+    {
+        prop_gate_->setValue(0);
+    }
+
+    void done()
+    {
+        bangTo(1);
+    }
+
     bool checkValues(float a, float r)
     {
         return a >= 0 && r >= 0;
@@ -57,6 +108,11 @@ private:
         prop_attack_->setValue(a);
         prop_release_->setValue(r);
         return true;
+    }
+
+    float length() const
+    {
+        return prop_attack_->value() + prop_release_->value();
     }
 };
 
--- ceammc/ext/src/env/env_asr.cpp
+++ ceammc/ext/src/env/env_asr.cpp
@@ -1,14 +1,105 @@
 #include "env_asr.h"
+#include "ceammc_clock.h"
+#include "ceammc_factory.h"
+#include "datatype_env.h"
 
-EXTERNAL_NEW
+static t_symbol* SYM_PROP_ATTACK = gensym("@attack");
+static t_symbol* SYM_PROP_SUSTAIN = gensym("@sustain");
+static t_symbol* SYM_PROP_RELEASE = gensym("@release");
+static t_symbol* SYM_PROP_GATE = gensym("@gate");
+
+using namespace ceammc;
+
+class EnvAsr : public faust_env_asr_tilde {
+    ClockMemberFunction<EnvAsr> attack_done_;
+    ClockMemberFunction<EnvAsr> release_done_;
+
+    UIProperty* prop_attack_;
+    UIProperty* prop_sustain_;
+    UIProperty* prop_release_;
+    UIProperty* prop_gate_;
+
+public:
+    EnvAsr(const PdArgs& args)
+        : faust_env_asr_tilde(args)
+        , attack_done_(this, &EnvAsr::attackDone)
+        , release_done_(this, &EnvAsr::releaseDone)
+        , prop_attack_((UIProperty*)property(SYM_PROP_ATTACK))
+        , prop_sustain_((UIProperty*)property(SYM_PROP_SUSTAIN))
+        , prop_release_((UIProperty*)property(SYM_PROP_RELEASE))
+        , prop_gate_((UIProperty*)property(SYM_PROP_GATE))
+    {
+        bindPositionalArgsToProps({ SYM_PROP_ATTACK, SYM_PROP_SUSTAIN, SYM_PROP_RELEASE });
+        createProperty(new CombinedProperty("@asr",
+            { property(SYM_PROP_ATTACK), property(SYM_PROP_SUSTAIN), property(SYM_PROP_RELEASE) }));
+
+        createOutlet();
+    }
+
+    void onList(const AtomList& l) override
+    {
+        if (!checkArgs(l, ARG_FLOAT, ARG_FLOAT, ARG_FLOAT)) {
+            OBJ_ERR << "ATTACK SUSTAIN RELEASE values expected: " << l;
+            return;
+        }
+
+        if (!set(l[0].asFloat(), l[1].asFloat(), l[2].asFloat()))
+            OBJ_ERR << "can't set envelope";
+    }
+
+    void onDataT(const DataTypeEnv& env)
+    {
+        if (!env.isAR()) {
+            OBJ_ERR << "not an AR envelope: " << env;
+            return;
+        }
+
+        float attack = env.pointAt(1).timeMs() - env.pointAt(0).timeMs();
+        float sustain = env.pointAt(1).value;
+        float release = env.pointAt(2).timeMs() - env.pointAt(1).timeMs();
+
+        if (!set(attack, sustain, release))
+            OBJ_ERR << "can't set envelope: " << env;
+    }
+
+    void m_reset(t_symbol*, const AtomList&)
+    {
+        dsp_->instanceClear();
+        attack_done_.unset();
+        release_done_.unset();
+    }
+
+private:
+    void attackDone()
+    {
+    }
+
+    void releaseDone()
+    {
+    }
+
+    bool checkValues(float a, float s, float r)
+    {
+        return a >= 0 && r >= 0 && s >= 0 && s <= 100;
+    }
+
+    bool set(float a, float s, float r)
+    {
+        if (!checkValues(a, s, r)) {
+            OBJ_ERR << "invalid values: " << a << ", " << s << ", " << r;
+            return false;
+        }
+
+        prop_attack_->setValue(a);
+        prop_sustain_->setValue(s);
+        prop_release_->setValue(r);
+        return true;
+    }
+};
+
+void setup_env_asr_tilde()
 {
-    FAUST_EXT* x = reinterpret_cast<FAUST_EXT*>(pd_new(FAUST_EXT_CLASS));
-
-    PdArgParser p(x, argc, argv);
-    p.initFloatArg("attack", 1);
-    p.initFloatArg("sustain", 2);
-    p.initFloatArg("release", 3);
-    return p.pd_obj();
+    SoundExternalFactory<EnvAsr> obj("env.asr~");
+    obj.processData<DataTypeEnv>();
+    obj.addMethod("reset", &EnvAsr::m_reset);
 }
-
-EXTERNAL_SETUP(env)
