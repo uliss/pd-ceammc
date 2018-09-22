fc7d2aa6abcd844c3ff07c44b9847123c8830484
Serge Poltavski <serge.poltavski@gmail.com>
2018-09-22 03:25:57 +0300

    clip to pan position added

--- ceammc/ext/src/spat/pan_linsig.cpp
+++ ceammc/ext/src/spat/pan_linsig.cpp
@@ -1,6 +1,6 @@
+#include "pan_linsig.h"
 #include "ceammc_convert.h"
 #include "ceammc_factory.h"
-#include "pan_linsig.h"
 
 PanLinSig::PanLinSig(const PdArgs& args)
     : SoundExternal(args)
@@ -15,7 +15,7 @@ void PanLinSig::processBlock(const t_sample** in, t_sample** out)
     const size_t bs = blockSize();
 
     for (size_t i = 0; i < bs; i++) {
-        auto r = (in[1][i] + 1) * 0.5;
+        auto r = (clip<t_float>(in[1][i], -1, 1) + 1) * 0.5;
         auto l = 1 - r;
         auto v = in[0][i];
 
