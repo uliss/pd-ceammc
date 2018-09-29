24d276056097922c68854edc2a4b9e767bdaf77a
Serge Poltavski <serge.poltavski@gmail.com>
2018-09-30 01:08:29 +0300

    abstractions generation added

---
+++ ceammc/ext/abstractions/faust2ui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+UI_DIR="@FAUST_UI_DIR@"
+OUT_DIR="@ABSTRACTIONS_DIR@"
+CMD=pd_faust2ui
+
+cd "$OUT_DIR"
+
+ls -1 $UI_DIR/*.json | while read json
+do
+    echo "Generating UI for \"$(basename $json)\" ..."
+    $CMD "$json"
+done
