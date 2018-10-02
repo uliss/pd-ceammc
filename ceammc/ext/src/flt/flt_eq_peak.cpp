9b3a27a2026bcf38d5f1d013857a3a353f18465d
Serge Poltavski <serge.poltavski@gmail.com>
2018-10-03 01:47:04 +0300

    dead code removal

--- ceammc/ext/src/flt/flt_eq_peak.cpp
+++ ceammc/ext/src/flt/flt_eq_peak.cpp
@@ -21,14 +21,3 @@ void setup_flt_eq_peak_tilde()
     SoundExternalFactory<FxEqPeak> obj("flt.eq_peak~");
 }
 
-//EXTERNAL_NEW
-//{
-//    FAUST_EXT* x = reinterpret_cast<FAUST_EXT*>(pd_new(FAUST_EXT_CLASS));
-//    PdArgParser p(x, argc, argv);
-//    p.initFloatArg("freq", 1);
-//    p.initFloatArg("gain", 2);
-//    p.initFloatArg("bandwidth", 3);
-//    return p.pd_obj();
-//}
-
-//EXTERNAL_SETUP(flt);
