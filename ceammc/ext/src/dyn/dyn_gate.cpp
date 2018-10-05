eab2a8b294ca85a1e3db2837937275d169ac20ce
Serge Poltavski <serge.poltavski@gmail.com>
2018-10-05 16:07:08 +0300

    faust and ui: dyn.gate{1,2} update

--- ceammc/ext/src/dyn/dyn_gate.cpp
+++ ceammc/ext/src/dyn/dyn_gate.cpp
@@ -1,15 +1,22 @@
 #include "dyn_gate.h"
+#include "ceammc_factory.h"
+using namespace ceammc;
 
-EXTERNAL_NEW
+static t_symbol* SYM_PROP_THRESHOLD = gensym("@threshold");
+static t_symbol* SYM_PROP_ATTACK = gensym("@attack");
+static t_symbol* SYM_PROP_HOLD = gensym("@hold");
+static t_symbol* SYM_PROP_RELEASE = gensym("@release");
+
+class DynGate : public faust_dyn_gate_tilde {
+public:
+    DynGate(const PdArgs& args)
+        : faust_dyn_gate_tilde(args)
+    {
+        bindPositionalArgsToProps({ SYM_PROP_THRESHOLD, SYM_PROP_ATTACK, SYM_PROP_HOLD, SYM_PROP_RELEASE });
+    }
+};
+
+void setup_dyn_gate_tilde()
 {
-    FAUST_EXT* x = reinterpret_cast<FAUST_EXT*>(pd_new(FAUST_EXT_CLASS));
-
-    PdArgParser p(x, argc, argv);
-    p.initFloatArg("threshold", 1);
-    p.initFloatArg("attack", 2);
-    p.initFloatArg("hold", 3);
-    p.initFloatArg("release", 4);
-    return p.pd_obj();
+    SoundExternalFactory<DynGate> obj("dyn.gate~");
 }
-
-EXTERNAL_SETUP(dyn);
--- ceammc/faust/dyn_gate.dsp
+++ ceammc/faust/dyn_gate.dsp
@@ -1,9 +1,11 @@
+declare name "dyn.gate";
+
 ef = library("misceffects.lib");
 import("ceammc.lib");
 
-thresh = vslider("threshold", 40, 0, 100, 0.1) : db_pd2faust : si.smoo;
-atk    = vslider("attack", 0.1, 0, 500, 0.1) : time_pd2faust;
-hold   = vslider("hold", 100, 1, 500, 0.1) : time_pd2faust;
-rel    = vslider("release", 20, 1, 500, 0.1) : time_pd2faust;
+thresh = vslider("threshold [unit: db]", 40, 0, 100, 0.1) : db_pd2faust : si.smoo;
+atk    = vslider("attack [unit:ms]", 0.1, 0, 500, 0.1) : time_pd2faust;
+hold   = vslider("hold [unit:ms]", 100, 1, 500, 0.1) : time_pd2faust;
+rel    = vslider("release [unit:ms]", 20, 1, 500, 0.1) : time_pd2faust;
 
 process = ef.gate_mono(thresh, atk, hold, rel);
