3f0326542e2018c61406be4b1c672d6b7eaa5d56
Serge Poltavski <serge.poltavski@gmail.com>
2018-10-05 15:58:04 +0300

    faust and ui update: dyn.comp2~

--- ceammc/ext/src/dyn/mod_dyn.cpp
+++ ceammc/ext/src/dyn/mod_dyn.cpp
@@ -6,12 +6,11 @@ extern "C" void setup_dyn0x2elimit2_tilde();
 extern "C" void setup_dyn0x2elimit_tilde();
 extern "C" void setup_dyn0x2esoftclip_tilde();
 
-extern "C" void setup_dyn0x2ecomp2_tilde();
+extern void setup_dyn_comp2_tilde();
 extern void setup_dyn_comp_tilde();
 
 void ceammc_dyn_setup()
 {
-    setup_dyn0x2ecomp2_tilde();
     setup_dyn0x2egate2_tilde();
     setup_dyn0x2egate_tilde();
     setup_dyn0x2elimit2_tilde();
@@ -19,4 +18,5 @@ void ceammc_dyn_setup()
     setup_dyn0x2esoftclip_tilde();
 
     setup_dyn_comp_tilde();
+    setup_dyn_comp2_tilde();
 }
