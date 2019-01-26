33a1973d43d75586c86f421035444b8cb324b6da
Serge Poltavski <serge.poltavski@gmail.com>
2019-01-26 03:36:24 +0300

    tuple init fix

--- ceammc/ext/tests/test_wrapper_tuple.cpp
+++ ceammc/ext/tests/test_wrapper_tuple.cpp
@@ -166,9 +166,9 @@ TEST_CASE("wrapper template", "[class-wrapper]")
 
             std::tuple<float, float, float, float> t(1, 2, 3, 4);
             REQUIRE(find_first(t, Less3()) == 0);
-            t = { 4, 3, 2, 1 };
+            t = std::make_tuple(4.f, 3.f, 2.f, 1.f);
             REQUIRE(find_first(t, Less3()) == 2);
-            t = { 3, 4, 5, 6 };
+            t = std::make_tuple(3.f, 4.f, 5.f, 6.f);
             REQUIRE(find_first(t, Less3()) == -1);
         }
 
