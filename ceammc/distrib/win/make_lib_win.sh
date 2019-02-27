6a14fc25931fdaa6e899c6aefc90c5225dd98f3d
Serge Poltavski <serge.poltavski@gmail.com>
2019-02-28 00:00:29 +0300

    change dll name on windows
    libpuredata-core.dll renamed to pd.dll for using with vanilla versions

--- ceammc/distrib/win/make_lib_win.sh
+++ ceammc/distrib/win/make_lib_win.sh
@@ -33,27 +33,25 @@ echo "Making CEAMMC library from build directory: $BINDIR"
 mkdir -p "${OUTDIR}"
 rm -f "${OUTDIR}/*"
 
-echo "Copying libraries to ${OUTDIR} ..."
-find "${BINDIR}" -name *.dll -print0 | while read -r -d '' file
+echo "Copying externals to ${OUTDIR} ..."
+find "${BINDIR}/extra/ceammc" -name *.dll -print0 | while read -r -d '' file
 do
     cp "$file" "${OUTDIR}"
-    echo "+ Lib:  $(basename $file)"
+    echo "+ Ext:  $(basename $file)"
 done
 
-
-echo "Copying [system.serial] extension files to ${OUTDIR} ..."
-find "${BINDIR}/../extra/comport" -name *.dll -print0 | while read -r -d '' file
+echo "Copying Dll's to ${OUTDIR} ..."
+find "${BINDIR}/bin" -name lib*.dll -print0 | while read -r -d '' file
 do
-    ext_name=$(basename $file)
-    skip_ext $file
-    if [ $? -eq 1 ]
-    then
-        echo "- Skip: '$ext_name'"
-        continue
-    fi
+    cp "$file" "${OUTDIR}"
+    echo "+ Dll:  $(basename $file)"
+done
 
-    cp "$file" "${OUTDIR}/${ext_name}"
-    echo "+ Copy: '$ext_name'"
+echo "Copying TCL files to ${OUTDIR} ..."
+find "${SRCDIR}/extra/hcs" -name *\\.tcl | while read file
+do
+    cp "$file" "${OUTDIR}"
+    echo "+ Tcl:  $(basename $file)"
 done
 
 echo "Copying help files to ${OUTDIR} ..."
@@ -72,8 +70,12 @@ cp "${SRCDIR}/ext/doc/stargazing.mod" "${OUTDIR}"
 echo "    prs.txt"
 cp "${SRCDIR}/ext/doc/prs.txt" "${OUTDIR}"
 
-echo "+ Fix soundtouch link in index-help.pd..."
-sed -i "" 's/ceammc\/soundtouch-help\.pd/soundtouch-help.pd/' "${OUTDIR}/index-help.pd"
+
+echo "Copying Soundtouch files to ${OUTDIR} ..."
+cp "${SRCDIR}/extra/SoundTouch/pd/soundtouch-help.pd" "${OUTDIR}"
+cp "${SRCDIR}/extra/SoundTouch/pd/soundtouch~-help.pd" "${OUTDIR}"
+cat "${OUTDIR}/index-help.pd" | sed 's/ceammc\/soundtouch-help\.pd/soundtouch-help.pd/' > tmp
+mv tmp "${OUTDIR}/index-help.pd"
 
 cd "$3"
 7z a "${OUTFILE}" $(basename $OUTDIR)
