7e556b3cc030ea16efc869ce3f8e0b596ff06e9a
Serge Poltavski <serge.poltavski@gmail.com>
2019-03-06 00:28:07 +0300

    style added

---
+++ src/g_style.c
@@ -0,0 +1,53 @@
+/*****************************************************************************
+ * Copyright 2019 Serge Poltavsky. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#include "g_style.h"
+
+#include "g_all_guis.h"
+#include "g_canvas.h"
+#include "m_imp.h"
+
+#define STYLE_CORD_AUDIO_WIDTH 2
+#define STYLE_CORD_CONTROL_WIDTH 1
+
+int style_cord_width(t_glist* x, t_object* ob, int nout)
+{
+    return (obj_issignaloutlet(ob, nout) ? STYLE_CORD_AUDIO_WIDTH : STYLE_CORD_CONTROL_WIDTH) * x->gl_zoom;
+}
+
+int style_border_width(t_glist* glist)
+{
+    return glist->gl_zoom;
+    //    return STYLE_BORDER_WIDTH;
+}
+
+int style_xlet_height(t_glist* glist)
+{
+    //    return 1 - 2*glist->gl_zoom;
+    return 1 + 1; // EXTRAPIX;
+}
+
+int style_cord_color()
+{
+    return STYLE_CORD_NORMAL;
+}
+
+int style_cord_select_color()
+{
+    return STYLE_SELECT_COLOR;
+}
+
+int style_cord_width_by_type(int issignal)
+{
+    return issignal ? STYLE_CORD_AUDIO_WIDTH : STYLE_CORD_CONTROL_WIDTH;
+}
--- src/g_style.h
+++ src/g_style.h
@@ -2,44 +2,36 @@
 #define G_STYLE_H
 
 #include "m_pd.h"
-#include "m_imp.h"
 
-#define STYLE_NORMAL_COLOR  "#444444"
-#define STYLE_SELECT_COLOR  "#1441E6"
-#define STYLE_EDIT_COLOR    "#F62459"
+#define STYLE_NORMAL_COLOR 0x444444
+#define STYLE_SELECT_COLOR 0x1441E6
+#define STYLE_EDIT_COLOR 0xF62459
 
 #define STYLE_BORDER_WIDTH 1
 #define STYLE_BORDER_WIDTH_CLICKED 3
 #define STYLE_SLIDER_KNOB_WIDTH 3
 #define STYLE_XLET_WIDTH 7
 
-#define STYLE_CONTROL_XLET_COLOR "#000000"
-#define STYLE_AUDIO_XLET_COLOR "#2200AA"
-#define STYLE_BORDER_COLOR "#BFBFBF"
-#define STYLE_BORDER_COLOR_CLICKED "#00AAFF"
-#define STYLE_FILL_COLOR  "#F5F8F8"
+#define STYLE_CONTROL_XLET_COLOR 0x000000
+#define STYLE_AUDIO_XLET_COLOR 0x2200AA
+#define STYLE_BORDER_COLOR 0xBFBFBF
+#define STYLE_BORDER_COLOR_CLICKED 0x00AAFF
+#define STYLE_FILL_COLOR 0xF5F8F8
 #define STYLE_TEXT_NORMAL_COLOR STYLE_NORMAL_COLOR
 #define STYLE_TEXT_SELECT_COLOR STYLE_SELECT_COLOR
-
-#define STYLE_IEM_BORDER_COLOR "#000000"
+#define STYLE_IEM_BORDER_COLOR 0x000000
 
 #define STYLE_CORD_SELECTED STYLE_SELECT_COLOR
-#define STYLE_CORD_NORMAL   STYLE_NORMAL_COLOR
+#define STYLE_CORD_NORMAL STYLE_NORMAL_COLOR
 
-#define STYLE_CANVAS_GOP_RECT_COLOR "#ff8080"
+#define STYLE_CANVAS_GOP_RECT_COLOR 0xFF8080
 
-static inline int style_cord_width(t_canvas* x, t_object* ob, int closest) {
-    return (obj_issignaloutlet(ob, closest) ? 2 : 1) * x->gl_zoom;
-}
+int style_cord_width_by_type(int issignal);
+int style_cord_width(t_canvas* x, t_object* ob, int nout);
+int style_cord_color();
+int style_cord_select_color();
 
-static inline int style_border_width(t_glist * glist) {
-//    glist->gl_zoom
-    return STYLE_BORDER_WIDTH;
-}
-
-static inline int style_xlet_height(t_glist * glist) {
-//    return 1 - 2*glist->gl_zoom;
-    return 1 + 1 ;// EXTRAPIX;
-}
+int style_border_width(t_glist* glist);
+int style_xlet_height(t_glist* glist);
 
 #endif // G_STYLE_H
