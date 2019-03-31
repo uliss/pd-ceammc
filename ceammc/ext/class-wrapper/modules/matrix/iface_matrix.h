eab805023ed8dd19cc8357da9c02b03ad98f240b
Serge Poltavski <serge.poltavski@gmail.com>
2019-03-31 17:30:39 +0300

    matrix.conv2 doc update

--- ceammc/ext/class-wrapper/modules/matrix/iface_matrix.h
+++ ceammc/ext/class-wrapper/modules/matrix/iface_matrix.h
@@ -746,17 +746,17 @@ public:
      * |
      * [get list(
      * |
-     * [ui.matrix @size=100x100 @cols=10 @rows=10 #d]
+     * [ui.matrix @size=101x101 @cols=16 @rows=16 #d]
      * |
      * |
      * |
      * |                    [loadbang]
      * |                    |
-     * |                    [10 10(
+     * |                    [16 16(
      * |                    |
      * [route list]         [matrix.randu]
      * |                    |
-     * [msg 10 10]          [matrix.> 0.6]
+     * [msg 16 16]          [matrix.> 0.6]
      * |                    |
      * [matrix.new        #a]
      * |
