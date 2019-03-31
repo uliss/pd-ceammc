09fd698e0bed4a91292b323b4aee8538bf0167e6
Serge Poltavski <serge.poltavski@gmail.com>
2019-04-01 01:37:34 +0300

    struct size assertion added

--- ceammc/ext/src/lib/cicm/Sources/ecommon.cpp
+++ ceammc/ext/src/lib/cicm/Sources/ecommon.cpp
@@ -120,19 +120,7 @@ void epd_init(void)
         sys_gui("append col [format {%4.4x} $nB]\n");
         sys_gui("return #$col\n");
         sys_gui("}\n");
-        
-        // DRAW CUBIC //
-        /*
-        sys_gui("proc eobj_bezier_fill{id points } {\n");
-        sys_gui("set size [expr {[llength $points] / 2}]\n");
-        sys_gui("for {set i 0} {$i < $size} {set i [expr {$i + 3}]} {\n");
-        sys_gui("for {set j 0} {$j < 20} {incr j} {\n");
-        
-        sys_gui(" if {$filled} {$id create line $x1 $y1}");
-        sys_gui(" else {$id create polygon $x1 $y1}");
-        sys_gui("}\n");
-        */
-        
+
         epd_symbol->s_thing = (t_class **)1;
     }
 }
