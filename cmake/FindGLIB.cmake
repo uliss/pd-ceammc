5dc9871a4b03efa889e7d762807afa8abd596f88
Serge Poltavski <serge.poltavski@gmail.com>
2019-04-22 22:17:47 +0300

    win32: FindGlib2 hell

--- cmake/FindGLIB.cmake
+++ cmake/FindGLIB.cmake
@@ -38,7 +38,7 @@ if(GLIB_ROOT)
         PATH_SUFFIXES include
         NO_DEFAULT_PATH)
 
-    find_library(GLIB_LIBRARIES2
+    find_library(GLIB2_LIBRARIES
         NAMES glib-2.0
         PATHS ${GLIB_ROOT}
         PATH_SUFFIXES lib
@@ -67,88 +67,107 @@ if(GLIB_ROOT)
     )
 
 else() # search via pkg-config
-
     find_package(PkgConfig QUIET)
 
     if(PKG_CONFIG_FOUND)
-        pkg_check_modules(PKGCONFIG_GLIB glib-2.0)
+        pkg_check_modules(PKGCONFIG_GLIB glib-2.0 QUIET)
+        pkg_check_modules(PKGCONFIG_GTHREAD gthread-2.0 QUIET)
     endif()
 
-    set(_include_paths
-        ${INCLUDE_INSTALL_DIR}
-        /usr/include
-        /opt/local/include
-        /sw/include
-        /usr/local/mingw/i686-w64-mingw32/include)
+    if(PKGCONFIG_GLIB_FOUND AND PKGCONFIG_GTHREAD_FOUND)
+        set(GLIB_CONFIG_INCLUDE_DIR ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_INCLUDE_DIRS})
 
-    set(_lib_paths
-        ${LIB_INSTALL_DIR}
-        /usr/lib
-        /usr/local/lib
-        /opt/local/lib
-        /sw/lib
-        /usr/local/mingw/i686-w64-mingw32/lib)
+        set(GLIB_INCLUDE_DIR ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_INCLUDE_DIRS})
+        set(GLIB2_LIBRARIES ${PKGCONFIG_GLIB_LIBRARIES})
+        set(GLIB2_LIBRARY_DIRS ${PKGCONFIG_GLIB_LIBRARY_DIRS})
 
-    find_path(GLIB_INCLUDE_DIR
-        NAMES glib.h
-        HINTS ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_INCLUDE_DIRS}
-        PATHS ${_include_paths} ${_lib_paths}
-        PATH_SUFFIXES glib-2.0 glib-2.0/include glib-2.0/include/glib include/glib-2.0
-        NO_DEFAULT_PATH)
+        set(GTHREAD_INCLUDE_DIR ${PKGCONFIG_GTHREAD_INCLUDEDIR} ${PKGCONFIG_GTHREAD_INCLUDE_DIRS})
+        set(GTHREAD_LIBRARIES ${PKGCONFIG_GTHREAD_LIBRARIES})
+        set(GTHREAD_LIBRARY_DIRS ${PKGCONFIG_GTHREAD_LIBRARY_DIRS})
+    else()
+        set(_include_paths
+            ${INCLUDE_INSTALL_DIR}
+            /usr/include
+            /opt/local/include
+            /sw/include
+            /usr/local/mingw/i686-w64-mingw32/include)
 
-    find_path(ICONV_INCLUDE_DIR
-        NAMES iconv.h
-        HINTS ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_INCLUDE_DIRS}
-        PATHS ${_include_paths} ${_lib_paths}
-        NO_DEFAULT_PATH)
+        set(_lib_paths
+            ${LIB_INSTALL_DIR}
+            /usr/lib
+            /usr/local/lib
+            /opt/local/lib
+            /sw/lib
+            /usr/local/mingw/i686-w64-mingw32/lib)
 
-    find_path(GLIB_CONFIG_INCLUDE_DIR
-        NAMES glibconfig.h
-        HINTS ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_INCLUDE_DIRS}
-        PATHS ${_include_paths} ${_lib_paths}
-        PATH_SUFFIXES glib-2.0/include
-        NO_DEFAULT_PATH)
+        find_path(GLIB_INCLUDE_DIR
+            NAMES glib.h
+            HINTS ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_INCLUDE_DIRS}
+            PATHS ${_include_paths} ${_lib_paths}
+            PATH_SUFFIXES glib-2.0 glib-2.0/include glib-2.0/include/glib include/glib-2.0
+            NO_DEFAULT_PATH)
 
-    # libs
-    find_library(GLIB_LIBRARIES2
-        NAMES glib-2.0
-        HINTS ${PKGCONFIG_GLIB_LIBDIR} ${PKGCONFIG_GLIB_LIBRARY_DIRS}
-        PATHS ${_lib_paths}
-        NO_DEFAULT_PATH
-    )
+        find_path(ICONV_INCLUDE_DIR
+            NAMES iconv.h
+            HINTS ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_INCLUDE_DIRS}
+            PATHS ${_include_paths} ${_lib_paths}
+            NO_DEFAULT_PATH)
 
-    find_library(GTHREAD_LIBRARIES
-        NAMES gthread-2.0
-        HINTS ${PKGCONFIG_GLIB_LIBDIR} ${PKGCONFIG_GLIB_LIBRARY_DIRS}
-        PATHS ${_lib_paths}
-        NO_DEFAULT_PATH
-    )
+        find_path(GLIB_CONFIG_INCLUDE_DIR
+            NAMES glibconfig.h
+            HINTS ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_INCLUDE_DIRS}
+            PATHS ${_include_paths} ${_lib_paths}
+            PATH_SUFFIXES glib-2.0/include
+            NO_DEFAULT_PATH)
 
-    find_library(ICONV_LIBRARIES
-        NAMES libiconv iconv
-        HINTS ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_LIBRARY_DIRS}
-        PATHS ${_lib_paths}
-        NO_DEFAULT_PATH
-    )
+        find_path(GTHREAD_INCLUDE_DIR
+            NAMES gthread.h
+            HINTS ${PKGCONFIG_GTHREAD_INCLUDEDIR} ${PKGCONFIG_GTHREAD_INCLUDE_DIRS}
+            PATHS ${_include_paths} ${_lib_paths}
+            PATH_SUFFIXES glib-2.0)
 
-    find_library(INTL_LIBRARIES
-        NAMES intl
-        HINTS ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_LIBRARY_DIRS}
-        PATHS ${_lib_paths}
-        NO_DEFAULT_PATH
-    )
+        # libs
+        find_library(GLIB2_LIBRARIES
+            NAMES glib-2.0
+            HINTS ${PKGCONFIG_GLIB_LIBDIR} ${PKGCONFIG_GLIB_LIBRARY_DIRS}
+            PATHS ${_lib_paths}
+            NO_DEFAULT_PATH
+        )
 
+        find_library(GTHREAD_LIBRARIES
+            NAMES gthread-2.0
+            HINTS ${PKGCONFIG_GLIB_LIBDIR} ${PKGCONFIG_GLIB_LIBRARY_DIRS}
+            PATHS ${_lib_paths}
+            NO_DEFAULT_PATH
+        )
+
+        find_library(ICONV_LIBRARIES
+            NAMES libiconv iconv
+            HINTS ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_LIBRARY_DIRS}
+            PATHS ${_lib_paths}
+            NO_DEFAULT_PATH
+        )
+
+        find_library(INTL_LIBRARIES
+            NAMES intl
+            HINTS ${PKGCONFIG_GLIB_INCLUDEDIR} ${PKGCONFIG_GLIB_LIBRARY_DIRS}
+            PATHS ${_lib_paths}
+            NO_DEFAULT_PATH
+        )
+    endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GLIB
     DEFAULT_MSG GLIB_INCLUDE_DIR GLIB_CONFIG_INCLUDE_DIR
-    GLIB_LIBRARIES2 GTHREAD_LIBRARIES
+    GTHREAD_INCLUDE_DIR
+    GLIB2_LIBRARIES GTHREAD_LIBRARIES
     ICONV_INCLUDE_DIR)
 
 if(GLIB_FOUND)
     set(GLIB_INCLUDES ${GLIB_INCLUDE_DIR} ${GLIB_CONFIG_INCLUDE_DIR} ${ICONV_INCLUDE_DIR})
-    set(GLIB_LIBRARIES ${GLIB_LIBRARIES2} ${GTHREAD_LIBRARIES})
+    set(GLIB_LIBRARIES ${GLIB2_LIBRARIES} ${GTHREAD_LIBRARIES})
+    set(GLIB_LIBRARY_DIRS ${GLIB2_LIBRARY_DIRS} ${GTHREAD_LIBRARY_DIRS})
     if(ICONV_LIBRARIES)
         list(APPEND GLIB_LIBRARIES ${ICONV_LIBRARIES})
     endif()
@@ -156,16 +175,11 @@ if(GLIB_FOUND)
         list(APPEND GLIB_LIBRARIES ${INTL_LIBRARIES})
     endif()
 
-    message(STATUS "glib-2.0 and iconv files: ")
-    foreach(_inc ${GLIB_INCLUDES})
-        message(STATUS "    ${_inc}")
-    endforeach()
-
-    foreach(_lib ${GLIB_LIBRARIES})
-        message(STATUS "    ${_lib}")
-    endforeach()
+    list(REMOVE_DUPLICATES GLIB_INCLUDES)
+    list(REMOVE_DUPLICATES GLIB_LIBRARIES)
+    list(REMOVE_DUPLICATES GLIB_LIBRARY_DIRS)
 else()
     message(STATUS "glib-2.0 not found")
 endif()
 
-mark_as_advanced(GLIB_INCLUDES GLIB_LIBRARIES)
+mark_as_advanced(GLIB_INCLUDES GLIB_LIBRARIES GLIB_LIBRARY_DIRS)
