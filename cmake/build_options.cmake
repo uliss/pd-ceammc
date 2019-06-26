8420dfd00463cc582404cf2028cc99e96db57c8f
Serge Poltavski <serge.poltavski@gmail.com>
2019-06-26 15:51:12 +0900

    cmake option WITH_FFTW updated

--- cmake/build_options.cmake
+++ cmake/build_options.cmake
@@ -3,5 +3,6 @@ option(WITH_FLUIDLITE "Build with FluidLite support" ON)
 option(WITH_EXPERIMENTAL "Build with experimental objects" OFF)
 option(WITH_BENCHMARK "Build with benchmarks" ON)
 option(WITH_MODPLUG "Build with libmodplug support" ON)
+option(WITH_FFTW "Use fftw3 library (http://www.fftw.org/)" ON)
 option(ENABLE_TESTS "Enable tests" ON)
 
