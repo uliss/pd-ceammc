c2164e6f8e1afab2b01e1e9d47a9106e24f6dbf1
Serge Poltavski <serge.poltavski@gmail.com>
2019-10-05 02:18:06 +0300

    formatting

--- ceammc/ext/src/flow/flow_group.cpp
+++ ceammc/ext/src/flow/flow_group.cpp
@@ -23,10 +23,9 @@ FlowGroup::FlowGroup(const PdArgs& a)
     group_size_ = new IntProperty("@by", positionalFloatArgument(0, 1));
     createProperty(group_size_);
 
-    {
-        Property* p = createCbProperty("@free", &FlowGroup::propFree);
-        p->info().setType(PropertyInfoType::INTEGER);
-    }
+    createCbProperty("@free", &FlowGroup::propFree)
+        ->info()
+        .setType(PropertyInfoType::INTEGER);
 }
 
 void FlowGroup::onFloat(float v)
