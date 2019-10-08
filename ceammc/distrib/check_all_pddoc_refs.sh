119bb5bc98dc0349b3b672695cca2f0305fc24e2
Serge Poltavski <serge.poltavski@gmail.com>
2019-10-08 22:02:43 +0300

    check script for all pddoc references added

---
+++ ceammc/distrib/check_all_pddoc_refs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PDDOC_LS="pd_doc2ls"
+OBJ_CHECK="pd_objcheck"
+DB="@PROJECT_SOURCE_DIR@/ceammc/ext/doc/ceammc.db"
+
+for pddoc in @PROJECT_SOURCE_DIR@/ceammc/ext/doc/*.pddoc
+do
+    echo "checking $(basename $pddoc) ..."
+    ${PDDOC_LS} "$pddoc" | sort | uniq | while read line
+    do
+        echo "    - $line"
+        ${OBJ_CHECK} --xlet-db "${DB}" $line 1>/dev/null
+    done
+done
