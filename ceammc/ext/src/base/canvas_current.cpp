4c6f922ddc6965785f4af16844714eec25488168
Serge Poltavski <serge.poltavski@gmail.com>
2020-03-17 01:38:40 +0300

    cleanup

--- ceammc/ext/src/base/canvas_current.cpp
+++ ceammc/ext/src/base/canvas_current.cpp
@@ -35,65 +35,10 @@ void CanvasCurrent::onBang()
     dataTo(0, DataPtr(dict));
 }
 
-AtomList CanvasCurrent::p_name() const
-{
-    return Atom(canvas_info_name(canvas()));
-}
-
-AtomList CanvasCurrent::p_dir() const
-{
-    return Atom(canvas_info_dir(canvas()));
-}
-
-AtomList CanvasCurrent::p_root() const
-{
-    return Atom(canvas_info_is_root(canvas()) ? 1 : 0);
-}
-
-AtomList CanvasCurrent::p_abstraction() const
-{
-    return Atom(canvas_info_is_abstraction(canvas()) ? 1 : 0);
-}
-
-AtomList CanvasCurrent::p_args() const
-{
-    return canvas_info_args(canvas());
-}
-
-AtomList CanvasCurrent::p_font() const
-{
-    return Atom(canvas_info_font(canvas()));
-}
-
-AtomList CanvasCurrent::p_paths() const
-{
-    return canvas_info_paths(canvas());
-}
-
 AtomList CanvasCurrent::p_size() const
 {
     t_rect bb = canvas_info_rect(canvas());
     return AtomList(Atom(bb.w), Atom(bb.h));
-}
-
-AtomList CanvasCurrent::p_width() const
-{
-    return Atom(canvas_info_rect(canvas()).w);
-}
-
-AtomList CanvasCurrent::p_height() const
-{
-    return Atom(canvas_info_rect(canvas()).h);
-}
-
-AtomList CanvasCurrent::p_x() const
-{
-    return Atom(canvas_info_rect(canvas()).x);
-}
-
-AtomList CanvasCurrent::p_y() const
-{
-    return Atom(canvas_info_rect(canvas()).y);
 }
 
 void setup_canvas_current()
