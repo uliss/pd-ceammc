3e7b4ec599cc185c38222c9e3d091117cd3e1d54
Serge Poltavski <serge.poltavski@gmail.com>
2020-06-28 11:30:47 +0300

    reset method added

--- ceammc/ext/src/flt/flt_bpf12.cpp
+++ ceammc/ext/src/flt/flt_bpf12.cpp
@@ -10,9 +10,15 @@ public:
     {
         bindPositionalArgsToProps({ gensym("@freq"), gensym("@q") });
     }
+
+    void m_reset(t_symbol*, const AtomList&)
+    {
+        dsp_->instanceClear();
+    }
 };
 
 void setup_flt_bpf12_tilde()
 {
     SoundExternalFactory<FltBpf12> obj("flt.bpf12~");
+    obj.addMethod("reset", &FltBpf12::m_reset);
 }
--- ceammc/ext/src/flt/flt_bpf24.cpp
+++ ceammc/ext/src/flt/flt_bpf24.cpp
@@ -10,9 +10,15 @@ public:
     {
         bindPositionalArgsToProps({ gensym("@freq"), gensym("@q") });
     }
+
+    void m_reset(t_symbol*, const AtomList&)
+    {
+        dsp_->instanceClear();
+    }
 };
 
 void setup_flt_bpf24_tilde()
 {
     SoundExternalFactory<FltBpf24> obj("flt.bpf24~");
+    obj.addMethod("reset", &FltBpf24::m_reset);
 }
