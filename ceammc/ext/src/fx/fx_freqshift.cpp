8284dfa6debd1638309c8c7b7df4a961801a88e4
Serge Poltavski <serge.poltavski@gmail.com>
2020-08-18 02:25:25 +0300

    freqshift property update

--- ceammc/ext/src/fx/fx_freqshift.cpp
+++ ceammc/ext/src/fx/fx_freqshift.cpp
@@ -8,14 +8,12 @@ public:
     FxFreqShift(const PdArgs& args)
         : faust_fx_freqshift_tilde(args)
     {
-        static t_symbol* SYM_PROP_DELAY = gensym("@delay");
-        static t_symbol* SYM_PROP_FEEDBACK = gensym("@feedback");
-
-        bindPositionalArgsToProps({ SYM_PROP_DELAY, SYM_PROP_FEEDBACK });
     }
 };
 
 void setup_fx_freqshift_tilde()
 {
     SoundExternalFactory<FxFreqShift> obj("fx.freqshift~");
+
+    obj.setXletsInfo({ "input signal", "freqshift (hz)" }, { "output (freq+shift)", "output (freq-shift)" });
 }
