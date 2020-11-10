e5ce89ae5fb2dbd9abd76fabd9290b9eedfe84d1
Serge Poltavski <serge.poltavski@gmail.com>
2020-11-10 09:48:10 +0300

    seq.toggles fix

--- ceammc/ext/src/flow/mod_flow.cpp
+++ ceammc/ext/src/flow/mod_flow.cpp
@@ -39,7 +39,9 @@ void setup_flow_delay();
 void setup_flow_match();
 void setup_flow_pipe();
 void setup_flow_space();
+
 void setup_seq_bangs();
+void setup_seq_toggles();
 
 void ceammc_flow_setup()
 {
@@ -83,4 +85,5 @@ void ceammc_flow_setup()
     setup_flow_tee_tilde();
 
     setup_seq_bangs();
+    setup_seq_toggles();
 }
