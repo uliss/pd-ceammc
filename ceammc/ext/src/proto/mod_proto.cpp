a1962eb4d38bc2ef1802f740be496f114e78a6e2
Serge Poltavski <serge.poltavski@gmail.com>
2020-11-20 00:35:51 +0300

    HUI proto added

--- ceammc/ext/src/proto/mod_proto.cpp
+++ ceammc/ext/src/proto/mod_proto.cpp
@@ -1,9 +1,11 @@
 #include "mod_proto.h"
 #include "proto_firmata.h"
+#include "proto_hui.h"
 #include "proto_sp_alpaca.h"
 
 void ceammc_proto_setup()
 {
     proto_firmata_setup();
+    setup_proto_hui();
     setup_proto_sp_alpaca();
 }
