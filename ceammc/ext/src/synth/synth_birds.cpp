6a6271561d831d1f99fcc8783023b014d7b687de
Serge Poltavski <serge.poltavski@gmail.com>
2020-11-25 01:59:16 +0300

    synth.birds bang support added

--- ceammc/ext/src/synth/synth_birds.cpp
+++ ceammc/ext/src/synth/synth_birds.cpp
@@ -1,18 +1,34 @@
 #include "synth_birds.h"
+#include "ceammc_clock.h"
 #include "ceammc_factory.h"
 
 using namespace ceammc;
 
+static t_symbol* PROP_GATE;
+
 class SynthBirds : public faust_synth_birds_tilde {
+    UIProperty* gate_;
+    ClockLambdaFunction clock_;
+
 public:
     SynthBirds(const PdArgs& args)
         : faust_synth_birds_tilde(args)
+        , gate_(static_cast<UIProperty*>(property(PROP_GATE)))
+        , clock_([this]() { gate_->setValue(0); })
     {
         bindPositionalArgsToProps({ gensym("@speed"), gensym("@probability") });
+    }
+
+    void onBang() override
+    {
+        gate_->setValue(1);
+        clock_.delay(10);
     }
 };
 
 void setup_synth_birds_tilde()
 {
+    PROP_GATE = gensym("@gate");
+
     SoundExternalFactory<SynthBirds> obj("synth.birds~", OBJECT_FACTORY_DEFAULT);
 }
