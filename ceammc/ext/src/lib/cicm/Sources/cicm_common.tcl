5d2af0893a1b7a4e0ca4438565fb3b7e203f6b44
Serge Poltavski <serge.poltavski@gmail.com>
2020-12-27 12:54:15 +0300

    tcl: scrolled frame added

--- ceammc/ext/src/lib/cicm/Sources/cicm_common.tcl
+++ ceammc/ext/src/lib/cicm/Sources/cicm_common.tcl
@@ -113,6 +113,9 @@ proc ceammc_category_apply {id var_name_state controls} {
             grid config $w
         }
     }
+
+    event generate . <Expose>
+    update
 }
 
 proc ceammc_category_toggle {id var_name_state controls} {
@@ -150,6 +153,117 @@ if { [catch {package require tooltip} ] } {
 
 namespace eval ::ceammc {
 namespace eval ui {
+
+# https://wiki.tcl-lang.org/page/A+scrolled+frame
+# sframe.tcl
+# Paul Walton
+# Create a ttk-compatible, scrollable frame widget.
+#   Usage:
+#       sframe new <path> ?-toplevel true?  ?-anchor nsew?
+#       -> <path>
+#
+#       sframe content <path>
+#       -> <path of child frame where the content should go>
+namespace eval sframe {
+   # Create a scrollable frame or window.
+   proc new {path args} {
+       # Use the ttk theme's background for the canvas and toplevel
+       set bg [ttk::style lookup TFrame -background]
+       if { [ttk::style theme use] eq "aqua" } {
+           # Use a specific color on the aqua theme as 'ttk::style lookup' is not accurate.
+           set bg "#e9e9e9"
+       }
+
+       # Create the main frame or toplevel.
+       if { [dict exists $args -toplevel]  &&  [dict get $args -toplevel] } {
+           toplevel $path -bg $bg
+       } else {
+           ttk::frame $path
+       }
+
+       # Create a scrollable canvas with scrollbars which will always be the same size as the main frame.
+       set canvas [canvas $path.canvas -bg $bg -bd 0 -highlightthickness 0 -yscrollcommand [list $path.scrolly set]]
+       ttk::scrollbar $path.scrolly -orient vertical -command [list $canvas yview]
+
+       # Create a container frame which will always be the same size as the canvas or content, whichever is greater.
+       # This allows the child content frame to be properly packed and also is a surefire way to use the proper ttk background.
+       set container [ttk::frame $canvas.container]
+       pack propagate $container 0
+
+       # Create the content frame. Its size will be determined by its contents. This is useful for determining if the
+       # scrollbars need to be shown.
+       set content [ttk::frame $container.content]
+
+       # Pack the content frame and place the container as a canvas item.
+       set anchor "n"
+       if { [dict exists $args -anchor] } {
+           set anchor [dict get $args -anchor]
+       }
+       pack $content -anchor $anchor
+       $canvas create window 0 0 -window $container -anchor nw
+
+       # Grid the scrollable canvas sans scrollbars within the main frame.
+       grid $canvas -row 0 -column 0 -sticky nsw
+       grid rowconfigure    $path 0 -weight 1
+       grid columnconfigure $path 0 -weight 1
+
+       # Make adjustments when the sframe is resized or the contents change size.
+       bind $path.canvas <Expose> [list ::ceammc::ui::sframe::resize $path]
+
+       # Mousewheel bindings for scrolling.
+       bind [winfo toplevel $path] <MouseWheel> [list ::ceammc::ui::sframe::scroll $path yview %W %D]
+       return $path
+   }
+
+
+   # Given the toplevel path of an sframe widget, return the path of the child frame suitable for content.
+   proc content {path} {
+       return $path.canvas.container.content
+   }
+
+   # Make adjustments when the the sframe is resized or the contents change size.
+   proc resize {path} {
+       set canvas    $path.canvas
+       set container $canvas.container
+       set content   $container.content
+
+       # Set the size of the container. At a minimum use the same width & height as the canvas.
+       set width  [winfo width $canvas]
+       set height [winfo height $canvas]
+
+       # If the requested width or height of the content frame is greater then use that width or height.
+       if { [winfo reqwidth $content] > $width } {
+           set width [winfo reqwidth $content]
+       }
+       if { [winfo reqheight $content] > $height } {
+           set height [winfo reqheight $content]
+       }
+
+       $canvas configure -width $width -height $height
+       $container configure -width $width -height $height
+
+       wm geometry [winfo toplevel $path] [expr $width + 40]x[winfo height $path]
+
+       # Configure the canvas's scroll region to match the height and width of the container.
+       $canvas configure -scrollregion [list 0 0 $width $height]
+
+       # Vertical scrolling.
+       if { [winfo reqheight $content] > [winfo height $canvas] } {
+           grid $path.scrolly -row 0 -column 1 -sticky ns
+       } else {
+           grid forget $path.scrolly
+       }
+       return
+   }
+
+   # Handle mousewheel scrolling.
+   proc scroll {path view W D} {
+       if { [winfo exists $path.canvas]  &&  [string match $path.canvas* $W] } {
+           $path.canvas $view scroll [expr {-$D}] units
+       }
+       return -code break
+   }
+}
 
 # xlets tooltip for UI objects
 namespace eval xlet_tooltip {
