f205b4281c87bc9f7682bc2c544344f0d573dca6
Serge Poltavski <serge.poltavski@gmail.com>
2020-12-27 23:58:06 +0300

    mac: update deploy script

--- ceammc/distrib/mac/github_upload_release.sh
+++ ceammc/distrib/mac/github_upload_release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 1 ];
+if [ $# -lt 1 ];
 then
-    echo "Usage: $(basename $0) DMG-file"
+    echo "Usage: $(basename $0) FILE [TAG]"
     exit 1
 fi
 
@@ -19,72 +19,32 @@ then
     exit 1
 fi
 
-DMG="$1"
+FILE="$1"
 VERSION="@CEAMMC_DISTRIB_VERSION@"
 CEAMMC_VERSION="@CEAMMC_LIB_VERSION@"
 
-if [ ! -f "$DMG" ]
+if [ ! -f "$FILE" ]
 then
     tput setaf 9
     echo "ERROR: "
     tput setaf 15
-    echo "  file is not found: $DMG. Exiting..."
+    echo "  file is not found: $FILE. Exiting..."
     exit 1
 fi
 
-TAG="v$VERSION"
+if [ $# -ne 2 ]
+then
+    TAG="v$VERSION"
+else
+    TAG=$2
+fi
+
 USER=uliss
 REPO="pure-data"
-NAME=$(basename $DMG)
-DIR=$(dirname $DMG)
-
-SRC_TAR="${DIR}/pd-ceammc-${VERSION}-src.tar.gz"
-
-if [ -f "$SRC_TAR" ]
-then
-    echo "uploading $(basename $SRC_TAR) to github..."
-    github-release upload --user $USER \
-        --repo $REPO \
-        --tag $TAG \
-        --name $(basename $SRC_TAR) \
-        --file "$SRC_TAR" \
-        --replace
-fi
-
-SRC_ZIP="${DIR}/pd-ceammc-${VERSION}-src.zip"
-
-if [ -f "$SRC_ZIP" ]
-then
-    echo "uploading $(basename $SRC_ZIP) to github..."
-    github-release upload --user $USER \
-        --repo $REPO \
-        --tag $TAG \
-        --name $(basename $SRC_ZIP) \
-        --file "$SRC_ZIP" \
-        --replace
-fi
-
-CEAMMC_TAR_GZ="${DIR}/ceammc-${CEAMMC_VERSION}-macosx-*.tar.gz"
-
-if [ -f ${CEAMMC_TAR_GZ} ]
-then
-    fname=$(basename ${CEAMMC_TAR_GZ})
-    echo "uploading $fname to github..."
-    github-release upload --user $USER \
-        --repo $REPO \
-        --tag $TAG \
-        --name $fname \
-        --file ${CEAMMC_TAR_GZ} \
-        --replace
-fi
-
-echo "uploading release DMG ($TAG) to github..."
-
-github-release upload --user $USER \
-    --repo $REPO \
-    --tag $TAG \
-    --name $NAME \
-    --file "$DMG" \
-    --replace
+NAME=$(basename $FILE)
+DIR=$(dirname $FILE)
 
 
+echo "uploading release FILE ($TAG) to github..."
+
+gpgenv exec github-asset "$USER/$REPO" upload $TAG "$FILE"
