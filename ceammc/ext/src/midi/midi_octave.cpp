5774cf9798957fc53e6dd46e293fa12653d136cf
Serge Poltavski <serge.poltavski@gmail.com>
2021-01-19 09:45:05 +0300

    midi.oct added

---
+++ ceammc/ext/src/midi/midi_octave.cpp
@@ -0,0 +1,93 @@
+/*****************************************************************************
+ * Copyright 2021 Serge Poltavsky. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#include "midi_octave.h"
+#include "ceammc_args.h"
+#include "ceammc_convert.h"
+#include "ceammc_factory.h"
+
+#include <memory>
+
+static std::unique_ptr<ArgChecker> onlist_chk;
+
+static t_symbol* SYM_TRANSPOSE;
+static t_symbol* SYM_SET;
+
+MidiOctave::MidiOctave(const PdArgs& args)
+    : BaseObject(args)
+    , oct_(nullptr)
+    , mode_(nullptr)
+{
+    oct_ = new IntProperty("@oct", 0);
+    oct_->setArgIndex(0);
+    oct_->checkClosedRange(-16, 16);
+    addProperty(oct_);
+
+    mode_ = new SymbolEnumProperty("@mode", { SYM_TRANSPOSE, SYM_SET });
+    addProperty(mode_);
+
+    addProperty(new SymbolEnumAlias("@set", mode_, SYM_SET));
+    addProperty(new SymbolEnumAlias("@transpose", mode_, SYM_TRANSPOSE));
+
+    createInlet();
+    createOutlet();
+}
+
+void MidiOctave::onFloat(t_float note)
+{
+    floatTo(0, octave(note));
+}
+
+void MidiOctave::onList(const AtomList& lst)
+{
+    if (!onlist_chk->check(lst.view())) {
+        OBJ_ERR << "NOTE VEL [DUR] expected, got: " << lst;
+        return;
+    }
+
+    const size_t N = lst.size();
+    assert(N == 2 || N == 3);
+
+    Atom msg[3] = { octave(lst[0].asFloat()), lst[1], N == 3 ? lst[2] : 0. };
+    listTo(0, AtomListView(msg, N));
+}
+
+void MidiOctave::onInlet(size_t n, const AtomListView& lv)
+{
+    mode_->setList(lv);
+}
+
+t_float MidiOctave::octave(t_float note) const
+{
+    const auto oct = oct_->value();
+    note = clip<t_float, 0, 127>(note);
+
+    if (mode_->value() == SYM_SET) {
+        note = std::fmod(note, 12);
+        if (oct < 0)
+            OBJ_ERR << "warning: octave value < 0.0 in set mode";
+    }
+
+    auto res = note + 12 * oct;
+    return (res < 0 || res > 127) ? note : res;
+}
+
+void setup_midi_octave()
+{
+    onlist_chk.reset(new ArgChecker("f0..127 f0..127 f?"));
+
+    SYM_TRANSPOSE = gensym("transpose");
+    SYM_SET = gensym("set");
+
+    ObjectFactory<MidiOctave> obj("midi.oct");
+}
---
+++ ceammc/ext/tests/midi/test_midi_oct.cpp
@@ -0,0 +1,125 @@
+/*****************************************************************************
+ * Copyright 2017 Serge Poltavsky. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#include "midi_octave.h"
+#include "test_midi_base.h"
+
+PD_COMPLETE_TEST_SETUP(MidiOctave, midi, octave)
+
+TEST_CASE("midi.oct", "[externals]")
+{
+    pd_test_init();
+    test::pdPrintToStdError();
+
+    SECTION("init")
+    {
+        SECTION("default")
+        {
+            TExt t("midi.oct");
+            REQUIRE(t.numInlets() == 2);
+            REQUIRE(t.numOutlets() == 1);
+            REQUIRE_PROPERTY(t, @oct, 0);
+            REQUIRE_PROPERTY(t, @mode, "transpose");
+            REQUIRE_PROPERTY(t, @set, 0);
+            REQUIRE_PROPERTY(t, @transpose, 1);
+        }
+
+        SECTION("args")
+        {
+            TExt t("midi.oct", -5);
+            REQUIRE_PROPERTY(t, @oct, -5);
+        }
+    }
+
+    SECTION("@transpose")
+    {
+        TExt t("midi.oct");
+
+        t << 0.;
+        REQUIRE(t.outputFloatAt(0) == 0);
+        t << 64.5;
+        REQUIRE(t.outputFloatAt(0) == 64.5);
+        t << 127;
+        REQUIRE(t.outputFloatAt(0) == 127);
+
+        // invalid
+        t << 128;
+        REQUIRE(t.outputFloatAt(0) == 127);
+        t << -1;
+        REQUIRE(t.outputFloatAt(0) == 0);
+
+        // list
+        t << LF(1, 2);
+        REQUIRE(t.outputListAt(0) == LF(1, 2));
+        t << LF(1, 2, 3);
+        REQUIRE(t.outputListAt(0) == LF(1, 2, 3));
+        t << LF(1, 2, 3, 4);
+        REQUIRE_FALSE(t.hasOutputAt(0));
+
+        // @oct -2
+        t->setProperty("@oct", LF(-2));
+        t << 0.;
+        REQUIRE(t.outputFloatAt(0) == 0);
+        t << 1;
+        REQUIRE(t.outputFloatAt(0) == 1);
+        t << 12;
+        REQUIRE(t.outputFloatAt(0) == 12);
+        t << 23;
+        REQUIRE(t.outputFloatAt(0) == 23);
+        t << 24;
+        REQUIRE(t.outputFloatAt(0) == 0);
+        t << 25;
+        REQUIRE(t.outputFloatAt(0) == 1);
+        t << 124;
+        REQUIRE(t.outputFloatAt(0) == 100);
+
+        t << LF(1, 64);
+        REQUIRE(t.outputListAt(0) == LF(1, 64));
+        t << LF(23, 64);
+        REQUIRE(t.outputListAt(0) == LF(23, 64));
+        t << LF(24, 64);
+        REQUIRE(t.outputListAt(0) == LF(0, 64));
+        t << LF(124, 64);
+        REQUIRE(t.outputListAt(0) == LF(100, 64));
+        t << LF(124, 64, 100);
+        REQUIRE(t.outputListAt(0) == LF(100, 64, 100));
+
+        // @oct +5
+        t->setProperty("@oct", LF(5));
+        t << 0.;
+        REQUIRE(t.outputFloatAt(0) == 60);
+        t << 1;
+        REQUIRE(t.outputFloatAt(0) == 61);
+        t << 12;
+        REQUIRE(t.outputFloatAt(0) == 72);
+        t << 60;
+        REQUIRE(t.outputFloatAt(0) == 120);
+        t << 67;
+        REQUIRE(t.outputFloatAt(0) == 127);
+        t << 68;
+        REQUIRE(t.outputFloatAt(0) == 68);
+        t << 69;
+        REQUIRE(t.outputFloatAt(0) == 69);
+
+        t << LF(60, 64);
+        REQUIRE(t.outputListAt(0) == LF(120, 64));
+        t << LF(67, 64);
+        REQUIRE(t.outputListAt(0) == LF(127, 64));
+        t << LF(68, 64);
+        REQUIRE(t.outputListAt(0) == LF(68, 64));
+        t << LF(67, 64, 100);
+        REQUIRE(t.outputListAt(0) == LF(127, 64, 100));
+        t << LF(68, 64, 100);
+        REQUIRE(t.outputListAt(0) == LF(68, 64, 100));
+    }
+}
