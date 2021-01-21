8ca0b39bf4c5f485cfbf1967dd4134117f5ce62b
Serge Poltavski <serge.poltavski@gmail.com>
2021-01-21 23:24:57 +0300

    system.exit added

--- ceammc/ext/src/system/mod_system.cpp
+++ ceammc/ext/src/system/mod_system.cpp
@@ -3,6 +3,7 @@
 void setup_system_colorpanel();
 void setup_system_cursor();
 void setup_system_exec();
+void setup_system_exit();
 void setup_system_getenv();
 void setup_system_hostname();
 void setup_system_memsize();
@@ -14,6 +15,7 @@ void ceammc_system_setup()
     setup_system_colorpanel();
     setup_system_cursor();
     setup_system_exec();
+    setup_system_exit();
     setup_system_getenv();
     setup_system_hostname();
     setup_system_memsize();
---
+++ ceammc/ext/src/system/system_exit.cpp
@@ -0,0 +1,55 @@
+/*****************************************************************************
+ * Copyright 2021 Serge Poltavsky. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#include "system_exit.h"
+#include "ceammc_factory.h"
+
+SystemExit::SystemExit(const PdArgs& args)
+    : BaseObject(args)
+    , clock_([this]() { exit(rc_->value()); })
+    , delay_(nullptr)
+    , rc_(nullptr)
+{
+    delay_ = new FloatProperty("@delay", 0);
+    delay_->checkMinEq(0);
+    delay_->setUnitsMs();
+    delay_->setArgIndex(0);
+    addProperty(delay_);
+
+    rc_ = new IntProperty("@rc", 0);
+    addProperty(rc_);
+}
+
+void SystemExit::initDone()
+{
+    auto ms = delay_->value();
+
+    if (ms > 0)
+        clock_.delay(ms);
+}
+
+void SystemExit::m_exit(t_symbol* s, const AtomListView& lv)
+{
+    exit(lv.intAt(0, rc_->value()));
+}
+
+void SystemExit::exit(int rc)
+{
+    sys_vgui("exit %d\n", rc);
+}
+
+void setup_system_exit()
+{
+    ObjectFactory<SystemExit> obj("system.exit");
+    obj.addMethod("exit", &SystemExit::m_exit);
+}
