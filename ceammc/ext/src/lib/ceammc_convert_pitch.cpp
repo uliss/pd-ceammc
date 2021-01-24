53ad8aabfe8aa2a3b89531694d292011666a193a
Serge Poltavski <serge.poltavski@gmail.com>
2021-01-24 21:37:58 +0300

    pitch to midi rest support added

--- ceammc/ext/src/lib/ceammc_convert_pitch.cpp
+++ ceammc/ext/src/lib/ceammc_convert_pitch.cpp
@@ -1,67 +1,147 @@
 #include "ceammc_convert.h"
+#include "ceammc_log.h"
 
-#include <boost/unordered_map.hpp>
+#include <cstdint>
 #include <cstring>
-#include <string>
 
-typedef boost::unordered_map<std::string, int> PitchMap;
 class SPN {
-    PitchMap map_;
+public:
+    enum State {
+        ST_INIT,
+        ST_NOTE,
+        ST_REST,
+        ST_ALT_SHARP,
+        ST_ALT_FLAT,
+        ST_OCTAVE,
+        ST_DONE,
+        ST_ERROR
+    };
+
+private:
+    State st_;
+    uint8_t note_;
+    int8_t alt_;
+    uint8_t oct_;
+    char err_[64];
+    static const int defOct = 4;
 
 public:
     SPN()
+        : st_(ST_INIT)
+        , note_(0)
+        , alt_(0)
+        , oct_(0)
     {
-        map_["Cbb"] = -2;
-        map_["Cb"] = -1;
-        map_["C"] = 0;
-        map_["C#"] = 1;
-        map_["C##"] = 2;
-
-        map_["Dbb"] = 0;
-        map_["Db"] = 1;
-        map_["D"] = 2;
-        map_["D#"] = 3;
-        map_["D##"] = 4;
-
-        map_["Ebb"] = 2;
-        map_["Eb"] = 3;
-        map_["E"] = 4;
-        map_["E#"] = 5;
-        map_["E##"] = 6;
-
-        map_["Fbb"] = 3;
-        map_["Fb"] = 4;
-        map_["F"] = 5;
-        map_["F#"] = 6;
-        map_["F##"] = 7;
-
-        map_["Gbb"] = 5;
-        map_["Gb"] = 6;
-        map_["G"] = 7;
-        map_["G#"] = 8;
-        map_["G##"] = 9;
-
-        map_["Abb"] = 7;
-        map_["Ab"] = 8;
-        map_["A"] = 9;
-        map_["A#"] = 10;
-        map_["A##"] = 11;
-
-        map_["Bbb"] = 9;
-        map_["Bb"] = 10;
-        map_["B"] = 11;
-        map_["B#"] = 12;
-        map_["B##"] = 13;
     }
 
-    bool findPitch(const std::string& str, int* dest) const
-    {
-        PitchMap::const_iterator it = map_.find(str);
-        if (it == map_.end())
-            return false;
+    State state() const { return st_; }
+    const char* error() const { return err_; }
 
-        *dest = it->second;
-        return true;
+    void reset()
+    {
+        st_ = ST_INIT;
+        note_ = 0;
+        alt_ = 0;
+        oct_ = 0;
+        err_[0] = '\0';
+    }
+
+    bool isNote() const { return note_ != 0 && !isRest(); }
+    bool isRest() const { return note_ == 'R'; }
+    int midi() const { return ((oct_ + 1) * 12 + midiNote(note_)) + alt_; }
+
+    State setState(State st, const char* msg = nullptr)
+    {
+        st_ = st;
+        if (msg) {
+            strncpy(err_, msg, sizeof(err_));
+            err_[sizeof(err_) - 1] = '\0';
+        }
+
+        return st;
+    }
+
+    State put(char c)
+    {
+        switch (st_) {
+        case ST_INIT: {
+            if (c >= 'A' && c <= 'G') {
+                note_ = c;
+                return setState(ST_NOTE);
+            } else if (c == 'R') {
+                note_ = c;
+                return setState(ST_REST);
+            } else {
+                return setState(ST_ERROR, "Note name expected");
+            }
+        } break;
+        case ST_REST: {
+            if (c == '\0')
+                return setState(ST_DONE);
+            else
+                return setState(ST_ERROR, "extra character after Rest");
+        } break;
+        case ST_NOTE: {
+            if (c == '#') {
+                alt_++;
+                return setState(ST_ALT_SHARP);
+            } else if (c == 'b') {
+                alt_--;
+                return setState(ST_ALT_FLAT);
+            } else if (isdigit(c)) {
+                oct_ = c - '0';
+                return setState(ST_OCTAVE);
+            } else if (c == '\0') {
+                oct_ = defOct;
+                return setState(ST_DONE);
+            } else {
+                return setState(ST_ERROR, "Alteration or Octave expected");
+            }
+        } break;
+        case ST_ALT_FLAT: {
+            if (c == 'b') {
+                alt_--;
+                return setState(ST_ALT_FLAT);
+            } else if (isdigit(c)) {
+                oct_ = c - '0';
+                return setState(ST_OCTAVE);
+            } else if (c == '\0') {
+                oct_ = defOct;
+                return setState(ST_DONE);
+            } else {
+                return setState(ST_ERROR, "Flat or Octave expected");
+            }
+        } break;
+        case ST_ALT_SHARP: {
+            if (c == '#') {
+                alt_++;
+                return setState(ST_ALT_SHARP);
+            } else if (isdigit(c)) {
+                oct_ = c - '0';
+                return setState(ST_OCTAVE);
+            } else if (c == '\0') {
+                oct_ = defOct;
+                return setState(ST_DONE);
+            } else {
+                return setState(ST_ERROR, "Sharp or Octave expected");
+            }
+        } break;
+        case ST_OCTAVE: {
+            if (c == '\0') {
+                return setState(ST_DONE);
+            } else {
+                return setState(ST_ERROR, "extra characters after OCTAVE");
+            }
+        } break;
+        default:
+            return setState(ST_ERROR, "extra characters");
+        }
+    }
+
+    static int midiNote(uint8_t n)
+    {
+        auto deg = (n >= 'C') ? (n - 'C') : (n - 'A' + 5);
+        return 2 * deg - (deg > 2);
     }
 };
 
@@ -69,18 +149,18 @@ static SPN spn;
 
 int ceammc::convert::spn2midi(const char* p)
 {
-    const size_t len = strlen(p);
-    if (len < 2)
-        return -1;
+    const char* pp = p;
+    spn.reset();
 
-    int char_oct = p[len - 1];
-    if (!isdigit(char_oct))
-        return -1;
-
-    int oct = char_oct - '0' + 1;
-    int pitch = 0;
-    if (!spn.findPitch(std::string(p, len - 1), &pitch))
-        return -1;
-
-    return 12 * oct + pitch;
+    while (true) {
+        switch (spn.put(*pp)) {
+        case SPN::ST_ERROR:
+            LIB_ERR << "SPN parse: " << spn.error();
+            return MIDI_NONE;
+        case SPN::ST_DONE:
+            return spn.isRest() ? MIDI_REST : spn.midi();
+        default:
+            ++pp;
+        }
+    }
 }
