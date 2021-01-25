7236d944af745cce94fd93e4be7f54ac678661f7
Serge Poltavski <serge.poltavski@gmail.com>
2021-01-25 11:24:48 +0300

    ui button update

--- ceammc/ext/src/ui/ui_button.tcl
+++ ceammc/ext/src/ui/ui_button.tcl
@@ -2,20 +2,20 @@ namespace eval ::ui {
 
 proc button_tag {id} { return  ${id}_#all }
 
-proc button_update {cnv id w h c0 c1 state} {
+proc button_update {cnv id w h zoom c0 c1 state} {
     set c [::ceammc::ui::widget_canvas $cnv $id]
     set t [button_tag $id]
     $c delete $t
 
     if { $state == 1 } { set color $c1 } { set color $c0 }
-    set xpad [expr {round($w/10)} + 2]
-    set ypad [expr {round($h/10)} + 2]
+    set xpad [expr {round($w/10)} + 1 + $zoom]
+    set ypad [expr {round($h/10)} + 1 + $zoom]
 
     set x0 [expr $xpad]
     set x1 [expr $w - $xpad]
     set y0 [expr $ypad]
     set y1 [expr $h - $ypad]
-    $c create rectangle $x0 $y0 $x1 $y1 -fill $color -outline $color -width 1 -tags $t
+    $c create rectangle $x0 $y0 $x1 $y1 -fill $color -outline $color -width $zoom -tags $t
 }
 
 }
