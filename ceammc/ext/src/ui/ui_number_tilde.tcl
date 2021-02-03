08d2e0bca06f71adca21f79f57bb14205d4f4ce3
Serge Poltavski <serge.poltavski@gmail.com>
2021-02-03 05:43:18 +0300

    win32: font size fix

--- ceammc/ext/src/ui/ui_number_tilde.tcl
+++ ceammc/ext/src/ui/ui_number_tilde.tcl
@@ -1,6 +1,21 @@
 namespace eval ui {
 
-proc number_font { zoom } { return "Helvetica [expr $zoom * 11] normal roman" }
+namespace eval number_tilde {
+    variable font_size 11
+    variable font_family "Helvetica"
+}
+
+switch -- $::windowingsystem {
+    "aqua" { }
+    "x11"  { }
+    "win32" {
+        set number_tilde::font_size 9
+    }
+}
+
+proc number_font { zoom } {
+    return "$number_tilde::font_family [expr $zoom * $number_tilde::font_size] normal roman"
+}
 
 proc number_draw_box {c tag w h zoom bdcolor acolor tcolor value} {
     set x [expr $h * 0.7]
