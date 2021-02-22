0f05f1234e7a456e56d538b102a9935e725f94d2
Serge Poltavski <serge.poltavski@gmail.com>
2021-02-23 01:34:08 +0300

    slider value update

--- ceammc/ext/src/lib/nui/tcl_nui.tcl
+++ ceammc/ext/src/lib/nui/tcl_nui.tcl
@@ -422,7 +422,10 @@ namespace eval slider {
     proc tag_box  { id } { return "#sl${id}_box" }
     proc tag_fill { id } { return "#sl${id}_fill" }
     proc tag_knob { id } { return "#sl${id}_kn" }
-    proc tag_labels { id } { return "#sl${id}_lbl" }
+    proc tag_min { id } { return "#sl${id}_min" }
+    proc tag_max { id } { return "#sl${id}_max" }
+    proc tag_value { id } { return "#sl${id}_val" }
+
     proc minmax_font { zoom } { return "Helvetica [expr $zoom*8]" }
 
     proc knob_xpos { x w pos } { return [expr ($w*$pos)+$x] }
@@ -433,7 +436,9 @@ namespace eval slider {
         set tb [tag_box $id]
         set tf [tag_fill $id]
         set tkn [tag_knob $id]
-        set tl [tag_labels $id]
+        set tmin [tag_min $id]
+        set tmax [tag_max $id]
+        set tval [tag_value $id]
 
         # draw fill
         $c create rectangle $x $y [expr $x+$w] [expr $y+$h] \
@@ -442,11 +447,16 @@ namespace eval slider {
         # draw min/max
         $c create text [expr $x+2] [expr $y+($h*0.5)] -text "$min" \
             -fill $out_color -font [minmax_font $zoom] -justify left\
-            -anchor w -tags [list $ta $tl]
+            -anchor w -tags [list $ta $tmin]
 
         $c create text [expr $x+$w-2] [expr $y+($h*0.5)] -text "$max" \
             -fill $out_color -font [minmax_font $zoom] -justify right \
-            -anchor e -tags [list $ta $tl]
+            -anchor e -tags [list $ta $tmax]
+
+        # draw value
+        $c create text [expr $x+($w*0.5)] [expr $y+($h*0.5)] -text "$value" \
+            -fill $out_color -font [minmax_font $zoom] -justify center \
+            -anchor center -tags [list $ta $tval]
 
         # draw knob
         set kx [knob_xpos $x $w $pos]
@@ -455,19 +465,18 @@ namespace eval slider {
         $c create line $kx $ky0 $kx $ky1 \
             -fill $knob_color -width [expr 2*$zoom] -tags [list $ta $tkn]
 
-
         # draw box
         $c create rectangle $x $y [expr $x+$w] [expr $y+$h] \
             -fill {} -outline $out_color -width $zoom -tags [list $ta $tb]
-
-        ::nui::widget_tooltip $cnv $model $tkn "value: $value"
     }
 
     proc hupdate { cnv model id pos value min max out_color fill_color knob_color } {
         set c [::nui::widget_canvas $cnv $model]
         set tb [tag_box $id]
         set tf [tag_fill $id]
-        set tl [tag_labels $id]
+        set tmin [tag_min $id]
+        set tmax [tag_max $id]
+        set tval [tag_value $id]
         set tkn [tag_knob $id]
 
         # update box
@@ -475,7 +484,10 @@ namespace eval slider {
         $c itemconfigure $tf -fill $fill_color
 
         # update labels
-        $c itemconfigure $tl -fill $out_color
+        $c itemconfigure $tmin -fill $out_color -text "$min"
+        $c itemconfigure $tmax -fill $out_color -text "$max"
+        # update value
+        $c itemconfigure $tval -fill $out_color -text "$value"
 
         lassign [$c coords $tb] x0 y0 x1 y1
         set w [expr $x1-$x0]
@@ -485,9 +497,6 @@ namespace eval slider {
         lassign [$c coords $tkn] x0 y0 x1 y1
         $c coords $tkn $x $y0 $x $y1
         $c itemconfigure $tkn -fill $knob_color
-
-        # update tooltip
-        ::nui::widget_tooltip $cnv $model $tkn "value: $value"
     }
 
     proc erase { cnv model id } {
