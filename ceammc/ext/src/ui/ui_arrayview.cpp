95347a0c5b39e3469647d3518b68bd7af78e608e
Serge Poltavski <serge.poltavski@gmail.com>
2021-03-08 14:59:53 +0300

    prop fixes

--- ceammc/ext/src/ui/ui_arrayview.cpp
+++ ceammc/ext/src/ui/ui_arrayview.cpp
@@ -654,12 +654,12 @@ void UIArrayView::output()
     }
 }
 
-t_float UIArrayView::cursorPosSample() const
+t_int UIArrayView::cursorPosSample() const
 {
     return cursor_sample_pos_;
 }
 
-void UIArrayView::setCursorPosSample(t_float pos)
+void UIArrayView::setCursorPosSample(t_int pos)
 {
     if (pos < 0) {
         UI_ERR << "invalid sample value: " << pos;
@@ -932,7 +932,7 @@ void UIArrayView::invalidateCursor()
     cursor_layer_.invalidate();
 }
 
-t_float UIArrayView::sizeSamples() const
+t_int UIArrayView::sizeSamples() const
 {
     if (!array_.open(prop_array))
         return 0;
@@ -954,14 +954,14 @@ t_float UIArrayView::sizeMs() const
     return sizeSec() * 1000;
 }
 
-AtomList UIArrayView::labelTopRight() const
+t_symbol* UIArrayView::labelTopRight() const
 {
-    return Atom(gensym(str_label_top_right_.c_str()));
+    return gensym(str_label_top_right_.c_str());
 }
 
-void UIArrayView::setLabelTopRight(const AtomListView& lv)
+void UIArrayView::setLabelTopRight(t_symbol* s)
 {
-    auto str = to_string(lv);
+    auto str = s->s_name;
     if (str != str_label_top_right_) {
         str_label_top_right_ = str;
         bg_layer_.invalidate();
@@ -969,14 +969,14 @@ void UIArrayView::setLabelTopRight(const AtomListView& lv)
     }
 }
 
-AtomList UIArrayView::labelBottomRight() const
+t_symbol* UIArrayView::labelBottomRight() const
 {
-    return Atom(gensym(str_label_bottom_right_.c_str()));
+    return gensym(str_label_bottom_right_.c_str());
 }
 
-void UIArrayView::setLabelBottomRight(const AtomListView& lv)
+void UIArrayView::setLabelBottomRight(t_symbol* s)
 {
-    auto str = to_string(lv);
+    auto str = s->s_name;
     if (str != str_label_bottom_right_) {
         str_label_bottom_right_ = str;
         bg_layer_.invalidate();
@@ -1040,9 +1040,9 @@ void UIArrayView::setup()
     obj.addProperty("label_top", &UIArrayView::labelTopRight, &UIArrayView::setLabelTopRight);
     obj.addProperty("label_bottom", &UIArrayView::labelBottomRight, &UIArrayView::setLabelBottomRight);
 
-    obj.addProperty("size_samp", &UIArrayView::sizeSamples, 0);
-    obj.addProperty("size_sec", &UIArrayView::sizeSec, 0);
-    obj.addProperty("size_ms", &UIArrayView::sizeMs, 0);
+    obj.addProperty("size_samp", &UIArrayView::sizeSamples);
+    obj.addProperty("size_sec", &UIArrayView::sizeSec);
+    obj.addProperty("size_ms", &UIArrayView::sizeMs);
     obj.setPropertyUnits(gensym("size_samp"), gensym("samp"));
     obj.setPropertyUnits(gensym("size_sec"), gensym("sec"));
     obj.setPropertyUnits(gensym("size_ms"), gensym("msec"));
--- ceammc/ext/src/ui/ui_arrayview.h
+++ ceammc/ext/src/ui/ui_arrayview.h
@@ -80,7 +80,7 @@ class UIArrayView : public UIObject {
     UILayer wave_layer_;
     ClockMemberFunction<UIArrayView> render_clock_;
     size_t render_index_;
-    long cursor_sample_pos_;
+    t_int cursor_sample_pos_;
     UIFont font_;
     UITextLayout label_top_left_;
     UITextLayout label_top_right_;
@@ -140,8 +140,8 @@ private:
     void drawWaveformSegment(UIPainter& p, int pixel_begin, int pixel_end, const t_rgba& color);
     void output();
 
-    t_float cursorPosSample() const;
-    void setCursorPosSample(t_float pos);
+    t_int cursorPosSample() const;
+    void setCursorPosSample(t_int pos);
 
     t_float cursorPosPhase() const;
     void setCursorPosPhase(t_float phase);
@@ -176,14 +176,14 @@ private:
     EditMode keyMod2EditMode(long mod, int x) const;
 
 public:
-    t_float sizeSamples() const;
+    t_int sizeSamples() const;
     t_float sizeSec() const;
     t_float sizeMs() const;
 
-    AtomList labelTopRight() const;
-    void setLabelTopRight(const AtomListView& lv);
-    AtomList labelBottomRight() const;
-    void setLabelBottomRight(const AtomListView& lv);
+    t_symbol* labelTopRight() const;
+    void setLabelTopRight(t_symbol* s);
+    t_symbol* labelBottomRight() const;
+    void setLabelBottomRight(t_symbol* s);
 
     AtomList propArray() const;
     void propSetArray(const AtomListView& lv);
