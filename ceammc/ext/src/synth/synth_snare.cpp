4661a958b60d1a9ce20ce1e6963a6a00bf948108
Serge Poltavski <serge.poltavski@gmail.com>
2021-05-02 23:24:36 +0300

    synth.snare~ updates

--- ceammc/ext/src/synth/synth_snare.cpp
+++ ceammc/ext/src/synth/synth_snare.cpp
@@ -1,15 +1,22 @@
 #include "synth_snare.h"
 #include "ceammc_clock.h"
+#include "ceammc_convert.h"
 #include "ceammc_factory.h"
 
 using namespace ceammc;
+
+constexpr t_float MIN_DUR = 10;
+constexpr t_float MAX_DUR = 1000;
+constexpr t_float DEFAULT_DUR = 200;
 
 class SynthSnare : public faust_synth_snare_tilde {
     UIProperty* gate_;
     UIProperty* attack_;
     UIProperty* decay_;
     UIProperty* release_;
-    ClockLambdaFunction clock_;
+    BoolProperty* env_wait_;
+    ClockLambdaFunction gate_timer_;
+    ClockLambdaFunction release_timer_;
 
 public:
     SynthSnare(const PdArgs& args)
@@ -18,17 +25,64 @@ public:
         , attack_(findProperty("@attack"))
         , decay_(findProperty("@decay"))
         , release_(findProperty("@release"))
-        , clock_([this]() { gate_->setValue(0); })
+        , env_wait_(nullptr)
+        , gate_timer_([this]() {
+            gate_->setValue(0);
+            // go to release phase
+            release_timer_.delay(release_->value());
+        })
+        , release_timer_([this]() { bangTo(1); })
     {
+        createOutlet();
+
+        env_wait_ = new BoolProperty("@envwait", false);
+        addProperty(env_wait_);
+
+        gate_->setSuccessFn([this](Property*) {
+            if (gate_->value() == 0) {
+                // go to release phase
+                release_timer_.delay(release_->value());
+            } else if (release_timer_.isActive())
+                release_timer_.unset();
+        });
     }
 
     void onBang() override
     {
-        if (gate_->value())
+        if (isBusy())
             return;
 
-        gate_->setValue(1);
-        clock_.delay(std::ceil(envLength() + 2));
+        play(1);
+    }
+
+    void onFloat(t_float gain) override
+    {
+        if (isBusy())
+            return;
+
+        play(gain);
+    }
+
+    void onList(const AtomList& lst) override
+    {
+        const auto N = lst.size();
+        const bool ok = (N >= 1 && N <= 2) && lst[0].isFloat();
+        if (!ok) {
+            OBJ_ERR << "usage: GAIN[0..1] DUR[ms]?";
+            return;
+        }
+
+        if (isBusy())
+            return;
+
+        const auto gain = lst[0].asT<t_float>();
+
+        if (N == 2) {
+            const auto rel = clip<t_float>(lst[1].asFloat(DEFAULT_DUR) - attackPhaseMs(), MIN_DUR, MAX_DUR);
+            release_->setValue(rel);
+        }
+
+        play(gain);
     }
 
 private:
@@ -37,9 +91,25 @@ private:
         return static_cast<UIProperty*>(property(gensym(name)));
     }
 
-    t_float envLength() const
+    t_float attackPhaseMs() const
     {
-        return attack_->value() + decay_->value() + release_->value();
+        return std::ceil(attack_->value() + decay_->value() + 4);
+    }
+
+    bool isBusy() const
+    {
+        return env_wait_->value() && (gate_timer_.isActive() || release_timer_.isActive());
+    }
+
+    void play(t_float gain)
+    {
+        if (gain <= 0)
+            return;
+
+        gate_->setValue(gain);
+        gate_timer_.delay(attackPhaseMs());
+        if (release_timer_.isActive())
+            release_timer_.unset();
     }
 };
 
@@ -47,4 +117,9 @@ void setup_synth_snare_tilde()
 {
     SoundExternalFactory<SynthSnare> obj("synth.snare~", OBJECT_FACTORY_DEFAULT);
     obj.addMethod("reset", &SynthSnare::m_reset);
+
+    obj.setXletsInfo({ "bang: play snare\n"
+                       "float: play with GAIN\n"
+                       "list: play with GAIN DUR" },
+        { "audio: output" });
 }
