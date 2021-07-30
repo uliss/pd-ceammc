b5f1f02388772db264765185aa223ca656af25e1
Serge Poltavski <serge.poltavski@gmail.com>
2021-07-30 20:23:51 +0300

    path.dirname: data:string support

--- ceammc/ext/src/path/path_dirname.cpp
+++ ceammc/ext/src/path/path_dirname.cpp
@@ -1,6 +1,8 @@
+#include "ceammc_data.h"
 #include "ceammc_factory.h"
 #include "ceammc_object.h"
 #include "ceammc_platform.h"
+#include "datatype_string.h"
 
 using namespace ceammc;
 
@@ -16,9 +18,15 @@ public:
     {
         symbolTo(0, gensym(platform::dirname(s->s_name).c_str()));
     }
+
+    void onDataT(const StringAtom& str)
+    {
+        atomTo(0, StringAtom(platform::dirname(str->str().c_str())));
+    }
 };
 
 void setup_path_dirname()
 {
     ObjectFactory<PathDirname> obj("path.dirname");
+    obj.processData<DataTypeString>();
 }
