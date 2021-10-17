9849512a2b05f1cde0879317dd0ee0e58cb267fe
Serge Poltavski <serge.poltavski@gmail.com>
2021-10-18 02:53:48 +0300

    ui menu update

--- ceammc/gui/plugins/ceammc_popup_menu_tree.tcl
+++ ceammc/gui/plugins/ceammc_popup_menu_tree.tcl
@@ -55,12 +55,12 @@
         array.p2s array.s2p conv.amp2dbfs conv.amp2dbfs~ conv.bits2bang
         conv.bits2int conv.bits2note conv.bits2pos conv.bpm2hz conv.bpm2ms
         conv.bpm2sec conv.car2pol conv.cc2amp conv.dbfs2amp conv.dbfs2amp~
-        conv.degree2key conv.hex2int conv.int2bits conv.lin2curve conv.lin2exp
-        conv.lin2lin conv.lin2lin~ conv.list2props conv.midi2freq conv.ms2bpm
-        conv.ms2samp conv.ms2samp~ conv.phase2rad conv.phase2rad~
-        conv.pitch2midi conv.pol2car conv.rad2phase conv.rad2phase~
-        conv.samp2ms conv.samp2ms~ conv.samp2sec conv.sec2bpm conv.sec2samp
-        conv.sec2str conv.sig2float~ conv.str2sec music.dur2time
+        conv.degree2key conv.edge2bang conv.hex2int conv.int2bits
+        conv.lin2curve conv.lin2exp conv.lin2lin conv.lin2lin~ conv.list2props
+        conv.midi2freq conv.ms2bpm conv.ms2samp conv.ms2samp~ conv.phase2rad
+        conv.phase2rad~ conv.pitch2midi conv.pol2car conv.rad2phase
+        conv.rad2phase~ conv.samp2ms conv.samp2ms~ conv.samp2sec conv.sec2bpm
+        conv.sec2samp conv.sec2str conv.sig2float~ conv.str2sec music.dur2time
         music.voice2midi
     }}
     {data {
