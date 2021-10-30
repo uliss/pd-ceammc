25db9c9434d1243023bec981d4de102fd77c0581
Serge Poltavski <serge.poltavski@gmail.com>
2021-10-30 12:44:41 +0300

    win: mingw mlir package added

--- build/win/install_deps_mingw32.sh
+++ build/win/install_deps_mingw32.sh
@@ -9,5 +9,5 @@ pacman -Syu --noconfirm
 # build tools
 pacman -S --needed --noconfirm mingw-w64-${ARCH}-toolchain git make patch
 # dependencies
-pacman -S --needed --noconfirm mingw-w64-${ARCH}-{cmake,ninja,glib2,libmodplug,tcllib,tcl,tk,tklib,portaudio,fftw,libsndfile,boost,dlfcn,armadillo,llvm,polly,rubberband}
+pacman -S --needed --noconfirm mingw-w64-${ARCH}-{cmake,ninja,glib2,libmodplug,tcllib,tcl,tk,tklib,portaudio,fftw,libsndfile,boost,dlfcn,armadillo,llvm,mlir,polly,rubberband}
 
