452cc039dae0dd00c71e6d77c86b9d63b13c47af
Serge Poltavski <serge.poltavski@gmail.com>
2021-11-16 22:54:49 +0300

    doc: release note update

--- ceammc/distrib/make_release_patch.py
+++ ceammc/distrib/make_release_patch.py
@@ -4,6 +4,23 @@ import sys
 import os
 import re
 
+STATE_CTL_BEGIN = 0
+STATE_CTL_NONE = -1
+
+ctl_state = STATE_CTL_NONE
+
+def ctl_begin():
+    global ctl_state
+    ctl_state = STATE_CTL_BEGIN
+    print("1 # [;")
+
+def ctl_end():
+    global ctl_state
+    ctl_state = STATE_CTL_NONE
+    print("next 1;")
+    print("# ];")
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("usage: {0}" % (sys.argv[0]))
@@ -15,7 +32,17 @@ if __name__ == "__main__":
 
     n = 0
     for l in lines:
-        if len(l) > 0 and l[0] == '#':
+        if len(l) < 1:
+            continue
+
+        if l[0:2] == "//": # comment
+            continue
+
+        elif l[0] == '#': # CTL
+
+            if ctl_state == STATE_CTL_NONE:
+                ctl_begin()
+
             m = re.match(r'#\s*([^\s]+)\s+(.*)', l)
             g1 = m.group(1)
             if g1 == "voice":
@@ -24,7 +51,12 @@ if __name__ == "__main__":
                 msg = m.group(1)
 
             print("{0} {1};".format(msg, m.group(2)))
-        elif len(l) > 0 and l[0] == '.':
+
+        elif l[0] == '.': # command
+
+            if ctl_state == STATE_CTL_BEGIN:
+                ctl_end()
+
             l = l.replace(',', ' \\, ')
             l = l.replace(':', '')
 
@@ -37,9 +69,5 @@ if __name__ == "__main__":
             else:
                 msg = m.group(1)
 
-            if n == 0:
-                print("{1} {2};".format(n, msg, m.group(2)))
-            else:
-                print("{0} {1} {2};".format(n, msg, m.group(2)))
-
-            n += 1
+            print("{0} {1} {2};".format(n, msg, m.group(2)))
+            n += 10
