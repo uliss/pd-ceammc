c26168af5a4550b6a642a75c3def2b6116890f26
Serge Poltavski <serge.poltavski@gmail.com>
2021-11-19 07:14:13 +0300

    mac: deploy scripts update

---
+++ ceammc/distrib/mac/build_all.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+build_target() {
+    tput setaf 2
+    echo "build target: '$1'"
+    tput sgr0
+    @CMAKE_COMMAND@ --build "@PROJECT_BINARY_DIR@" --target $1
+}
+
+rm -f "@PROJECT_BINARY_DIR@/dist/"*.app
+rm -f "@PROJECT_BINARY_DIR@/dist/"*.zip
+rm -f "@PROJECT_BINARY_DIR@/dist/"*.dmg
+
+build_target dev_touch_version
+build_target all
+build_target dmg
+build_target app-zip
+build_target ceammc_lib
