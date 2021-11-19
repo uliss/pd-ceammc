6d2d5c911e4c534d347d0b2493f2135530900fe3
Serge Poltavski <serge.poltavski@gmail.com>
2021-11-19 03:24:41 +0300

    win: dev_fixdoc target added

--- ceammc/distrib/win/win_fix_pddoc_key_modifiers.sh
+++ ceammc/distrib/win/win_fix_pddoc_key_modifiers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-SRCDIR="@PROJECT_SOURCE_DIR@/ceammc"
+
 BINDIR="@CMAKE_INSTALL_PREFIX@"
 
-echo "Fixing help files in ${SRCDIR} ..."
-find "${BINDIR}/extra/ceammc" -name *-help\\.pd | while read file
+echo "Fixing help files in ${BINDIR} ..."
+find "${BINDIR}/extra/ceammc" -name '*-help\.pd' | while read file
 do
     help=$(basename $file)
     sed -i -e 's/⌘/⌃/g' "$file"
