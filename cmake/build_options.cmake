7fd40dbbfe247f4eb9cf9886e3ba5f8c9850bbdd
Serge Poltavski <serge.poltavski@gmail.com>
2022-06-18 02:05:02 +0300

    RHVoice build options added

--- cmake/build_options.cmake
+++ cmake/build_options.cmake
@@ -5,7 +5,8 @@ option(WITH_BENCHMARK "Build with benchmarks" ON)
 option(WITH_CLANG_TIDY "Build with clang-tidy checks" OFF)
 
 # externals
-option(WITH_TTS_FLITE  "Build with Flite TTS support" ON)
+option(WITH_TTS_FLITE    "Build with Flite TTS support" ON)
+option(WITH_TTS_RHVOICE  "Build with RHVoice TTS support" OFF)
 option(WITH_FLUIDSYNTH "Build with FluidSynth support" ON)
 option(WITH_SFIZZ      "Build with Sfizz support" ON)
 option(WITH_FFTW "Use fftw3 library (http://www.fftw.org/)" ON)
