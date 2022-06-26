e326f038bbdac7b5bfbae35fbd68b44634766206
Serge Poltavski <serge.poltavski@gmail.com>
2022-06-26 15:01:44 +0300

    trim fixes

--- ceammc/ext/src/data/editor_data.cpp
+++ ceammc/ext/src/data/editor_data.cpp
@@ -27,10 +27,48 @@ void editorAppend(EditorLineList& res, const DataTypeDict& dict, int indentLevel
             str->append(kv.second);
         }
 
+        str->trim();
         str->append(']');
 
         res.push_back(str);
     } else {
+        auto s_ob = EditorStringPool::pool().allocate();
+        auto s_cb = EditorStringPool::pool().allocate();
+        const auto nspaces_0 = indentLevel * 4;
+        const auto nspaces_1 = (indentLevel + 1) * 4;
+        const std::string indent_0 = std::string(nspaces_0, ' ');
+        const std::string indent_1 = std::string(nspaces_1, ' ');
+
+        s_ob->append(indent_0.c_str());
+        s_ob->append('[');
+        s_cb->append(indent_0.c_str());
+        s_cb->append(']');
+
+        res.push_back(s_ob);
+
+        for (auto& kv : dict) {
+            auto str = EditorStringPool::pool().allocate();
+            str->append(indent_1.c_str());
+
+            str->append(kv.first->s_name);
+            str->append(':');
+            str->append(' ');
+
+            auto& l = kv.second;
+            const bool simple_content = std::all_of(l.begin(), l.end(), [](const Atom& a) { return !a.isData(); });
+
+            if (simple_content) {
+                for (auto& a : l) {
+                    str->append(a);
+                    str->append(' ');
+                }
+
+                str->trim();
+                res.push_back(str);
+            }
+        }
+
+        res.push_back(s_cb);
     }
 }
 
@@ -51,9 +89,7 @@ void editorAppend(EditorLineList& res, const DataTypeMList& lst, int indentLevel
             str->append(' ');
         }
 
-        if (str->length() > 1)
-            str->pop();
-
+        str->trim();
         str->append(")");
 
         res.push_back(str);
@@ -71,8 +107,6 @@ void editorAppend(EditorLineList& res, const DataTypeMList& lst, int indentLevel
         s_cb->append(")");
 
         res.push_back(s_ob);
-
-        const std::string indent(indentLevel, ' ');
 
         for (const Atom& a : lst) {
             auto str = EditorStringPool::pool().allocate();
@@ -93,5 +127,4 @@ void editorAppend(EditorLineList& res, const DataTypeMList& lst, int indentLevel
         res.push_back(s_cb);
     }
 }
-
 }
