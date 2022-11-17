bb7a11cb9aa60bd34460e224e092d3192bd60e2b
Serge Poltavski <serge.poltavski@gmail.com>
2022-11-18 01:04:14 +0300

    Update speech_rhvoice_tilde_stub.cpp

--- ceammc/ext/src/misc/speech_rhvoice_tilde_stub.cpp
+++ ceammc/ext/src/misc/speech_rhvoice_tilde_stub.cpp
@@ -1,3 +1,4 @@
 #include "ceammc_stub.h"
 
-AUDIO_OBJECT_STUB("speech.rhvoice~", 0, 1, 1, 1, "compiled without RHVoice support", speech_rhvoice_tilde);
+AUDIO_OBJECT_STUB(0, 1, 1, 1, "compiled without RHVoice support");
+OBJECT_STUB_SETUP("speech.rhvoice~", speech_rhvoice_tilde, "rhvoice~", "rhvoice~");
