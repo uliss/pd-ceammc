6086f97bbce79383c387aa5be657ba8d421c2c0a
Serge Poltavski <serge.poltavski@gmail.com>
2022-11-25 22:24:52 +0300

    tcl: object text color

--- tcl/pd_colors.tcl
+++ tcl/pd_colors.tcl
@@ -2,17 +2,17 @@ package provide pd_colors 0.1
 
 namespace eval ::pd_colors:: {
     variable window_background "lightgray"
-    variable text_background "white"
-    variable xlet_signal "#2200AA"
-    variable xlet_control "#555555"
-    variable obj_fill "#F5F8F8"
-    variable obj_text "#444444"
-    variable obj_border "#BFBFBF"
-    variable log_print "black"
-    variable log_debug "#484848"
-    variable cord_control "#444444"
-    variable cord_signal "#2200AA"
-
+    variable text_background    "white"
+    variable xlet_signal        "#2200AA"
+    variable xlet_control       "#555555"
+    variable obj_fill           "#F5F8F8"
+    variable obj_text           "#444444"
+    variable obj_text_selected  "#0022EE"
+    variable obj_border         "#BFBFBF"
+    variable log_print          "black"
+    variable log_debug          "#484848"
+    variable cord_control       "#444444"
+    variable cord_signal        "#2200AA"
 }
 
 if {[tk windowingsystem] eq "aqua"} {
@@ -21,7 +21,8 @@ if {[tk windowingsystem] eq "aqua"} {
     set ::pd_colors::log_print systemTextColor
     set ::pd_colors::log_debug systemControlAccentColor
     set ::pd_colors::xlet_control systemWindowBackgroundColor7
-    set ::pd_colors::obj_text systemWindowBackgroundColor7
+    set ::pd_colors::obj_text systemTextColor
+    set ::pd_colors::obj_text_selected systemControlAccentColor
     set ::pd_colors::obj_fill systemWindowBackgroundColor
     set ::pd_colors::obj_border systemWindowBackgroundColor6
     set ::pd_colors::cord_control systemWindowBackgroundColor6
