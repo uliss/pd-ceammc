57b54d760ee5b4c2d2f19e430d20a0532b573dc2
Serge Poltavski <serge.poltavski@gmail.com>
2022-11-25 23:03:51 +0300

    tcl color update

--- tcl/pd_colors.tcl
+++ tcl/pd_colors.tcl
@@ -3,7 +3,7 @@ package provide pd_colors 0.1
 namespace eval ::pd_colors:: {
     variable window_background "lightgray"
     variable text_background    "white"
-    variable xlet_signal        "#2200AA"
+    variable xlet_signal        "#110088"
     variable xlet_control       "#555555"
     variable obj_fill           "#F5F8F8"
     variable obj_text           "#444444"
@@ -11,8 +11,8 @@ namespace eval ::pd_colors:: {
     variable obj_border         "#BFBFBF"
     variable log_print          "black"
     variable log_debug          "#484848"
-    variable cord_control       "#444444"
-    variable cord_signal        "#2200AA"
+    variable cord_normal        "#555555"
+    variable cord_selected      "#1441E6"
     variable comment_border     "#BFBFBF"
 }
 
@@ -26,6 +26,5 @@ if {[tk windowingsystem] eq "aqua"} {
     set ::pd_colors::obj_text_selected systemControlAccentColor
     set ::pd_colors::obj_fill systemWindowBackgroundColor
     set ::pd_colors::obj_border systemWindowBackgroundColor6
-    set ::pd_colors::cord_control systemWindowBackgroundColor6
     set ::pd_colors::comment_border systemWindowBackgroundColor6
 }
