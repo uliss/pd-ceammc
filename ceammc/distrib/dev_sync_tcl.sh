893a82c22b1d0204fa94b79a48d9b5381a13f85d
Serge Poltavski <serge.poltavski@gmail.com>
2022-11-30 01:16:14 +0300

    macos dev fix

--- ceammc/distrib/dev_sync_tcl.sh
+++ ceammc/distrib/dev_sync_tcl.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 DIST_DIR="@PROJECT_BINARY_DIR@/dist"
-APP_DIR="${DIST_DIR}/@PD_MACOSX_APP@"
-TCL_DIR="${APP_DIR}/Contents/Resources/tcl"
+if [ `uname` == "Darwin" ]
+then
+    APP_DIR="${DIST_DIR}/@PD_MACOSX_APP@"
+    TCL_DIR="${APP_DIR}/Contents/Resources/tcl"
+else
+    TCL_DIR="@CMAKE_INSTALL_DIR@/@PD_GUI_PLUGINS_PATH@"
+fi
 
-cp @PROJECT_SOURCE_DIR@/tcl/*.tcl "${TCL_DIR}/"
+cp -v @PROJECT_SOURCE_DIR@/tcl/*.tcl "${TCL_DIR}/"
