9ad4d8bcadc5cb930a89ba16806d0b6f710e59c9
Serge Poltavski <serge.poltavski@gmail.com>
2022-11-30 06:54:34 +0300

    win: colors update

--- tcl/pd_colors.tcl
+++ tcl/pd_colors.tcl
@@ -20,18 +20,35 @@ namespace eval ::pd_colors:: {
     variable msg_clicked            "#00AAFF" 
 }
 
-if {[tk windowingsystem] eq "aqua"} {
-    set ::pd_colors::window_background  systemWindowBackgroundColor
-    set ::pd_colors::window_transparent systemTransparent
-    set ::pd_colors::text               systemTextColor
-    set ::pd_colors::text_background    systemTextBackgroundColor
-    set ::pd_colors::log_print          systemTextColor
-    set ::pd_colors::log_debug          systemControlAccentColor
-    set ::pd_colors::xlet_control       systemWindowBackgroundColor7
-    set ::pd_colors::obj_text           systemTextColor
-    set ::pd_colors::obj_text_selected  systemControlAccentColor
-    set ::pd_colors::obj_fill           systemWindowBackgroundColor
-    set ::pd_colors::obj_border         systemWindowBackgroundColor6
-    set ::pd_colors::msg_clicked        systemControlAccentColor
-    set ::pd_colors::comment_border     systemWindowBackgroundColor6
+switch -- [tk windowingsystem] {
+    "aqua" {
+        set ::pd_colors::window_background  systemWindowBackgroundColor
+        set ::pd_colors::window_transparent systemTransparent
+        set ::pd_colors::text               systemTextColor
+        set ::pd_colors::text_background    systemTextBackgroundColor
+        set ::pd_colors::log_print          systemTextColor
+        set ::pd_colors::log_debug          systemControlAccentColor
+        set ::pd_colors::xlet_control       systemWindowBackgroundColor7
+        set ::pd_colors::obj_text           systemTextColor
+        set ::pd_colors::obj_text_selected  systemControlAccentColor
+        set ::pd_colors::obj_fill           systemWindowBackgroundColor
+        set ::pd_colors::obj_border         systemWindowBackgroundColor6
+        set ::pd_colors::msg_clicked        systemControlAccentColor
+        set ::pd_colors::comment_border     systemWindowBackgroundColor6
+    }
+    "win32" {
+        set ::pd_colors::window_background  systemWindow
+        set ::pd_colors::window_transparent systemWindow
+        set ::pd_colors::text               systemWindowText
+        set ::pd_colors::text_background    systemWindow
+        set ::pd_colors::log_print          systemWindowText
+        set ::pd_colors::log_debug          systemDisabledText
+        set ::pd_colors::xlet_control       system3dDarkShadow
+        set ::pd_colors::obj_text           systemWindowText
+        set ::pd_colors::obj_text_selected  systemHighlight
+        set ::pd_colors::obj_fill           systemMenu
+        set ::pd_colors::obj_border         system3dDarkShadow
+        set ::pd_colors::msg_clicked        systemHighlight
+    }
 }
+
