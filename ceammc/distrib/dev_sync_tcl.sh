a2f88e1347834d8847615e42f270c5e7f8ea5615
Serge Poltavski <serge.poltavski@gmail.com>
2022-11-30 06:24:11 +0300

    cmake: var fix

--- ceammc/distrib/dev_sync_tcl.sh
+++ ceammc/distrib/dev_sync_tcl.sh
@@ -6,7 +6,7 @@ then
     APP_DIR="${DIST_DIR}/@PD_MACOSX_APP@"
     TCL_DIR="${APP_DIR}/Contents/Resources/tcl"
 else
-    TCL_DIR="@CMAKE_INSTALL_DIR@/@PD_GUI_PLUGINS_PATH@"
+    TCL_DIR="@CMAKE_INSTALL_PREFIX@/@PD_GUI_PLUGINS_PATH@"
 fi
 
 cp -v @PROJECT_SOURCE_DIR@/tcl/*.tcl "${TCL_DIR}/"
