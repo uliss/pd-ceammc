0161833b06c5ac60885e3817659dc3191d025de7
Serge Poltavski <serge.poltavski@gmail.com>
2023-01-06 02:44:42 +0300

    doc check update

--- ceammc/ext/src/ui/ui_meter.cpp
+++ ceammc/ext/src/ui/ui_meter.cpp
@@ -223,6 +223,7 @@ void UIMeter::setup()
     obj.addColorProperty("over_color", _("Overload signal color"), "1 0 0 1", &UIMeter::prop_color_over);
 
     obj.addIntProperty("interval", _("Refresh interval (ms)"), 50, &UIMeter::prop_interval_ms, _("Main"));
+    obj.setPropertyMin("interval", 20);
     obj.setPropertyUnits("interval", "msec");
 }
 
