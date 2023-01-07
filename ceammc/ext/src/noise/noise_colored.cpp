76782e2f19ad3240e9506dbcde4615731b06196c
Serge Poltavski <serge.poltavski@gmail.com>
2023-01-07 12:56:57 +0300

    noise.colored~ added

---
+++ ceammc/ext/src/noise/noise_colored.cpp
@@ -0,0 +1,24 @@
+#include "noise_colored.h"
+#include "ceammc_factory.h"
+
+using namespace ceammc;
+
+class NoiseColored : public faust_noise_colored_tilde {
+public:
+    NoiseColored(const PdArgs& args)
+        : faust_noise_colored_tilde(args)
+    {
+        bindPositionalArgsToProps({ gensym("@alpha") });
+    }
+
+    void onFloat(t_float f) override
+    {
+        setProperty(gensym("@alpha"), AtomList(Atom(f)));
+    }
+};
+
+void setup_noise_colored_tilde()
+{
+    SoundExternalFactory<NoiseColored> obj("noise.colored~", OBJECT_FACTORY_DEFAULT);
+    obj.setXletsInfo({ "float: set roll-off factor" }, { "signal: output" });
+}
