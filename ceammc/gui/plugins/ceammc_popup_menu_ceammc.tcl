905a6175453ff52d5de28c3969f8f39a11b986dc
Serge Poltavski <serge.poltavski@gmail.com>
2023-01-07 03:37:58 +0300

    ui update

--- ceammc/gui/plugins/ceammc_popup_menu_ceammc.tcl
+++ ceammc/gui/plugins/ceammc_popup_menu_ceammc.tcl
@@ -71,7 +71,7 @@
         fx.bitdown~ fx.chorus~ fx.distortion1~ fx.distortion2~ fx.distortion3~
         fx.distortion~ fx.drive~ fx.drone_box~ fx.echo2~ fx.echo~ fx.flanger~
         fx.freeverb2~ fx.freeverb~ fx.freqshift~ fx.granulator~ fx.greyhole~
-        fx.infrev~ fx.looper~ fx.pitchshift_s~ fx.pitchshift~
+        fx.infrev~ fx.jcrev~ fx.looper~ fx.pitchshift_s~ fx.pitchshift~
         fx.rb_pitchshift~ fx.recho~ fx.room~ fx.satrev~ fx.sdelay~ fx.secho~
         fx.shimmer~ fx.stutter~ fx.tapiir~ fx.vocoder~ fx.wahwah~
         fx.zita_rev1~
