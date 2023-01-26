1f7d61729a481a069a97dbd09f5090155df1ed9f
Serge Poltavski <serge.poltavski@gmail.com>
2023-01-26 04:53:05 +0300

    cleanup includes

--- ceammc/ext/src/base/is_float.cpp
+++ ceammc/ext/src/base/is_float.cpp
@@ -1,4 +1,4 @@
-#include <m_pd.h>
+#include "m_pd.h"
 
 t_class* is_float_class;
 struct t_is_float {
--- ceammc/ext/src/base/is_list.cpp
+++ ceammc/ext/src/base/is_list.cpp
@@ -1,4 +1,4 @@
-#include <m_pd.h>
+#include "m_pd.h"
 
 t_class* is_list_class;
 struct t_is_list {
