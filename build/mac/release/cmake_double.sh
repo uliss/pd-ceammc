d02a8ffa7885c61bc1321e95144807ced8423a2a
Serge Poltavski <serge.poltavski@gmail.com>
2023-02-04 16:17:51 +0300

    mac m1: cmake updates

--- build/mac/release/cmake_double.sh
+++ build/mac/release/cmake_double.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export LLVM_DIR=/opt/local/libexec/llvm-10/lib/cmake/llvm
 # M1 apple check (Faust not build at this moment)
 BUILD_WITH_FAUST=ON
 if [[ $(uname -m) == 'arm64' ]]; then
-  BUILD_WITH_FAUST=OFF
+  BUILD_WITH_FAUST=ON
+  export LLVM_DIR=/opt/local/libexec/llvm-11/lib/cmake/llvm
 fi
 
 cmake -G Ninja \
