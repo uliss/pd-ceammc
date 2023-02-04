178185f024b1af0f9629d78c01fc0ab48d1ba59e
Serge Poltavski <serge.poltavski@gmail.com>
2023-02-04 12:50:32 +0300

    pacman fixes

--- ceammc/distrib/appveyor/install.bat
+++ ceammc/distrib/appveyor/install.bat
@@ -16,7 +16,7 @@ REM Update
 bash -lc "pacman -Syu --noconfirm"
 
 REM build tools
-bash -lc "pacman -S --needed --noconfirm mingw-w64-${MSYS2_ARCH}-toolchain git make patch"
+bash -lc "pacman -S --needed --noconfirm mingw-w64-${MSYS2_ARCH}-{binutils,crt,gcc,gcc-libs,headers-git,libgccjit,libmangle-git,libwinpthread-git,tools-git,winpthreads-git,winstorecompat-git} git make patch"
 REM dependencies
 bash -lc "pacman -S --needed --noconfirm mingw-w64-${MSYS2_ARCH}-{cmake,ninja,glib2,libmodplug,tcllib,tcl,tk,tklib,portaudio,fftw,libsndfile,boost,dlfcn,armadillo,llvm,polly}"
 
