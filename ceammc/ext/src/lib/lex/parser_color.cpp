d2c9879e44326db4b40204c31c8e520948107031
Serge Poltavski <serge.poltavski@gmail.com>
2023-03-09 04:04:04 +0300

    new named colors added

--- ceammc/ext/src/lib/lex/parser_color.cpp
+++ ceammc/ext/src/lib/lex/parser_color.cpp
@@ -896,7 +896,7 @@ bool ColorFullMatch::parse(const Atom& a)
 
 #line 898 "lex/parser_color.cpp"
 static const int named_color_start = 1;
-static const int named_color_first_final = 17;
+static const int named_color_first_final = 57;
 static const int named_color_error = 0;
 
 static const int named_color_en_main = 1;
@@ -945,9 +945,16 @@ bool ColorFullMatch::parse(const char* str)
 case 1:
 	switch( (*p) ) {
 		case 98: goto st2;
-		case 103: goto st7;
-		case 114: goto st11;
-		case 119: goto st13;
+		case 99: goto st7;
+		case 103: goto st10;
+		case 108: goto st17;
+		case 109: goto st20;
+		case 110: goto st26;
+		case 111: goto st29;
+		case 112: goto st37;
+		case 114: goto st46;
+		case 119: goto st48;
+		case 121: goto st52;
 	}
 	goto st0;
 st0:
@@ -981,63 +988,65 @@ st5:
 		goto _test_eof5;
 case 5:
 	if ( (*p) == 107 )
-		goto st17;
+		goto st57;
 	goto st0;
-st17:
+st57:
 	if ( ++p == pe )
-		goto _test_eof17;
-case 17:
+		goto _test_eof57;
+case 57:
 	goto st0;
 st6:
 	if ( ++p == pe )
 		goto _test_eof6;
 case 6:
 	if ( (*p) == 101 )
-		goto st18;
+		goto st58;
 	goto st0;
-st18:
+st58:
 	if ( ++p == pe )
-		goto _test_eof18;
-case 18:
+		goto _test_eof58;
+case 58:
 	goto st0;
 st7:
 	if ( ++p == pe )
 		goto _test_eof7;
 case 7:
-	if ( (*p) == 114 )
+	if ( (*p) == 121 )
 		goto st8;
 	goto st0;
 st8:
 	if ( ++p == pe )
 		goto _test_eof8;
 case 8:
-	if ( (*p) == 101 )
+	if ( (*p) == 97 )
 		goto st9;
 	goto st0;
 st9:
 	if ( ++p == pe )
 		goto _test_eof9;
 case 9:
-	if ( (*p) == 101 )
-		goto st10;
+	if ( (*p) == 110 )
+		goto st59;
+	goto st0;
+st59:
+	if ( ++p == pe )
+		goto _test_eof59;
+case 59:
 	goto st0;
 st10:
 	if ( ++p == pe )
 		goto _test_eof10;
 case 10:
-	if ( (*p) == 110 )
-		goto st19;
-	goto st0;
-st19:
-	if ( ++p == pe )
-		goto _test_eof19;
-case 19:
+	switch( (*p) ) {
+		case 111: goto st11;
+		case 114: goto st13;
+	}
 	goto st0;
 st11:
 	if ( ++p == pe )
 		goto _test_eof11;
 case 11:
-	if ( (*p) == 101 )
+	if ( (*p) == 108 )
 		goto st12;
 	goto st0;
 st12:
@@ -1045,93 +1054,522 @@ st12:
 		goto _test_eof12;
 case 12:
 	if ( (*p) == 100 )
-		goto st20;
+		goto st60;
 	goto st0;
-st20:
+st60:
 	if ( ++p == pe )
-		goto _test_eof20;
-case 20:
+		goto _test_eof60;
+case 60:
 	goto st0;
 st13:
 	if ( ++p == pe )
 		goto _test_eof13;
 case 13:
-	if ( (*p) == 104 )
-		goto st14;
+	switch( (*p) ) {
+		case 97: goto st14;
+		case 101: goto st15;
+	}
 	goto st0;
 st14:
 	if ( ++p == pe )
 		goto _test_eof14;
 case 14:
-	if ( (*p) == 105 )
-		goto st15;
+	if ( (*p) == 121 )
+		goto st61;
+	goto st0;
+st61:
+	if ( ++p == pe )
+		goto _test_eof61;
+case 61:
 	goto st0;
 st15:
 	if ( ++p == pe )
 		goto _test_eof15;
 case 15:
-	if ( (*p) == 116 )
+	if ( (*p) == 101 )
 		goto st16;
 	goto st0;
 st16:
 	if ( ++p == pe )
 		goto _test_eof16;
 case 16:
+	if ( (*p) == 110 )
+		goto st62;
+	goto st0;
+st62:
+	if ( ++p == pe )
+		goto _test_eof62;
+case 62:
+	goto st0;
+st17:
+	if ( ++p == pe )
+		goto _test_eof17;
+case 17:
+	if ( (*p) == 105 )
+		goto st18;
+	goto st0;
+st18:
+	if ( ++p == pe )
+		goto _test_eof18;
+case 18:
+	if ( (*p) == 109 )
+		goto st19;
+	goto st0;
+st19:
+	if ( ++p == pe )
+		goto _test_eof19;
+case 19:
 	if ( (*p) == 101 )
+		goto st63;
+	goto st0;
+st63:
+	if ( ++p == pe )
+		goto _test_eof63;
+case 63:
+	goto st0;
+st20:
+	if ( ++p == pe )
+		goto _test_eof20;
+case 20:
+	if ( (*p) == 97 )
 		goto st21;
 	goto st0;
 st21:
 	if ( ++p == pe )
 		goto _test_eof21;
 case 21:
+	if ( (*p) == 103 )
+		goto st22;
+	goto st0;
+st22:
+	if ( ++p == pe )
+		goto _test_eof22;
+case 22:
+	if ( (*p) == 101 )
+		goto st23;
+	goto st0;
+st23:
+	if ( ++p == pe )
+		goto _test_eof23;
+case 23:
+	if ( (*p) == 110 )
+		goto st24;
+	goto st0;
+st24:
+	if ( ++p == pe )
+		goto _test_eof24;
+case 24:
+	if ( (*p) == 116 )
+		goto st25;
+	goto st0;
+st25:
+	if ( ++p == pe )
+		goto _test_eof25;
+case 25:
+	if ( (*p) == 97 )
+		goto st64;
+	goto st0;
+st64:
+	if ( ++p == pe )
+		goto _test_eof64;
+case 64:
+	goto st0;
+st26:
+	if ( ++p == pe )
+		goto _test_eof26;
+case 26:
+	if ( (*p) == 97 )
+		goto st27;
+	goto st0;
+st27:
+	if ( ++p == pe )
+		goto _test_eof27;
+case 27:
+	if ( (*p) == 118 )
+		goto st28;
+	goto st0;
+st28:
+	if ( ++p == pe )
+		goto _test_eof28;
+case 28:
+	if ( (*p) == 121 )
+		goto st65;
+	goto st0;
+st65:
+	if ( ++p == pe )
+		goto _test_eof65;
+case 65:
+	goto st0;
+st29:
+	if ( ++p == pe )
+		goto _test_eof29;
+case 29:
+	switch( (*p) ) {
+		case 108: goto st30;
+		case 114: goto st33;
+	}
+	goto st0;
+st30:
+	if ( ++p == pe )
+		goto _test_eof30;
+case 30:
+	if ( (*p) == 105 )
+		goto st31;
+	goto st0;
+st31:
+	if ( ++p == pe )
+		goto _test_eof31;
+case 31:
+	if ( (*p) == 118 )
+		goto st32;
+	goto st0;
+st32:
+	if ( ++p == pe )
+		goto _test_eof32;
+case 32:
+	if ( (*p) == 101 )
+		goto st66;
+	goto st0;
+st66:
+	if ( ++p == pe )
+		goto _test_eof66;
+case 66:
+	goto st0;
+st33:
+	if ( ++p == pe )
+		goto _test_eof33;
+case 33:
+	if ( (*p) == 97 )
+		goto st34;
+	goto st0;
+st34:
+	if ( ++p == pe )
+		goto _test_eof34;
+case 34:
+	if ( (*p) == 110 )
+		goto st35;
+	goto st0;
+st35:
+	if ( ++p == pe )
+		goto _test_eof35;
+case 35:
+	if ( (*p) == 103 )
+		goto st36;
+	goto st0;
+st36:
+	if ( ++p == pe )
+		goto _test_eof36;
+case 36:
+	if ( (*p) == 101 )
+		goto st67;
+	goto st0;
+st67:
+	if ( ++p == pe )
+		goto _test_eof67;
+case 67:
+	goto st0;
+st37:
+	if ( ++p == pe )
+		goto _test_eof37;
+case 37:
+	if ( (*p) == 105 )
+		goto st38;
+	goto st0;
+st38:
+	if ( ++p == pe )
+		goto _test_eof38;
+case 38:
+	if ( (*p) == 110 )
+		goto st39;
+	goto st0;
+st39:
+	if ( ++p == pe )
+		goto _test_eof39;
+case 39:
+	if ( (*p) == 107 )
+		goto st40;
+	goto st0;
+st40:
+	if ( ++p == pe )
+		goto _test_eof40;
+case 40:
+	if ( (*p) == 112 )
+		goto tr54;
+	goto st0;
+tr54:
+#line 86 "lex/ragel_color.rl"
+	{ color.setHex(0xFFC0CB); }
+	goto st41;
+st41:
+	if ( ++p == pe )
+		goto _test_eof41;
+case 41:
+#line 1308 "lex/parser_color.cpp"
+	if ( (*p) == 117 )
+		goto st42;
+	goto st0;
+st42:
+	if ( ++p == pe )
+		goto _test_eof42;
+case 42:
+	if ( (*p) == 114 )
+		goto st43;
+	goto st0;
+st43:
+	if ( ++p == pe )
+		goto _test_eof43;
+case 43:
+	if ( (*p) == 112 )
+		goto st44;
+	goto st0;
+st44:
+	if ( ++p == pe )
+		goto _test_eof44;
+case 44:
+	if ( (*p) == 108 )
+		goto st45;
+	goto st0;
+st45:
+	if ( ++p == pe )
+		goto _test_eof45;
+case 45:
+	if ( (*p) == 101 )
+		goto st68;
+	goto st0;
+st68:
+	if ( ++p == pe )
+		goto _test_eof68;
+case 68:
+	goto st0;
+st46:
+	if ( ++p == pe )
+		goto _test_eof46;
+case 46:
+	if ( (*p) == 101 )
+		goto st47;
+	goto st0;
+st47:
+	if ( ++p == pe )
+		goto _test_eof47;
+case 47:
+	if ( (*p) == 100 )
+		goto st69;
+	goto st0;
+st69:
+	if ( ++p == pe )
+		goto _test_eof69;
+case 69:
+	goto st0;
+st48:
+	if ( ++p == pe )
+		goto _test_eof48;
+case 48:
+	if ( (*p) == 104 )
+		goto st49;
+	goto st0;
+st49:
+	if ( ++p == pe )
+		goto _test_eof49;
+case 49:
+	if ( (*p) == 105 )
+		goto st50;
+	goto st0;
+st50:
+	if ( ++p == pe )
+		goto _test_eof50;
+case 50:
+	if ( (*p) == 116 )
+		goto st51;
+	goto st0;
+st51:
+	if ( ++p == pe )
+		goto _test_eof51;
+case 51:
+	if ( (*p) == 101 )
+		goto st70;
+	goto st0;
+st70:
+	if ( ++p == pe )
+		goto _test_eof70;
+case 70:
+	goto st0;
+st52:
+	if ( ++p == pe )
+		goto _test_eof52;
+case 52:
+	if ( (*p) == 101 )
+		goto st53;
+	goto st0;
+st53:
+	if ( ++p == pe )
+		goto _test_eof53;
+case 53:
+	if ( (*p) == 108 )
+		goto st54;
+	goto st0;
+st54:
+	if ( ++p == pe )
+		goto _test_eof54;
+case 54:
+	if ( (*p) == 108 )
+		goto st55;
+	goto st0;
+st55:
+	if ( ++p == pe )
+		goto _test_eof55;
+case 55:
+	if ( (*p) == 111 )
+		goto st56;
+	goto st0;
+st56:
+	if ( ++p == pe )
+		goto _test_eof56;
+case 56:
+	if ( (*p) == 119 )
+		goto st71;
+	goto st0;
+st71:
+	if ( ++p == pe )
+		goto _test_eof71;
+case 71:
 	goto st0;
 	}
 	_test_eof2: cs = 2; goto _test_eof; 
 	_test_eof3: cs = 3; goto _test_eof; 
 	_test_eof4: cs = 4; goto _test_eof; 
 	_test_eof5: cs = 5; goto _test_eof; 
-	_test_eof17: cs = 17; goto _test_eof; 
+	_test_eof57: cs = 57; goto _test_eof; 
 	_test_eof6: cs = 6; goto _test_eof; 
-	_test_eof18: cs = 18; goto _test_eof; 
+	_test_eof58: cs = 58; goto _test_eof; 
 	_test_eof7: cs = 7; goto _test_eof; 
 	_test_eof8: cs = 8; goto _test_eof; 
 	_test_eof9: cs = 9; goto _test_eof; 
+	_test_eof59: cs = 59; goto _test_eof; 
 	_test_eof10: cs = 10; goto _test_eof; 
-	_test_eof19: cs = 19; goto _test_eof; 
 	_test_eof11: cs = 11; goto _test_eof; 
 	_test_eof12: cs = 12; goto _test_eof; 
-	_test_eof20: cs = 20; goto _test_eof; 
+	_test_eof60: cs = 60; goto _test_eof; 
 	_test_eof13: cs = 13; goto _test_eof; 
 	_test_eof14: cs = 14; goto _test_eof; 
+	_test_eof61: cs = 61; goto _test_eof; 
 	_test_eof15: cs = 15; goto _test_eof; 
 	_test_eof16: cs = 16; goto _test_eof; 
+	_test_eof62: cs = 62; goto _test_eof; 
+	_test_eof17: cs = 17; goto _test_eof; 
+	_test_eof18: cs = 18; goto _test_eof; 
+	_test_eof19: cs = 19; goto _test_eof; 
+	_test_eof63: cs = 63; goto _test_eof; 
+	_test_eof20: cs = 20; goto _test_eof; 
 	_test_eof21: cs = 21; goto _test_eof; 
+	_test_eof22: cs = 22; goto _test_eof; 
+	_test_eof23: cs = 23; goto _test_eof; 
+	_test_eof24: cs = 24; goto _test_eof; 
+	_test_eof25: cs = 25; goto _test_eof; 
+	_test_eof64: cs = 64; goto _test_eof; 
+	_test_eof26: cs = 26; goto _test_eof; 
+	_test_eof27: cs = 27; goto _test_eof; 
+	_test_eof28: cs = 28; goto _test_eof; 
+	_test_eof65: cs = 65; goto _test_eof; 
+	_test_eof29: cs = 29; goto _test_eof; 
+	_test_eof30: cs = 30; goto _test_eof; 
+	_test_eof31: cs = 31; goto _test_eof; 
+	_test_eof32: cs = 32; goto _test_eof; 
+	_test_eof66: cs = 66; goto _test_eof; 
+	_test_eof33: cs = 33; goto _test_eof; 
+	_test_eof34: cs = 34; goto _test_eof; 
+	_test_eof35: cs = 35; goto _test_eof; 
+	_test_eof36: cs = 36; goto _test_eof; 
+	_test_eof67: cs = 67; goto _test_eof; 
+	_test_eof37: cs = 37; goto _test_eof; 
+	_test_eof38: cs = 38; goto _test_eof; 
+	_test_eof39: cs = 39; goto _test_eof; 
+	_test_eof40: cs = 40; goto _test_eof; 
+	_test_eof41: cs = 41; goto _test_eof; 
+	_test_eof42: cs = 42; goto _test_eof; 
+	_test_eof43: cs = 43; goto _test_eof; 
+	_test_eof44: cs = 44; goto _test_eof; 
+	_test_eof45: cs = 45; goto _test_eof; 
+	_test_eof68: cs = 68; goto _test_eof; 
+	_test_eof46: cs = 46; goto _test_eof; 
+	_test_eof47: cs = 47; goto _test_eof; 
+	_test_eof69: cs = 69; goto _test_eof; 
+	_test_eof48: cs = 48; goto _test_eof; 
+	_test_eof49: cs = 49; goto _test_eof; 
+	_test_eof50: cs = 50; goto _test_eof; 
+	_test_eof51: cs = 51; goto _test_eof; 
+	_test_eof70: cs = 70; goto _test_eof; 
+	_test_eof52: cs = 52; goto _test_eof; 
+	_test_eof53: cs = 53; goto _test_eof; 
+	_test_eof54: cs = 54; goto _test_eof; 
+	_test_eof55: cs = 55; goto _test_eof; 
+	_test_eof56: cs = 56; goto _test_eof; 
+	_test_eof71: cs = 71; goto _test_eof; 
 
 	_test_eof: {}
 	if ( p == eof )
 	{
 	switch ( cs ) {
-	case 17: 
+	case 57: 
 #line 75 "lex/ragel_color.rl"
 	{ color.setHex(0x000000); }
 	break;
-	case 21: 
+	case 58: 
 #line 76 "lex/ragel_color.rl"
-	{ color.setHex(0xFFFFFF); }
-	break;
-	case 18: 
-#line 77 "lex/ragel_color.rl"
 	{ color.setHex(0x0000FF); }
 	break;
-	case 20: 
-#line 78 "lex/ragel_color.rl"
-	{ color.setHex(0xFF0000); }
+	case 59: 
+#line 77 "lex/ragel_color.rl"
+	{ color.setHex(0x00FFFF); }
 	break;
-	case 19: 
+	case 60: 
+#line 78 "lex/ragel_color.rl"
+	{ color.setHex(0xFFD700); }
+	break;
+	case 61: 
 #line 79 "lex/ragel_color.rl"
+	{ color.setHex(0x808080); }
+	break;
+	case 62: 
+#line 80 "lex/ragel_color.rl"
+	{ color.setHex(0x008000); }
+	break;
+	case 63: 
+#line 81 "lex/ragel_color.rl"
 	{ color.setHex(0x00FF00); }
 	break;
-#line 1135 "lex/parser_color.cpp"
+	case 64: 
+#line 82 "lex/ragel_color.rl"
+	{ color.setHex(0xFF00FF); }
+	break;
+	case 65: 
+#line 83 "lex/ragel_color.rl"
+	{ color.setHex(0x000080); }
+	break;
+	case 66: 
+#line 84 "lex/ragel_color.rl"
+	{ color.setHex(0x808000); }
+	break;
+	case 67: 
+#line 85 "lex/ragel_color.rl"
+	{ color.setHex(0xFFA500); }
+	break;
+	case 68: 
+#line 87 "lex/ragel_color.rl"
+	{ color.setHex(0x800080); }
+	break;
+	case 69: 
+#line 88 "lex/ragel_color.rl"
+	{ color.setHex(0xFF0000); }
+	break;
+	case 70: 
+#line 89 "lex/ragel_color.rl"
+	{ color.setHex(0xFFFFFF); }
+	break;
+	case 71: 
+#line 90 "lex/ragel_color.rl"
+	{ color.setHex(0xFFFF00); }
+	break;
+#line 1573 "lex/parser_color.cpp"
 	}
 	}
 
@@ -1140,7 +1578,7 @@ case 21:
 
 #line 286 "lex/parser_color.rl"
 
-        const auto ok = cs >= 17;
+        const auto ok = cs >= 57;
         if (ok) {
             color_.r = color.r;
             color_.g = color.g;
