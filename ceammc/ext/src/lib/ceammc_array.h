4790b324472a073fe85a0a9e4bd09bf8579c4de4
Serge Poltavski <serge.poltavski@gmail.com>
2023-03-27 22:56:41 +0300

    iterator data access

--- ceammc/ext/src/lib/ceammc_array.h
+++ ceammc/ext/src/lib/ceammc_array.h
@@ -47,6 +47,8 @@ public:
     t_float& operator*() { return data_->w_float; }
     const t_float& operator*() const { return data_->w_float; }
     t_float& operator[](const size_t n) { return data_[n].w_float; }
+    word* data() { return data_; }
+    const word* data() const { return data_; }
 
     inline ArrayIterator& operator++()
     {
