f3e2759a5f0016e216421a40155c8bfb5a4df4ca
Serge Poltavski <serge.poltavski@gmail.com>
2023-04-24 00:00:19 +0300

    xlet info added

--- ceammc/ext/src/noise/noise_crackle.cpp
+++ ceammc/ext/src/noise/noise_crackle.cpp
@@ -3,25 +3,32 @@
 
 using namespace ceammc;
 
+constexpr const char* PROP_FREQ = "@freq";
+
 class NoiseCrackle : public faust_noise_crackle_tilde {
+    UIProperty* freq_ { nullptr };
+
 public:
     NoiseCrackle(const PdArgs& args)
         : faust_noise_crackle_tilde(args)
+        , freq_(findUIProperty("@freq"))
     {
-        bindPositionalArgsToProps({ gensym("@freq") });
+        bindPositionalArgsToProps({ PROP_FREQ });
     }
 
     void onFloat(t_float f) override
     {
-        setProperty(gensym("@freq"), AtomList(Atom(f)));
+        if (freq_)
+            freq_->setValue(f, true);
     }
 };
 
 void setup_noise_crackle_tilde()
 {
     SoundExternalFactory<NoiseCrackle> obj("noise.crackle~", OBJECT_FACTORY_DEFAULT);
+    obj.setXletsInfo({ "float: freq of impulses in Hz" }, { "signal: random impulses in -1..+1 range" });
 
     obj.setDescription("sparse noise generator");
     obj.setCategory("noise");
-    obj.setKeywords({"noise", "crackle"});
+    obj.setKeywords({ "noise", "crackle" });
 }
