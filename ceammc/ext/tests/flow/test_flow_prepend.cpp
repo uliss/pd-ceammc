b03cb9ea421d95cd9d30ca270e9a113b555f9479
Serge Poltavski <serge.poltavski@gmail.com>
2023-06-05 23:25:19 +0300

    test updates

--- ceammc/ext/tests/flow/test_flow_prepend.cpp
+++ ceammc/ext/tests/flow/test_flow_prepend.cpp
@@ -112,122 +112,122 @@ TEST_CASE("flow.prepend", "[externals]")
 
         SECTION("list explicit")
         {
-            TExt t("flow.append", LA("list", 1, 2, 3));
+            TExt t("flow.prepend", LA("list", 1, 2, 3));
             t.sendFloat(100);
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].atomValue() == A(100));
-            REQUIRE(t.messagesAt(0)[1].listValue() == LF(1, 2, 3));
+            REQUIRE(t.messagesAt(0)[0].listValue() == LF(1, 2, 3));
+            REQUIRE(t.messagesAt(0)[1].atomValue() == A(100));
         }
 
         SECTION("any")
         {
-            TExt t("flow.append", LA("any"));
+            TExt t("flow.prepend", LA("any"));
             t.sendFloat(100);
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].atomValue() == A(100));
-            REQUIRE(t.messagesAt(0)[1].isAny());
-            REQUIRE(t.messagesAt(0)[1].anyValue() == LA("any"));
+            REQUIRE(t.messagesAt(0)[0].isAny());
+            REQUIRE(t.messagesAt(0)[0].anyValue() == LA("any"));
+            REQUIRE(t.messagesAt(0)[1].atomValue() == A(100));
         }
 
         SECTION("any2")
         {
-            TExt t("flow.append", LA("any", 1, 2, 3));
+            TExt t("flow.prepend", LA("any", 1, 2, 3));
             t.sendFloat(100);
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].atomValue() == A(100));
-            REQUIRE(t.messagesAt(0)[1].isAny());
-            REQUIRE(t.messagesAt(0)[1].anyValue() == LA("any", 1, 2, 3));
+            REQUIRE(t.messagesAt(0)[0].isAny());
+            REQUIRE(t.messagesAt(0)[0].anyValue() == LA("any", 1, 2, 3));
+            REQUIRE(t.messagesAt(0)[1].atomValue() == A(100));
         }
 
         SECTION("any prop")
         {
-            TExt t("flow.append", LA("\"@any\"", 1, 2, 3));
+            TExt t("flow.prepend", LA("\"@any\"", 1, 2, 3));
             t.sendFloat(100);
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].atomValue() == A(100));
-            REQUIRE(t.messagesAt(0)[1].isAny());
-            REQUIRE(t.messagesAt(0)[1].anyValue() == LA("@any", 1, 2, 3));
+            REQUIRE(t.messagesAt(0)[0].isAny());
+            REQUIRE(t.messagesAt(0)[0].anyValue() == LA("@any", 1, 2, 3));
+            REQUIRE(t.messagesAt(0)[1].atomValue() == A(100));
             t.clearAll();
 
             t.sendMessageTo(Message(SYM("@any2"), LF(3, 4, 5)), 1);
             t.sendFloat(200);
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].atomValue() == A(200));
-            REQUIRE(t.messagesAt(0)[1].anyValue() == LA("@any2", 3, 4, 5));
+            REQUIRE(t.messagesAt(0)[0].anyValue() == LA("@any2", 3, 4, 5));
+            REQUIRE(t.messagesAt(0)[1].atomValue() == A(200));
         }
 
         SECTION("all")
         {
-            TExt t("flow.append", LF(1, 2, 3));
+            TExt t("flow.prepend", LF(1, 2, 3));
 
             t.clearAll();
             t.sendMessageTo(Message::makeBang(), 1);
             t.sendFloat(100);
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].atomValue() == A(100));
-            REQUIRE(t.messagesAt(0)[1].isBang());
+            REQUIRE(t.messagesAt(0)[0].isBang());
+            REQUIRE(t.messagesAt(0)[1].atomValue() == A(100));
 
             t.clearAll();
             t.sendMessageTo(Message(-50), 1);
             t.sendBang();
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].isBang());
-            REQUIRE(t.messagesAt(0)[1].atomValue() == A(-50));
+            REQUIRE(t.messagesAt(0)[0].atomValue() == A(-50));
+            REQUIRE(t.messagesAt(0)[1].isBang());
 
             t.clearAll();
             t.sendMessageTo(Message(SYM("ABC")), 1);
             t.sendList(LF(1, 2, 3));
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].listValue() == LF(1, 2, 3));
-            REQUIRE(t.messagesAt(0)[1].atomValue() == A("ABC"));
+            REQUIRE(t.messagesAt(0)[0].atomValue() == A("ABC"));
+            REQUIRE(t.messagesAt(0)[1].listValue() == LF(1, 2, 3));
 
             t.clearAll();
             t.sendMessageTo(Message(LF(20, 30)), 1);
             t.sendSymbol("DEF");
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].atomValue() == A("DEF"));
-            REQUIRE(t.messagesAt(0)[1].listValue() == LF(20, 30));
+            REQUIRE(t.messagesAt(0)[0].listValue() == LF(20, 30));
+            REQUIRE(t.messagesAt(0)[1].atomValue() == A("DEF"));
 
             t.clearAll();
             t.sendMessageTo(Message(SYM("any"), LF(20, 30)), 1);
             t.sendSymbol("DEF");
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].atomValue() == A("DEF"));
-            REQUIRE(t.messagesAt(0)[1].anyValue() == LA("any", 20, 30));
+            REQUIRE(t.messagesAt(0)[0].anyValue() == LA("any", 20, 30));
+            REQUIRE(t.messagesAt(0)[1].atomValue() == A("DEF"));
 
             t.clearAll();
             t.sendMessageTo(Message(SYM("@prop"), LF(20, 30)), 1);
             t.sendSymbol("DEF");
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].atomValue() == A("DEF"));
-            REQUIRE(t.messagesAt(0)[1].anyValue() == LA("@prop", 20, 30));
+            REQUIRE(t.messagesAt(0)[0].anyValue() == LA("@prop", 20, 30));
+            REQUIRE(t.messagesAt(0)[1].atomValue() == A("DEF"));
 
             t.clearAll();
             t.sendMessageTo(Message(SYM("@gate"), LF(0)), 1);
             t.sendMessage(Message(SYM("@gate"), LF(1)));
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].anyValue() == LA("@gate", 1));
-            REQUIRE(t.messagesAt(0)[1].anyValue() == LA("@gate", 0.));
+            REQUIRE(t.messagesAt(0)[0].anyValue() == LA("@gate", 0.));
+            REQUIRE(t.messagesAt(0)[1].anyValue() == LA("@gate", 1));
 
             t.clearAll();
             t.sendMessageTo(Message(SYM("@gate"), LF(0)), 1);
             t.sendMessage(Message(SYM("@delay"), LF(1000)));
 
             REQUIRE(t.messagesAt(0).size() == 2);
-            REQUIRE(t.messagesAt(0)[0].anyValue() == LA("@delay", 1000));
-            REQUIRE(t.messagesAt(0)[1].anyValue() == LA("@gate", 0.));
+            REQUIRE(t.messagesAt(0)[0].anyValue() == LA("@gate", 0.));
+            REQUIRE(t.messagesAt(0)[1].anyValue() == LA("@delay", 1000));
         }
     }
 }
