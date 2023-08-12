e19b76d16a9797260381470048c003ee4bf60119
Serge Poltavski <serge.poltavski@gmail.com>
2023-08-12 14:59:27 +0300

    pep cleanup

--- ceammc/distrib/win/fix_dll_deps.py
+++ ceammc/distrib/win/fix_dll_deps.py
@@ -2,15 +2,15 @@
 
 import sys
 import os
-import fnmatch
 import re
 import subprocess
 import shutil
 
-OBJDUMP_BIN="@OBJDUMP_EXE@"
-MINGW_PREFIX=b"@CEAMMC_MINGW_PREFIX@"
-DEST_DIR=b"@CMAKE_INSTALL_PREFIX@/@PD_EXE_INSTALL_PATH@"
+OBJDUMP_BIN = "@OBJDUMP_EXE@"
+MINGW_PREFIX = b"@CEAMMC_MINGW_PREFIX@"
+DEST_DIR = b"@CMAKE_INSTALL_PREFIX@/@PD_EXE_INSTALL_PATH@"
 DLL_SET = set()
+
 
 def find_all_bins():
     pattern = re.compile(r'.+\.(dll|m_i386|m_amd64|exe)$')
@@ -28,12 +28,13 @@ def find_all_bins():
 
     return bins
 
+
 def find_bin_dlls(path):
     #    print(os.path.basename(path))
     for line in subprocess.check_output([OBJDUMP_BIN, "-p", path]).split(b'\r\n'):
         idx = line.find(b'DLL Name: ')
         if idx > 0:
-            dll = line[idx+10:];
+            dll = line[idx+10:]
             dll_path = os.path.join(MINGW_PREFIX, b"bin", dll)
             if dll_path in DLL_SET:
                 continue
@@ -44,15 +45,13 @@ def find_bin_dlls(path):
                 find_bin_dlls(dll_path)
 
 
-    pass
-
 if __name__ == "__main__":
     bin_list = sorted(list(find_all_bins()))
     for f in bin_list:
         find_bin_dlls(f)
 
     for dll in sorted(DLL_SET):
-        src = os.path.basename(dll).decode();
+        src = os.path.basename(dll).decode()
         dest = os.path.join(DEST_DIR, os.path.basename(dll)).decode()
         shutil.copyfile(dll, dest)
         print(f"'{src}' -> '{dest}'")
