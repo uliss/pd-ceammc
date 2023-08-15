a67f982620ad1d3c45ea4fa289ab39f9a910b0cf
Serge Poltavski <serge.poltavski@gmail.com>
2023-08-15 03:06:05 +0300

    SoxR resampler interface

---
+++ ceammc/ext/src/lib/ceammc_soxr_resampler.cpp
@@ -0,0 +1,142 @@
+/*****************************************************************************
+ * Copyright 2023 Serge Poltavski. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#include "ceammc_soxr_resampler.h"
+#include "fmt/core.h"
+#include "soxr.h"
+
+#include <iostream>
+
+using namespace ceammc;
+
+constexpr soxr_datatype_t SAMP_INTERLEAVE
+    = std::is_same<t_float, float>::value
+    ? SOXR_FLOAT32_I
+    : SOXR_FLOAT64_I;
+
+constexpr soxr_datatype_t SAMP_SPLIT
+    = std::is_same<t_float, float>::value
+    ? SOXR_FLOAT32_S
+    : SOXR_FLOAT64_S;
+
+static int quality2soxr(SoxrResampler::Quality q)
+{
+    const static int sq[] = {
+        SOXR_QQ,
+        SOXR_LQ,
+        SOXR_MQ,
+        SOXR_HQ,
+        SOXR_VHQ,
+    };
+
+    auto idx = static_cast<size_t>(q);
+    return sq[idx];
+}
+
+SoxrResampler::SoxrResampler(double inRate, double outRate, size_t numChannels, Quality q, bool variable, bool splitChannels)
+    : in_rate_(inRate)
+    , out_rate_(outRate)
+    , num_chan_(numChannels)
+    , soxr_(nullptr, soxr_delete)
+{
+
+    soxr_io_spec_t io_spec {
+        splitChannels ? SAMP_SPLIT : SAMP_INTERLEAVE,
+        splitChannels ? SAMP_SPLIT : SAMP_INTERLEAVE,
+        1,
+        nullptr,
+        0,
+    };
+
+    auto q_spec = soxr_quality_spec(quality2soxr(q), SOXR_LINEAR_PHASE);
+
+    if (variable)
+        q_spec.flags |= SOXR_VR;
+
+    soxr_error_t error;
+    soxr_.reset(soxr_create(inRate, outRate, numChannels, &error, &io_spec, &q_spec, nullptr));
+
+    if (error)
+        throw Exception(fmt::format("soxr_create() error: {}", error));
+}
+
+bool SoxrResampler::setResampleRatio(double x)
+{
+    auto err = soxr_set_io_ratio(soxr_.get(), x, 0);
+    return !err;
+}
+
+double SoxrResampler::delay() const
+{
+    return soxr_delay(soxr_.get());
+}
+
+void SoxrResampler::reset()
+{
+    soxr_clear(soxr_.get());
+}
+
+SoxrResampler::ResultCode SoxrResampler::process(const t_sample* in, size_t numFrames, WriterCallback cb)
+{
+    if (!soxr_)
+        return Error;
+
+    constexpr size_t OUT_BUF_SIZE = 1024;
+    t_sample out_buf[OUT_BUF_SIZE];
+    const auto IN_SAMPLES = numFrames * num_chan_;
+
+    auto it = in;
+    const auto end = it + IN_SAMPLES;
+    auto in_frames = numFrames;
+    const auto out_frames = OUT_BUF_SIZE / num_chan_;
+
+    while (it != end) {
+        size_t in_frames_done = 0, out_frames_done = 0;
+        auto err = soxr_process(soxr_.get(), it, in_frames, &in_frames_done, out_buf, out_frames, &out_frames_done);
+
+        if (err) {
+            std::cerr << fmt::format("soxr_process() error: {}", err);
+            return Error;
+        }
+
+        it += (in_frames_done * num_chan_);
+        in_frames -= in_frames_done;
+
+        if (!cb(out_buf, out_frames_done))
+            return CallbackQuit;
+    }
+
+    return Ok;
+}
+
+SoxrResampler::ResultCode SoxrResampler::processDone(WriterCallback cb)
+{
+    constexpr size_t OUT_BUF_SIZE = 1024;
+    t_sample out_buf[OUT_BUF_SIZE];
+    const auto out_frames = OUT_BUF_SIZE / num_chan_;
+
+    size_t in_frames_done = 0, out_frames_done = 0;
+    auto err = soxr_process(soxr_.get(), nullptr, 0, &in_frames_done, out_buf, out_frames, &out_frames_done);
+
+    if (err) {
+        std::cerr << fmt::format("soxr_process() error: {}", err);
+        return Error;
+    }
+
+    if (!cb(out_buf, out_frames_done))
+        return CallbackQuit;
+    else
+        return Ok;
+}
+
+SoxrResampler::~SoxrResampler() = default;
---
+++ ceammc/ext/src/lib/ceammc_soxr_resampler.h
@@ -0,0 +1,85 @@
+/*****************************************************************************
+ * Copyright 2023 Serge Poltavski. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#ifndef CEAMMC_SOXR_RESAMPLER_H
+#define CEAMMC_SOXR_RESAMPLER_H
+
+#include <array>
+#include <functional>
+#include <memory>
+#include <stdexcept>
+
+#include "m_pd.h"
+
+// forward declaration
+struct soxr;
+
+namespace ceammc {
+
+class SoxrResampler {
+    using SoxRPtr = std::unique_ptr<soxr, void (*)(soxr*)>;
+    double in_rate_, out_rate_;
+    size_t num_chan_;
+    SoxRPtr soxr_;
+
+public:
+    using WriterCallback = std::function<bool(const t_sample*, size_t)>;
+    using Exception = std::runtime_error;
+
+    enum ResultCode {
+        Ok,
+        Error,
+        CallbackQuit,
+    };
+
+    enum Quality {
+        QUICK,
+        LOW,
+        MEDIUM,
+        HIGH,
+        BEST,
+    };
+
+public:
+    /**
+     * @brief SoxrResampler
+     * @param inRate - input rate
+     * @param outRate - output rate
+     * @param numChannels - number of input channels
+     * @param q - resample quality
+     * @throw Exception on error
+     */
+    SoxrResampler(double inRate, double outRate, size_t numChannels, Quality q = QUICK, bool variable = false, bool splitChannels = false);
+    ~SoxrResampler();
+
+    double ratio() const { return in_rate_ / out_rate_; }
+    size_t numChannels() const { return num_chan_; }
+
+    bool setResampleRatio(double x);
+
+    double delay() const;
+    void reset();
+
+    ResultCode process(const t_sample* in, size_t numFrames, WriterCallback cb);
+    ResultCode processDone(WriterCallback cb);
+
+};
+
+}
+
+class ceammc_soxr_resampler {
+public:
+    ceammc_soxr_resampler();
+};
+
+#endif // CEAMMC_SOXR_RESAMPLER_H
