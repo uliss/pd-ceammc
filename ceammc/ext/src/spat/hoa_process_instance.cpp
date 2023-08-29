7d84ff501f5718881ab697b5fefeaace17ca3f0f
Serge Poltavski <serge.poltavski@gmail.com>
2023-08-29 16:02:01 +0300

    crash fix

--- ceammc/ext/src/spat/hoa_process_instance.cpp
+++ ceammc/ext/src/spat/hoa_process_instance.cpp
@@ -124,7 +124,6 @@ bool ProcessInstance::init(t_symbol* name, const AtomListView& args)
     t_canvas* new_c = (t_canvas*)pd_this->pd_newest;
     pd_this->pd_newest = nullptr;
 
-    new_c->gl_owner = 0;
     new_c->gl_isclone = 1;
 
     setCanvas(new_c);
