169bd74a2f3e6bfd0fb7438a72e59775d58f2c95
Serge Poltavski <serge.poltavski@gmail.com>
2023-10-20 14:26:31 +0300

    Update live_capture.h

--- ceammc/ext/src/misc/live_capture.h
+++ ceammc/ext/src/misc/live_capture.h
@@ -565,32 +565,30 @@ class live_capture : public live_capture_dsp {
 	
  private:
 	
-	FAUSTFLOAT fCheckbox0;
-	int iVec0[2];
-	int iRec2[2];
 	int fSampleRate;
 	float fConst0;
 	float fConst1;
 	FAUSTFLOAT fHslider0;
 	float fConst2;
 	float fConst3;
-	float fRec3[2];
+	float fRec2[2];
+	FAUSTFLOAT fCheckbox0;
+	int iVec0[2];
+	int iRec3[2];
 	float fConst4;
 	FAUSTFLOAT fHslider1;
 	float fRec4[2];
+	int iRec5[2];
 	FAUSTFLOAT fHslider2;
-	float fRec5[2];
-	int iRec6[2];
-	FAUSTFLOAT fHslider3;
-	float fRec7[2];
+	float fRec6[2];
 	int IOTA0;
 	float fVec1[8388608];
 	float fConst5;
-	FAUSTFLOAT fHslider4;
-	int iRec8[2];
+	FAUSTFLOAT fHslider3;
+	int iRec7[2];
 	float fRec1[2];
-	FAUSTFLOAT fHslider5;
-	float fRec9[2];
+	FAUSTFLOAT fHslider4;
+	float fRec8[2];
 	float fVec2[2];
 	float fRec0[2];
 	
@@ -609,7 +607,7 @@ class live_capture : public live_capture_dsp {
 		m->declare("copyright", "(c)GRAME 2006");
 		m->declare("delays.lib/name", "Faust Delay Library");
 		m->declare("delays.lib/version", "0.1");
-		m->declare("envelopes.lib/adsr:author", "Yann Orlarey and Andrey Bundin");
+		m->declare("envelopes.lib/asr:author", "Yann Orlarey, Stéphane Letz");
 		m->declare("envelopes.lib/author", "GRAME");
 		m->declare("envelopes.lib/copyright", "GRAME");
 		m->declare("envelopes.lib/license", "LGPL with exception");
@@ -619,7 +617,7 @@ class live_capture : public live_capture_dsp {
 		m->declare("filters.lib/dcblocker:author", "Julius O. Smith III");
 		m->declare("filters.lib/dcblocker:copyright", "Copyright (C) 2003-2019 by Julius O. Smith III <jos@ccrma.stanford.edu>");
 		m->declare("filters.lib/dcblocker:license", "MIT-style STK-4.3 license");
-		m->declare("filters.lib/lowpass0_highpass1", "MIT-style STK-4.3 license");
+		m->declare("filters.lib/lowpass0_highpass1", "Copyright (C) 2003-2019 by Julius O. Smith III <jos@ccrma.stanford.edu>");
 		m->declare("filters.lib/name", "Faust Filters Library");
 		m->declare("filters.lib/pole:author", "Julius O. Smith III");
 		m->declare("filters.lib/pole:copyright", "Copyright (C) 2003-2019 by Julius O. Smith III <jos@ccrma.stanford.edu>");
@@ -655,63 +653,59 @@ class live_capture : public live_capture_dsp {
 	virtual void instanceConstants(int sample_rate) {
 		fSampleRate = sample_rate;
 		fConst0 = std::min<float>(1.92e+05f, std::max<float>(1.0f, float(fSampleRate)));
-		fConst1 = 0.0441f / fConst0;
+		fConst1 = 0.441f / fConst0;
 		fConst2 = 44.1f / fConst0;
 		fConst3 = 1.0f - fConst2;
-		fConst4 = 0.441f / fConst0;
+		fConst4 = 0.0441f / fConst0;
 		fConst5 = 32.0f * fConst0;
 	}
 	
 	virtual void instanceResetUserInterface() {
+		fHslider0 = FAUSTFLOAT(1e+02f);
 		fCheckbox0 = FAUSTFLOAT(0.0f);
-		fHslider0 = FAUSTFLOAT(5e+01f);
-		fHslider1 = FAUSTFLOAT(1e+02f);
-		fHslider2 = FAUSTFLOAT(0.0f);
-		fHslider3 = FAUSTFLOAT(5e+01f);
-		fHslider4 = FAUSTFLOAT(1.0f);
-		fHslider5 = FAUSTFLOAT(0.0f);
+		fHslider1 = FAUSTFLOAT(5e+01f);
+		fHslider2 = FAUSTFLOAT(5e+01f);
+		fHslider3 = FAUSTFLOAT(1.0f);
+		fHslider4 = FAUSTFLOAT(0.0f);
 	}
 	
 	virtual void instanceClear() {
 		for (int l0 = 0; l0 < 2; l0 = l0 + 1) {
-			iVec0[l0] = 0;
+			fRec2[l0] = 0.0f;
 		}
 		for (int l1 = 0; l1 < 2; l1 = l1 + 1) {
-			iRec2[l1] = 0;
+			iVec0[l1] = 0;
 		}
 		for (int l2 = 0; l2 < 2; l2 = l2 + 1) {
-			fRec3[l2] = 0.0f;
+			iRec3[l2] = 0;
 		}
 		for (int l3 = 0; l3 < 2; l3 = l3 + 1) {
 			fRec4[l3] = 0.0f;
 		}
 		for (int l4 = 0; l4 < 2; l4 = l4 + 1) {
-			fRec5[l4] = 0.0f;
+			iRec5[l4] = 0;
 		}
 		for (int l5 = 0; l5 < 2; l5 = l5 + 1) {
-			iRec6[l5] = 0;
-		}
-		for (int l6 = 0; l6 < 2; l6 = l6 + 1) {
-			fRec7[l6] = 0.0f;
+			fRec6[l5] = 0.0f;
 		}
 		IOTA0 = 0;
-		for (int l7 = 0; l7 < 8388608; l7 = l7 + 1) {
-			fVec1[l7] = 0.0f;
+		for (int l6 = 0; l6 < 8388608; l6 = l6 + 1) {
+			fVec1[l6] = 0.0f;
+		}
+		for (int l7 = 0; l7 < 2; l7 = l7 + 1) {
+			iRec7[l7] = 0;
 		}
 		for (int l8 = 0; l8 < 2; l8 = l8 + 1) {
-			iRec8[l8] = 0;
+			fRec1[l8] = 0.0f;
 		}
 		for (int l9 = 0; l9 < 2; l9 = l9 + 1) {
-			fRec1[l9] = 0.0f;
+			fRec8[l9] = 0.0f;
 		}
 		for (int l10 = 0; l10 < 2; l10 = l10 + 1) {
-			fRec9[l10] = 0.0f;
+			fVec2[l10] = 0.0f;
 		}
 		for (int l11 = 0; l11 < 2; l11 = l11 + 1) {
-			fVec2[l11] = 0.0f;
-		}
-		for (int l12 = 0; l12 < 2; l12 = l12 + 1) {
-			fRec0[l12] = 0.0f;
+			fRec0[l11] = 0.0f;
 		}
 	}
 	
@@ -735,66 +729,58 @@ class live_capture : public live_capture_dsp {
 	
 	virtual void buildUserInterface(UI* ui_interface) {
 		ui_interface->openVerticalBox("live.capture");
-		ui_interface->addHorizontalSlider(".div", &fHslider4, FAUSTFLOAT(1.0f), FAUSTFLOAT(1.0f), FAUSTFLOAT(8.0f), FAUSTFLOAT(1.0f));
-		ui_interface->declare(&fHslider0, "style", "knob");
-		ui_interface->declare(&fHslider0, "unit", "ms");
-		ui_interface->addHorizontalSlider("attack", &fHslider0, FAUSTFLOAT(5e+01f), FAUSTFLOAT(0.0f), FAUSTFLOAT(1e+05f), FAUSTFLOAT(1.0f));
+		ui_interface->addHorizontalSlider(".div", &fHslider3, FAUSTFLOAT(1.0f), FAUSTFLOAT(1.0f), FAUSTFLOAT(8.0f), FAUSTFLOAT(1.0f));
+		ui_interface->declare(&fHslider1, "style", "knob");
+		ui_interface->declare(&fHslider1, "unit", "ms");
+		ui_interface->addHorizontalSlider("attack", &fHslider1, FAUSTFLOAT(5e+01f), FAUSTFLOAT(0.0f), FAUSTFLOAT(1e+05f), FAUSTFLOAT(1.0f));
+		ui_interface->declare(&fHslider4, "unit", "db");
+		ui_interface->addHorizontalSlider("gain", &fHslider4, FAUSTFLOAT(0.0f), FAUSTFLOAT(-6e+01f), FAUSTFLOAT(12.0f), FAUSTFLOAT(0.001f));
+		ui_interface->addCheckButton("gate", &fCheckbox0);
 		ui_interface->declare(&fHslider2, "style", "knob");
 		ui_interface->declare(&fHslider2, "unit", "ms");
-		ui_interface->addHorizontalSlider("decay", &fHslider2, FAUSTFLOAT(0.0f), FAUSTFLOAT(0.0f), FAUSTFLOAT(1e+05f), FAUSTFLOAT(1.0f));
-		ui_interface->declare(&fHslider5, "unit", "db");
-		ui_interface->addHorizontalSlider("gain", &fHslider5, FAUSTFLOAT(0.0f), FAUSTFLOAT(-6e+01f), FAUSTFLOAT(12.0f), FAUSTFLOAT(0.001f));
-		ui_interface->addCheckButton("gate", &fCheckbox0);
-		ui_interface->declare(&fHslider3, "style", "knob");
-		ui_interface->declare(&fHslider3, "unit", "ms");
-		ui_interface->addHorizontalSlider("release", &fHslider3, FAUSTFLOAT(5e+01f), FAUSTFLOAT(0.0f), FAUSTFLOAT(1e+05f), FAUSTFLOAT(1.0f));
-		ui_interface->declare(&fHslider1, "style", "knob");
-		ui_interface->declare(&fHslider1, "unit", "percent");
-		ui_interface->addHorizontalSlider("sustain", &fHslider1, FAUSTFLOAT(1e+02f), FAUSTFLOAT(0.0f), FAUSTFLOAT(1e+02f), FAUSTFLOAT(0.001f));
+		ui_interface->addHorizontalSlider("release", &fHslider2, FAUSTFLOAT(5e+01f), FAUSTFLOAT(0.0f), FAUSTFLOAT(1e+05f), FAUSTFLOAT(1.0f));
+		ui_interface->declare(&fHslider0, "style", "knob");
+		ui_interface->declare(&fHslider0, "unit", "percent");
+		ui_interface->addHorizontalSlider("sustain", &fHslider0, FAUSTFLOAT(1e+02f), FAUSTFLOAT(0.0f), FAUSTFLOAT(1e+02f), FAUSTFLOAT(0.001f));
 		ui_interface->closeBox();
 	}
 	
 	virtual void compute(int count, FAUSTFLOAT** RESTRICT inputs, FAUSTFLOAT** RESTRICT outputs) {
 		FAUSTFLOAT* input0 = inputs[0];
 		FAUSTFLOAT* output0 = outputs[0];
-		int iSlow0 = int(float(fCheckbox0));
-		float fSlow1 = fConst1 * float(fHslider0);
+		float fSlow0 = fConst1 * float(fHslider0);
+		int iSlow1 = int(float(fCheckbox0));
 		float fSlow2 = fConst4 * float(fHslider1);
-		float fSlow3 = fConst1 * float(fHslider2);
-		int iSlow4 = iSlow0 == 0;
-		float fSlow5 = fConst1 * float(fHslider3);
-		float fSlow6 = 1.0f / float(int(float(fHslider4)));
-		float fSlow7 = fConst2 * std::pow(1e+01f, 0.05f * float(fHslider5));
+		int iSlow3 = iSlow1 == 0;
+		float fSlow4 = fConst4 * float(fHslider2);
+		float fSlow5 = 1.0f / float(int(float(fHslider3)));
+		float fSlow6 = fConst2 * std::pow(1e+01f, 0.05f * float(fHslider4));
 		for (int i0 = 0; i0 < count; i0 = i0 + 1) {
-			iVec0[0] = iSlow0;
-			iRec2[0] = iSlow0 + iRec2[1] * (iVec0[1] >= iSlow0);
-			float fTemp0 = float(iRec2[0]);
-			fRec3[0] = fSlow1 + fConst3 * fRec3[1];
-			float fTemp1 = std::max<float>(1.0f, fConst0 * fRec3[0]);
+			fRec2[0] = fSlow0 + fConst3 * fRec2[1];
+			iVec0[0] = iSlow1;
+			iRec3[0] = iSlow1 + iRec3[1] * (iVec0[1] >= iSlow1);
 			fRec4[0] = fSlow2 + fConst3 * fRec4[1];
-			fRec5[0] = fSlow3 + fConst3 * fRec5[1];
-			iRec6[0] = iSlow4 * (iRec6[1] + 1);
-			fRec7[0] = fSlow5 + fConst3 * fRec7[1];
-			float fTemp2 = std::max<float>(0.0f, std::min<float>(fTemp0 / fTemp1, std::max<float>((1.0f - fRec4[0]) * (fTemp1 - fTemp0) / std::max<float>(1.0f, fConst0 * fRec5[0]) + 1.0f, fRec4[0])) * (1.0f - float(iRec6[0]) / std::max<float>(1.0f, fConst0 * fRec7[0])));
-			fVec1[IOTA0 & 8388607] = fRec1[1] * (1.0f - fTemp2) + float(input0[i0]) * fTemp2;
-			iRec8[0] = ((iSlow0 - iVec0[1]) <= 0) * (iSlow0 + iRec8[1]);
-			fRec1[0] = fVec1[(IOTA0 - int(std::min<float>(fConst5, std::max<float>(0.0f, fSlow6 * float(iRec8[0]) + -1.0f)))) & 8388607];
-			fRec9[0] = fSlow7 + fConst3 * fRec9[1];
-			float fTemp3 = fRec1[0] * fRec9[0];
-			fVec2[0] = fTemp3;
-			fRec0[0] = fTemp3 + 0.995f * fRec0[1] - fVec2[1];
+			iRec5[0] = iSlow3 * (iRec5[1] + 1);
+			fRec6[0] = fSlow4 + fConst3 * fRec6[1];
+			float fTemp0 = std::max<float>(0.0f, std::min<float>(fRec2[0] * float(iRec3[0]) / std::max<float>(1.0f, fConst0 * fRec4[0]), fRec2[0]) - fRec2[0] * float(iRec5[0]) / std::max<float>(1.0f, fConst0 * fRec6[0]));
+			fVec1[IOTA0 & 8388607] = fRec1[1] * (1.0f - fTemp0) + float(input0[i0]) * fTemp0;
+			iRec7[0] = ((iSlow1 - iVec0[1]) <= 0) * (iSlow1 + iRec7[1]);
+			fRec1[0] = fVec1[(IOTA0 - int(std::min<float>(fConst5, std::max<float>(0.0f, fSlow5 * float(iRec7[0]) + -1.0f)))) & 8388607];
+			fRec8[0] = fSlow6 + fConst3 * fRec8[1];
+			float fTemp1 = fRec1[0] * fRec8[0];
+			fVec2[0] = fTemp1;
+			fRec0[0] = fTemp1 + 0.995f * fRec0[1] - fVec2[1];
 			output0[i0] = FAUSTFLOAT(fRec0[0]);
+			fRec2[1] = fRec2[0];
 			iVec0[1] = iVec0[0];
-			iRec2[1] = iRec2[0];
-			fRec3[1] = fRec3[0];
+			iRec3[1] = iRec3[0];
 			fRec4[1] = fRec4[0];
-			fRec5[1] = fRec5[0];
-			iRec6[1] = iRec6[0];
-			fRec7[1] = fRec7[0];
+			iRec5[1] = iRec5[0];
+			fRec6[1] = fRec6[0];
 			IOTA0 = IOTA0 + 1;
-			iRec8[1] = iRec8[0];
+			iRec7[1] = iRec7[0];
 			fRec1[1] = fRec1[0];
-			fRec9[1] = fRec9[0];
+			fRec8[1] = fRec8[0];
 			fVec2[1] = fVec2[0];
 			fRec0[1] = fRec0[0];
 		}
