84120bbbe9eac2d32397e6655b7295295eaae94f
Serge Poltavski <serge.poltavski@gmail.com>
2024-03-25 22:18:01 +0300

    rust: fix from cbindgen updates on windows

--- ceammc/extra/rust/core/build.rs
+++ ceammc/extra/rust/core/build.rs
@@ -5,6 +5,7 @@ use std::env;
 fn main() { 
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
+    #[cfg(target_os="macos")]
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
         .write_to_file("core_rust.hpp");
