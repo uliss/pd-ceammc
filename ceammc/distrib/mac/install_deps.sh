8c162caf494615bd545a3a1b7adffe2a9f6dd7f2
Serge Poltavski <serge.poltavski@gmail.com>
2024-05-30 22:41:52 +0300

    install deps script

--- ceammc/distrib/mac/install_deps.sh
+++ ceammc/distrib/mac/install_deps.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
-sudo port install libsndfile \
-	portaudio \
-	fftw-3-single \
-	libmad \
-	luajit \
-	rubberband \
-	llvm-10 \
-	cmake \
-	ninja \
-	glib2 \
-	boost \
-	ragel \
-	gettext \
-	pkgconfig \
-	llvm_select \
-	armadillo \
-	scons \
-	py310-pip \ 
-	liblo
+if ! command -v rustup &> /dev/null
+then
+    echo "rustup could not be found ..."
+    echo "you should install rust for build"
+    echo "run: curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
+    exit 1
+fi
 
-sudo port select llvm-10
+
+sudo port install libsndfile \
+        armadillo \
+        boost \
+        cairo \
+        cmake \
+        fftw-3-single \
+        gettext \
+        glib2 \
+        liblo \
+        libltc \
+        libmad \
+        libmagic \
+        libmodplug \
+        libsamplerate\
+        libsndfile \
+        llvm-10 \
+        llvm_select \
+        luajit \
+        ninja \
+        pkgconfig \
+        portaudio \
+        py310-pip \
+        ragel \
+        rubberband \
+        scons
+
+sudo port select llvm llvm-10
 sudo port select --set pip pip310
 sudo port select --set pip3 pip310
 pip3 install pddoc
