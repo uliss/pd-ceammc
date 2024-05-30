5cf85d64d661d3d4329b5dc10a6bc17421981cec
Serge Poltavski <serge.poltavski@gmail.com>
2024-05-31 01:00:13 +0300

    mac: build faust script

--- ceammc/distrib/mac/build_faust.sh
+++ ceammc/distrib/mac/build_faust.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 #
 
-cmake -DCMAKE_INSTALL_PREFIX=~/.local/faust \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DLLVM_BACKEND='STATIC DYNAMIC' \
-	-DC_BACKEND='STATIC DYNAMIC' \
-	-DCPP_BACKEND='STATIC DYNAMIC' \
-	-DINCLUDE_EMCC=OFF \
-	-DINCLUDE_OSC=OFF \
-	-DINCLUDE_HTTP=OFF \
-	-DINCLUDE_WASM_GLUE=OFF \
-	-DINCLUDE_STATIC=OFF \
-	-DINCLUDE_DYNAMIC=ON \
-	-DINCLUDE_EXECUTABLE=OFF \
-	-DSELF_CONTAINED_LIBRARY=ON \
-	.
+
+PWD="@PROJECT_BINARY_DIR@"
+
+mkdir -p "$PWD/_deps"
+cd "$PWD/_deps"
+echo "cloning faust info $(pwd)"
+
+if [[ -d faust ]]
+then
+    cd faust
+    git checkout
+    cd build
+else
+    git clone https://github.com/grame-cncm/faust.git
+    if [[ $? -ne 0 ]]
+    then
+        exit 1
+    fi
+
+    cd faust/build
+fi
+
+pwd
+ls
+
+echo "building faust ..."
+
+cmake -GNinja \
+    -DCMAKE_INSTALL_PREFIX=~/.local/faust \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_BACKEND='STATIC DYNAMIC' \
+    -DC_BACKEND='STATIC DYNAMIC EXECUTABLE' \
+    -DCPP_BACKEND='STATIC DYNAMIC EXECUTABLE' \
+    -DINCLUDE_EMCC=OFF \
+    -DINCLUDE_OSC=OFF \
+    -DINCLUDE_HTTP=OFF \
+    -DINCLUDE_WASM_GLUE=OFF \
+    -DINCLUDE_STATIC=OFF \
+    -DINCLUDE_DYNAMIC=ON \
+    -DINCLUDE_EXECUTABLE=ON \
+    -DSELF_CONTAINED_LIBRARY=ON \
+    .
 
 cmake -L
+
+ninja && ninja install
+
