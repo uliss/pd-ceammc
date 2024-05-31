e06254ebdecd105d738c5da61fc4da73efcf8bb8
Serge Poltavski <serge.poltavski@gmail.com>
2024-06-01 02:57:01 +0300

    linux verovio script

---
+++ ceammc/distrib/linux/build_verovio.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+PWD="@PROJECT_BINARY_DIR@"
+
+mkdir -p "$PWD/_deps"
+cd "$PWD/_deps"
+echo "cloning verovio info $(pwd)"
+
+if [[ -d verovio ]]
+then
+    cd verovio
+    git checkout
+    git submodule update --init --recursive
+else
+    git clone https://github.com/rism-digital/verovio
+    if [[ $? -ne 0 ]]
+    then
+        exit 1
+    fi
+
+    cd verovio
+    git submodule update --init --recursive
+fi
+
+echo "building verovio ..."
+
+#rm -rf build
+mkdir -p build
+cd build
+rm -f CMakeCache.txt
+
+cmake -GNinja \
+    $CLANG $CLANGXX \
+    -DCMAKE_INSTALL_PREFIX=~/.local/verovio \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DNO_RUNTIME=OFF \
+    -DBUILD_AS_LIBRARY=ON \
+    ../cmake
+
+cmake -L
+
+rm -rf ~/.local/verovio
+ninja clean
+ninja && ninja install
+
