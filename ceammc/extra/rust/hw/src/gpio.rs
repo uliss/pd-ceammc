70548061014ecd27652a54745aef17f90f1413c7
Serge Poltavski <serge.poltavski@gmail.com>
2025-02-11 00:44:19 +0300

    Update gpio.rs

--- ceammc/extra/rust/hw/src/gpio.rs
+++ ceammc/extra/rust/hw/src/gpio.rs
@@ -1,14 +1,14 @@
 use std::{
-    ffi::{CString},
+    ffi::CString,
     ptr::null_mut,
     sync::{Arc, Mutex},
     time::Duration,
 };
 
+use crate::hw_error_cb;
 use lazy_static::lazy_static;
 use log::{debug, error};
 use tokio::time::sleep;
-use crate::hw_error_cb;
 
 #[cfg(target_os = "linux")]
 use rppal::{
@@ -96,28 +96,28 @@ impl Drop for hw_gpio {
 }
 
 struct HwGpio {
-    _rx: tokio::sync::mpsc::Receiver<HwGpioReply>,
+    rx: tokio::sync::mpsc::Receiver<HwGpioReply>,
     tx: tokio::sync::mpsc::Sender<HwGpioRequest>,
 }
 
 pub enum HwGpioRequest {
     Quit,
-    Read,
-    SetHigh(u8),
-    SetLow(u8),
+    Read(u8),
+    Write(u8, bool),
     Toggle(u8),
     SetPwmFreq(u8, f64, f64),
     SetPwm(u8, f64, f64),
     ClearPwm(u8),
     SetResetOnDrop(u8, bool),
-    IsHigh(u8),
-    IsLow(u8),
     SetBias(u8, u8),
     SetInterrupt(u8, u8, f64),
     ClearInterrupt(u8),
 }
 
-enum HwGpioReply {}
+#[derive(Clone)]
+enum HwGpioReply {
+    PinLevel(u8, bool),
+}
 
 #[cfg(target_os = "linux")]
 fn gpio_output_pin(gpio: &Gpio, pin: u8) -> Result<OutputPin, CString> {
@@ -127,7 +127,17 @@ fn gpio_output_pin(gpio: &Gpio, pin: u8) -> Result<OutputPin, CString> {
 }
 
 #[cfg(target_os = "linux")]
-fn gpio_set_pin(gpio: &Gpio, pin: u8, state: bool) -> Result<(), CString> {
+fn gpio_read_pin(gpio: &Gpio, pin: u8) -> Result<bool, CString> {
+    gpio.get(pin)
+        .map_err(|err| CString::new(err.to_string()).unwrap_or_default())
+        .map(|x| match x.read() {
+            gpio::Level::Low => false,
+            gpio::Level::High => true,
+        })
+}
+
+#[cfg(target_os = "linux")]
+fn gpio_write_pin(gpio: &Gpio, pin: u8, state: bool) -> Result<(), CString> {
     gpio_output_pin(gpio, pin).map(|mut x| {
         x.write(match state {
             true => gpio::Level::High,
@@ -148,7 +158,7 @@ impl HwGpio {
                 debug!("creating tokio runtime ...");
 
                 let (req_tx, mut req_rx) = tokio::sync::mpsc::channel::<HwGpioRequest>(16);
-                let (_reply_tx, reply_rx) = tokio::sync::mpsc::channel::<HwGpioReply>(64);
+                let (reply_tx, mut reply_rx) = tokio::sync::mpsc::channel::<HwGpioReply>(16);
 
                 std::thread::spawn(move || {
                     debug!("[worker thread] starting ...");
@@ -159,26 +169,30 @@ impl HwGpio {
                         #[cfg(target_os = "linux")]
                         match &Gpio::new() {
                             Ok(gpio) => loop {
-                                if let Ok(req) = req_rx.try_recv() {
+                                if let Some(req) = req_rx.recv().await {
                                     match req {
                                         HwGpioRequest::Quit => {
                                             debug!("[worker thread] quit");
                                             return Ok(());
                                         }
-                                        HwGpioRequest::Read => todo!(),
-                                        HwGpioRequest::SetHigh(pin) => {
-                                            gpio_set_pin(gpio, pin, true)?
+                                        HwGpioRequest::Read(pin) => {
+                                            let level = gpio_read_pin(gpio, pin)?;
+                                            reply_tx
+                                                .send(HwGpioReply::PinLevel(pin, level))
+                                                .await
+                                                .map_err(|err| {
+                                                    CString::new(err.to_string())
+                                                        .unwrap_or_default()
+                                                })?;
                                         }
-                                        HwGpioRequest::SetLow(pin) => {
-                                            gpio_set_pin(gpio, pin, false)?
+                                        HwGpioRequest::Write(pin, state) => {
+                                            gpio_write_pin(gpio, pin, state)?
                                         }
                                         HwGpioRequest::Toggle(pin) => gpio_toggle_pin(gpio, pin)?,
                                         HwGpioRequest::SetPwmFreq(_, _, _) => todo!(),
                                         HwGpioRequest::SetPwm(_, _, _) => todo!(),
                                         HwGpioRequest::ClearPwm(_) => todo!(),
                                         HwGpioRequest::SetResetOnDrop(_, _) => todo!(),
-                                        HwGpioRequest::IsHigh(_) => todo!(),
-                                        HwGpioRequest::IsLow(_) => todo!(),
                                         HwGpioRequest::SetBias(_, _) => todo!(),
                                         HwGpioRequest::SetInterrupt(_, _, _) => todo!(),
                                         HwGpioRequest::ClearInterrupt(_) => todo!(),
@@ -201,7 +215,7 @@ impl HwGpio {
                 });
 
                 Ok(Arc::new(HwGpio {
-                    _rx: reply_rx,
+                    rx: reply_rx,
                     tx: req_tx,
                 }))
             }
@@ -238,6 +252,28 @@ pub extern "C" fn ceammc_hw_gpio_free(gpio: *mut hw_gpio) {
     }
 }
 
+/// process events
+/// @param gp - pointer to gpio struct
+#[no_mangle]
+pub extern "C" fn ceammc_hw_gpio_process_events(gp: *mut hw_gpio) {
+    if gp.is_null() {
+        log::error!("NULL gpio pointer");
+        return;
+    }
+
+    let gp = unsafe { &mut *gp };
+
+    while let Ok(reply) = Arc::get_mut(&mut gp.gpio).unwrap().rx.try_recv() {
+        match reply {
+            HwGpioReply::PinLevel(_, _) => todo!(),
+            // Ok(data) => match data {
+
+            // },
+            // Err(err) => gp.on_err.exec_cstr(&err),
+        }
+    }
+}
+
 /// set pin level
 /// @param gpio - pointer to gpio struct
 /// @param pin - pin number
@@ -251,11 +287,7 @@ pub extern "C" fn ceammc_hw_gpio_set_pin(gp: *mut hw_gpio, pin: u8, level: bool)
 
     let gp = unsafe { &mut *gp };
 
-    if level {
-        gp.send(HwGpioRequest::SetHigh(pin))
-    } else {
-        gp.send(HwGpioRequest::SetLow(pin))
-    }
+    gp.send(HwGpioRequest::Write(pin, level))
 }
 
 /// toggle pin level
@@ -270,4 +302,22 @@ pub extern "C" fn ceammc_hw_gpio_toggle_pin(gp: *mut hw_gpio, pin: u8) -> bool {
 
     let gp = unsafe { &mut *gp };
     gp.send(HwGpioRequest::Toggle(pin))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gpio_init() {
+        let g1 = Gpio::new();
+        assert!(g1.is_ok());
+        let g1 = g1.unwrap();
+        assert!(g1.get(0).is_ok());
+
+        let g2 = Gpio::new();
+        assert!(g2.is_ok());
+        let g2 = g2.unwrap();
+        assert!(g2.get(0).is_ok());
+    }
 }
