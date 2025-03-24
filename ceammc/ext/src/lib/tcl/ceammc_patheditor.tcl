f9daebc60255303460278e014d85334473bcd97d
Serge Poltavski <serge.poltavski@gmail.com>
2025-03-24 17:50:46 +0300

    Update ceammc_patheditor.tcl

--- ceammc/ext/src/lib/tcl/ceammc_patheditor.tcl
+++ ceammc/ext/src/lib/tcl/ceammc_patheditor.tcl
@@ -1,6 +1,12 @@
 package provide ceammc_patheditor 0.1
 
 namespace eval ::ceammc {
+namespace eval dialog {
+    proc escape2pd {str} {
+        return [string map {";" "\\;" "\$" "\\\$"} $str]
+    }
+}
+
 namespace eval patheditor {
 
     proc open {name title geometry path} {
