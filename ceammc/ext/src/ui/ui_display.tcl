063c017711b05908f8933883443a5193f4483974
Serge Poltavski <serge.poltavski@gmail.com>
2025-04-03 12:21:04 +0300

    Rpi update

--- ceammc/ext/src/ui/ui_display.tcl
+++ ceammc/ext/src/ui/ui_display.tcl
@@ -3,11 +3,15 @@ namespace eval ::ui {
 namespace eval display {
     variable font_size 11
     variable font_family "Helvetica"
+    variable txt_yoff 0
 }
 
 switch -- $::windowingsystem {
     "aqua"  { }
-    "x11"   { set display::font_size 10 }
+    "x11"   { 
+        set display::font_size 9
+        set display::txt_yoff 2
+    }
     "win32" { set display::font_size 7 }
 }
 
@@ -46,7 +50,7 @@ proc display_update {cnv id rid w h zoom hauto bdcolor bgcolor txtcolor typecolo
     ::ceammc::ui::widget_bg $cnv $id $bgcolor
 
     set txpad [expr $zoom*2+1]
-    set typad [expr $zoom*2+1]
+    set typad [expr $zoom*2+1+$::ui::display::txt_yoff]
     set tx $txpad
     set ty $typad
     set ft [display_type_font $zoom]
