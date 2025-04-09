6b1e0e79c07d648c0ff45e2776a05942da052d4b
Serge Poltavski <serge.poltavski@gmail.com>
2025-04-09 21:14:41 +0300

    Update lcd1602_impl.rs

--- ceammc/extra/rust/hw/src/lcd1602/lcd1602_impl.rs
+++ ceammc/extra/rust/hw/src/lcd1602/lcd1602_impl.rs
@@ -95,7 +95,7 @@ impl hw_lcd1602 {
                     Request::CursorPos(row, col) => {
                         let shift = match rows {
                             2 => *row * 0x40 + *col,
-                            4 => (*row * 0x14) + ((*row & 0x1) * 0x28) + *col,
+                            4 => (*row * 0x20) + ((*row & 0x1) * 0x28) + *col,
                             _ => *col,
                         };
 
