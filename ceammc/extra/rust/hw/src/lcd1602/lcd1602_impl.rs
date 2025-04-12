8401c682f7c32194ba1d8bfb69cb55712a5389af
Serge Poltavski <serge.poltavski@gmail.com>
2025-04-12 10:54:09 +0300

    Update lcd1602_impl.rs

--- ceammc/extra/rust/hw/src/lcd1602/lcd1602_impl.rs
+++ ceammc/extra/rust/hw/src/lcd1602/lcd1602_impl.rs
@@ -25,7 +25,7 @@ impl hw_lcd1602 {
         let (worker, rx, tx) = Lcd1602Worker::new(on_msg);
         worker.spawn(tx.clone(), notify, move || {
             let mut i2c = create_i2c_bus(i2c_bus, &tx, notify)?;
-            debug!("I2C init");
+            debug!("I2C init: {i2c:?}");
 
             let addr = match i2c_addr {
                 I2cAddress::Default => 0x27,
