a6554f642c20d763fc8a491439c1019edb84e6a3
Serge Poltavski <serge.poltavski@gmail.com>
2025-04-14 08:17:06 +0300

    Update hw_rpi_display_hd44780.cpp

--- ceammc/ext/src/hw/hw_rpi_display_hd44780.cpp
+++ ceammc/ext/src/hw/hw_rpi_display_hd44780.cpp
@@ -69,9 +69,9 @@ void HwRpiDisplayHd44780::m_clear_line(t_symbol* s, const AtomListView& lv)
 
     constexpr const char* SPACES_20 = "                    ";
 
-    ceammc_hw_lcd1602_cursor_pos(lcd_, line, 0);
-    ceammc_hw_lcd1602_write_text(lcd_, SPACES_20);
-    ceammc_hw_lcd1602_cursor_pos(lcd_, line, 0);
+    ceammc_hw_hd44780_cursor_pos(lcd_, line, 0);
+    ceammc_hw_hd44780_write_text(lcd_, SPACES_20);
+    ceammc_hw_hd44780_cursor_pos(lcd_, line, 0);
 }
 
 void HwRpiDisplayHd44780::m_backlight(t_symbol* s, const AtomListView& lv)
