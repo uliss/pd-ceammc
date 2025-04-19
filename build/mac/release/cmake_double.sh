b93062562ca514a4a22a585c8f5ab5153df35ee1
Serge Poltavski <serge.poltavski@gmail.com>
2025-04-19 21:08:17 +0300

    double config fix

--- build/mac/release/cmake_double.sh
+++ build/mac/release/cmake_double.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export LLVM_DIR=/opt/local/libexec/llvm-10/lib/cmake/llvm
+#export CLANG=clang-cpp-mp-14
+export LLVM_DIR=/opt/local/libexec/llvm-14/lib/cmake/llvm
 # M1 apple check (Faust not build at this moment)
 BUILD_WITH_FAUST=ON
 if [[ $(uname -m) == 'arm64' ]]; then
@@ -9,18 +10,22 @@ if [[ $(uname -m) == 'arm64' ]]; then
 fi
 
 cmake -G Ninja \
+	-DCMAKE_C_COMPILER=clang \
+	-DCMAKE_CXX_COMPILER=clang++ \
 	-DCMAKE_CXX_FLAGS=-ffast-math\
 	-DCMAKE_C_FLAGS=-ffast-math \
 	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=`pwd`/dist/pd_ceammc \
 	-DWITH_FLUIDSYNTH=ON \
-	-DWITH_DOUBLE_PRECISION=ON \
-	-DCMAKE_INSTALL_PREFIX=. \
 	-DARCH=x86_64 \
 	-DWISH_VERSION=8.6.13 \
 	-DLEAPMOTION_ROOT=~/work/src/LeapSDK \
 	-DENABLE_TESTS=ON \
-	-DWITH_BENCHMARK=OFF\
-	-DWITH_EXT_FLEXT=OFF\
-	-DWITH_FAUST=${BUILD_WITH_FAUST}\
 	-DWITH_TTS_RHVOICE=ON \
+	-DWITH_EXT_FLEXT=OFF\
+	-DWITH_VEROVIO=ON \
+	-DWITH_FAUST=${BUILD_WITH_FAUST}\
+	-DWITH_LIBSAMPLERATE=ON \
+	-DWITH_BENCHMARK=ON\
+	-DWITH_DOUBLE_PRECISION=ON \
 	../../../..
