6a6f030f0c28087346ac320d06c7c67270e2e76e
Serge Poltavski <serge.poltavski@gmail.com>
2025-04-30 16:51:19 +0300

    macos10.14 build script fixes

--- ceammc/distrib/mac/build_verovio.sh
+++ ceammc/distrib/mac/build_verovio.sh
@@ -2,11 +2,22 @@
 #
 
 PWD="@PROJECT_BINARY_DIR@"
+VEROVIO_SRC_DIR="$PWD/_deps/verovio"
+VEROVIO_BUILD_DIR="${VEROVIO_SRC_DIR}/build"
+VEROVIO_INSTALL_DIR=~/.local/verovio
+TAGS="version-5.2.0"
+COPY_INCLUDES=0
 
 case "$(sw_vers -productVersion | cut -d. -f1,2)" in
     "10.15")
         CLANG="-DCMAKE_C_COMPILER=clang-mp-11"
         CLANGXX="-DCMAKE_CXX_COMPILER=clang++-mp-11"
+    ;;
+    "10.14")
+        CLANG="-DCMAKE_C_COMPILER=clang-mp-11"
+        CLANGXX="-DCMAKE_CXX_COMPILER=clang++-mp-11"
+        TAGS="version-4.1.0"
+        COPY_INCLUDES=1
     ;;
     *)
         CLANG=""
@@ -18,27 +29,25 @@ mkdir -p "$PWD/_deps"
 cd "$PWD/_deps"
 echo "cloning verovio info $(pwd)"
 
-if [[ -d verovio ]]
+if [[ ! -d verovio ]]
 then
-    cd verovio
-    git checkout
-    git submodule update --init --recursive
-else
     git clone https://github.com/rism-digital/verovio
     if [[ $? -ne 0 ]]
     then
         exit 1
     fi
-
-    cd verovio
-    git submodule update --init --recursive
 fi
+
+cd "${VEROVIO_SRC_DIR}"
+git fetch --tags
+git checkout tags/$TAGS
+git submodule update --init --recursive
 
 echo "building verovio ..."
 
 #rm -rf build
 mkdir -p build
-cd build
+cd "${VEROVIO_BUILD_DIR}"
 rm -f CMakeCache.txt
 
 cmake -GNinja \
@@ -51,7 +60,20 @@ cmake -GNinja \
 
 cmake -L
 
-rm -rf ~/.local/verovio
-ninja clean
+rm -rf ${VEROVIO_INSTALL_DIR}
+# ninja clean
 ninja && ninja install
+
+# fin missing install for version-4.1.0
+if [[ "$COPY_INCLUDES" -ne 0 ]]
+then
+    echo "copy include files ..."
+    rm -rf ${VEROVIO_INSTALL_DIR}/lib
+    mv ${VEROVIO_INSTALL_DIR}/bin ${VEROVIO_INSTALL_DIR}/lib
+    mkdir -p ${VEROVIO_INSTALL_DIR}/include/verovio
+    cp -v ${VEROVIO_SRC_DIR}/include/vrv/*.h ${VEROVIO_INSTALL_DIR}/include/verovio
+    cp -v ${VEROVIO_SRC_DIR}/libmei/addons/*.h ${VEROVIO_INSTALL_DIR}/include/verovio
+    cp -v ${VEROVIO_SRC_DIR}/libmei/dist/*.h ${VEROVIO_INSTALL_DIR}/include/verovio
+    cp -v ${VEROVIO_SRC_DIR}/tools/*.h ${VEROVIO_INSTALL_DIR}/include/verovio
+fi
 
