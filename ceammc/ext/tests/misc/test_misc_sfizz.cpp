76cb139ca06bcfb8ac02a916689b6132bdd67a11
Serge Poltavski <serge.poltavski@gmail.com>
2026-02-14 06:21:30 +0300

    sfizz test added

---
+++ ceammc/ext/tests/misc/test_misc_sfizz.cpp
@@ -0,0 +1,34 @@
+/*****************************************************************************
+ * Copyright 2020 Serge Poltavsky. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+ *****************************************************************************/
+#include "catch.hpp"
+#include "sfizz_tilde.h"
+#include "test_base.h"
+#include "test_external.h"
+#include "test_sound.h"
+
+PD_COMPLETE_SND_TEST_SETUP(SfizzTilde, misc, sfizz_tilde)
+
+
+TEST_CASE("misc.sfizz~", "[externals]")
+{
+    pd_test_init();
+
+    SECTION("main")
+    {
+        TExt t("sfizz~");
+        REQUIRE(t.numInlets() == 1);
+        REQUIRE(t.numOutlets() == 2);
+        REQUIRE(t.numOutputChannels() == 2);
+    }
+}
