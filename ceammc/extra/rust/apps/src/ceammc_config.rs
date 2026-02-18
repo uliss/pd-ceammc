044e72c0b278418a15c7c5086744a6c8f275ba06
Serge Poltavski <serge.poltavski@gmail.com>
2026-02-18 20:33:34 +0300

    rust update

---
+++ ceammc/extra/rust/apps/src/ceammc_config.rs
@@ -0,0 +1,17 @@
+pub const CEAMMC_LIB_VERSION_MAJOR: u8 = 0;
+pub const CEAMMC_LIB_VERSION_MINOR: u8 = 9;
+pub const CEAMMC_LIB_VERSION_PATCH: u8 = 8;
+pub const CEAMMC_LIB_VERSION: &str = "0.9.8";
+pub const CEAMMC_LIB_HOME: &str = "https://github.com/uliss/pd-ceammc";
+pub const CEAMMC_DISTRIB_VERSION: &str = "2025.04";
+
+pub const PD_MAJOR_VERSION: u8 = 0;
+pub const PD_MINOR_VERSION: u8 = 53;
+pub const PD_BUGFIX_VERSION: u8 = 0;
+pub const PD_BUILD_SUFFIX: &str = "ceammc";
+pub const PD_TEXT_VERSION_SHORT: &str = "0.53.0";
+pub const PD_TEXT_VERSION_FULL: &str = "0.53.0_ceammc";
+
+pub const GIT_BRANCH: &str = "devel";
+pub const GIT_COMMIT: &str = "v2025.04-1750-gbdf5fb2e97";
+pub const BUILD_DATETIME: &str = "18 02 2026 UTC 17:26:42 ";
