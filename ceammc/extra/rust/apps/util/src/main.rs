383c0c5499937cdf03dbcfbacc75a4f6a43a1592
Serge Poltavski <serge.poltavski@gmail.com>
2026-02-19 20:30:37 +0300

    Update main.rs

--- ceammc/extra/rust/apps/util/src/main.rs
+++ ceammc/extra/rust/apps/util/src/main.rs
@@ -145,7 +145,7 @@ fn output_system() {
 
 fn dpkg_version() -> String {
     std::process::Command::new("dpkg-query")
-        .args(["-W", "-f", "'${Version}\n'", "pd-ceammc"])
+        .args(["-W", "-f", "'${Version}'", "pd-ceammc"])
         .output()
         .ok()
         .map(|x| String::from_utf8(x.stdout).unwrap_or("invalid output".to_string()))
