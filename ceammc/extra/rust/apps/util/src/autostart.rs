75e85c3ecb7f510814208a56e7e54ab457e74806
Serge Poltavski <serge.poltavski@gmail.com>
2026-02-19 21:39:06 +0300

    Update autostart.rs

--- ceammc/extra/rust/apps/util/src/autostart.rs
+++ ceammc/extra/rust/apps/util/src/autostart.rs
@@ -108,7 +108,7 @@ pub fn enable() -> Result<(), common::Error> {
         copy(&path, &dest)?;
     }
 
-    info!("OK");
+    info!("enabled");
     Ok(())
 }
 
@@ -137,7 +137,8 @@ fn remove_desktop() -> Result<(), common::Error> {
 pub fn disable() -> Result<(), common::Error> {
     remove_run_script()?;
     remove_desktop()?;
-    info!("OK");
+    
+    info!("disabled");
     Ok(())
 }
 
