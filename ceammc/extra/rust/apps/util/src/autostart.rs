58bd77d1a41056f4b667cef6ea1a609925acfbfa
Serge Poltavski <serge.poltavski@gmail.com>
2026-02-20 04:59:53 +0300

    Update autostart.rs

--- ceammc/extra/rust/apps/util/src/autostart.rs
+++ ceammc/extra/rust/apps/util/src/autostart.rs
@@ -124,7 +124,7 @@ fn fix_desktop_vars(path: &PathBuf) -> Result<(), common::Error> {
             .unwrap_or_default(),
     );
 
-    std::fs::write(path, data).map_err(|err| Error::Common(format!("file write error: {err}")))?;
+    std::fs::write(path, data).map_err(|err| Error::Common(format!("file write error: {err}")))
 }
 
 fn copy_desktop() -> Result<(), common::Error> {
