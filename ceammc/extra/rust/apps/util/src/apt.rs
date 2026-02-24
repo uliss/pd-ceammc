75afb29db314ec7e032563e26576ec475dd8b16c
Serge Poltavski <serge.poltavski@gmail.com>
2026-02-24 20:06:41 +0300

    Update apt.rs

--- ceammc/extra/rust/apps/util/src/apt.rs
+++ ceammc/extra/rust/apps/util/src/apt.rs
@@ -2,7 +2,7 @@
 pub fn apt_version() -> Option<String> {
     use rust_apt::new_cache;
 
-    let cache = new_cache!()?;
+    let cache = new_cache!().ok()?;
     let pkg = cache.get("pd-ceammc")?;
     Some(pkg.installed()?.version().to_string())
 }
