c057d5d6a13ec36973ccfa5f2ea0bf8a3e3343e0
Serge Poltavski <serge.poltavski@gmail.com>
2026-02-26 01:57:06 +0300

    Update hw_rpi_spi_ws2812.cpp

--- ceammc/ext/src/hw/hw_rpi_spi_ws2812.cpp
+++ ceammc/ext/src/hw/hw_rpi_spi_ws2812.cpp
@@ -119,7 +119,7 @@ void HwSpiWs2812::m_flush(t_symbol* s, const AtomListView& lv)
 
 void HwSpiWs2812::m_fx(t_symbol* s, const AtomListView& lv)
 {
-    static const args::ArgChecker chk("FX:s=rainbow|breathe|cycle|collision|bounce"
+    static const args::ArgChecker chk("FX:s=rainbow|breathe|cycle|collision|bounce "
                                       "ARG:f[0,1]? START:i? LEN:i>=0?");
     if (!chk.check(lv, this))
         return chk.usage(this, s);
