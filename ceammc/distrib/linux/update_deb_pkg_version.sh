c2783145d6011a39a5125b88ea8c545a68d5b52b
Serge Poltavski <serge.poltavski@gmail.com>
2026-02-27 15:58:33 +0300

    deb package version update target added

---
+++ ceammc/distrib/linux/update_deb_pkg_version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILE="@PROJECT_SOURCE_DIR@/cmake/version.cmake"
+
+cd "@PROJECT_SOURCE_DIR@"
+let REV=$(git describe | cut -d- -f2)
+if [ -z "$REV" ]; then
+    echo "can't get git revision"
+    exit 1
+fi
+echo "trying to set deb revision: r$REV ..."
+
+gsed -E -i 's/LINUX_DEBIAN_PACKAGE_REVISION[[:space:]]+"\.r2002"/LINUX_DEBIAN_PACKAGE_REVISION ".r'$REV'"/' "$FILE" &&
+  echo "updated to: r$REV"
