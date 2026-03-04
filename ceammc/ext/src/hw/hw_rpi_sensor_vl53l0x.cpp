c236756819a99dec5994123dac0b8e601de344d4
Serge Poltavski <serge.poltavski@gmail.com>
2026-03-04 18:46:03 +0300

    vl53l0x update

--- ceammc/ext/src/hw/hw_rpi_sensor_vl53l0x.cpp
+++ ceammc/ext/src/hw/hw_rpi_sensor_vl53l0x.cpp
@@ -4,43 +4,31 @@
 
 HwRpiSensorVl53l0x::HwRpiSensorVl53l0x(const PdArgs& args)
     : RustDispatchedObject<BaseObject>(args)
+    , vc_(nullptr, &ceammc_hw_sensor_vl53l0x_free)
 {
     createOutlet();
 
     i2c_addr_ = addI2cAddrProperty();
     i2c_bus_ = addI2cBusProperty();
-}
 
-HwRpiSensorVl53l0x::~HwRpiSensorVl53l0x()
-{
-    ceammc_hw_sensor_vl53l0x_free(vc_);
-}
-
-void HwRpiSensorVl53l0x::initDone()
-{
-    std::int8_t bus = 0;
-    if (!i2c_bus_->getBus(bus))
-        return;
-
-    vc_ = ceammc_hw_sensor_vl53l0x_new(bus,
-        i2c_addr_->value(),
-        on_notify(), //
-        { this, [](void* user, std::uint16_t mm) {
-             auto obj = static_cast<HwRpiSensorVl53l0x*>(user);
-             if (obj)
-                 obj->floatTo(0, mm);
-         } },
-        on_message());
+    connect_ = new BoolProperty("@connect", false);
+    connect_->setSuccessFn([this](Property*) {
+        i2c_connect(connect_->value());
+    });
+    addProperty(connect_);
 }
 
 bool HwRpiSensorVl53l0x::notify(int code)
 {
-    return ceammc_hw_sensor_vl53l0x_proc_reply(vc_);
+    return ceammc_hw_sensor_vl53l0x_proc_reply(vc_.get());
 }
 
 void HwRpiSensorVl53l0x::onBang()
 {
-    ceammc_hw_sensor_vl53l0x_read_mm(vc_);
+    if (!check_connected(true))
+        return;
+
+    ceammc_hw_sensor_vl53l0x_read_mm(vc_.get());
 }
 
 void HwRpiSensorVl53l0x::m_poll(t_symbol* s, const AtomListView& lv)
@@ -49,7 +37,10 @@ void HwRpiSensorVl53l0x::m_poll(t_symbol* s, const AtomListView& lv)
     if (!chk.check(lv, this))
         return chk.usage(this, s);
 
-    ceammc_hw_sensor_vl53l0x_poll(vc_, lv.boolAt(0, true));
+    if (!check_connected(true))
+        return;
+
+    ceammc_hw_sensor_vl53l0x_poll(vc_.get(), lv.boolAt(0, true));
 }
 
 void HwRpiSensorVl53l0x::m_address(t_symbol* s, const AtomListView& lv)
@@ -58,7 +49,45 @@ void HwRpiSensorVl53l0x::m_address(t_symbol* s, const AtomListView& lv)
     if (!chk.check(lv, this))
         return chk.usage(this, s);
 
-    ceammc_hw_sensor_vl53l0x_set_address(vc_, lv.intAt(0, true));
+    if (!check_connected(true))
+        return;
+
+    ceammc_hw_sensor_vl53l0x_set_address(vc_.get(), lv.intAt(0, true));
+}
+
+void HwRpiSensorVl53l0x::i2c_connect(bool state)
+{
+    if (state) {
+        std::int8_t bus = 0;
+        if (!i2c_bus_->getBus(bus)) {
+            OBJ_ERR << "i2c bus is not specified";
+            return;
+        }
+
+        vc_.reset(ceammc_hw_sensor_vl53l0x_new(bus,
+            i2c_addr_->value(),
+            on_notify(), //
+            { this, [](void* user, std::uint16_t mm) {
+                 auto obj = static_cast<HwRpiSensorVl53l0x*>(user);
+                 if (obj)
+                     obj->floatTo(0, mm);
+             } },
+            on_message()));
+
+        if (!vc_)
+            OBJ_ERR << "can't connect to i2c bus";
+    } else {
+        vc_.reset();
+    }
+}
+
+bool HwRpiSensorVl53l0x::check_connected(bool print_err)
+{
+    if (!vc_ && print_err) {
+        OBJ_ERR << "device is not connected to the I2C bus";
+    }
+
+    return vc_.get();
 }
 
 void setup_hw_rpi_sensor_vl53l0x()
--- ceammc/ext/src/hw/hw_rpi_sensor_vl53l0x.h
+++ ceammc/ext/src/hw/hw_rpi_sensor_vl53l0x.h
@@ -5,22 +5,27 @@
 #include "rust_dispatched_object.h"
 using namespace ceammc;
 
+using VL53L0XPtr = std::unique_ptr<ceammc_hw_sensor_vl53l0x, void (*)(ceammc_hw_sensor_vl53l0x*)>;
+
 class HwRpiSensorVl53l0x : public RustDispatchedObject<BaseObject> {
-    ceammc_hw_sensor_vl53l0x* vc_ { nullptr };
+    VL53L0XPtr vc_;
     I2cBusProperty* i2c_bus_ { nullptr };
     I2cAddrProperty* i2c_addr_ { nullptr };
+    BoolProperty* connect_ { nullptr };
 
 public:
     explicit HwRpiSensorVl53l0x(const PdArgs& args);
-    ~HwRpiSensorVl53l0x();
 
-    void initDone() final;
     bool notify(int code) final;
 
     void onBang() final;
 
     void m_poll(t_symbol* s, const AtomListView& lv);
     void m_address(t_symbol* s, const AtomListView& lv);
+
+private:
+    bool check_connected(bool print_err);
+    void i2c_connect(bool state);
 };
 
 void setup_hw_rpi_sensor_vl53l0x();
