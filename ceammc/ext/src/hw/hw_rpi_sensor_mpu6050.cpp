2cbd4ab2c3f2429f3fd081936cb78dbc0c03b9ff
Serge Poltavski <serge.poltavski@gmail.com>
2026-03-04 20:25:21 +0300

    hw: updates

--- ceammc/ext/src/hw/hw_rpi_sensor_mpu6050.cpp
+++ ceammc/ext/src/hw/hw_rpi_sensor_mpu6050.cpp
@@ -5,53 +5,31 @@
 
 HwRpiSensorMpu6050::HwRpiSensorMpu6050(const PdArgs& args)
     : RustDispatchedObject<BaseObject>(args)
+    , mpu_(nullptr, ceammc_hw_mpu6050_free)
 {
     createOutlet();
 
     i2c_bus_ = addI2cBusProperty();
     i2c_addr_ = addI2cAddrProperty();
-}
 
-HwRpiSensorMpu6050::~HwRpiSensorMpu6050()
-{
-    ceammc_hw_mpu6050_free(mpu_);
-}
-
-void HwRpiSensorMpu6050::initDone()
-{
-    std::int8_t bus = 0;
-    if (!i2c_bus_->getBus(bus))
-        return;
-
-    mpu_ = ceammc_hw_mpu6050_new(bus,
-        i2c_addr_->value(),
-        on_notify(),
-        on_message(),
-        { this,
-            [](void* user, float yaw, float pitch, float roll) {
-                auto obj = static_cast<HwRpiSensorMpu6050*>(user);
-                if (!obj)
-                    return;
-
-                obj->outputData(yaw, pitch, roll);
-            },
-            [](void* user, float temp) {
-                auto obj = static_cast<HwRpiSensorMpu6050*>(user);
-                if (!obj)
-                    return;
-
-                obj->anyTo(0, gensym("temp"), temp);
-            } });
+    connect_ = new BoolProperty("@connect", false);
+    connect_->setSuccessFn([this](Property*) {
+        i2c_connect(connect_->value());
+    });
+    addProperty(connect_);
 }
 
 bool HwRpiSensorMpu6050::notify(int code)
 {
-    return ceammc_hw_mpu6050_process_reply(mpu_);
+    return ceammc_hw_mpu6050_process_reply(mpu_.get());
 }
 
 void HwRpiSensorMpu6050::m_calibrate(t_symbol* s, const AtomListView& lv)
 {
-    ceammc_hw_mpu6050_calibrate(mpu_);
+    if (!check_connected(true))
+        return;
+
+    ceammc_hw_mpu6050_calibrate(mpu_.get());
 }
 
 void HwRpiSensorMpu6050::m_poll(t_symbol* s, const AtomListView& lv)
@@ -60,13 +38,61 @@ void HwRpiSensorMpu6050::m_poll(t_symbol* s, const AtomListView& lv)
     if (!chk.check(lv, this, s))
         return chk.usage(this, s);
 
-    ceammc_hw_mpu6050_poll(mpu_, lv.boolAt(0, false));
+    if (!check_connected(true))
+        return;
+
+    ceammc_hw_mpu6050_poll(mpu_.get(), lv.boolAt(0, false));
 }
 
-void HwRpiSensorMpu6050::outputData(float yaw, float pitch, float roll)
+void HwRpiSensorMpu6050::output_data(float yaw, float pitch, float roll)
 {
     AtomArray<3> ypr { yaw, pitch, roll };
     anyTo(0, gensym("ypr"), ypr.view());
+}
+
+bool HwRpiSensorMpu6050::check_connected(bool print_err)
+{
+    if (!mpu_ && print_err) {
+        OBJ_ERR << "device is not connected";
+    }
+
+    return mpu_.get();
+}
+
+void HwRpiSensorMpu6050::i2c_connect(bool state)
+{
+    if (state) {
+        std::int8_t bus = 0;
+        if (!i2c_bus_->getBus(bus)) {
+            OBJ_ERR << "i2c bus is no set";
+            return;
+        }
+
+        mpu_.reset(ceammc_hw_mpu6050_new(bus,
+            i2c_addr_->value(),
+            on_notify(),
+            on_message(),
+            { this,
+                [](void* user, float yaw, float pitch, float roll) {
+                    auto obj = static_cast<HwRpiSensorMpu6050*>(user);
+                    if (!obj)
+                        return;
+
+                    obj->output_data(yaw, pitch, roll);
+                },
+                [](void* user, float temp) {
+                    auto obj = static_cast<HwRpiSensorMpu6050*>(user);
+                    if (!obj)
+                        return;
+
+                    obj->anyTo(0, gensym("temp"), temp);
+                } }));
+
+        if (!mpu_)
+            OBJ_ERR << "can't connect to device";
+    } else {
+        mpu_.reset();
+    }
 }
 
 void setup_hw_rpi_sensor_mpu6050()
--- ceammc/ext/src/hw/hw_rpi_sensor_mpu6050.h
+++ ceammc/ext/src/hw/hw_rpi_sensor_mpu6050.h
@@ -7,22 +7,24 @@
 using namespace ceammc;
 
 class HwRpiSensorMpu6050 : public RustDispatchedObject<BaseObject> {
-    ceammc_hw_mpu6050* mpu_ { nullptr };
+    using Sensor = std::unique_ptr<ceammc_hw_mpu6050, void (*)(ceammc_hw_mpu6050*)>;
+    Sensor mpu_;
     I2cBusProperty* i2c_bus_ { nullptr };
     I2cAddrProperty* i2c_addr_ { nullptr };
+    BoolProperty* connect_ { nullptr };
 
 public:
     explicit HwRpiSensorMpu6050(const PdArgs& args);
-    ~HwRpiSensorMpu6050();
 
-    void initDone() final;
     bool notify(int code) final;
 
     void m_calibrate(t_symbol* s, const AtomListView& lv);
     void m_poll(t_symbol* s, const AtomListView& lv);
 
 private:
-    void outputData(float yaw, float pitch, float roll);
+    void output_data(float yaw, float pitch, float roll);
+    bool check_connected(bool print_err);
+    void i2c_connect(bool state);
 };
 
 void setup_hw_rpi_sensor_mpu6050();
