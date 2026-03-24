1001faf631da90ad0aa23a33b753b355ee64d213
Serge Poltavski <serge.poltavski@gmail.com>
2026-03-25 02:10:43 +0300

    Update pn532_timer.rs

--- ceammc/extra/rust/hw/src/nfc/pn532_timer.rs
+++ ceammc/extra/rust/hw/src/nfc/pn532_timer.rs
@@ -1,22 +1,12 @@
-use std::{
-    convert::Infallible,
-    time::{Duration, Instant},
-};
+use std::{convert::Infallible, time::Duration};
 
 use pn532::{nb, CountDown};
 
-/// A timer based on [`std::time::Instant`], which is a monotonically nondecreasing clock.
-pub struct Timer {
-    start: Instant,
-    duration: Duration,
-}
+pub struct Timer {}
 
 impl Timer {
     pub fn new() -> Timer {
-        Timer {
-            start: Instant::now(),
-            duration: Duration::from_millis(0),
-        }
+        Timer {}
     }
 }
 
@@ -33,15 +23,10 @@ impl CountDown for Timer {
     where
         T: Into<Self::Time>,
     {
-        self.start = Instant::now();
-        self.duration = count.into();
+        std::thread::sleep(count.into());
     }
 
     fn wait(&mut self) -> nb::Result<(), Infallible> {
-        if (Instant::now() - self.start) >= self.duration {
-            Ok(())
-        } else {
-            Err(nb::Error::WouldBlock)
-        }
+        Ok(())
     }
 }
