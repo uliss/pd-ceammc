f8dd4de26ab83a699a6f953ed4867d59b9d1f328
Serge Poltavski <serge.poltavski@gmail.com>
2026-04-03 21:07:17 +0300

    pd method args

--- cmake/pd_method_args.cmake
+++ cmake/pd_method_args.cmake
@@ -3,10 +3,11 @@ find_program(METHOD_GENERATOR "method_args")
 function(pd_method_args target name)
   if(METHOD_GENERATOR)
     add_custom_command(
-      OUTPUT "${name}_args.hpp"
-      DEPENDS "${name}.cpp"
-      COMMAND ${METHOD_GENERATOR} --force "${name}.cpp"
-      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-    target_sources(${target} PRIVATE "${name}_args.hpp")
+      OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/${name}_args.hpp"
+      DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${name}.cpp"
+      COMMAND ${METHOD_GENERATOR} --force
+              "${CMAKE_CURRENT_SOURCE_DIR}/${name}.cpp")
+    target_sources(${target}
+                   PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/${name}_args.hpp")
   endif()
 endfunction()
