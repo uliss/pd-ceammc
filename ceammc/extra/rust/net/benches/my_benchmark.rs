60f6fe5e02deb52adf2ef73a2528b9a9901ea336
Serge Poltavski <serge.poltavski@gmail.com>
2026-04-24 01:17:48 +0300

    Update my_benchmark.rs

--- ceammc/extra/rust/net/benches/my_benchmark.rs
+++ ceammc/extra/rust/net/benches/my_benchmark.rs
@@ -1,9 +1,9 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-// use net_rust::utils;
+use criterion::{criterion_group, criterion_main, Criterion};
+// use net_rust::utils::parse_ascii_filename;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     // c.bench_function("fib 20", |b| b.iter(|| 
-    //     net_rust::utils::parse_ascii_filename(black_box("filename=\"abc.jpg\""))));
+        // parse_ascii_filename(black_box("filename=\"abc.jpg\""))));
 }
 
 criterion_group!(benches, criterion_benchmark);
