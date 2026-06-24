6621887bbe17384bb0a766f845f5211d1123bd1a
Serge Poltavski <serge.poltavski@gmail.com>
2026-06-24 04:10:19 +0300

    cleanup unused code

--- ceammc/extra/rust/hw/src/gpio/trajectory_planner.rs
+++ ceammc/extra/rust/hw/src/gpio/trajectory_planner.rs
@@ -68,24 +68,6 @@ impl hw_trajectory {
         self.input.target_acceleration[0] = 0.0;
     }
 
-    fn set_limits(&mut self, vel: f64, accel: f64, jerk: f64) {
-        self.input.max_velocity[0] = vel;
-        self.input.max_acceleration[0] = accel;
-        self.input.max_jerk[0] = jerk;
-    }
-
-    fn current_pos(&self) -> f64 {
-        self.input.current_position[0]
-    }
-
-    fn target_pos(&self) -> f64 {
-        self.input.target_position[0]
-    }
-
-    fn new_pos(&self) -> f64 {
-        self.output.new_position[0]
-    }
-
     fn update(&mut self) -> hw_trajectory_result {
         match self.otg.update(&self.input, &mut self.output) {
             Ok(res) => match res {
@@ -229,32 +211,5 @@ pub extern "C" fn ceammc_hw_trajectory_new_output(
         let traj = unsafe { &*traj };
         traj.new_output(pos, vel, accel, jerk, time);
         true
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::gpio::trajectory_planner::{hw_trajectory, hw_trajectory_result, LogErrorHandler};
-    use approx::{assert_abs_diff_eq, assert_relative_eq};
-    use rsruckig::{
-        input_parameter::InputParameter, output_parameter::OutputParameter, result::RuckigResult, ruckig::Ruckig,
-    };
-
-    #[test]
-    fn full_test() {
-        let mut t = hw_trajectory::new(10.0);
-        t.set_target_pos(10.0);
-        assert_eq!(t.current_pos(), 0.0);
-        assert_eq!(t.target_pos(), 10.0);
-        assert_eq!(t.new_pos(), 0.0);
-        t.set_limits(1.0, 2.0, 3.0);
-        while t.update() == hw_trajectory_result::Working {}
-
-        assert_eq!(t.update(), hw_trajectory_result::Finished);
-        assert_abs_diff_eq!(t.current_pos(), 10.0, epsilon = 1e-6);
-        t.set_target_pos(5.0);
-        t.set_limits(1.0, 2.0, 3.0);
-        while t.update() == hw_trajectory_result::Working {}
-        assert_abs_diff_eq!(t.current_pos(), 5.0, epsilon = 1e-6);
     }
 }
